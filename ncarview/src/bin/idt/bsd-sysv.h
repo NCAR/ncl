7ad8eea293445cf84587b831183b1791acb041e1
John Clyne <clyne@ucar.edu>
1991-01-09 10:54:38 +0000

    Initial revision
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@31 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarview/src/bin/idt/bsd-sysv.h
@@ -0,0 +1,17 @@
+/***********************************************************************
+*                                                                      *
+*                          Copyright (C)  1990                         *
+*            University Corporation for Atmospheric Research           *
+*                          All Rights Reserved                         *
+*                                                                      *
+*                          NCAR View V3.00alpha                        *
+*                                                                      *
+***********************************************************************/
+
+#ifdef	SYSV
+#ifndef	RS6000
+	extern	int	sprintf();
+#endif
+#else
+	extern	char	*sprintf();
+#endif
