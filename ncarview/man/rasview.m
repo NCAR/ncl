010c61859f14be34e301409f6f2bcf13dedd18de
John Clyne <clyne@ucar.edu>
1991-06-18 14:44:59 +0000

    Initial revision
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@80 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarview/man/rasview.m
@@ -0,0 +1,45 @@
+.\"
+.\"	$Id: rasview.m,v 1.1 1991-06-18 14:44:59 clyne Exp $
+.\"
+.TH XTOHDF 1-local "March 1991" NCAR "NCAR Local Command"
+.SH NAME
+rasview \- X11 windows raster file previewer.
+.SH SYNOPSIS
+.B rasview
+.RB [ " \-pal"
+.IR "palette " ]
+.RI [ " rasterfile.ext " ".\|.\|." ]
+.SH DESCRIPTION
+.LP
+.B rasview
+displays raster imagery stored in a number of different file formats to
+an X window. The currently supported raster formats are xwd (X11 Window Dump),
+hdf (Hidden Data Format), Sun, and nrif (NCAR Raster Image Format). 
+.B rasview
+determines the format of a file by the file name extension. The extensions
+for the aforementioned formats are 
+.IR .xwd , " .hdf" , " .sun " and " .nrif" 
+respectively.
+.B rasview
+will display any number of files each containing any number of images. 
+.SH OPTIONS
+.TP
+.BI \-pal " palette"
+Use the color palette contained in the file
+.IR palette .
+for displaying images. This palette will override the color palette stored
+with the image.
+.SH "SEE ALSO"
+.BR palette (local),
+.BR ctrans (local),
+.BR xwd (1),
+.BR xwud (1)
+.br
+.ne 5
+.SH BUGS/CAVEATS
+.LP
+24-bit images are dithered to 8 bits.
+.LP
+.B rasview
+has only been tested on devices which use "PseudoColor" as 
+the default visual class.
