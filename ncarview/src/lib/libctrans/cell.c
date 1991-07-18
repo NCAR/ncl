e59c4564e513b88a8db1e1f2ff8b0576fae58d39
John Clyne <clyne@ucar.edu>
1991-07-18 16:25:00 +0000

    Cell arrays with various corner boundries now displayed correctly on all devices.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@92 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/src/lib/libctrans/cell.c
+++ ncarview/src/lib/libctrans/cell.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: cell.c,v 1.3 1991-03-12 14:47:40 clyne Exp $
+ *	$Id: cell.c,v 1.4 1991-07-18 16:25:00 clyne Exp $
  */
 /*
  *	cell.c
@@ -11,53 +11,115 @@
  *	Massage cell array data necessary for all devices
  */
 
-#include <stdio.h>
-#include <math.h>
 #include "cgmc.h"
 #include "ctrandef.h"
 
+
 /*
- *	cell_prep
- *	[exported]
+ *	SetUpCellArrayAddressing
+ *	[internal]
+ *
+ *	calulate addressing information for manipulating ximage data for
+ *	rendering a cell array. This function assumes the corner points
+ *	P,Q,R are as described in the ANSI standard for CGM.
+ *
+ * on entry
+ *	P,Q,R		: cell array corners
+ *	image_size	: size of image data in bytes
+ *	pad		: number of bytes of padding at end of a scan line
+ *	bpp		: size of pixel in bytes
+ *	bpl		: number of bytes per line in the ximage
+ *	xoff, yoff	: offset of origin in destination image
+ * on exit
+ *	*step_x		: increment in x direction
+ *	*step_y		: increment in y direction
+ *	*start_x	: x destination of ximage in drawable
+ *	*start_y	: y destination of ximage in drawable
+ *	*data		: initialized to proper starting address
+ */
+void	SetUpCellArrayAddressing(Pcoord, Qcoord, Rcoord, image_size, pad, 
+		bpp, bpl, xoff, yoff, step_x, step_y, start_x, start_y, data)
+
+	Ptype		Pcoord, Qcoord, Rcoord;
+	unsigned int	image_size,
+			pad,
+			bpp,
+			bpl;
+	int		xoff, yoff;
+	int		*step_x,
+			*step_y;
+	int		*start_x,
+			*start_y;
+	char		**data;
+{
+
+	/*
+	 * determine addressing information based on relative positions of 
+	 * P, Q and R.
+	 */
+
+	*data += (yoff * bpl) + xoff;
+
+	if (Pcoord.x < Rcoord.x) {
+		*start_x = Pcoord.x;
+		*step_x = bpp;
+		if (Rcoord.y < Qcoord.y) {
+			*start_y = Rcoord.y;
+			*step_y = bpl;
+			*data += 0;
+		}
+		else {
+			*start_y = Qcoord.y;
+			*step_y = -bpl;
+			*data += (image_size - bpl);
+		}
+	}
+	else {
+		*start_x = Rcoord.x;
+		*step_x = -bpp;
+		if (Rcoord.y < Qcoord.y) {
+			*start_y = Rcoord.y;
+			*step_y = bpl;
+			*data += (bpl - pad - bpp);
+		}
+		else {
+			*start_y = Qcoord.y;
+			*step_y = -bpl;
+			*data += (image_size - (bpp + pad));
+		}
+	}
+}
+
+
+
+/*
+ *	SetUpCellArrayIndexing
+ *	[internal]
  *
  *	Perform interpolation of Cell array data necessitated by imperfect
- *	mapping of cell array cells onto device pixels
+ *	mapping of cell array cells onto device pixels.
+ *
  * on entry
- *	nx		: number of columns in the cell array
- *	ny		: number of rows in the cell array
+ *	image_height	: height of image in pixels
+ *	image_width	: width of image in pixels
  *	*rows		: Has dimension ny
  *	*cols		: Has dimension nx
+ *	nx		: number of cells per row
+ *	ny		: number of cells per column
  * on exit
  *	*rows		: row[i] specifies number of pixels for a cell in row i
  *	*cols		: col[i] specifies number of pixels for a cell in col i
  */
-cell_prep(P, Q, R, rows, cols, nx, ny)
-	Ptype	P, Q, R;
-	int	*rows, 
-		*cols;
+void	SetUpCellArrayIndexing(image_width, image_height, rows, cols, nx, ny)
+	unsigned int	image_width,
+			image_height;
+	int		*rows, 
+			*cols;
 	unsigned	nx, ny;
 {
 	int	i;
 	int	left, right;
 	double	inc, tmp;
-
-#if defined(CRAY) || (defined(u370) && defined(unix)) || defined(hpux)
-#define	rint(X)	((int) X)
-#else
-	extern	double	rint();
-#endif
-
-
-	/*	
-	 *	programmers unfamiliar with CGM representation of Cell arrays
-	 *	should see section 5.6.9  in the ANSI document on 
-	 *	Computer Graphic Metafiles.
-	 *
-	 *	Note:
-	 *		NCAR's CGM generator lables the lower left corner 
-	 *	of the cell array P. The corner P should be the upper left 
-	 *	corner of the cell array. This is a Bug in the generator.
-	 */
 
 	/*
 	 * map cell array onto available pixels. Use current IEEE
@@ -65,90 +127,19 @@ cell_prep(P, Q, R, rows, cols, nx, ny)
 	 * a boundry pixel or not. rows[i] and cols[j] contain
 	 * the number of pixels that make up cell[i,j] 
 	 */
-	inc = (double) abs((int) (P.x - R.x)) / (double) nx;
+	inc = (double) image_width / (double) nx;
 	for( right = 0, tmp = 0.0,i = 0; i < nx; i++) {	/* map cols*/
 		left = right;
 		tmp += inc;
-		right =  (int) rint(tmp);
+		right =  (int) ROUND(tmp);
 		cols[i] = right - left;
 	}
 
-	inc = (double) abs((int) (Q.y - R.y)) / (double) ny;
+	inc = (double) image_height / (double) ny;
 	for( right = 0, tmp = 0.0,i = 0; i < ny; i++) {	/* map rows*/
 		left = right;
 		tmp += inc;
-		right =  (int) rint(tmp);
+		right =  (int) ROUND(tmp);
 		rows[i] = right - left;
 	}
-
-
-}
-
-
-irregular_cell_prep (P, Q, R, nx, ny,
-		delta_pr_x, delta_pr_y, 
-		delta_qr_x, delta_qr_y, 
-		fudge_x, fudge_y)
-
-	Ptype	P, Q, R;
-	unsigned	nx, ny;
-	int	*delta_pr_x,	/* length of x vector for delta_pr	*/ 
-		*delta_pr_y;	/* length of y vector for delta_pr	*/ 
-
-	int	*delta_qr_x,	/* length of x vector for delta_qr	*/
-		*delta_qr_y;	/* length of y vector for delta_qr	*/
-
-	int	*fudge_x, *fudge_y;
-
-
-{
-
-	float	delta_pr,	/* length of segment of a cell along line pr */
-		delta_qr;	/* length of segment of a cell along line qr */
-	float	pr, qr;		/* length of lines pr and qr		*/
-
-	/*
-	 *	diagram of values
-	 */
-
-	/*
-						    pr.
-		              _______________________________________________Q
-		             /	              /
-		            /  	       	     /
-	 		   /	   	    /
-		     	  /   	     	   /
-	  delta_qr_x.    /	          /
-	         ______ /________________/__________
-          	|      /   delta_pr^    /
-	  	|     /		       /
-	  	|    /		      /
-    delta_qr_y> |   / < delta_qr     /
-	  	|  /		    /
-	  	| /	           /
-	  	|/________________/________________________________
-	  	P						   R
-					pr^
-	*/
-
-
-	/*	calculate lengths of cell array boundries	*/
-	pr = (float) sqrt((double) (SQR(P.y - R.y) + SQR(P.x - R.x)));
-	qr = (float) sqrt((double) (SQR(Q.y - R.y) + SQR(Q.x - R.x)));
-
-	/*	calculate fudge factor			*/
-	*fudge_x = (int) pr % nx;
-	*fudge_y = (int) qr % ny;
-
-	/*	calculate length of individual cell boundries	*/
-	delta_pr = pr / nx;
-	delta_qr = qr / ny;
-
-	/* calculate lengths of vectors describing a cell boundry*/
-	*delta_pr_x  = (int) ((delta_pr * (R.x - P.x)) / pr);
-	*delta_pr_y  = (int) ((delta_pr * (R.y - P.y)) / pr);
-
-	*delta_qr_x  = (int) ((delta_qr * (Q.x - R.x)) / qr);
-	*delta_qr_y  = (int) ((delta_qr * (Q.y - R.y)) / qr);
-
 }
