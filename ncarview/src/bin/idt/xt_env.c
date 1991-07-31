1d1259f4473b826a249887a1148a670142d50a07
John Clyne <clyne@ucar.edu>
1991-07-31 17:19:15 +0000

    Initial revision
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@103 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarview/src/bin/idt/xt_env.c
@@ -0,0 +1,62 @@
+/*
+ *      $Id: xt_env.c,v 1.1 1991-07-31 17:19:15 clyne Exp $
+ */
+/*
+ *	File:		xt_env.c
+ *
+ *	Author:		John Clyne
+ *			National Center for Atmospheric Research
+ *			PO 3000, Boulder, Colorado
+ *
+ *	Date:		Tue Jul 30 15:45:32 MDT 1991
+ *
+ *	Description:	This file contains routines that muck around with the
+ *			X environment.
+ */
+
+#include <stdio.h>
+#include <ncarv.h>
+
+/*
+ *	XAppDirPath
+ *
+ *	This routine attempts to ensure that the correct X application 
+ *	resource file is used for the application. If the user's environment
+ *	doesn't have XUSERFILESEARCHPATH or XAPPLRESDIR environment variables
+ *	set which point to the localtion of the app resoure file then  the
+ *	XUSERFILESEARCHPATH env variable is set to where we think the resource
+ *	file lives. We use the NCAR G param file to get our information.
+ */
+void	XAppDirPath()
+{
+	char	*xapp_path;
+	char	*xufsp_env	= "XUSERFILESEARCHPATH";
+
+	static	char	*bufptr = NULL;
+
+	/*
+	 * if either the XUSERFILESEARCHPATH or XAPPLRESDIR environment
+	 * variables are set do nothing.
+	 */
+	if (getenv(xufsp_env)) return;
+	if (getenv("XAPPLRESDIR")) return;
+
+	xapp_path = GetNCARGPath("XAPPDIR");
+
+	/*
+	 * if param file bombed use the default path that we were
+	 * compiled with.
+	 */
+	xapp_path = xapp_path ? xapp_path : XAPPDIR_DEFAULT;
+
+	if (bufptr) free (bufptr);
+	
+	bufptr = icMalloc ((unsigned) 
+		(strlen(xufsp_env) + strlen("=") + strlen(xapp_path) + 1));
+
+	(void) strcpy(bufptr, xufsp_env);
+	(void) strcat(bufptr, "=");
+	(void) strcat(bufptr, xapp_path);
+
+	(void) putenv (xapp_path);
+}
