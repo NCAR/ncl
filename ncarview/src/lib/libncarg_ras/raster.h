4dd71061b3f70e1fd3ac95d53d6b4d58c4198a9d
don <don>
1991-11-15 17:11:45 +0000

    Initial revision
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@173 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarview/src/lib/libncarg_ras/raster.h
@@ -0,0 +1,9 @@
+/* $Id: raster.h,v 1.1 1991-11-15 17:11:45 don Exp $ */
+
+/* These strings must be changed if the enum typedef is changed. */
+
+static char	*raster_encodings[] = {
+	"RAS_INVALID_ENCODING",
+	"RAS_INDEXED",
+	"RAS_DIRECT"
+};
