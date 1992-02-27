2ed444f3e1c982d9ea52d08da6bcab7bc4244a6a
don <don>
1992-02-27 22:48:31 +0000

    Fixed 16 bit padding in Sun raster input and output routines.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@259 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/src/lib/libncarg_ras/sunraster.c
+++ ncarview/src/lib/libncarg_ras/sunraster.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: sunraster.c,v 1.6 1992-02-12 14:23:57 clyne Exp $
+ *	$Id: sunraster.c,v 1.7 1992-02-27 22:48:31 don Exp $
  */
 /***********************************************************************
 *                                                                      *
@@ -140,7 +140,13 @@ SunOpenWrite(name, nx, ny, comment, encoding)
 	ras->red	= (unsigned char *) calloc((unsigned) ras->ncolor, 1);
 	ras->green	= (unsigned char *) calloc((unsigned) ras->ncolor, 1);
 	ras->blue	= (unsigned char *) calloc((unsigned) ras->ncolor, 1);
-	ras->data	= (unsigned char *) calloc((unsigned) ras->length, 1);
+
+	/*
+	Allocate a few extra bytes at the end of the image, so that the 
+	output routines can pad to 16 bits without referencing bogus
+	memory.
+	*/
+	ras->data	= (unsigned char *)calloc((unsigned)(ras->length+4),1);
 
 	if (encoding != RAS_INDEXED) {
 		(void) RasterSetError(RAS_E_UNSUPPORTED_ENCODING);
@@ -170,17 +176,21 @@ SunWrite(ras)
 {
 	SunInfo		*dep;
 	int		nb;
+	int		y;
+	unsigned char	*p;
 	unsigned long	swaptest = 1;
 
 	dep = (SunInfo *) ras->dep;
 
-	/* Swap bytes if necessary. */
+	/* Write the header, swapping bytes if necessary. */
 
 	if (*(char *) &swaptest)
 		_swaplong((char *) dep, SUN_HEADER_SIZE);
 
 	nb = write(ras->fd, (char *) ras->dep, SUN_HEADER_SIZE);
 	if (nb != SUN_HEADER_SIZE) return(RAS_EOF);
+
+	/* Write the color table. */
 
 	nb = write(ras->fd, (char *) ras->red, ras->ncolor);
 	if (nb != ras->ncolor) return(RAS_EOF);
@@ -191,8 +201,25 @@ SunWrite(ras)
 	nb = write(ras->fd, (char *) ras->blue, ras->ncolor);
 	if (nb != ras->ncolor) return(RAS_EOF);
 
-	nb = write(ras->fd, (char *) ras->data, ras->nx * ras->ny);
-	if (nb != ras->nx * ras->ny) return(RAS_EOF);
+	/*
+	Write the image, padding to 16 bits if "nx" is odd.
+	Padding is accomplished by using multiple writes instead
+	of one long one. Each padded write just includes an extra byte.
+	The memory allocation routines allocate a little extra memory
+	so this won't result in a segmentation fault.
+	*/
+
+	/* Code below handles only indexed files, no compression. */
+
+	if (ras->nx % 2 == 0) {
+		nb = write(ras->fd, (char *) ras->data, ras->nx * ras->ny);
+		if (nb != ras->nx * ras->ny) return(RAS_EOF);
+	}
+	else {
+		for(p = ras->data, y=0; y<ras->ny; y++, p += ras->nx) {
+			nb = write(ras->fd,(char *) p, ras->nx + 1);
+		}
+	}
 
 	return(RAS_OK);
 }
@@ -228,6 +255,7 @@ SunRead(ras)
 	SunInfo			*dep;
 	int			i;
 	int			count, value; 
+	int			length;
 	int			status;
 	int			x, y;
 	char			*malloc(), *calloc(), *realloc();
@@ -237,6 +265,7 @@ SunRead(ras)
 	static unsigned char	*tmpbuf = (unsigned char *) NULL;
 	static int		tmpbuf_size = 0;
 	unsigned char		rgb_buf[256];
+	unsigned char		*p;
 
 	/* Allocate the raster format dependent (header) structure. */
 
@@ -250,12 +279,10 @@ SunRead(ras)
 
 	dep = (SunInfo *) ras->dep;
 
-	/* Read the Sun raster file header. */
+	/* Read the Sun raster file header and swap bytes if necessary. */
 
 	status = fread((char *) dep, 1, SUN_HEADER_SIZE, ras->fp);
 	if (status != sizeof(SunInfo)) return(RAS_EOF);
-
-	/* Swap bytes if necessary. */
 
 	if (*(char *) &swaptest) {
 		_swaplong((char *) dep, SUN_HEADER_SIZE);
@@ -274,12 +301,14 @@ SunRead(ras)
 	ras->ny = dep->ras_height;
 
 	if (dep->ras_maptype == RMT_EQUAL_RGB) {
-		ras->ncolor = dep->ras_maplength / 3;
-		ras->type = RAS_INDEXED;
+		ras->ncolor	= dep->ras_maplength / 3;
+		ras->type	= RAS_INDEXED;
+		ras->length	= ras->nx * ras->ny;
 	}
 	else if (dep->ras_maptype == RMT_NONE) {
-		ras->ncolor = 0;
-		ras->type = RAS_DIRECT;
+		ras->ncolor	= 0;
+		ras->type	= RAS_DIRECT;
+		ras->length	= ras->nx * ras->ny * 3;
 	}
 	else {
 		(void) RasterSetError(RAS_E_UNSUPPORTED_ENCODING);
@@ -289,25 +318,15 @@ SunRead(ras)
 	/* If not initialized, allocate image memory */
 
 	if (ras->data == (unsigned char *) NULL) {
-		if (ras->type == RAS_INDEXED) {
-			image_size = ras->nx * ras->ny;
-		}
-		else if (ras->type == RAS_DIRECT) {
-			image_size = ras->nx * ras->ny * 3;
-		}
-		else {
-		  (void) RasterSetError(RAS_E_INTERNAL_PROGRAMMING);
-		  return(RAS_ERROR);
-		}
 
-		ras->data = (unsigned char *) calloc( image_size, 1);
+		ras->data = (unsigned char *) calloc( ras->length, 1);
 
 		if (ras->data == (unsigned char *) NULL) {
 			(void) RasterSetError(RAS_E_SYSTEM);
 			return(RAS_ERROR);
 		}
 
-		/* Allocate memory for indexed colors if necessary */
+		/* Allocate memory for colormap if necessary. */
 
 		if (dep->ras_maptype == RMT_EQUAL_RGB) {
 			ras->red = (unsigned char *) calloc(256, 1);
@@ -350,10 +369,19 @@ SunRead(ras)
 	*/
 
 	if (dep->ras_type == RT_STANDARD && dep->ras_depth == 8) {
-		/* Standard encoding */
-		status = fread( (char *) ras->data, 1,
-			(int) dep->ras_length, ras->fp);
-		if (status != dep->ras_length) return(RAS_EOF);
+
+		/* Indexed color encoding - handle 16bit padding. */
+
+		length = ras->nx;
+		if (ras->nx % 2 != 0) length++;
+
+		for(p=ras->data, y=0; y<ras->ny-1; y++, p+=ras->nx) {
+			status = fread( (char *) p, 1, length, ras->fp);
+			if (status != length) return(RAS_EOF);
+		}
+		status = fread( (char *) p, 1, ras->nx, ras->fp);
+		if (status != ras->nx) return(RAS_EOF);
+
 	}
 	else if (dep->ras_type == RT_STANDARD && dep->ras_depth == 32) {
 		/*
@@ -433,11 +461,11 @@ int
 SunClose(ras)
 	Raster	*ras;
 {
-	free( (char *) ras->data);
-	free( (char *) ras->red);
-	free( (char *) ras->green);
-	free( (char *) ras->blue);
-	free( (char *) ras->dep);
+	if (ras->data  != (unsigned char *) NULL) free( (char *) ras->data);
+	if (ras->red   != (unsigned char *) NULL) free( (char *) ras->red);
+	if (ras->green != (unsigned char *) NULL) free( (char *) ras->green);
+	if (ras->blue  != (unsigned char *) NULL) free( (char *) ras->blue);
+	if (ras->dep   !=  (char *) NULL)         free( (char *) ras->dep);
 	return(RAS_OK);
 }
 
