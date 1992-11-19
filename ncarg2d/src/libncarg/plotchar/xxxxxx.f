634361ee8ff4cdd639950c113cf98b9de812d104
fred <fred>
1992-11-19 02:31:44 +0000

    I can't believe it let me check this in.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@606 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/plotchar/xxxxxx.f
+++ ncarg2d/src/libncarg/plotchar/xxxxxx.f
@@ -1,6 +1,15 @@
       SUBROUTINE XXXXXX (R)
+<<<<<<< xxxxxx.f
+	R = R+1.
+        R = R*10.
+C  
+C  Put some comments in here.
+C
+        R = R**10
+=======
 	R=R+1.
 	R=R*R
 	R=R*10.
+>>>>>>> 1.3
 	RETURN
       END
