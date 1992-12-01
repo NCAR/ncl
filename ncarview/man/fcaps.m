9109dfdbd51f69e03819903c03f2588ea0d2850a
John Clyne <clyne@ucar.edu>
1992-12-01 03:38:39 +0000

    Added discussion of NCARG_ROOT env variable.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@617 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/man/fcaps.m
+++ ncarview/man/fcaps.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: fcaps.m,v 1.5 1992-08-12 23:07:50 clyne Exp $
+.\"	$Id: fcaps.m,v 1.6 1992-12-01 03:38:37 clyne Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH FCAPS 1NCAR "JUNE 1990"
@@ -19,8 +19,26 @@ metafile translation.
 .TP
 .B \-V
 Print the version number and then exit.
+.SH ENVIRONMENT
+.TP
+.B NCARG_ROOT
+Path to root of NCAR Graphics installation.
+.TP
+.B NCARG_LIB
+If set this variable contains the path to the installed NCAR Graphics 
+libraries. 
+.B NCARG_LIB
+overrides 
+.BR NCARG_ROOT .
+.SH FILES
+.TP 40
+$NCARG_ROOT/lib/ncarg/fontcaps/*
+The fontcaps.
+.TP 40
+$NCARG_LIB/ncarg/fontcaps/*
+The fontcaps.
 .SH "SEE ALSO"
-.BR fcaps(1NCARG),
+.BR gcaps(5NCARG),
 .BR ctrans(1NCARG),
 .BR ictrans(1NCARG),
-.BR fontcap(1NCARG)
+.BR fontcap(5NCARG)
--- ncarview/man/gcaps.m
+++ ncarview/man/gcaps.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: gcaps.m,v 1.6 1992-08-12 23:07:52 clyne Exp $
+.\"	$Id: gcaps.m,v 1.7 1992-12-01 03:38:39 clyne Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH GCAPS 1NCAR "JUNE 1990"
@@ -19,6 +19,24 @@ metafile translation.
 .TP
 .B \-V
 Print the version number and then exit.
+.SH ENVIRONMENT
+.TP
+.B NCARG_ROOT
+Path to root of NCAR Graphics installation.
+.TP
+.B NCARG_LIB
+If set this variable contains the path to the installed NCAR Graphics 
+libraries. 
+.B NCARG_LIB
+overrides 
+.BR NCARG_ROOT .
+.SH FILES
+.TP 40
+$NCARG_ROOT/lib/ncarg/graphcaps/*
+The graphcaps.
+.TP 40
+$NCARG_LIB/ncarg/graphcaps/*
+The graphcaps.
 .SH "SEE ALSO"
 .BR fcaps(1NCARG),
 .BR ctrans(1NCARG),
