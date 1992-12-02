df3a7b5e9b63733a4a8551afa12517bf75f23f8a
Dave Brown <dbrown@ucar.edu>
1992-12-02 23:28:32 +0000

    Revised version of fy.f that includes both the FY and MYF functions. Handles EZMAP transformations but not polar coordinates or non-linear grids. Included primarily for compatibilty with the old code.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@625 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/libncarg/velvct/fy.f
@@ -0,0 +1,186 @@
+C
+C	$Id: fy.f,v 1.1 1992-12-02 23:28:32 dbrown Exp $
+C
+      FUNCTION FY(X,Y)
+C
+C
+C The mapping common block: made available to user mapping routines
+C
+      COMMON /VVMAP/
+     +                IMAP       ,
+     +                XVPL       ,XVPR       ,YVPB       ,YVPT       ,
+     +                WXMN       ,WXMX       ,WYMN       ,WYMX       ,
+     +                XLOV       ,XHIV       ,YLOV       ,YHIV       ,
+     +                SXDC       ,SYDC       ,NXCT       ,NYCT       ,
+     +                RLEN       ,LNLG       ,INVX       ,INVY       ,
+     +                ITRT       ,IWCT       ,FW2W       ,FH2H       ,
+     +                DVMN       ,DVMX       ,RBIG       ,IBIG
+C
+      SAVE /VVMAP/
+C
+C Math constants
+C
+      PARAMETER (PDTOR  = 0.017453292519943,
+     +           PRTOD  = 57.2957795130823,
+     +           P1XPI  = 3.14159265358979,
+     +           P2XPI  = 6.28318530717959,
+     +           P1D2PI = 1.57079632679489,
+     +           P5D2PI = 7.85398163397448) 
+C
+C --------------------------------------------------------------------
+C
+      PARAMETER (PRCFAC=1E5,
+     +           PVFRAC=0.001,
+     +           PFOVFL=1E12,
+     +           IPMXCT=10,
+     +           PDUVML=2.0)
+C
+C
+C     The function FY behaves in an analagous manner to FX as
+C     described above.
+C
+      GOTO (10,20,30)  IMAP+1
+C
+C     The identity transformation.
+C
+10    CONTINUE
+      FY = Y
+      RETURN
+C
+C     EZMAP overlaying.
+C
+20    CONTINUE
+      XLON = XLOV + (X-1.)*(XHIV-XLOV)/(FLOAT(NXCT)-1.)
+      YLAT = YLOV + (Y-1.)*(YHIV-YLOV)/(FLOAT(NYCT)-1.)
+
+      IF (IFIX(ABS(YLAT)*PRCFAC) .EQ. IFIX(90.*PRCFAC)) THEN
+         FY=RBIG
+         RETURN
+      END IF
+
+      CALL MAPTRN(YLAT,XLON,XDUM,FYLAT)
+C
+      FY = FYLAT
+      RETURN
+C
+C     Polar coordinate transformation.
+C
+30    CONTINUE
+      THETA=YLOV+YHIV*(Y-1)/(FLOAT(NYCT)-1.0)
+      R=XLOV+(XHIV-XLOV)*(X-1.)/(FLOAT(NXCT)-1.)
+      FY=R*SIN(PDTOR*THETA)
+C
+      RETURN
+      END
+C
+C
+      FUNCTION MYF(X,Y,U,V,SFX,SFY,MX,MY)
+C
+C
+C The mapping common block: made available to user mapping routines
+C
+      COMMON /VVMAP/
+     +                IMAP       ,
+     +                XVPL       ,XVPR       ,YVPB       ,YVPT       ,
+     +                WXMN       ,WXMX       ,WYMN       ,WYMX       ,
+     +                XLOV       ,XHIV       ,YLOV       ,YHIV       ,
+     +                SXDC       ,SYDC       ,NXCT       ,NYCT       ,
+     +                RLEN       ,LNLG       ,INVX       ,INVY       ,
+     +                ITRT       ,IWCT       ,FW2W       ,FH2H       ,
+     +                DVMN       ,DVMX       ,RBIG       ,IBIG
+C
+      SAVE /VVMAP/
+C
+C Math constants
+C
+      PARAMETER (PDTOR  = 0.017453292519943,
+     +           PRTOD  = 57.2957795130823,
+     +           P1XPI  = 3.14159265358979,
+     +           P2XPI  = 6.28318530717959,
+     +           P1D2PI = 1.57079632679489,
+     +           P5D2PI = 7.85398163397448) 
+C
+C --------------------------------------------------------------------
+C
+      PARAMETER (PRCFAC=1E5,
+     +           PVFRAC=0.001,
+     +           PFOVFL=1E12,
+     +           IPMXCT=10,
+     +           PDUVML=2.0)
+C
+C
+C     The function FY behaves in an analagous manner to FX as
+C     described above.
+C
+      GOTO (10,20,30)  IMAP+1
+C
+C     The identity transformation.
+C     Polar coordinate transformation.
+C
+10    CONTINUE
+      MYF = MY + IFIX(SFY * V)
+      RETURN
+C
+C     EZMAP overlaying.
+C
+20    CONTINUE
+c
+      UVLEN=SQRT(U**2+V**2)
+C
+C
+C Check the vector magnitude
+C
+      IF (IFIX(UVLEN*PRCFAC) .EQ. 0) THEN
+         MYF=IBIG
+         RETURN
+      END IF
+C
+      CLMT=PVFRAC/UVLEN
+C
+      XLON = XLOV + (X-1.)*(XHIV-XLOV)/(FLOAT(NXCT)-1.)
+      YLAT = YLOV + (Y-1.)*(YHIV-YLOV)/(FLOAT(NYCT)-1.)
+c
+      CFCT=COS(YLAT*PDTOR)
+      ICNT = 0
+      ISGN = 1
+C
+      CALL MAPTRN(YLAT,XLON,X1,Y1)
+C
+ 35   CONTINUE
+      CALL MAPTRN(YLAT + ISGN*CLMT*V,XLON + ISGN*CLMT*U/CFCT,X2,Y2)
+
+      VCLEN=SQRT((X2-X1)**2+(Y2-Y1)**2)
+C
+      IF (VCLEN .GT. RLEN) THEN
+         IF (ISGN .EQ. -1) THEN
+            MYF=IBIG
+            RETURN
+         ELSE
+            ISGN=-1
+            GO TO 35
+         END IF
+      ELSE IF (IFIX(VCLEN*PRCFAC) .EQ. 0) THEN
+         IF (ICNT .LT. 10) THEN
+            ICNT = ICNT + 1
+            CLMT = CLMT * 2.0
+            GO TO 35
+         ELSE
+            MYF = IBIG
+            RETURN
+         END IF
+      END IF
+C
+      IF (ABS(Y2) .GE. PFOVFL) THEN
+         MYF = IBIG
+         RETURN
+      END IF
+C
+      T=ISGN*((Y2-Y1)/VCLEN)*UVLEN
+C
+      MYF=MY+IFIX(SFY*T)
+      RETURN
+C
+30    CONTINUE
+C
+      RETURN
+      END
