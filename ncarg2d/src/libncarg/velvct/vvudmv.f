223d811bc57f98b17c7cfec5ac3cd1697b4d9043
Dave Brown <dbrown@ucar.edu>
1992-12-03 00:08:16 +0000

    This file is a simple version of the user-definable draw masked vectors subroutine, VVUDMV. Its name must be passed as a parameter to VVECTR whenever the MSK parameter is set to a non-zero value.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@631 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/libncarg/velvct/vvudmv.f
@@ -0,0 +1,31 @@
+C
+C	$Id: vvudmv.f,v 1.1 1992-12-03 00:08:16 dbrown Exp $
+C
+      SUBROUTINE VVUDMV(XCS,YCS,NCS,IAI,IAG,NAI)
+C
+      DIMENSION XCS(NCS),YCS(NCS),IAI(NAI),IAG(NAI)
+C
+C
+C Example version of user masked drawing program
+C
+C XCS,YCS    - Arrays of X,Y coordinates
+C NCS        - Number of points
+C IAI        - Area identifier array
+C IAG        - Area group array
+C NAI        - number of area/group identifiers
+C
+C --------------------------------------------------------------------
+C
+      DO 101 I=1,NAI
+         IF (IAI(I).LT.0) RETURN
+ 101  CONTINUE
+C
+C If drawing is turned on, draw the polyline.
+C
+      CALL CURVE(XCS,YCS,NCS)
+C
+C Done.
+C
+      RETURN
+C
+      END
