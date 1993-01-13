182265861651e8dee47f4bdecc25e57caaa1740a
Mary Haley <haley@ucar.edu>
1993-01-13 18:17:21 +0000

    Added AREAS tutorial examples.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@706 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/tutorial/tareas/caredg.f
@@ -0,0 +1,147 @@
+      PROGRAM CAREDG
+C
+C Produce a Mercator projection of the Americas, using simplified 
+C continental outlines.  See the routine MAPEOD, below.
+C And draw a basic contour plot beside it.
+C
+        PARAMETER (M=40, N=40)
+        REAL Z(M,N), RWRK (2000), PLIM1(2),PLIM2(2),PLIM3(2),PLIM4(2)
+        INTEGER IWRK (2000)
+
+        DATA PLIM1 /-60.,0./
+        DATA PLIM2 /-170.,0./
+        DATA PLIM3 /75.,0./
+        DATA PLIM4 /-30.,0./
+C
+C Get some data
+C
+        CALL GENDAT(Z,M,M,N,1,25,1.0,25.)
+C
+C Open GKS.
+C
+      CALL OPNGKS
+C
+C Turn off the clipping indicator.
+C
+      CALL GSCLIP (0)
+C
+C Draw the map.
+C
+        CALL MAPPOS (0.0,.53,0.53,1.)
+        CALL SUPMAP (9,0.,0.,0.,PLIM1,PLIM2,PLIM3,PLIM4,2,0,2,0,IERR)
+        CALL GETSET (VPL,VPR,VPB,VPT,WL,WR,WB,WT,LOG)
+        CALL GSELNT (0)
+        CALL PLCHHQ (.25,.50,'Geographic Map',.013,0.,0.)
+C
+C Draw contour plot
+C
+        CALL SET (.53, 1.,VPB,VPT,1.,REAL(M),1.,REAL(N),LOG)
+        CALL CPSETR ('SET',0.)
+        CALL CPSETR ('LLP',0.)
+        CALL CPRECT (Z, M, M, N, RWRK, 2000, IWRK, 2000)
+        CALL PERIM (0,0,0,0)
+        CALL CPCLDR (Z, RWRK, IWRK)
+        CALL GSELNT (0)
+        CALL PLCHHQ (.75,.50,'Contour Map',.013,0.,0.)
+
+C
+C Draw Vertical Strips
+C
+        CALL SET(0.,1.,0.,1.,0.,1.,0.,1.,1)
+	CALL LINE (.2,.05,.8,.05)
+	CALL LINE (.2,.45,.8,.45)
+	CALL LINE (.2,.05,.2,.45)
+	CALL LINE (.3,.05,.3,.45)
+	CALL LINE (.4,.05,.4,.45)
+	CALL LINE (.5,.05,.5,.45)
+	CALL LINE (.6,.05,.6,.45)
+	CALL LINE (.7,.05,.7,.45)
+	CALL LINE (.8,.05,.8,.45)
+        CALL GSELNT (0)
+        CALL PLCHHQ (.50,.01,'Vertical Strips',.013,0.,0.)
+C
+C Close GKS.
+C
+      CALL CLSGKS
+C
+C Done.
+C
+      STOP
+C
+      END
+
+      SUBROUTINE GENDAT (DATA,IDIM,M,N,MLOW,MHGH,DLOW,DHGH)
+C
+C This is a routine to generate test data for two-dimensional graphics
+C routines.  Given an array "DATA", dimensioned "IDIM x 1", it fills
+C the sub-array ((DATA(I,J),I=1,M),J=1,N) with a two-dimensional field
+C of data having approximately "MLOW" lows and "MHGH" highs, a minimum
+C value of exactly "DLOW" and a maximum value of exactly "DHGH".
+C
+C "MLOW" and "MHGH" are each forced to be greater than or equal to 1
+C and less than or equal to 25.
+C
+C The function used is a sum of exponentials.
+C
+        DIMENSION DATA(IDIM,1),CCNT(3,50)
+C
+        FOVM=9./FLOAT(M)
+        FOVN=9./FLOAT(N)
+C
+        NLOW=MAX0(1,MIN0(25,MLOW))
+        NHGH=MAX0(1,MIN0(25,MHGH))
+        NCNT=NLOW+NHGH
+C
+        DO 101 K=1,NCNT
+          CCNT(1,K)=1.+(FLOAT(M)-1.)*FRAN()
+          CCNT(2,K)=1.+(FLOAT(N)-1.)*FRAN()
+          IF (K.LE.NLOW) THEN
+            CCNT(3,K)=-1.
+          ELSE
+            CCNT(3,K)=+1.
+          END IF
+  101   CONTINUE
+C
+        DMIN=+1.E36
+        DMAX=-1.E36
+        DO 104 J=1,N
+          DO 103 I=1,M
+            DATA(I,J)=.5*(DLOW+DHGH)
+            DO 102 K=1,NCNT
+              TEMP=-((FOVM*(FLOAT(I)-CCNT(1,K)))**2+
+     +               (FOVN*(FLOAT(J)-CCNT(2,K)))**2)
+              IF (TEMP.GE.-20.) DATA(I,J)=DATA(I,J)+
+     +            .5*(DHGH-DLOW)*CCNT(3,K)*EXP(TEMP)
+  102       CONTINUE
+            DMIN=AMIN1(DMIN,DATA(I,J))
+            DMAX=AMAX1(DMAX,DATA(I,J))
+  103     CONTINUE
+  104   CONTINUE
+C
+        DO 106 J=1,N
+          DO 105 I=1,M
+            DATA(I,J)=(DATA(I,J)-DMIN)/(DMAX-DMIN)*(DHGH-DLOW)+DLOW
+  105     CONTINUE
+  106   CONTINUE
+C
+        RETURN
+C
+      END
+      FUNCTION FRAN ()
+        DIMENSION RSEQ (100)
+        SAVE ISEQ
+        DATA RSEQ / .749,.973,.666,.804,.081,.483,.919,.903,.951,.960 ,
+     +              .039,.269,.270,.756,.222,.478,.621,.063,.550,.798 ,
+     +              .027,.569,.149,.697,.451,.738,.508,.041,.266,.249 ,
+     +              .019,.191,.266,.625,.492,.940,.508,.406,.972,.311 ,
+     +              .757,.378,.299,.536,.619,.844,.342,.295,.447,.499 ,
+     +              .688,.193,.225,.520,.954,.749,.997,.693,.217,.273 ,
+     +              .961,.948,.902,.104,.495,.257,.524,.100,.492,.347 ,
+     +              .981,.019,.225,.806,.678,.710,.235,.600,.994,.758 ,
+     +              .682,.373,.009,.469,.203,.730,.588,.603,.213,.495 ,
+     +              .884,.032,.185,.127,.010,.180,.689,.354,.372,.429 /
+        DATA ISEQ / 0 /
+        ISEQ=MOD(ISEQ,100)+1
+        FRAN=RSEQ(ISEQ)
+        RETURN
+      END
---
+++ ncarg2d/src/tutorial/tareas/carfill.f
@@ -0,0 +1,176 @@
+c23456789112345678921234567893123456789412345678951234567896123456789712
+        PROGRAM ARFILL
+
+        PARAMETER (LMAP=5000, NMAP=43, NPTS=50, NCNTR=5, NGRPS=2)
+        PARAMETER (NWRK=1000)
+
+        INTEGER MAP(LMAP), IAREA(NGRPS), IGRP(NGRPS)
+        REAL XGEO(NMAP), YGEO(NMAP), X(12), Y(12)
+        REAL XCNTR(NPTS), YCNTR1(NPTS), YCNTR2(NPTS), YCNTR3(NPTS)
+        REAL YCNTR4(NPTS), YCNTR5(NPTS), XPERIM(5), YPERIM(5)
+	REAL XWRK(NWRK), YWRK(NWRK)
+        CHARACTER*14 STRING
+
+        DATA XGEO / .63, .12, .05, .07, .10, .04, .19, .31, .31, .41,
+     +              .39, .47, .64, .63, .70, .66, .67, .69, .76, .92,
+     2              .95, .69, .64, .53, .53, .60, .63, .63, .72, .74,
+     3              .79, .75, .75, .80, .75, .70, .68, .64, .63, .55,
+     4              .55, .63, .63/
+        DATA YGEO / .94, .95, .92, .85, .83, .78, .84, .75, .69, .58,
+     +              .64, .55, .47, .37, .30, .05, .03, .05, .13, .26,
+     2              .38, .52, .50, .57, .63, .63, .59, .64, .72, .71,
+     3              .75, .75, .77, .78, .85, .83, .86, .86, .77, .80,
+     4              .86, .90, .94/
+        DATA XPERIM /0.0, 1.0, 1.0, 0.0, 0.0/
+        DATA YPERIM /0.0, 0.0, 1.0, 1.0, 0.0/
+        DATA X /.10, .22, .25, .25, .25, .50,
+     +          .30, .47, .50, .77, .75, .68/
+        DATA Y /.98, .70, .55, .38, .18, .18,
+     +          .90, .85, .70, .35, .18, .05/
+
+        EXTERNAL FILL
+
+        XCNTR(1) = 0.0
+        YCNTR1(1) = 0.25
+        YCNTR2(1) = 0.40
+        YCNTR3(1) = 0.60
+        YCNTR4(1) = 0.80
+        YCNTR5(1) = 0.95
+        DO 10 J=2,NPTS
+           DIST = REAL(J)/REAL(NPTS)
+           XCNTR(J) = DIST
+           YCNTR1(J) = .1*COS(REAL(4*3.14*DIST))+.15
+           YCNTR2(J) = .1*COS(REAL(4*3.14*DIST))+.30
+           YCNTR3(J) = .1*COS(REAL(4*3.14*DIST))+.50
+           YCNTR4(J) = .1*COS(REAL(4*3.14*DIST))+.70
+           YCNTR5(J) = .1*COS(REAL(4*3.14*DIST))+.85
+ 10     CONTINUE
+
+        CALL OPNGKS
+
+        CALL COLOR
+C
+C Set the fill area option to solid
+C
+	CALL GSFAIS(1)
+
+C
+C Initialize Areas
+C
+        CALL ARINAM (MAP,LMAP)
+C
+C Add continents to area map in group 1.
+C
+        CALL AREDAM (MAP, XGEO, YGEO, NMAP, 1, 2, 1)
+C
+C Add contours and perimeter to area map in group 3.
+C
+        CALL AREDAM (MAP, XCNTR, YCNTR1, NPTS, 3, 2, 1)
+        CALL AREDAM (MAP, XCNTR, YCNTR2, NPTS, 3, 3, 2)
+        CALL AREDAM (MAP, XCNTR, YCNTR3, NPTS, 3, 4, 3)
+        CALL AREDAM (MAP, XCNTR, YCNTR4, NPTS, 3, 5, 4)
+        CALL AREDAM (MAP, XCNTR, YCNTR5, NPTS, 3, 6, 5)
+C
+C Fill water blue and contour lines according to area id.
+C
+	CALL ARSCAM(MAP,XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS,FILL)
+C
+C Write out area and group identifiers for each area, using red for
+C geographic identifiers, and green for contour identifiers.
+C
+	CALL SFLUSH
+        CALL GSPLCI(0)
+        DO 20 I=1,12
+           CALL ARGTAI(MAP, X(I), Y(I), IAREA, IGRP, NGRPS, NAI, 0)
+           DO 30 J=1,2
+	      CALL GSLWSC (3.)
+	      IF ((I.LE.6).OR.(I.EQ.12)) CALL GSPLCI(3)
+	      IF (I.EQ.7) CALL GSPLCI(0)
+	      IF (I.EQ.8) CALL GSPLCI(3)
+	      IF (I.EQ.9) CALL GSPLCI(6)
+	      IF (I.EQ.10) CALL GSPLCI(0)
+	      IF (I.EQ.11) CALL GSPLCI(3)
+              WRITE (STRING,11) J,IAREA(J),J,IGRP(J)
+              IF (IGRP(J).EQ.1) THEN
+                 CALL PLCHHQ (X(I), Y(I), STRING, .01, 0., 0.)
+              ENDIF
+              IF (IGRP(J).EQ.3) THEN
+                 CALL PLCHHQ (X(I), Y(I)-.02, STRING, .01, 0., 0.)
+              ENDIF
+	      CALL GSLWSC (1.)
+ 30        CONTINUE
+ 20     CONTINUE
+C
+C Draw contours and perimeter in white
+C
+	CALL GSPLCI (6)
+	CALL CURVE (XCNTR,YCNTR1,NPTS)
+	CALL CURVE (XCNTR,YCNTR2,NPTS)
+	CALL CURVE (XCNTR,YCNTR3,NPTS)
+	CALL CURVE (XCNTR,YCNTR4,NPTS)
+	CALL CURVE (XCNTR,YCNTR5,NPTS)
+	CALL CURVE (XPERIM,YPERIM,5)
+
+        CALL FRAME
+        CALL CLSGKS
+C
+C Write out amount of space needed for the area map
+C
+	ISPACE = MAP(1) - MAP(6) + MAP(5)
+	WRITE (6,*) 'Area Map Space Used: ',ISPACE
+
+ 11     FORMAT ('A(',I1,')=',I1,' G(',I1,')=',I1)
+        STOP
+        END
+
+        SUBROUTINE COLOR
+
+C
+C Define color table
+C
+C Background is black
+        CALL GSCR(1,0,0.,0.,0.)
+C Red
+        CALL GSCR(1,1,1.,0.,0.)
+C Green
+        CALL GSCR(1,2,0.,1.,0.)
+C Yellow
+        CALL GSCR(1,3,1.,1.,0.)
+C Blue
+        CALL GSCR(1,4,0.,0.,1.)
+C Magenta
+        CALL GSCR(1,5,1.,0.,1.)
+C White
+        CALL GSCR(1,6,1.,1.,1.)
+C Aqua
+        CALL GSCR(1,7,0.,1.,1.)
+
+        RETURN
+        END
+
+        SUBROUTINE FILL(XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS)
+
+        INTEGER IAREA(NGRPS),IGRP(NGRPS),IDGEO,IDCNTR
+        REAL XWRK(NWRK),YWRK(NWRK)
+
+	IDGEO=-1
+	IDCNTR=-1
+C Retrieve geographic and contour area identifier info
+	DO 10, I=1,NGRPS
+	   IF (IGRP(I).EQ.1) IDGEO=IAREA(I)
+	   IF (IGRP(I).EQ.3) IDCNTR=IAREA(I)
+ 10	CONTINUE
+
+C If the contour segment is over water, then fill it in aquamarine
+        IF (IDGEO .EQ. 1) THEN
+           CALL GSFACI(7)
+           CALL GFA(NWRK,XWRK,YWRK)
+C If the contour band is over land, then fill it according to it's 
+C area identifier info.
+	ELSE IF ((IDGEO .EQ. 2) .AND. (IDCNTR .GE. 1)) THEN
+           CALL GSFACI(IDCNTR)
+           CALL GFA(NWRK,XWRK,YWRK)
+        ENDIF
+
+        RETURN
+        END
