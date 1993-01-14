c3095d8d78fd2b0d76798fc178161098b0f6a63d
fred <fred>
1993-01-14 19:54:41 +0000

    Augmented SFLUSH to update all open workstations.  For metafile output this has the effect of flushing the GKS buffers as well as the system-level I/O buffers.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@731 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/spps/sflush.f
+++ ncarg2d/src/libncarg/spps/sflush.f
@@ -1,11 +1,33 @@
 C
-C	$Id: sflush.f,v 1.1.1.1 1992-04-17 22:32:29 ncargd Exp $
+C	$Id: sflush.f,v 1.2 1993-01-14 19:54:41 fred Exp $
 C
       SUBROUTINE SFLUSH
 C
-C SFLUSH currently does nothing except flush the pen-move buffer.
+C SFLUSH flushes the GKS pen-move buffer as well as executing
+C an UPDATE WORKSTATION on all open workstations.  In particular,
+C for a CGM workstation, this effects a flush of the system level
+C I/O buffers.
+C
+      INTEGER WKID
+C
+C Flush the GKS pen move buffer.
 C
       CALL PLOTIF (0.,0.,2)
+C
+C Execute UPDATE WORKSTATION on all open workstations.
+C
+C
+C  If no workstations are open, return.
+C
+      CALL GQOPWK (1,IER,NO,ID)
+      IF (NO .EQ. 0) RETURN
+C
+C  Update all workstations.
+C
+      DO 200 I=1,NO
+        CALL GQOPWK (I,IERR,NO,WKID)
+        CALL GUWK(WKID,0)
+  200 CONTINUE
 C
 C Done.
 C
