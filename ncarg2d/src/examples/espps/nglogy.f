c506707945794a3f2b766c311244831c0ea19e0e
Mary Haley <haley@ucar.edu>
1993-01-14 00:28:05 +0000

    Added Bob's four SPPS examples.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@724 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/examples/espps/nglogy.f
@@ -0,0 +1,72 @@
+      PROGRAM NGLOGY
+C
+C  This test program is an example of logarithmic axis scaling
+C   in the NCAR Graphics User Coordinate System.
+C
+C       Dimension a line of 100 points
+C
+      DIMENSION X(100),Y(100)
+
+C
+C       Open GKS
+C
+      CALL OPNGKS
+C
+C       Turn clipping off
+C
+      CALL GSCLIP(0)
+C
+C       Generate a straight line of 100 points.
+C
+      DO 1 I=1,100
+      X(I) = I
+      Y(I) = 10.*I
+  1   CONTINUE
+C
+C       Select axes of linear in X, logarithmic in Y,
+C        in NCAR Graphics user coordinates.
+C
+      CALL SET(.05,.95,.20,.95,1.,100.,10.,1000.,2)
+C
+C       Set attributes for output
+C        Assign yellow to color index 2
+C
+      CALL GSCR(1,2,1.,1.,0.)
+C
+C       Generate output (GKS, SPPS, or NCAR utilities)
+C
+C         Set polyline color index to yellow
+C
+      CALL GSPLCI(2)
+C
+C         Initialize the AUTOGRAPH entry EZXY so that
+C         the frame is not advanced and the Y axis is logarithmic.
+C
+      CALL DISPLA(2,0,2)
+C
+C         Output the polyline (X,Y) using EZXY.
+C
+      CALL EZXY(X,Y,100,'')
+C
+C       Add a yellow title.
+C
+C        PLOTCHAR uses stroked characters; thus, the yellow polyline
+C        color index previously set will still apply.
+C
+C       Return the window to fractional coordinates for the title.
+C       Also, return scaling to linear, linear.
+C
+      CALL SET(0.,1.,0.,1.,0.,1.,0.,1.,1)
+      CALL PLCHHQ(0.5,.05,'Example 5.1.  Log Scaling with SPPS',
+     1 .019,0.,0.)
+C
+C       Advance the frame to ensure all output is plotted
+C
+      CALL FRAME
+C
+C       Close GKS
+C
+      CALL CLSGKS
+C
+      STOP
+      END
