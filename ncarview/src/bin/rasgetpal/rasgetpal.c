03dc77b8040b9446f94644e54a6494e914d493c6
don <don>
1993-02-17 23:38:58 +0000

    Added file output to original standard output, improved error reporting, and added examples to -help.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@993 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/src/bin/rasgetpal/rasgetpal.c
+++ ncarview/src/bin/rasgetpal/rasgetpal.c
@@ -25,14 +25,17 @@ static	Option	get_options[] = {
 };
 
 static void	Usage();
+static void	Examples();
 static int	PrintPalette();
 
 main(argc, argv)
 	int	argc;
 	char	*argv[];
 {
-	int	i;
-	int	opt_id;
+	int		i;
+	int		opt_id;
+	static char	*srcfile = (char *) NULL;
+	static char	*dstfile = (char *) NULL;
 
 	ProgramName = argv[0];
 
@@ -58,9 +61,16 @@ main(argc, argv)
 		exit(0);
 	}
 
-	if (argc == 1 || opt.help) {
+	if (opt.help) {
 		Usage(ProgramName, (char *) NULL, opt_id);
 		exit(0);
+	}
+
+	if (argc == 1) {
+		(void) fprintf(stderr,
+			"%s: No filename arguments\n",
+			ProgramName);
+		exit(1);
 	}
 
 	/* Make sure nothing left on command line execpt file names. */
@@ -80,20 +90,49 @@ main(argc, argv)
 			Usage(ProgramName, (char *) NULL, opt_id);
 		}
 		else {
-			(void) PrintPalette(argv[i]);
+			if (srcfile == (char *) NULL) {
+				srcfile = argv[i];
+			}
+			else if (dstfile == (char *) NULL) {
+				dstfile = argv[i];
+			}
+			else {
+				(void) fprintf(stderr,
+				"%s: Too many filename arguments\n",
+				ProgramName);
+				exit(1);
+			}
 		}
 	}
+
+	if (srcfile == (char *) NULL) {
+		(void) fprintf(stderr,
+			"%s: No input rasterfile\n",
+			ProgramName);
+		exit(1);
+	}
+
+	if (dstfile == (char *) NULL) {
+		PrintPalette(srcfile, "stdout");
+	}
+	else {
+		PrintPalette(srcfile, dstfile);
+	}
+
+	return(0);
 }
 
-static int PrintPalette(name)
-	char	*name;
+static int PrintPalette(rasfile, palfile)
+	char	*rasfile;
+	char	*palfile;
 {
 	int		i, status;
 	Raster		*ras, *RasterOpen();
+	unsigned char	colors[768];
 
-	ras = RasterOpen(name, (char *) NULL);
+	ras = RasterOpen(rasfile, (char *) NULL);
 	if (ras == (Raster *) NULL) {
-		(void) RasterPrintError(name);
+		(void) RasterPrintError();
 		return(RAS_ERROR);
 	}
 
@@ -102,24 +141,38 @@ static int PrintPalette(name)
 	/* Terminate for error or EOF */
 
 	if (status != RAS_OK) {
-		(void) RasterPrintError(name);
+		(void) RasterPrintError();
 		return(RAS_ERROR);
 	}
 
 	if (ras->type == RAS_DIRECT) {
 		(void) fprintf(stderr,
 		"%s: %s is a DirectColor file and has no palette\n",
-		ProgramName, name);
-		return(RAS_OK);
+		ProgramName, rasfile);
+		return(RAS_ERROR);
 	}
 	else if (ras->type == RAS_INDEXED) {
-		for(i=0; i<ras->ncolor; i++) {
-			(void) fprintf(stdout,
-				"%3d %3d %3d %3d\n", i,
-				ras->red[i],
-				ras->green[i],
-				ras->blue[i]);
+		for(i=0; i<256; i++) {
+			colors[i]     = ras->red[i];
+			colors[i+256] = ras->green[i];
+			colors[i+512] = ras->blue[i];
 		}
+
+		if (!strcmp(palfile, "stdout")) {
+			status = PaletteWrite(palfile, "txt", colors);
+		}
+		else {
+			status = PaletteWrite(palfile, (char *) NULL, colors);
+		}
+
+		if (status != RAS_OK) {
+			(void) fprintf(stderr,
+				"%s: %s\n",
+				ProgramName, ErrGetMsg());
+			return(status);
+		}
+			
+		if (status != RAS_OK) return(status);
 	}
 
 	return(RAS_OK);
@@ -135,10 +188,32 @@ static	void	Usage(progName, message, opt_id)
 	}
 
 	(void) fprintf(stderr, 
-		"%s: Usage: %s [-help] [-Version] rasterfile\n",
-		progName, progName);
+	"Usage: %s [-help] [-Version] rasterfile palfile\n",
+	progName);
+
+	(void) fprintf(stderr, 
+	"Usage: %s [-help] [-Version] rasterfile >palfile\n",
+	progName);
+
 	PrintOptionHelp(opt_id, stderr);
-	(void) fprintf(stderr,
-		"\n    Example:   rasgetpal temp.sun >temp.txt\n\n");
+
+	Examples();
+
 	exit(1);
+}
+
+static	void	Examples()
+{
+	int		i;
+
+	static char	*help_msg[] = {
+	"\n",
+	"Examples:",
+	"	rasgetpal temp.sun temp.txt",
+	"	rasgetpal temp.sun >temp.txt",
+	"	rasgetpal temp.sun temp.pal"};
+
+	for(i=0; i<sizeof(help_msg)/sizeof(char *); i++) {
+		(void) fprintf(stderr, "%s\n", help_msg[i]);
+	}
 }
