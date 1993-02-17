67f65bffb330f9737d814a295c8ee6f2a1e291f2
don <don>
1993-02-17 23:20:58 +0000

    Added examples, "see also's", and cleaned up text.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@991 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/man/rassplit.m
+++ ncarview/man/rassplit.m
@@ -1,17 +1,17 @@
 .\"
-.\"	$Id: rassplit.m,v 1.2 1993-02-16 17:33:04 don Exp $
+.\"	$Id: rassplit.m,v 1.3 1993-02-17 23:20:58 don Exp $
 .\"
 .TH RASSPLIT 1NCARG "January 1993" NCARG "NCAR VIEW"
-.SH NAME
+.SH \fBNAME\fP
 rassplit \- split a multi-frame rasterfile into single-frame rasterfiles.
-.SH SYNOPSIS
+.SH \fBSYNOPSIS\fP
 \fBrassplit\fP 
 [\fB\-f f1 f2 f3 ...\fR]
 [\fB\-ofmt output-format\fR]
 [\fB\-verbose\fR]
 [\fB\-Version\fR]
 \fIrasterfile\fP
-.SH DESCRIPTION
+.SH \fBDESCRIPTION\fP
 .sp
 The program \fBrassplit\fP will open \fIrasterfile\fP and copy each frame
 to it's own separate output file. By default, all frames are
@@ -24,7 +24,7 @@ The names of the output rasterfiles are derived from the name
 of the source rasterfile, the output format, and
 the frame numbers. The examples given below will demonstrate
 this.
-.SH OPTIONS
+.SH \fBOPTIONS\fP
 .TP
 .BI \-f " f1 f2 f3 ..."
 This option allows the user to specify a list of frames
@@ -43,12 +43,39 @@ Print a message each time an output rasterfile is written.
 .BI \-Version
 Print the version number.
 .sp
-.SH "CAVEATS"
-If you're splitting an HDF file, ask for a different output
+.SH \fBEXAMPLES\fP
+.LP
+Assume you have a file called 'temp.nrif' that has three frames and
+that you'd like to split it into three separate frames, also in NRIF
+format.
+.LP
+.in +1.0i
+.nf
+rassplit temp.nrif
+.fi
+.in -1.0i
+.LP
+This will result in three output files named temp.0001.nrif,
+temp.0002.nrif, and temp.003.nrif.
+.LP
+Assume you only want frames two and three, and you want them produced
+in HDF format instead of NRIF.
+.LP
+.in +1.0i
+.nf
+rassplit -fmt hdf -f 2 3 temp.nrif
+.fi
+.in -1.0i
+.LP
+This will result in two output files named temp.0001.hdf and
+temp.0002.hdf.
+.sp
+.SH \fBCAVEATS\fP
+If you're splitting an HDF file, ask for a non-HDF output
 format. Otherwise you'll just get the first frame over and
 over again.
 .sp
-.SH "SEE ALSO"
+.SH "\fBSEE ALSO\fP"
 .LP
-ras_formats(5NCARG), rasview(1NCARG), rascat(1NCARG), rasls(1NCARG),
-rasdiff(1NCARG), rasgetpal(1NCARG)
+\fBrasview\fP(1NCARG),\fBrascat\fP(1NCARG),\fBrasls\fP(1NCARG),
+\fBrasgetpal\fP(1NCARG),\fBras_formats\fP(5NCARG),\fBras_palette\fP(5NCARG)
