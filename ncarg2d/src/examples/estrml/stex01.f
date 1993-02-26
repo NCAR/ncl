3643122d29446a0c156bf935a2ea818d97c5bce0
Dave Brown <dbrown@ucar.edu>
1993-02-26 23:50:01 +0000

    Change for Version 3.2: 1) Updated STREAM argument list to be compatible with changes to stream.f 2) Removed obsolete comments, and unused declarations.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1078 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/examples/estrml/stex01.f
+++ ncarg2d/src/examples/estrml/stex01.f
@@ -1,44 +1,11 @@
 C
-C	$Id: stex01.f,v 1.3 1993-02-20 00:46:23 dbrown Exp $
+C	$Id: stex01.f,v 1.4 1993-02-26 23:49:57 dbrown Exp $
 C
       PROGRAM STEX01
 C
-C     This program produces plots illustrating non-trivial
-C     usage of the NCAR Utilities.  This program is an
-C     exact copy of an implementation that runs on a UNIX
-C     machine at NCAR.  To implement this program on another
-C     machine only the following steps should be required:
-C
-C       1.)  Modify the OPEN to the EZMAP dataset that
-C            appears in the main program.
-C
-C       2.)  Modify the OPEN to the file of random numbers
-C            RANFDAT in subroutine RANDNO.
-C
-C     The utilities AUTOGRPH, VELVCT, CONREC, EZMAP, GRIDAL,
-C     and DASHCHAR are required.  Since CONREC and VELVCT plots
-C     are overlaid on EZMAP, the arithmetic statement functions
-C     for FX and FY in these two utilities should be changed
-C     to the statement:
-C
-C      EXTERNAL FX,FY
-C
-C     This will force the functions FX and FY supplied in this
-C     package to be loaded.  The documentation for FX explains
-C     the transformations provided.
-C
       PARAMETER ( M=20 , N=36 , NPR=155)
-      PARAMETER (PI=3.14159 , TWOPI=2.*PI , EPS=PI/6.)
       DIMENSION A(M,N),B(M,N)
       DIMENSION WRK(2*M*N)
-      COMMON  /TRANS/  MA,NA,ITRANS,A1,A2,D1,D2,ALNMN,ALNMX,ALTMN,ALTMX
-      CHARACTER*1  ZERO
-      CHARACTER*16 LDASH(1)
-      DATA  LDASH(1) /'$$''$$''$$''$$''$$''$'/
-C
-C Modification for UNIX Version.
-C       OPEN statement removed.
-C End of modification for UNIX Version.
 C
 C     Open GKS, open workstation, activate workstation.
 C
@@ -64,7 +31,7 @@ C     CALL STSETI('TRT -- Transform Type', 1)
 C
       CALL STINIT(A,M,B,M,IDM,IDM,M,N,WRK,2*M*N)
       CALL GSPLCI(7)
-      CALL STREAM(A,B,WRK,IDM,IDM)
+      CALL STREAM(A,B,IDM,IDM,IDM,WRK)
       CALL GSPLCI(2)
       DO 120 I = 1,M
          DO 110 J = 1,N
@@ -73,7 +40,7 @@ C
  110     CONTINUE
  120  CONTINUE
       CALL STINIT(A,M,B,M,IDM,IDM,M,N,WRK,2*M*N)
-      CALL STREAM(A,B,WRK,IDM,IDM)
+      CALL STREAM(A,B,IDM,IDM,IDM,WRK)
       CALL FRAME
 C
 C     Deactivate and close workstation, close GKS.
