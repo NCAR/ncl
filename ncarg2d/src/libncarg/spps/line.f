dc8d6b90e45b574ccb07e17f6ffe1639637f7c2b
kennison <kennison>
1993-03-02 17:32:41 +0000

    I added a single line to force dumping of the PLOTIT/PLOTIF line-draw buffers before drawing the line.  This was intended to make LINE draw things using the current color/line-width.  As I write this, I realize that this "solution" is not entirely correct, so I will almost certainly change the code again within the hour.  Sigh.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1085 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/spps/line.f
+++ ncarg2d/src/libncarg/spps/line.f
@@ -1,11 +1,12 @@
 C
-C	$Id: line.f,v 1.1.1.1 1992-04-17 22:32:30 ncargd Exp $
+C $Id: line.f,v 1.2 1993-03-02 17:32:41 kennison Exp $
 C
       SUBROUTINE LINE (X1,Y1,X2,Y2)
 C
 C Draw a line connecting the point (X1,Y1) to the point (X2,Y2), in the
 C user coordinate system.
 C
+      CALL PLOTIF (0.,0.,2)
       CALL PLOTIF (CUFX(X1),CUFY(Y1),0)
       CALL PLOTIF (CUFX(X2),CUFY(Y2),1)
       RETURN
