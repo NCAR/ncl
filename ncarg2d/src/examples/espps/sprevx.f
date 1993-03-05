248037f38128d3a6b82d1562fe142fd2fa5b934a
Mary Haley <haley@ucar.edu>
1993-03-05 00:39:50 +0000

    Finally, <gasp> <pant> I am renaming these examples to follow the naming convention of SPPS examples being represented by the two-letter "sp" code.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1092 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/examples/espps/sprevx.f
@@ -0,0 +1,76 @@
+      PROGRAM NGREVX
+C
+C  This test program is an example of X axis scale reversal
+C   in the NCAR Graphics User Coordinate System.
+C
+C       Dimension a line of 100 points
+C
+      DIMENSION X(100),Y(100)
+
+C
+C       Open GKS
+C
+      CALL OPNGKS
+C
+C       Turn clipping off
+C
+      CALL GSCLIP(0)
+C
+C       Generate a straight line of 100 points.
+C
+      DO 1 I=1,100
+      X(I) = I
+      Y(I) = 10.*I
+  1   CONTINUE
+C
+C       Select the normalization transformation
+C        window and viewport.  Reverse X in the window.
+C
+      CALL SET(.10,.95,.20,.95,100.,1.,10.,1000.,1)
+C
+C       Set attributes for output
+C        Assign yellow to color index 2
+C
+      CALL GSCR(1,2,1.,1.,0.)
+C
+C       Generate output (GKS, SPPS, or NCAR utilities)
+C
+C         Set polyline color index to yellow
+C
+      CALL GSPLCI(2)
+C
+C         Initialize the AUTOGRAPH entry EZXY so that
+C         the frame is not advanced.
+C
+      CALL DISPLA(2,0,1)
+C
+C         Tell EZXY that the SET ordering of the window
+C         is to be used (LSET 3 or 4).
+C
+      CALL ANOTAT(' ',' ',1,4,0,' ')
+C
+C         Output the polyline (X,Y) using EZXY.
+C
+      CALL EZXY(X,Y,100,' ')
+C
+C       Add a yellow title.
+C
+C        PLOTCHAR uses stroked characters; thus, the yellow polyline
+C        color index previously set will still apply.
+C
+C       Return the window to fractional coordinates for the title.
+C
+      CALL SET(0.,1.,0.,1.,0.,1.,0.,1.,1)
+      CALL PLCHHQ(0.5,.05,'Example 5.2.  X Axis Reversal with SPPS',
+     1 .019,0.,0.)
+C
+C       Advance the frame to ensure all output is plotted
+C
+      CALL FRAME
+C
+C       Close GKS
+C
+      CALL CLSGKS
+C
+      STOP
+      END
---
+++ ncarg2d/src/examples/espps/spset1.f
@@ -0,0 +1,108 @@
+      PROGRAM NGSET1
+C
+C  This example shows the mapping from GKS world coordinates to
+C   GKS normalized device coordinates.
+C
+	DIMENSION XCRA(101),YCRA(101)
+	DATA DSL,DSR,DSB,DST / .59,.99,.40,.70 /
+	DATA PFL,PFR,PFB,PFT / .71,.98,.42,.69 /
+	CALL OPNGKS
+	CALL GSCLIP (0)
+	CALL GSLWSC (2.)
+	CALL SET    (0.,1.,0.,1.,0.,1.,0.,1.,1)
+	CALL PLCHMQ (.50,.98,
+     +           'MAPPING FROM A WINDOW IN THE WORLD COORDINATE SYSTEM',
+     +                                                       .015,0.,0.)
+	CALL PLCHMQ (.50,.95,
+     +       'TO A VIEWPORT IN THE NORMALIZED DEVICE COORDINATE SYSTEM',
+     +                                                       .015,0.,0.)
+	CALL PLCHMQ (.50,.92,'(USED IN CALLS TO GKS ROUTINES LIKE GCA, G
+     +FA, GPL, GPM, AND GTX)',
+     +                                                       .012,0.,0.)
+	CALL PLCHMQ (.50,.15,
+     +        'Assume the current window is:  100., 1000., 100., 1000.',
+     +                                                       .012,0.,0.)
+	CALL PLCHMQ (.50,.12,
+     +            'Assume the current viewport is:  .15, .95, .10, .90',
+     +                                                       .012,0.,0.)
+	CALL PLCHMQ (.50,.06,
+     +                                                       'Figure 1',
+     +                                                       .012,0.,0.)
+	CALL SET    (.01,.55,.29,.83,0.,1100.,0.,1100.,1)
+	CALL LINE   (   0.,   0.,1100.,   0.)
+	CALL LINE   (1100.,   0.,1050.,  25.)
+	CALL LINE   (1100.,   0.,1050., -25.)
+	CALL PLCHMQ (CFUX(CUFX(1100.)+.015),0.,'X',.015,0.,0.)
+	CALL LINE   (   0.,   0.,   0.,1100.)
+	CALL LINE   (   0.,1100.,  25.,1050.)
+	CALL LINE   (   0.,1100., -25.,1050.)
+	CALL PLCHMQ (0.,CFUY(CUFY(1100.)+.015),'Y',.015,0.,0.)
+	CALL LINE   ( 100., 100.,1000., 100.)
+	CALL LINE   (1000., 100.,1000.,1000.)
+	CALL LINE   (1000.,1000., 100.,1000.)
+	CALL LINE   ( 100.,1000., 100., 100.)
+	CALL PLCHMQ (CFUX(CUFX(100.)+.005),CFUY(CUFY(1000.)-.01),
+     +               'WINDOW',.01,0.,-1.)
+	CALL PLCHMQ (100.,CFUY(CUFY(100.)-.01),'100',.008,0.,0.)
+	CALL PLCHMQ (1000.,CFUY(CUFY(100.)-.01),'1000',.008,0.,0.)
+	CALL PLCHMQ (CFUX(CUFX(100.)-.005),100.,'100',.008,0.,1.)
+	CALL PLCHMQ (CFUX(CUFX(100.)-.005),1000.,'1000',.008,0.,1.)
+	DO 101 I=1,101
+	  XCRA(I)=200.+7.*REAL(I-1)
+	  YCRA(I)=550.+425.*SIN(EXP((XCRA(I)-200.)/300.)-1.)
+  101   CONTINUE
+	CALL CURVE  (XCRA,YCRA,101)
+	XCLW=CUFX(100.)
+	XCRW=CUFX(1000.)
+	YCBW=CUFY(100.)
+	YCTW=CUFY(1000.)
+	CALL SET    (DSL,DSR,DSB,DST,0.,1.,0.,1.,1)
+	CALL LINE   (0.,0.,1.,0.)
+	CALL LINE   (1.,0.,1.,1.)
+	CALL LINE   (1.,1.,0.,1.)
+	CALL LINE   (0.,1.,0.,0.)
+	CALL PLCHMQ (CFUX(CUFX(0.)+.005),CFUY(CUFY(1.)-.01),
+     +               'DEVICE',.01,0.,-1.)
+	CALL SET    (PFL,PFR,PFB,PFT,0.,1.,0.,1.,1)
+	CALL LINE   (0.,0.,1.,0.)
+	CALL LINE   (1.,0.,1.,1.)
+	CALL LINE   (1.,1.,0.,1.)
+	CALL LINE   (0.,1.,0.,0.)
+	CALL PLCHMQ (CFUX(CUFX(0.)+.005),CFUY(CUFY(1.)-.01),
+     +               'PLOTTER FRAME',.01,0.,-1.)
+	CALL PLCHMQ (0.,CFUY(CUFY(0.)-.01),'0',.008,0.,0.)
+	CALL PLCHMQ (1.,CFUY(CUFY(0.)-.01),'1',.008,0.,0.)
+	CALL PLCHMQ (CFUX(CUFX(0.)-.005),0.,'0',.008,0.,1.)
+	CALL PLCHMQ (CFUX(CUFX(0.)-.005),1.,'1',.008,0.,1.)
+	VPL=PFL+.15*(PFR-PFL)
+	VPR=PFL+.95*(PFR-PFL)
+	VPB=PFB+.10*(PFT-PFB)
+	VPT=PFB+.90*(PFT-PFB)
+	CALL SET    (VPL,VPR,VPB,VPT, 100.,1000., 100.,1000.,1)
+	CALL LINE   ( 100., 100.,1000., 100.)
+	CALL LINE   (1000., 100.,1000.,1000.)
+	CALL LINE   (1000.,1000., 100.,1000.)
+	CALL LINE   ( 100.,1000., 100., 100.)
+	CALL PLCHMQ (CFUX(CUFX(100.)+.005),CFUY(CUFY(1000.)-.01),
+     +               'VIEWPORT',.01,0.,-1.)
+	CALL PLCHMQ (100.,CFUY(CUFY(100.)-.01),'.15',.008,0.,0.)
+	CALL PLCHMQ (1000.,CFUY(CUFY(100.)-.01),'.95',.008,0.,0.)
+	CALL PLCHMQ (CFUX(CUFX(100.)-.005),100.,'.10',.008,0.,1.)
+	CALL PLCHMQ (CFUX(CUFX(100.)-.005),1000.,'.90',.008,0.,1.)
+	CALL CURVE  (XCRA,YCRA,101)
+	XCLV=CUFX(100.)
+	XCRV=CUFX(1000.)
+	YCBV=CUFY(100.)
+	YCTV=CUFY(1000.)
+	CALL SET    (0.,1.,0.,1.,0.,1.,0.,1.,1)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (1.)
+	CALL DASHDC ('$''',3,1)
+	CALL LINED  (XCLW,YCBW,XCLV,YCBV)
+	CALL LINED  (XCRW,YCBW,XCRV,YCBV)
+	CALL LINED  (XCRW,YCTW,XCRV,YCTV)
+	CALL LINED  (XCLW,YCTW,XCLV,YCTV)
+	CALL FRAME
+	CALL CLSGKS
+	STOP
+      END
