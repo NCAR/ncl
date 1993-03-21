33febc38095d0b7b76deb65d5c88ee49d759ef73
Mary Haley <haley@ucar.edu>
1993-03-21 01:53:28 +0000

    *** empty log message ***
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1152 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- common/man/ncargmv.m
+++ common/man/ncargmv.m
@@ -1,7 +1,7 @@
 .\"
-.\"	$Id: ncargmv.m,v 1.2 1992-12-01 23:25:08 clyne Exp $
+.\"	$Id: ncargmv.m,v 1.3 1993-03-21 01:53:17 haley Exp $
 .\"
-.TH NCARGMV 8NCARG "JUNE 1991" NCAR "NCAR GRAPHICS"
+.TH NCARGMV 8NCARG "March 1993 NCAR "NCAR GRAPHICS"
 .SH NAME
 ncargmv \- Move an installed version of NCAR Graphics.
 .SH SYNOPSIS
@@ -11,4 +11,5 @@ ncargmv \- Move an installed version of NCAR Graphics.
 .B ncargmv
 has no arguments and must be run interactively.  It is intended for use by a
 system administrator, and will move an installed version of NCAR Graphics (at
-revision level 3.1.2 or later) from its current position to a new position.
+revision level 3.2.0 or later) from its current position to a new position.
+
--- common/man/ncargtar.m
+++ common/man/ncargtar.m
@@ -1,7 +1,7 @@
 .\"
-.\"	$Id: ncargtar.m,v 1.2 1992-12-01 23:25:10 clyne Exp $
+.\"	$Id: ncargtar.m,v 1.3 1993-03-21 01:53:22 haley Exp $
 .\"
-.TH NCARGTAR 8NCARG "JUNE 1991" NCAR "NCAR GRAPHICS"
+.TH NCARGTAR 8NCARG "March 1993" NCAR "NCAR GRAPHICS"
 .SH NAME
 ncargtar \- Create tar file for NCAR Graphics.
 .SH SYNOPSIS
@@ -12,7 +12,7 @@ ncargtar \- Create tar file for NCAR Graphics.
 has no arguments.  It creates, in the current directory, a tar file
 containing all files written by the last installation of NCAR Graphics.
 The names of the files in the archive are given as full path names,
-preceded by a period (for example, "./u2/ncargfx/3.1.2/bin/ncargtar").
+preceded by a period (for example, "./u2/ncargfx/3.2.0/bin/ncargtar").
 Thus, if the files are extracted to the root directory "/", NCAR Graphics
 is restored; if the files are extracted to some other directory (for
 example, "/tmp/ncargfx", the directory hierarchy is created and the files
