6d5d281e5bd912ce866b3bab4e8dbb95828ce56e
Mary Haley <haley@ucar.edu>
1993-03-24 21:24:03 +0000

    Added Tim's ngmisc examples.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1182 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/fundamental/fngmisc/fnmngdts.f
@@ -0,0 +1,148 @@
+C
+	REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
+
+	DATA PLIM1 /0.,0./
+	DATA PLIM2 /0.,0./
+	DATA PLIM3 /0.,0./
+	DATA PLIM4 /0.,0./
+C
+C Open GKS, Turn Clipping off
+C
+      CALL OPNGKS 
+C
+C INVOKE DEMO DRIVER
+C
+      CALL CMPTRA('OR',35.,-105.,0.,'PO','CO',22.,-122.,47.,-65.)
+C
+C     DEACTIVATE AND CLOSE WORKSTATION, CLOSE GKS.
+C
+      CALL CLSGKS
+
+	STOP
+	END
+
+      SUBROUTINE CMPTRA(PROJ, PLAT, PLON, ROTA, OUTLN,
+     +	JLIM, PLIM1, PLIM2, PLIM3, PLIM4)
+
+	CHARACTER*2 PROJ, OUTLN, JLIM
+	REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
+        DATA NUMSTS/3/
+        REAL LAT(3), LON(3)
+        REAL XCOOR(3), YCOOR(3)
+        CHARACTER CITY(3)*20
+
+C LAT, LON coordinates for Atlanta, Boulder, and Seattle
+        DATA LAT/33.5,40.0,47.5/
+        DATA LON/-84.5,-105,-122.0/
+        DATA XCOOR/0.,0.,0./
+        DATA YCOOR/0.,0.,0./
+        DATA CITY/'Atlanta, GA', 'Boulder, CO', 'Seattle, WA'/
+C
+C CMPTRA demonstrates marking points on a map
+C
+C
+C Set up a color table
+C
+       CALL DFCLRS
+C
+C Draw Continental, political outlines 
+C
+        CALL MAPSTC ('OU - OUTLINE DATASET SELECTOR',OUTLN)
+C
+C Set up projection
+C
+	CALL MAPROJ (PROJ,PLAT,PLON,ROTA)
+C
+C If it's a satellite projection, choose a satellite distance
+C
+	IF (PROJ.EQ.'SV') CALL MAPSTR ('SA - SATELLITE DISTANCE',5.)
+C
+C Set limits of map
+C
+        CALL MAPSET (JLIM,PLIM1,PLIM2,PLIM3,PLIM4)
+C
+C Turn off Grid lines
+C
+	CALL MAPSTR ('GR',0.)
+C
+C Draw map
+C
+	CALL MAPDRW
+
+        DO 100 I=1,NUMSTS
+C
+C Transform the Coordinates
+C
+   	   CALL MAPTRA(LAT(I),LON(I),X,Y)
+           XCOOR(I) = X
+           YCOOR(I) = Y
+C
+C Draw a label
+C
+           CALL PLCHHQ (X+.015,Y,CITY(I), .015, 0., -1.)
+C
+C Save last indice
+C
+           INDICE = I
+ 100    CONTINUE
+
+C
+C Draw filled circles at selected sites.
+C
+        IF (XCOOR(INDICE).NE.1.E12) THEN
+           CALL NGDOTS (XCOOR,YCOOR,NUMSTS,.02,15)
+        ENDIF
+C
+C Draw stars over the selected sites and connect them with lines.
+C
+        CALL POINTS (XCOOR, YCOOR, NUMSTS, -3, 1)
+C
+C Advance the frame.
+C
+        CALL FRAME
+C
+C Done.
+C
+        RETURN
+	END
+
+      SUBROUTINE DFCLRS
+C
+C Define a set of RGB color triples for colors 1 through 15.
+C
+        DIMENSION RGBV(3,15)
+C
+C Define the RGB color triples needed below.
+C
+        DATA RGBV / 0.00 , 0.00 , 0.00 ,
+     +              0.70 , 0.70 , 0.70 ,
+     +              0.75 , 0.50 , 1.00 ,
+     +              0.50 , 0.00 , 1.00 ,
+     +              0.00 , 0.00 , 1.00 ,
+     +              0.00 , 0.50 , 1.00 ,
+     +              0.00 , 1.00 , 1.00 ,
+     +              0.00 , 1.00 , 0.60 ,
+     +              0.00 , 1.00 , 0.00 ,
+     +              0.70 , 1.00 , 0.00 ,
+     +              1.00 , 1.00 , 0.00 ,
+     +              1.00 , 0.75 , 0.00 ,
+     +              1.00 , 0.38 , 0.38 ,
+     +              1.00 , 0.00 , 0.38 ,
+     +              1.00 , 0.00 , 0.00 /
+C
+C Define 16 different color indices, for indices 0 through 15.  The
+C color corresponding to index 0 is black and the color corresponding
+C to index 1 is white.
+C
+        CALL GSCR (1,0,1.,1.,1.)
+C
+        DO 101 I=1,15
+          CALL GSCR (1,I,RGBV(1,I),RGBV(2,I),RGBV(3,I))
+  101   CONTINUE
+C
+C Done.
+C
+        RETURN
+C
+      END
+
---
+++ ncarg2d/src/fundamental/fngmisc/fnmwsym.f
@@ -0,0 +1,176 @@
+      PROGRAM PLTCHR
+C
+C  Plots tables of all the weather symbols.
+C
+      CHARACTER*2 CFNT(7)
+      CHARACTER*25 YLAB(2,7)
+      INTEGER YLEN(2,7)
+      DATA CFNT/'a ','N ','W ','CH','CM','CL','C '/
+      DATA YLAB/ 'a','Pressure Tendency ',
+     +           'N','Sky Cover         ',
+     +           'W','Past Weather      ',
+     +           'CH','High Clouds      ',
+     +           'CM','Medium Clouds    ',
+     +           'CL','Low Clouds       ',
+     +           'C','Cloud Types       '/
+      DATA YLEN/1,17,
+     +          1, 9,
+     +          1,12,
+     +          2,11,
+     +          2,13,
+     +          2,10,
+     +          1,11/ 
+C
+C  Open GKS, set color table and line width.
+C
+      CALL OPNGKS
+      CALL GSCR(1,0,1.,1.,1.)
+      CALL GSCR(1,1,0.,0.,0.)
+      CALL GSCR(1,2,1.,1.,0.)
+      CALL GSLWSC(1.)
+C
+C  Font WW -- Present Weather table.
+C
+C  Table width.
+C
+      X1 = .025
+      X2 = .975
+C
+C  Table heights.
+C  
+      Y1 = 0.
+      Y2 = .95
+      Y3 = 1.
+C
+C  Number of columns and rows.
+C
+      NC = 10
+      NR = 10
+C
+      CALL GSPLCI(1)
+      CALL GSTXCI(1)
+      CALL DTABLE('WW',NR,NC,X1,Y1,X2,Y2,Y3,1,1,1)
+      SIZE = .4*(Y3-Y2)
+      YY = .5*(Y2+Y3)
+      CALL NPUTS(YY,'WW','Present Weather',SIZE)
+      CALL FRAME
+C
+      NR = 1
+      NC = 10
+      R = 4.5/12.
+      DY = .095
+      SPC = 1./7.-(1.+R)*DY
+      DO 10 NF=1,7
+        YB = (NF-1)*((1.+R)*DY+SPC)
+        Y1 = YB
+        Y2 = YB+DY
+        Y3 = YB+(1+R)*DY
+        CALL DTABLE(CFNT(NF),NR,NC,X1,Y1,X2,Y2,Y3,1,1,1)
+        YY = .5*(Y2+Y3)
+        CALL NPUTS(YY,YLAB(1,NF)(1:YLEN(1,NF)),YLAB(2,NF)(1:YLEN(2,NF)),
+     +             .5*(Y3-Y2))
+   10 CONTINUE
+      CALL FRAME
+C
+      CALL CLSGKS
+      STOP
+      END
+      SUBROUTINE DTABLE(FONT,NR,NC,X1,Y1,X2,Y2,Y3,ITX1,ITX2,ILNC)
+C
+C  Draw table of characters from font FONT with NR rows and NC columns.
+C  The boundary of the table in world coordinates is (X1,Y1) to (X2,Y2).
+C  The line color index is ILNC and the text color index for the label
+C  characters is ITX1 and for the displayed characters is ITX2.
+C
+      DIMENSION XX(2),YY(2),XC(10),YC(10),XLC(10),YLC(10)
+      CHARACTER*(*) FONT
+      CHARACTER*2 NLAB
+C
+C  Draw table grid and compute the centers of the grid boxes.
+C
+      CALL GSLWSC(2.)
+      CALL GSPLCI(ILNC)
+      YINC = (Y2-Y1)/REAL(NR)
+      DO 10 J=1,NR+1
+        YY(1) = Y1+(J-1)*YINC
+        YY(2) = YY(1)
+        XX(1) = X1
+        XX(2) = X2
+        CALL GPL(2,XX,YY)
+        IF (J .LE. NR) THEN
+          YC(J) = YY(1)+.4*YINC
+          YLC(J) = YY(1)+.8*YINC
+        ENDIF
+   10 CONTINUE
+C
+      XINC = (X2-X1)/REAL(NC)
+      DO 20 I=1,NC+1
+        YY(1) = Y1
+        YY(2) = Y2
+        XX(1) = X1+(I-1)*XINC
+        XX(2) = XX(1)
+        CALL GPL(2,XX,YY)
+        IF (I .LE. NC) THEN
+          XC(I) = XX(1)+.6*XINC
+          XLC(I) = XX(1)+.2*XINC
+        ENDIF
+   20 CONTINUE
+      CALL GSLWSC(1.)
+C
+C  Draw the characters in the boxes.
+C
+      CALL GSTXAL(2,3)
+      CALL GSTXFP(-36,2)
+C    
+      CALL GSTXCI(ITX2)
+      DO 30 J=1,NR
+        DO 40 I=1,NC
+          NUM = (NR-J)*NC+I-1
+          CALL NGWSYM(FONT,NUM,XC(I),YC(J),.43*YINC,1,0)
+   40   CONTINUE
+   30 CONTINUE
+C    
+C  Draw the character number in the upper left of the box.
+C
+      CHGT = .14*YINC
+      DO 50 J=1,NR
+        DO 60 I=1,NC
+          NUM = (NR-J)*NC+I-1
+          WRITE(NLAB,500) NUM
+  500     FORMAT(I2)
+          CALL PCSETI('FN',26)
+          CALL PCSETI('CC',ITX1)
+          CALL PLCHHQ(XLC(I),YLC(J),NLAB,6.*CHGT/7.,0.,0.)
+   60   CONTINUE
+   50 CONTINUE
+C
+      RETURN
+      END
+      SUBROUTINE NPUTS(YY,STR1,STR2,SIZE)
+C
+C  Put out STR1, then a dash, then STR2 at size SIZE.
+C
+      CHARACTER*(*) STR1,STR2
+      DIMENSION TX(4),TY(4)
+C
+      CALL GSTXFP(-22,2)
+      CALL GSTXAL(1,3)
+      CALL GSCHH(SIZE)
+      CALL GQTXX(1,0.,YY,STR1,IER,CPX,CPY,TX,TY)
+      DX1 = CPX
+      CALL GQTXX(1,0.,YY,STR2,IER,CPX,CPY,TX,TY)
+      DX3 = CPX
+      CALL GSTXFP(-34,2)
+      CALL GQTXX(1,0.,YY,' > ',IER,CPX,CPY,TX,TY)
+      DX2 = CPX
+      DX = DX1+DX2+DX3
+      STRTX = .5-.5*DX
+      CALL PCSETI('FN',22)
+      CALL PLCHHQ(STRTX,YY,STR1,6.*SIZE/7.,0.,-1.)
+      CALL PLCHHQ(STRTX+DX1+DX2,YY,STR2,6.*SIZE/7.,0.,-1.)
+      CALL GSTXFP(-34,2)
+      CALL PCSETI('FN',34)
+      CALL PLCHHQ(STRTX+DX1,YY,' > ',6.*SIZE/7.,0.,-1.)
+C
+      RETURN
+      END
