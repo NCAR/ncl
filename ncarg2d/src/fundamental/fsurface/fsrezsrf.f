4a7ba2c2045c6a7daaa7ccc8afe4f3fa9f3ade2e
Mary Haley <haley@ucar.edu>
1993-03-24 21:25:14 +0000

    Added Tim's surface examples.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1186 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/fundamental/fsurface/fsrezsrf.f
@@ -0,0 +1,125 @@
+C
+C OPEN GKS, OPEN WORKSTATION OF TYPE 1, ACTIVATE WORKSTATION
+C
+      CALL GOPKS (6,IDUM) 
+      CALL GOPWK (1, 2, 1)
+      CALL GACWK (1) 
+C
+C INVOKE DEMO DRIVER
+C
+      CALL TSRFAC(IERR)
+C
+C     DEACTIVATE AND CLOSE WORKSTATION, CLOSE GKS.
+C
+      CALL GDAWK (1)
+      CALL GCLWK (1)
+      CALL GCLKS
+C
+      STOP
+      END
+C
+      SUBROUTINE TSRFAC (IERROR)
+C
+C PURPOSE                To provide a simple demonstration of EZSRFC.
+C
+C USAGE                  CALL TSRFAC (IERROR)
+C
+C ARGUMENTS
+C
+C ON OUTPUT              IERROR
+C                          An integer variable
+C                          = 0, if the test was successful,
+C                          = 1, the test was not successful.
+C
+C I/O                    If the test is successful, the message
+C
+C               SRFACE TEST EXECUTED--SEE PLOT TO CERTIFY
+C
+C                        is printed on unit 6.  In addition, 1
+C                        frame is produced on the machine graphics
+C                        device.  In order to determine if the test
+C                        was successful, it is necessary to examine
+C                        the plot.
+C
+C PRECISION              Single
+C
+C LANGUAGE               FORTRAN 77
+C
+C REQUIRED ROUTINES      EZSRFC
+C
+C REQUIRED GKS LEVEL     0A
+C
+C ALGORITHM              The function
+C
+C                          Z(X,Y) = .25*(X + Y + 1./((X-.1)**2+Y**2+.09)
+C                                   -1./((X+.1)**2+Y**2+.09)
+C
+C                        for X = -1. to +1. in increments of .1, and
+C                            Y = -1.2 to +1.2 in increments of .1,
+C                        is computed.  Then, entry EZSRFC 
+C                        is called to a generate surface plot of Z.
+C
+C Z contains the Z function values; 
+C WORK is a work array;  ANGH contains the angle in degrees in the X-Y
+C plane to the line of sight;  and ANGV contains the angle in degrees
+C from the X-Y plane to the line of sight.
+C
+      REAL            Z(21,25),  WORK(1096)
+C
+      DATA  ANGH/45./, ANGV/15./
+C
+C Specify coordinates for plot titles.  The values CX and CY
+C define the center of the title string in a 0. to 1. range.
+C
+      DATA CX/.5/, CY/.9/
+C
+C Initialize the error parameter.
+C
+      IERROR = 0
+C
+C Set up a the background and foreground colors
+C
+C     White background
+      CALL GSCR (1,0,1.,1.,1.)
+C     Blue foreground
+      CALL GSCR (1,1,0.,0.,1.)
+C
+C Fill the Z function array.
+C
+      DO  20 I=1,21
+         X = .1*FLOAT(I-11)
+         DO  10 J=1,25
+            Y = .1*FLOAT(J-13)
+            Z(I,J) = (X+Y+1./((X-.1)**2+Y**2+.09)-
+     1                                      1./((X+.1)**2+Y**2+.09))*.25
+   10    CONTINUE
+   20 CONTINUE
+C
+C Select the normalization transformation 0.
+C
+      CALL GSELNT(0)
+C
+C Add the plot title using GKS calls.
+C
+C Set the text alignment to center the string in horizontal and vertical
+C
+      CALL GSTXAL(2,3)
+C
+C Set the character height.
+C
+      CALL GSCHH(.016)
+C
+C Write the text.
+C
+      CALL GTX(CX,CY,'DEMONSTRATION PLOT FOR EZSRFC ENTRY OF SRFACE')
+C
+C Draw the surface
+C
+      CALL EZSRFC (Z,21,25,ANGH,ANGV,WORK)
+C
+C This routine automatically generates frame advances.
+C
+      RETURN
+C
+C
+      END
---
+++ ncarg2d/src/fundamental/fsurface/fsrpwrzs.f
@@ -0,0 +1,155 @@
+C
+C	$Id: fsrpwrzs.f,v 1.1 1993-03-24 21:25:12 haley Exp $
+C
+C
+C OPEN GKS, OPEN WORKSTATION OF TYPE 1, ACTIVATE WORKSTATION
+C
+      CALL GOPKS (6,IDUM) 
+      CALL GOPWK (1, 2, 1)
+      CALL GACWK (1) 
+C
+C INVOKE DEMO DRIVER
+C
+      CALL TPWRZS(IERR)
+C
+C     DEACTIVATE AND CLOSE WORKSTATION, CLOSE GKS.
+C
+      CALL GDAWK (1)
+      CALL GCLWK (1)
+      CALL GCLKS
+C
+      STOP
+      END
+C
+      SUBROUTINE TPWRZS (IERROR)
+C
+C PURPOSE                To provide a simple demonstration of
+C                        entry PWRZS with the SRFACE utility.
+C
+C USAGE                  CALL TPWRZS (IERROR)
+C
+C ARGUMENTS
+C
+C ON OUTPUT              IERROR
+C                          An integer variable
+C                          = 0, if the test was successful,
+C                          = 1, otherwise
+C
+C I/O                    If the test is successful, the message
+C
+C               PWRZS TEST EXECUTED--SEE PLOTS TO CERTIFY
+C
+C                        is printed on unit 6.  In addition, 1
+C                        frame is produced on the machine graphics
+C                        device.  In order to determine if the test
+C                        was successful, it is necessary to examine
+C                        the plot.
+C
+C PRECISION              Single
+C
+C REQUIRED ROUTINES      PWRZS, SRFACE
+C
+C REQUIRED GKS LEVEL     0A
+C
+C LANGUAGE               FORTRAN 77
+C
+C ALGORITHM              A function of 2 variables is defined and the
+C                        values of the function on a 2-D rectangular
+C                        grid are stored in an array.  This routine
+C                        calls SRFACE to draw a surface representation
+C                        of the array values.  PWRZS is then called 3
+C                        times to label the front, side, and back of
+C                        the picture.
+C
+      DIMENSION       Z(20,30)   ,X(20)      ,Y(30)      ,MM(20,30,2),
+     1                S(6)
+C
+C Load the SRFACE common block needed to supress a NEWFM call.
+C
+      COMMON /SRFIP1/ IFR        ,ISTP       ,IROTS      ,IDRX       ,
+     1                IDRY       ,IDRZ       ,IUPPER     ,ISKIRT     ,
+     2                NCLA       ,THETA      ,HSKIRT     ,CHI        ,
+     3                CLO        ,CINC       ,ISPVAL
+C
+C Define the center of a plot title string on a square grid of size
+C 0. to 1.
+C
+      DATA TX/0.4375/, TY/0.9667/
+C
+C Specify grid loop indices and a line of sight.
+C
+      DATA M/20/, N/30/
+      DATA S/4.,5.,3.,0.,0.,0./
+C
+C Initial the error parameter.
+C
+      IERROR = 1
+C
+C Set up a color table
+C
+C     White background
+      CALL GSCR (1,0,1.,1.,1.)
+C     Black foreground
+      CALL GSCR (1,1,0.,0.,0.)
+C     Red
+      CALL GSCR (1,2,1.,0.,0.)
+C     Green
+      CALL GSCR (1,3,0.,1.,0.)
+C     Blue
+      CALL GSCR (1,4,0.,0.,1.)
+C
+C Define the function values and store them in the Z array.
+C
+      DO  10 I=1,M
+         X(I) = -1.+FLOAT(I-1)/FLOAT(M-1)*2.
+   10 CONTINUE
+      DO  20 J=1,N
+         Y(J) = -1.+FLOAT(J-1)/FLOAT(N-1)*2.
+   20 CONTINUE
+      DO  40 J=1,N
+         DO  30 I=1,M
+            Z(I,J) = EXP(-2.*SQRT(X(I)**2+Y(J)**2))
+   30    CONTINUE
+   40 CONTINUE
+C
+C Set SRFACE parameters to supress the FRAME call and draw contours.
+C
+      IFR = 0
+      IDRZ = 1
+C
+C Select normalization trans number 0.
+C
+      CALL GSELNT (0)
+C
+C Label the plot.
+C
+      CALL PLCHLQ (TX,TY,'DEMONSTRATION PLOT FOR PWRZS',16.,0.,0.)
+C
+C Draw the surface plot.
+C
+      CALL SRFACE (X,Y,Z,MM,M,M,N,S,0.)
+C
+C Put the PWRZS labels on the picture.
+C
+C     Set the label color
+      CALL GSPLCI(2)
+      ISIZE = 35
+      CALL PWRZS (0.,1.1,0.,'FRONT',5,ISIZE,-1,3,0)
+      CALL PWRZS (1.1,0.,0.,'SIDE',4,ISIZE,2,-1,0)
+      CALL PWRZS (0.,-1.1,.2,' BACK BACK BACK BACK BACK',25,ISIZE,-1,
+     1            3,0)
+      CALL FRAME
+C
+      IERROR = 0
+      WRITE (6,1001)
+C
+C Restore the SRFACE parameters to their default values.
+C
+      IFR = 1
+      IDRZ = 0
+C
+      RETURN
+C
+ 1001 FORMAT (' PWRZS TEST EXECUTED--SEE PLOT TO CERTIFY')
+C
+      END
