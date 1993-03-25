775d44a9eba3ff91e7b3c6122c780cc995dbb020
Mary Haley <haley@ucar.edu>
1993-03-25 00:19:53 +0000

    Bob has updated these.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1191 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/man/halftone/ezhftn.m
@@ -0,0 +1,65 @@
+.TH EZHFTN 3NCARG "March 1993" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+EZHFTN - draws a half-tone
+picture from data stored in a rectangular array with the
+intensity in the picture proportional to the data value.
+.SH UTILITY
+This routine is part of the Halftone utility in NCAR Graphics. To see
+the overview man page for this utility, type "man halftone".
+.SH STATUS
+EZHFTN is obsolete.  It has been replaced by the CPCICA entry of the
+Conpack contouring package.
+.sp
+EZHFTN continues to be provided for compatibility of early NCAR Graphics
+codes.  If you are writing new code, we suggest that you use CPCICA.
+.SH SYNOPSIS
+EZHFTN (Z,M,N)
+.SH DESCRIPTION 
+.IP Z 12
+(an input array of type REAL) defining a two-dimensional field to be
+half-tone plotted.
+.IP M 12
+(an input parameter of type INTEGER) which is the first dimension of
+the Z array.
+.IP N 12
+(an input parameter of type INTEGER) which is the extent of the
+second dimension of array Z to be plotted.
+.IP SPVAL 12
+(an iput parameter of type REAL) used to
+denote missing values in the Z array.
+This argument is ignored if ISPV = 0.
+.SH USAGE
+EZHFTN has a shortened argument list from entry HAFTON which is
+based upon the following assumptions.
+.nf
+
+1.  The entire Z array is to be plotted.
+2.  The lowest value in Z will be at the lowest intensity of the output device.
+3.  The highest value in Z will be at the highest intensity of the output device.
+4.  Other values in Z will appear linearly spaced.
+5.  The maximum number of intensities will be used.
+6.  The picture will have a perimeter drawn.
+7.  The frame will be advanced after the picture is drawn.
+8.  The Z array contains no missing values.
+
+.fi
+If these conditions are not met, use the HAFTON entry.
+.SH ACCESS 
+To use EZHFTN, load the NCAR Graphics libraries ncarg, ncarg_gks,
+and ncarg_loc, preferably in that order.
+.SH SEE ALSO
+Online:
+halftone, hafton, halftone_params,
+conpack, conpack_params, cpcica
+.sp
+Hardcopy:  
+NCAR Graphics Contouring and Mapping Tutorial;
+NCAR Graphics Fundamentals, UNIX Version;
+User's Guide for NCAR GKS-0A Graphics
+.SH COPYRIGHT
+Copyright 1987, 1988, 1989, 1991, 1993 University Corporation
+for Atmospheric Research
+.br
+All Rights Reserved
---
+++ ncarg2d/man/halftone/halftone.m
@@ -0,0 +1,35 @@
+.TH Halftone 3NCARG "March 1993" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+halftone - is a utility for drawing half-tone pictures in which the
+gray scale level is proportional to the value in a two dimensional array.
+.SH STATUS
+Halftone is obsolete.  It has been replaced by the CPCICA entry of the
+Conpack contouring package.
+.sp
+Halftone continues to be provided for compatibility of early NCAR Graphics
+codes.  If you are writing new code, we suggest that you use CPCICA.
+.SH SYNOPSIS
+EZHFTN(Z,M,N) - draws a half-tone picture based upon a set of default options.
+.br
+HAFTON (Z,L,M,N,FLO,HI,NLEV,NOPT,NPRM,ISPV,SPVAL) - Draws a half-tone
+picture from data stored in a rectangular array with the
+intensity in the picture proportional to the data value.
+.SH ACCESS 
+To use EZHFTN or HAFTON, load the NCAR Graphics libraries ncarg, ncarg_gks,
+and ncarg_loc, preferably in that order.
+.SH SEE ALSO
+Online:
+hafton, ezhftn, halftone_params,
+conpack, conpack_params, cpcica
+.sp
+Hardcopy:  
+NCAR Graphics Contouring and Mapping Tutorial;
+NCAR Graphics Fundamentals, UNIX Version;
+User's Guide for NCAR GKS-0A Graphics
+.SH COPYRIGHT
+Copyright 1987, 1988, 1989, 1991, 1993 University Corporation
+for Atmospheric Research
+.br
+All Rights Reserved
