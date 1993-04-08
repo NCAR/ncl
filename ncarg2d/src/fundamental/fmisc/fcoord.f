8ac9a3978c9ab8cc795bea6d5a82cffec7e914f0
Mary Haley <haley@ucar.edu>
1993-04-08 22:36:34 +0000

    Added miscellaneous fundamental examples.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1305 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/fundamental/fmisc/fcoord.f
@@ -0,0 +1,121 @@
+      PROGRAM EXMPLE
+C
+C Define arrays to hold data defining a spiral in the user coordinate
+C system.
+C
+        DIMENSION X(476),Y(476)
+C
+C Define arrays to hold the numbers defining the viewport and window,
+C as retrieved from GKS.
+C
+        DIMENSION VIEW(4),WIND(4)
+C
+C Define a character variable in which to construct labels.
+C
+        CHARACTER*26 CHRS
+C
+C Open GKS.  Open and activate a single metacode workstation.
+C
+        CALL OPNGKS
+C
+C Turn off clipping at the edges of the viewport (which GKS does by
+C default).
+C
+        CALL GSCLIP (0)
+C
+C Define the X and Y coordinates of a spiral in the user coordinate
+C system.  It lies in a rectangular region bounded by the lines
+C "X=100", "X=1000", "Y=100", and "Y=1000".
+C
+        DO 101 I=1,476
+          THETA=.031415926535898*FLOAT(I-1)
+          X(I)=500.+.9*FLOAT(I-1)*COS(THETA)
+          Y(I)=500.+.9*FLOAT(I-1)*SIN(THETA)
+  101   CONTINUE
+C
+C Loop through the possible values of 'LS'.
+C
+        DO 103 ILS=1,4
+C
+C Define the fractional coordinates of the left and right edges of the
+C viewport.
+C
+          VPL=REAL(ILS-1)/4.+.020
+          VPR=REAL(ILS  )/4.-.020
+C
+C For each of the possible values of 'LS', loop through the possible
+C values of 'MI'.
+C
+          DO 102 IMI=1,4
+C
+C Define the fractional coordinates of the bottom and top edges of the
+C viewport.
+C
+            VPB=REAL(4-IMI)/4.+.059
+            VPT=REAL(5-IMI)/4.-.001
+C
+C Outline the viewport.  PLOTIF expects fractional coordinates.
+C
+            CALL PLOTIF (VPL,VPB,0)
+            CALL PLOTIF (VPR,VPB,1)
+            CALL PLOTIF (VPR,VPT,1)
+            CALL PLOTIF (VPL,VPT,1)
+            CALL PLOTIF (VPL,VPB,1)
+C
+C Call SET to define the mapping from the user system to the plotter
+C frame.  The SET call specifies 'MI' = 1 (since the value of argument
+C 5 is less than that of argument 6 and the value of argument 7 is less
+C that of argument 8).  The SETUSV call overrides this to obtain the
+C desired value.
+C
+            CALL SET    (VPL,VPR,VPB,VPT,100.,1000.,100.,1000.,ILS)
+            CALL SETUSV ('MI (MIRROR IMAGING FLAG)',IMI)
+C
+C Call the routine CURVE to draw the spiral.
+C
+            CALL CURVE  (X,Y,476)
+C
+C Label the curve.  First, write the values of 'MI' and 'LS'.  Note
+C the use of CFUX and CFUY to map meaningful fractional coordinates
+C to the user coordinates required by PLCHMQ.
+C
+            WRITE (CHRS,'(''MI='',I1,'' LS='',I1)') IMI,ILS
+            CALL PLCHMQ (CFUX(.5*(VPL+VPR)),CFUY(VPB-.0120),
+     +                                 CHRS(1:9),.012,0.,0.)
+C
+C Retrieve the values defining the window and viewport, using GKS
+C calls.
+C
+            CALL GQNT (1,IERR,WIND,VIEW)
+C
+C Write them out, too.
+C
+            WRITE (CHRS,'(''VP='',F5.3,3('','',F5.3))') (VIEW(I),I=1,4)
+            CHRS( 4: 4)=' '
+            CHRS(10:10)=' '
+            CHRS(16:16)=' '
+            CHRS(22:22)=' '
+            CALL PLCHMQ (CFUX(.5*(VPL+VPR)),CFUY(VPB-.0320),
+     +                                CHRS(1:26),.008,0.,0.)
+            WRITE (CHRS,'(''WD='',F5.0,3('','',F5.0))') (WIND(I),I=1,4)
+            CALL PLCHMQ (CFUX(.5*(VPL+VPR)),CFUY(VPB-.0480),
+     +                                CHRS(1:26),.008,0.,0.)
+
+C
+C End of loop through the values of 'MI'.
+C
+  102     CONTINUE
+C
+C End of loop through the values of 'LS'.
+C
+  103   CONTINUE
+C
+C Terminate GKS.
+C
+        CALL CLSGKS
+C
+C Done.
+C
+        STOP
+C
+      END
---
+++ ncarg2d/src/fundamental/fmisc/fcoord2.f
@@ -0,0 +1,187 @@
+C
+C OPEN GKS, OPEN WORKSTATION OF TYPE 1, ACTIVATE WORKSTATION
+C
+      CALL GOPKS (6,IDUM) 
+      CALL GOPWK (1, 2, 1)
+      CALL GACWK (1) 
+C
+C INVOKE DEMO DRIVER
+C
+      CALL SPSET2(IERR)
+C
+C     DEACTIVATE AND CLOSE WORKSTATION, CLOSE GKS.
+C
+      CALL GDAWK (1)
+      CALL GCLWK (1)
+      CALL GCLKS
+      STOP
+      END
+      SUBROUTINE SPSET2 (IERROR)
+C
+C  This example shows the mapping from NCAR Grapics user coordinates to
+C   GKS normalized device coordinates.
+C
+	DIMENSION XCRA(101),YCRA(101)
+	DATA DSL,DSR,DSB,DST / .59,.99,.40,.70 /
+	DATA PFL,PFR,PFB,PFT / .71,.98,.42,.69 /
+	CALL GSCLIP (0)
+	CALL GSLWSC (2.)
+C
+C Employ the new high quality filled fonts in PLOTCHAR
+C
+      CALL PCSETC('FN','times-roman')
+C
+	CALL SET    (0.,1.,0.,1.,0.,1.,0.,1.,1)
+	CALL PLCHHQ (.50,.98,
+     +            'MAPPING FROM A WINDOW IN THE USER COORDINATE SYSTEM',
+     +                                                       .015,0.,0.)
+	CALL PLCHHQ (.50,.95,
+     +       'TO A VIEWPORT IN THE NORMALIZED DEVICE COORDINATE SYSTEM',
+     +                                                       .015,0.,0.)
+	CALL PLCHHQ (.50,.92,
+     +                  'USING A TRANSFORMATION DEFINED BY CALLING SET',
+     +                                                       .015,0.,0.)
+	CALL PLCHHQ (.50,.89,
+     +                 '(USED IN CALLS TO MOST NCAR GRAPHICS ROUTINES)',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.24,
+     +   'Assume a CALL SET (.15,.95,.10,.90,1000.,100.,100.,1000.,2).',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.21,
+     +                  'The GKS viewport will be   .15, .95, .10, .90',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.18,
+     +                   'The GKS window will be   100., 1000., 2., 3.',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.15,
+     +        'The value of ''MI'' will be 3 (mirror-imaging of X''s).',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.12,
+     +           'The value of ''LS'' will be 2 (log scaling of Y''s).',
+     +                                                       .012,0.,0.)
+	CALL PLCHHQ (.50,.06,
+     +                                                       'Figure 2',
+     +                                                       .012,0.,0.)
+	CALL SET    (.01,.55,.29,.83,0.,1100.,0.,1100.,1)
+	CALL LINE   (   0.,   0.,1100.,   0.)
+	CALL LINE   (1100.,   0.,1050.,  25.)
+	CALL LINE   (1100.,   0.,1050., -25.)
+	CALL PLCHHQ (CFUX(CUFX(1100.)+.015),0.,'X',.015,0.,0.)
+	CALL LINE   (   0.,   0.,   0.,1100.)
+	CALL LINE   (   0.,1100.,  25.,1050.)
+	CALL LINE   (   0.,1100., -25.,1050.)
+	CALL PLCHHQ (0.,CFUY(CUFY(1100.)+.015),'Y',.015,0.,0.)
+	CALL LINE   ( 100., 100.,1000., 100.)
+	CALL LINE   (1000., 100.,1000.,1000.)
+	CALL LINE   (1000.,1000., 100.,1000.)
+	CALL LINE   ( 100.,1000., 100., 100.)
+	CALL PLCHHQ (CFUX(CUFX(100.)+.005),CFUY(CUFY(1000.)-.01),
+     +               'WINDOW',.01,0.,-1.)
+	CALL PLCHHQ (100.,CFUY(CUFY(100.)-.01),'100',.008,0.,0.)
+	CALL PLCHHQ (1000.,CFUY(CUFY(100.)-.01),'1000',.008,0.,0.)
+	CALL PLCHHQ (CFUX(CUFX(100.)-.005),100.,'100',.008,0.,1.)
+	CALL PLCHHQ (CFUX(CUFX(100.)-.005),1000.,'1000',.008,0.,1.)
+	DO 101 I=1,101
+	  XCRA(I)=200.+7.*REAL(I-1)
+	  YCRA(I)=200.+700.*(10.**((XCRA(I)-200.)/700.)-1.)/9.+
+     +            100.*SIN((XCRA(I)-200.)/30.)
+  101   CONTINUE
+	CALL CURVE  (XCRA,YCRA,101)
+	XCLW=CUFX(100.)
+	XCRW=CUFX(1000.)
+	YCBW=CUFY(100.)
+	YCTW=CUFY(1000.)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (1.)
+	CALL LINE (100.,200.,1000.,200.)
+	CALL LINE (100.,300.,1000.,300.)
+	CALL LINE (100.,400.,1000.,400.)
+	CALL LINE (100.,500.,1000.,500.)
+	CALL LINE (100.,600.,1000.,600.)
+	CALL LINE (100.,700.,1000.,700.)
+	CALL LINE (100.,800.,1000.,800.)
+	CALL LINE (100.,900.,1000.,900.)
+	CALL LINE (200.,100.,200.,1000.)
+	CALL LINE (300.,100.,300.,1000.)
+	CALL LINE (400.,100.,400.,1000.)
+	CALL LINE (500.,100.,500.,1000.)
+	CALL LINE (600.,100.,600.,1000.)
+	CALL LINE (700.,100.,700.,1000.)
+	CALL LINE (800.,100.,800.,1000.)
+	CALL LINE (900.,100.,900.,1000.)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (2.)
+	CALL SET    (DSL,DSR,DSB,DST,0.,1.,0.,1.,1)
+	CALL LINE   (0.,0.,1.,0.)
+	CALL LINE   (1.,0.,1.,1.)
+	CALL LINE   (1.,1.,0.,1.)
+	CALL LINE   (0.,1.,0.,0.)
+	CALL PLCHHQ (CFUX(CUFX(0.)+.005),CFUY(CUFY(1.)-.01),
+     +               'DEVICE',.01,0.,-1.)
+	CALL SET    (PFL,PFR,PFB,PFT,0.,1.,0.,1.,1)
+	CALL LINE   (0.,0.,1.,0.)
+	CALL LINE   (1.,0.,1.,1.)
+	CALL LINE   (1.,1.,0.,1.)
+	CALL LINE   (0.,1.,0.,0.)
+	CALL PLCHHQ (CFUX(CUFX(0.)+.005),CFUY(CUFY(1.)-.01),
+     +               'PLOTTER FRAME',.01,0.,-1.)
+	CALL PLCHHQ (0.,CFUY(CUFY(0.)-.01),'0',.008,0.,0.)
+	CALL PLCHHQ (1.,CFUY(CUFY(0.)-.01),'1',.008,0.,0.)
+	CALL PLCHHQ (CFUX(CUFX(0.)-.005),0.,'0',.008,0.,1.)
+	CALL PLCHHQ (CFUX(CUFX(0.)-.005),1.,'1',.008,0.,1.)
+	VPL=PFL+.15*(PFR-PFL)
+	VPR=PFL+.95*(PFR-PFL)
+	VPB=PFB+.10*(PFT-PFB)
+	VPT=PFB+.90*(PFT-PFB)
+	CALL SET    (VPL,VPR,VPB,VPT,1000., 100., 100.,1000.,2)
+	CALL LINE   (1000., 100., 100., 100.)
+	CALL LINE   ( 100., 100., 100.,1000.)
+	CALL LINE   ( 100.,1000.,1000.,1000.)
+	CALL LINE   (1000.,1000.,1000., 100.)
+	CALL PLCHHQ (CFUX(CUFX(1000.)+.005),CFUY(CUFY(1000.)-.01),
+     +               'VIEWPORT',.01,0.,-1.)
+	CALL PLCHHQ (1000.,CFUY(CUFY(100.)-.01),'.15',.008,0.,0.)
+	CALL PLCHHQ (100.,CFUY(CUFY(100.)-.01),'.95',.008,0.,0.)
+	CALL PLCHHQ (CFUX(CUFX(1000.)-.005),100.,'.10',.008,0.,1.)
+	CALL PLCHHQ (CFUX(CUFX(1000.)-.005),1000.,'.90',.008,0.,1.)
+	CALL CURVE  (XCRA,YCRA,101)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (1.)
+	CALL LINE (100.,200.,1000.,200.)
+	CALL LINE (100.,300.,1000.,300.)
+	CALL LINE (100.,400.,1000.,400.)
+	CALL LINE (100.,500.,1000.,500.)
+	CALL LINE (100.,600.,1000.,600.)
+	CALL LINE (100.,700.,1000.,700.)
+	CALL LINE (100.,800.,1000.,800.)
+	CALL LINE (100.,900.,1000.,900.)
+	CALL LINE (200.,100.,200.,1000.)
+	CALL LINE (300.,100.,300.,1000.)
+	CALL LINE (400.,100.,400.,1000.)
+	CALL LINE (500.,100.,500.,1000.)
+	CALL LINE (600.,100.,600.,1000.)
+	CALL LINE (700.,100.,700.,1000.)
+	CALL LINE (800.,100.,800.,1000.)
+	CALL LINE (900.,100.,900.,1000.)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (2.)
+	XCLV=CUFX(1000.)
+	XCRV=CUFX(100.)
+	YCBV=CUFY(100.)
+	YCTV=CUFY(1000.)
+	CALL SET    (0.,1.,0.,1.,0.,1.,0.,1.,1)
+	CALL PLOTIT (0,0,0)
+	CALL GSLWSC (1.)
+	CALL DASHDC ('$''',3,1)
+	CALL LINED  (XCLW,YCBW,XCRV,YCBV)
+	CALL LINED  (XCRW,YCBW,XCLV,YCBV)
+	CALL LINED  (XCRW,YCTW,XCLV,YCTV)
+	CALL LINED  (XCLW,YCTW,XCRV,YCTV)
+	CALL FRAME
+      WRITE (6,1001)
+      RETURN
+C
+ 1001 FORMAT ('     SPSET2 TEST SUCCESSFUL',24X,
+     1        'SEE PLOTS TO VERIFY PERFORMANCE')
+C
+      END
