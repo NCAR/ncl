74b45531b651ea39e1395a56d2332e7c79245865
Mary Haley <haley@ucar.edu>
1993-04-12 22:12:31 +0000

    Mention latest examples added.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1337 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/ncargex.m
+++ ncarg2d/man/other/ncargex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargex.m,v 1.4 1993-04-08 16:14:05 haley Exp $
+.\"	$Id: ncargex.m,v 1.5 1993-04-12 22:12:31 haley Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGEX 1NCARG "March 1993" NCAR "NCAR GRAPHICS"
@@ -10,6 +10,7 @@ ncargex \- NCAR Graphics Fortran Examples and Tests
 [\fB\-all, -A\fR]
 [\fB\-allexamples, -E\fR]
 [\fB\-allfundamental, -F\fR]
+[\fB\-allpdocexamples, -P\fR]
 [\fB\-alltests, -T\fR]
 [\fB\-alltutorial, -U\fR]
 [\f\B\-areas]
@@ -21,6 +22,7 @@ ncargex \- NCAR Graphics Fortran Examples and Tests
 [\f\B\-conrec_family\fR]
 [\f\B\-dashline\fR]
 [\f\B\-ezmap\fR]
+[\f\B\-field_flow\fR]
 [\f\B\-gflash\fR]
 [\f\B\-gridall\fR]
 [\f\B\-halftone\fR]
@@ -73,6 +75,9 @@ Generate all available examples.
 .IP "\-allfundamental, \-F"
 Generate all available fundamental examples.
 .sp
+.IP "\-allpdocexamples, \-P"
+Generate all available programmer doc examples.
+.sp
 .IP "\-alltests, \-T"
 Generate all available tests.
 .sp
@@ -105,6 +110,9 @@ Generate all dashline examples.
 .sp
 .IP "\-ezmap\fR"
 Generate all ezmap examples.
+.sp
+.IP "\-field_flow\fR"
+Generate all field_flow examples.  Includes streamlines and vectors examples.
 .sp
 .IP "\-gflash\fR"
 Generate all gflash examples.
@@ -181,9 +189,9 @@ Specifies that the selected examples and/or tests should be generated one
 at a time and viewed as they are generated.  This is intended for use during
 testing of new releases at NCAR.
 .sp
-.I "EXAMPLES AND TESTS AVAILABLE"
+.I "EXAMPLES AVAILABLE"
 .sp
-.I "AREAS Examples:"
+.I "AREAS Example:"
 .nf
 	arex01
 .sp
@@ -193,7 +201,7 @@ testing of new releases at NCAR.
 	agex06 agex07 agex08 agex09 agex10
 	agex11 agex12 agex13
 .sp
-.I "BIVAR Examples:"
+.I "BIVAR Example:"
 .nf
 	cbex01
 .sp
@@ -208,15 +216,15 @@ testing of new releases at NCAR.
 	mpex06 mpex07 mpex08 mpex09 mpex10
 	mpexfi eezmpa
 .sp
-.I "LABELBAR Examples:"
+.I "LABELBAR Example:"
 .nf
 	elblba
 .sp
-.I "PLOTCHAR Examples:"
+.I "PLOTCHAR Example:"
 .nf
 	epltch
 .sp
-.I "SCROLLED_TITLE Examples:"
+.I "SCROLLED_TITLE Example:"
 .nf
 	slex01
 .sp
@@ -228,15 +236,15 @@ testing of new releases at NCAR.
 .nf
 	splogy sprevx spset1 spset2
 .sp
-.I "STRMLN Examples:"
+.I "STREAMLINES Examples:"
 .nf
 	stex01 stex02 stex03
 .sp
-.I "SURFACE Examples:"
+.I "SURFACE Example:"
 .nf
 	srex01
 .sp
-.I "VELVCT Examples:"
+.I "VECTORS Examples:"
 .nf
 	vvex01 vvex02
 .sp
@@ -248,15 +256,20 @@ testing of new releases at NCAR.
 .nf
 	cardb1 caredg carline cardb2 carfill carmap
 .sp
+.I "Tutorial CONPACK/BIVAR Example:"
+	cidsfft
+.sp
 .I "Tutorial CONPACK Examples:"
 .nf
-	ccpback ccpcff ccpcfx ccpcica ccpcir ccpcis ccpcit ccpclc ccpcld
-	ccpcldm ccpcldr ccpcll ccpclu ccpcnrc ccpdflt ccpezct ccpfil 
-	ccpga ccphand ccphcf ccphl ccphlt ccpila ccpils ccpilt ccpklb
-	ccplbam ccplbdr ccpline ccpllb ccpllc ccplll ccpllo ccpllp ccpllt 
-	ccpllw ccpmap ccpmovi ccpmpxy ccpncls ccpnet ccpnof ccpnsd ccppc
-	ccppc1 ccppc2 ccppc3 ccppc4 ccppkcl ccprc ccprect ccprwc ccprwu ccpscam 
-	ccpset ccpsps1 ccpsps2 ccpspv ccpt2d ccptitle ccpvp ccpvs cidsfft colcon
+	ccpback ccpcff ccpcfx ccpcica ccpcir ccpcis ccpcit ccpclc
+	ccpcld ccpcldm ccpcldr ccpcll ccpclu ccpcnrc ccpdflt
+	ccpezct ccpfil ccpga ccphand ccphcf ccphl ccphlt ccpila
+	ccpils ccpilt ccpklb ccplbam ccplbdr ccpline ccpllb
+	ccpllc ccplll ccpllo ccpllp ccpllt ccpllw ccpmap
+	ccpmovi ccpmpxy ccpncls ccpnet ccpnof ccpnsd ccppc ccppc1
+	ccppc2 ccppc3 ccppc4 ccppkcl ccppole ccpt2d ccprc ccprect
+	ccprwc ccprwu ccpscam ccpset ccpsps1 ccpsps2 ccpspv
+	ccptitle ccpvp ccpvs colcon
 .sp
 .I "Tutorial EZMAP Examples:"
 .nf
@@ -264,20 +277,37 @@ testing of new releases at NCAR.
 	cmpgci cmpgrd cmpgrp cmpita cmpitm cmplab cmplbl cmplot 
 	cmpmsk cmpou cmppos cmpsat cmpsup cmptit cmptra cmpusr
 .sp
+.I "Fundamental AUTOGRAPH Examples:"
+.nf
+	fagaxclr fagaxlbl fagaxmax fagcuclr fagcudsh fagezmxy
+	fagezmy fagezxy fagezy fagilclr fagovrvw
+.sp
+.I "Fundamental COLCONV Examples:"
+	fcce01 fcce02
+.sp
 .I "Fundamental DASHLINE Examples:"
 	fdlcurvd fdldashc fdldashd fdlsmth
 .sp
+.I "Fundamental FIELD_FLOW Examples:"
+.nf
+	stex01 stex02 stex03 vvex01 vvex02 tstrml tvelvc fstream
+	ffex00 ffex01 ffex02 ffex03 ffex04 ffex05
+.sp
 .I "Fundamental GKS Examples:"
-	fgkgpl fgkgpm fgkgtx fgklnclr fgklnwth
+	fgke01 fgke02 fgke03 fgke04 fcell fcell0 fgpm01 fgkgpl
+	fgkgpm fgkgtx fgklnclr fgklnwth
 .sp
 .I "Fundamental ISOSURFACE Examples:"
-	fiseziso fisissrf fispwrzi
+	fisissrf fispwrzi
 .sp
 .I "Fundamental NGMISC Examples:"
 	fngngdts fngwsym
 .sp
 .I "Fundamental PLOTCHAR Examples:"
-	fpchiqu fpcloqu
+	fpchiqu fpcloqu fpcfonts
+.sp
+.I "Fundamental SCROLLED_STITLE Example:"
+	fslfont
 .sp
 .I "Fundamental SOFTFILL Examples:"
 	fsfwrld fsfsgfa
@@ -285,70 +315,82 @@ testing of new releases at NCAR.
 .I "Fundamental SPPS  Examples:"
 	fspcurve fspline fsppoint fspponts
 .sp
+.I "Fundamental STREAMLINES Examples:"
+	fstream ffex00 ffex01 ffex03 ffex04
+.sp
 .I "Fundamental SURFACE Examples:"
 	fsrezsrf fsrpwrzs fsrsrfac
 .sp
-.I "AREAS Test Programs:"
+.I "Fundamental THREED Examples:"
+	fthex01 fthex02 fthex03 fthex04 fthex05
+.sp
+.I "Fundamental VECTORS Examples:"
+	ffex00 ffex01 ffex02 ffex05
+.sp
+.I "Programmer Doc GKS Examples:"
+.nf
+	pgkex01 pgkex02 pgkex03 pgkex04 pgkex05 pgkex06 pgkex07
+	pgkex08 pgkex09 pgkex10 pgkex11 pgkex12 pgkex13 pgkex14
+	pgkex15 pgkex16 pgkex17 pgkex18 pgkex19 pgkex20 pgkex21
+.sp
+.I "AREAS Test Program:"
     tareas
 .sp
 .I "AUTOGRAPH Test Programs:"
     tagupw tautog
 .sp
-.I "COLCONV Test Programs:"
+.I "COLCONV Test Program:"
     tcolcv
 .sp
-.I "CONPACK Test Programs:"
+.I "CONPACK Test Program:"
     tconpa
 .sp
-.I "CONRAN Test Programs:"
+.I "CONRAN_FAMILY Test Programs:"
     tconan tconaq tconas
 .sp
-.I "CONREC Test Programs:"
+.I "CONREC_FAMILY Test Programs:"
     tconre tcnqck tcnsmt tcnsup
 .sp
-.I "CONREC Test Programs:"
+.I "DASHLINE Test Programs:"
     tdashc tdashl tdashp tdashs
 .sp
 .I "EZMAP Test Programs:"
     tezmap tezmpa
 .sp
-.I "GFLASH Test Programs:"
+.I "GFLASH Test Program:"
     tgflas
 .sp
-.I "GRIDALL Test Programs:"
+.I "GRIDALL Test Program:"
     tgrida
 .sp
-.I "HALFTONE Test Programs:"
+.I "HALFTONE Test Program:"
     thafto
 .sp
-.I "HISTOGRAM Test Programs:"
+.I "HISTOGRAM Test Program:"
     thstgr
 .sp
-.I "ISOSRFHR Test Programs:"
+.I "ISOSRFHR Test Program:"
     tisohr
 .sp
 .I "ISOSURFACE Test Programs:"
     tisosr tpwrzi
 .sp
-.I "LABELBAR Test Programs:"
+.I "LABELBAR Test Program:"
     tlblba
 .sp
-.I "PLOTCHAR Test Programs:"
+.I "PLOTCHAR Test Program:"
     tpltch
 .sp
-.I "PWRITX Test Programs:"
-    tpwrtx
+.I "PWRITE_FAMILY Test Programs:"
+    tpwrtx tpwry
 .sp
-.I "PWRITY Test Programs:"
-    tpwry
-.sp
-.I "SCROLLED_TITLE Test Programs:"
+.I "SCROLLED_TITLE Test Program:"
     tstitl
 .sp
-.I "SOFTFILL Test Programs:"
+.I "SOFTFILL Test Program:"
     tsoftf
 .sp
-.I "STREAMLINES Test Programs:"
+.I "STREAMLINES Test Program:"
     tstrml
 .sp
 .I "SURFACE Test Programs:"
@@ -357,7 +399,7 @@ testing of new releases at NCAR.
 .I "THREED Test Programs:"
     tthree tpwrzt
 .sp
-.I "VECTORS Test Programs:"
+.I "VECTORS Test Program:"
     tvelvc
 .sp
 .fi
