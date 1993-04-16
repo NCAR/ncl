c0c3f542552777d738a1fec15002387bc8868c46
Mary Haley <haley@ucar.edu>
1993-04-16 17:01:40 +0000

    Moved these guys to their own directory.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1355 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/examples/ecolcv/coex01.f
@@ -0,0 +1,237 @@
+C
+C	$Id: coex01.f,v 1.1 1993-04-16 17:01:36 haley Exp $
+C
+C
+C  This program produces five 9 x 9 color charts.  Blue and green
+C  intensities are varied on each chart, and the red intensity is
+C  varied between charts.  To produce the charts, color indices
+C  1-84 are used.  If plotting on a device with fewer than 84 colors
+C  available, unsatisfactory results will obtain.
+C
+      PROGRAM CCHART
+C
+      DIMENSION RGB(3,405)
+C
+C  Open GKS.
+C
+      CALL GOPKS(6)
+      CALL GOPWK(1,2,1)
+      CALL GACWK(1)
+C
+C  Set fill area interior style to solid.
+C
+      CALL GSFAIS(1)
+C
+C  Define all permutations of RGB's needed.
+C
+      CALL COLSET(RGB)
+C
+C  Put out 5 color charts, varying the red value with each frame.
+C
+      DO 10 NPAGE=1,5
+        CALL SETCOL(RGB,NPAGE)
+        CALL TTLBAR()
+        CALL CUBE()
+        CALL TITLE(NPAGE)
+        CALL FRAME
+   10 CONTINUE
+C
+C  Close GKS.
+C
+      CALL GDAWK(1)
+      CALL GCLWK(1)
+      CALL GCLKS
+C
+      END
+      SUBROUTINE COLSET(RGB)
+C
+C  Define the RGB color triples needed.  This is done by filling the
+C  RGB array with all 405 permutations for a 9 x 9 color cube in 5
+C  plots.  All values are normalized to fall in the range 0 to 1.
+C
+      DIMENSION RGB(3,405)
+      DIMENSION CLRS(9)
+C
+      DATA CLRS(1),CLRS(2),CLRS(3)/   0.,  32.,  64./
+      DATA CLRS(4),CLRS(5),CLRS(6)/  96., 128., 160./
+      DATA CLRS(7),CLRS(8),CLRS(9)/ 192., 224., 255./
+C
+      INDEX = 1
+      DO 10 I=1,5
+        DO 20 J=1,9
+          DO 30 K=1,9
+            RGB (1,INDEX) = CLRS(2*I-1)/255.0
+            RGB (2,INDEX) = CLRS(J)/255.0
+            RGB (3,INDEX) = CLRS(K)/255.0
+            INDEX = INDEX + 1
+   30     CONTINUE
+   20   CONTINUE
+   10 CONTINUE
+C
+      RETURN
+      END
+      SUBROUTINE SETCOL(RGB,INDEX)
+C
+C  Define color indices 3-84 to contain the desired colors.
+C
+      DIMENSION RGB(3,405)
+      NDX = 81*(INDEX-1)+1
+      DO 10 N=1,81
+      CALL GSCR(1,N+2,RGB(1,NDX),RGB(2,NDX),RGB(3,NDX))
+      NDX = NDX + 1
+   10 CONTINUE
+C
+      RETURN
+      END
+      SUBROUTINE CUBE()
+C
+C  Draw 81 color squares of size DELX x DELY.  Start at position
+C  (X1,Y1).  Use blank space of GAPX and GAPY between the squares.
+C
+      PARAMETER (DELX=0.095, DELY=0.095)
+      PARAMETER (GAPX=DELX-0.003, GAPY=DELY-0.003)
+      DIMENSION CELLX(4), CELLY(4)
+      DATA X1,Y1/0.13, 0.075/
+C
+C  Set the parameters for the starting point.
+C
+      X = X1
+      Y = Y1
+C
+C  Start with color index 3 so that the foreground and background
+C  color indices are not touched.
+C
+      INDCOL = 3
+C
+C  Put out 9 x 9 squares.
+C
+      DO 10 I=1,9
+        DO 20 L=1,9
+C
+C  Define square.
+C
+          CELLX(1) = X
+          CELLY(1) = Y
+          CELLX(2) = X + GAPX
+          CELLY(2) = Y
+          CELLX(3) = X + GAPX
+          CELLY(3) = Y + GAPY
+          CELLX(4) = X
+          CELLY(4) = Y + GAPY
+C
+C  Set color to index INDCOL and draw square.
+C
+          CALL GSFACI(INDCOL)
+          CALL GFA(4,CELLX,CELLY)
+          X = X + DELX
+          INDCOL = INDCOL + 1
+   20   CONTINUE
+        Y = Y + DELY
+        X = X1
+   10 CONTINUE
+C
+      RETURN
+      END
+      SUBROUTINE TTLBAR()
+C
+C  Label the chart with the red intensity.
+C
+      DIMENSION CELLX(4), CELLY(4)
+      DATA X1,Y1,XSIDE,YSIDE/ 0.70, 0.93, 0.28, 0.04/
+C
+C  Define the fill area.
+C
+      CELLX(1) = X1
+      CELLY(1) = Y1
+      CELLX(2) = X1 + XSIDE
+      CELLY(2) = Y1
+      CELLX(3) = X1 + XSIDE
+      CELLY(3) = Y1 + YSIDE
+      CELLX(4) = X1
+      CELLY(4) = Y1 + YSIDE
+C
+C  Set the color and draw the fill area.
+C
+      CALL GSFACI(3)
+      CALL GFA(4,CELLX,CELLY)
+C
+      RETURN
+      END
+      SUBROUTINE TITLE(NPAGE)
+C
+C  Label the chart using white characters..
+C
+      DIMENSION AX1(3),AY1(3),AX2(3),AY2(3),IL(9)
+      CHARACTER RVAL*16,CVAL*3,DVAL*4
+      DATA AX1(1),AX1(2),AX1(3)/ .073, .048, .073/
+      DATA AY1(1),AY1(2),AY1(3)/ .900, .800, .800/
+      DATA AX2(1),AX2(2),AX2(3)/ .960, .860, .860/
+      DATA AY2(1),AY2(2),AY2(3)/ .028, .010, .028/
+      DATA DELX,DELY/.095,.095/
+      DATA IL(1),IL(2),IL(3),IL(4),IL(5),IL(6),IL(7),IL(8),IL(9)
+     1    /    3,    2,    2,    2,    1,    1,    1,    1,    1/
+C
+      CALL GSCR(1,1,1.,1.,1.)
+      CALL GSPLCI(1)
+C
+C  Print the title of each axis.
+C
+      CALL PLCHHQ(0.5,0.007,'Blue Axis' ,0.015,0.,0.)
+      CALL PLCHHQ(0.05,0.50,'Green Axis',0.015,90.,0.)
+C
+C  Draw the arrow-line on each axis.
+C
+      CALL LINE(0.073,0.90,0.073,0.078)
+      CALL GSFACI(1)
+      CALL GFA(3,AX1,AY1)
+C
+      CALL LINE(0.15,0.028,0.96,0.028)
+      CALL GSFACI(1)
+      CALL GFA(3,AX2,AY2)
+C
+C  Print the red value for the frame at hand.
+C
+      ITMP = 64*(NPAGE-1)
+      IF (NPAGE .EQ. 5) ITMP = 255
+      RLV = REAL(ITMP)/255.
+      WRITE(RVAL,100) ITMP,RLV
+  100 FORMAT('RED = ',I3,' = ',F4.2)
+      CALL PLCHHQ(.84,.95,RVAL,.014,0.,0.)
+C
+C  Print the Green values up the side.
+C
+      X = 0.10
+      Y1 = 0.125
+      Y2 = 0.108
+      DO 20 I=1,9
+        ITMP = 32*(I-1)
+        IF (I .EQ. 9) ITMP=255
+        WRITE(CVAL,110) ITMP
+        RVL = REAL(ITMP)/255.
+        WRITE(DVAL,120) RVL
+  110   FORMAT(I3)
+  120   FORMAT(F4.2)
+        CALL PLCHHQ(X,Y1,CVAL(IL(I):3),.008,0.,0.)
+        CALL PLCHHQ(X,Y2,DVAL,.008,0.,0.)
+        Y1 = Y1+DELY
+        Y2 = Y2+DELY
+   20 CONTINUE
+C
+C  Print the Blue values across the bottom.
+C
+      Y1 = 0.060
+      Y2 = 0.043
+      X = 0.18
+      DO 30 I=1,9
+        ITMP = 32*(I-1)
+        IF (I .EQ. 9) ITMP=255
+        WRITE(CVAL,110) ITMP
+        RVL = REAL(ITMP)/255.
+        WRITE(DVAL,120) RVL
+        CALL PLCHHQ(X,Y1,CVAL(IL(I):3),.008,0.,0.)
+        CALL PLCHHQ(X,Y2,DVAL,.008,0.,0.)
+        X = X+DELX
+   30 CONTINUE
+C
+      RETURN
+      END
---
+++ ncarg2d/src/examples/ecolcv/coex03.f
@@ -0,0 +1,118 @@
+C
+C	$Id: coex03.f,v 1.1 1993-04-16 17:01:38 haley Exp $
+C
+      PROGRAM CWHEEL
+C
+C Create color wheels in HSV space.  Each wheel is produced with
+C a different value of V (the value parameter in HSV space).  Each
+C wheel is composed of 16 wedges.  The value for the hue remains
+C the same within each wedge, and the value for the saturation
+C varies linearly from the center to the outer rim within each wedge.
+C The hues vary from 0. to 360. around the color wheel starting
+C at pure red, and returning to pure red.
+C
+C This program requires PLOTCHAR for drawing its characters.
+C
+C  Define the number of values, the number of hues, the number
+C  of saturations, and the number of points bounding a color box.
+C
+      PARAMETER(NW=3, NCOL=16, NSAT=4, NPTS = 4)
+      DIMENSION X(NPTS),Y(NPTS)
+      DIMENSION IASF(13)
+      DIMENSION VAL(NW),ST(NSAT)
+      CHARACTER*80 TITLE
+C
+      DATA IASF / 13*1 /
+      DATA PI   /  3.14159265/
+C
+C  Define the values to be used.
+C
+      DATA VAL  / 0.60, 0.80, 1.00 /
+C
+C  Y-coordinates for the saturation labels.
+C
+      DATA ST(1),ST(2),ST(3),ST(4)/-.1, -.375, -.625, -.850/
+C
+      CALL OPNGKS
+      CALL GSCLIP(0)
+      CALL GSASF(IASF)
+      CALL GSFAIS(1)
+C
+C  Background and foreground colors.
+C
+      CALL GSCR(1,0,0.0,0.0,0.0)
+      CALL GSCR(1,1,1.0,1.0,1.0)
+C
+C  Loop on the values.
+C
+      DO 30 IL = 1,NW
+        INDEX = 2
+        CALL SET(.1,.9,.1,.9,-1.2,1.2,-1.2,1.2,1)
+        RINC = 2 * PI / FLOAT(NCOL)
+        HINC = 360.0  / FLOAT(NCOL)
+        SINC =  1.00  / FLOAT(NSAT - 1)
+C
+C  Loop on the hues.
+C
+        DO 20 IHUE = 0,NCOL - 1
+          HUE    =  FLOAT(IHUE)      * HINC
+          THETA1 = (FLOAT(IHUE) -.5) * RINC
+          THETA2 = (FLOAT(IHUE) +.5) * RINC
+          X(1) = 0.0
+          X(4) = 0.0
+          Y(1) = 0.0
+          Y(4) = 0.0
+C
+C  Loop on the saturations
+C
+          DO 10 ISAT = 1,NSAT
+            SAT = (FLOAT(ISAT - 1) * SINC)
+            CALL HSVRGB(HUE,SAT,VAL(IL),R,G,B)
+            CALL GSCR(1,INDEX,R,G,B)
+            CALL GSFACI(INDEX)
+            RLEN = FLOAT(ISAT) / FLOAT(NSAT)
+            X(2) = COS(THETA1) * RLEN
+            Y(2) = SIN(THETA1) * RLEN
+            X(3) = COS(THETA2) * RLEN
+            Y(3) = SIN(THETA2) * RLEN
+            CALL GFA(4,X,Y)
+            X(1) = X(2)
+            X(4) = X(3)
+            Y(1) = Y(2)
+            Y(4) = Y(3)
+            INDEX = INDEX+1
+ 10       CONTINUE
+ 20     CONTINUE
+C
+C  Label the plots.
+C
+        CALL PCSETI('QU  - QUALITY FLAG',0)
+        CALL PCSETI('CD  - SELECT DUPLEX DATA SET',1)
+        WRITE(TITLE,700) VAL(IL)
+        CALL GSPLCI(1)
+        CALL PLCHHQ(0.0,1.25,TITLE(1:12),21.0,0.0,0.0)
+        DO 40 L=2,NSAT
+          SAT = (FLOAT(L-1)*SINC)
+          WRITE(TITLE,710) SAT
+          CALL PLCHHQ(0.0,ST(L),TITLE(1:6),15.0,0.0,0.0)
+   40   CONTINUE
+        CALL PLOTIF(0.,0.,2)
+        CALL GSPLCI(1)
+        CALL LINE(.98,0.,1.03,0.)
+        CALL PLCHHQ(1.08,0.,'Hue=0.',15.0,0.0,-1.0)
+        CALL LINE( .700,.700, .750,.740)
+        CALL PLCHHQ( .80,.740,'Hue=45.',15.0,0.0,-1.0)
+        CALL LINE(-.700,.700,-.750,.740)
+        CALL PLCHHQ(-.80,.740,'Hue=135.',15.0,0.0,1.0)
+        CALL LINE(-.700,-.700,-.750,-.740)
+        CALL PLCHHQ(-.80,-.740,'Hue=225.',15.0,0.0,1.0)
+        CALL LINE( .700,-.700, .750,-.740)
+        CALL PLCHHQ( .80,-.740,'Hue=315.',15.0,0.0,-1.0)
+        CALL FRAME
+ 30   CONTINUE
+      CALL CLSGKS
+C
+ 700  FORMAT('VALUE = ',F4.2)
+ 710  FORMAT('S=',F4.2)
+C
+      END
