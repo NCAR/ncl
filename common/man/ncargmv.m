31013527e890108c4f395453ad11b79bac8f5432
Mary Haley <haley@ucar.edu>
1993-04-20 14:42:05 +0000

    Mentioned the NCARG_ROOT env. vars.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1378 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- common/man/ncargmv.m
+++ common/man/ncargmv.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargmv.m,v 1.3 1993-03-21 01:53:17 haley Exp $
+.\"	$Id: ncargmv.m,v 1.4 1993-04-20 14:42:02 haley Exp $
 .\"
 .TH NCARGMV 8NCARG "March 1993 NCAR "NCAR GRAPHICS"
 .SH NAME
@@ -9,7 +9,11 @@ ncargmv \- Move an installed version of NCAR Graphics.
 .PP
 .SH DESCRIPTION
 .B ncargmv
-has no arguments and must be run interactively.  It is intended for use by a
-system administrator, and will move an installed version of NCAR Graphics (at
-revision level 3.2.0 or later) from its current position to a new position.
-
+has no arguments and must be run interactively.  It is intended for
+use by a system administrator, and will move an installed version of
+NCAR Graphics (at revision level 3.2.0 or later) from its current
+position to a new position.  The environment variable NCARG_ROOT or
+the collection of environment variables NCARG_LIB, NCARG_BIN,
+NCARG_INCLUDE, and NCARG_MAN must be set in order to run this script.
+For more information on these environment variables, see the
+Installer's Guide or the Release Notes.
--- common/man/ncargtar.m
+++ common/man/ncargtar.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargtar.m,v 1.3 1993-03-21 01:53:22 haley Exp $
+.\"	$Id: ncargtar.m,v 1.4 1993-04-20 14:42:05 haley Exp $
 .\"
 .TH NCARGTAR 8NCARG "March 1993" NCAR "NCAR GRAPHICS"
 .SH NAME
@@ -10,11 +10,16 @@ ncargtar \- Create tar file for NCAR Graphics.
 .SH DESCRIPTION
 .B ncargtar
 has no arguments.  It creates, in the current directory, a tar file
-containing all files written by the last installation of NCAR Graphics.
+containing all files written by the last installation of NCAR
+Graphics.  The environment variable NCARG_ROOT or the collection of
+environment variables NCARG_LIB, NCARG_BIN, NCARG_INCLUDE, and
+NCARG_MAN must be set in order to run this script.  For more
+information, see the Installer's Guide or the Release Notes.
+.sp
 The names of the files in the archive are given as full path names,
 preceded by a period (for example, "./u2/ncargfx/3.2.0/bin/ncargtar").
-Thus, if the files are extracted to the root directory "/", NCAR Graphics
-is restored; if the files are extracted to some other directory (for
-example, "/tmp/ncargfx", the directory hierarchy is created and the files
-in it may be checked for correctness without overwriting the current
-working version of NCAR Graphics.
+Thus, if the files are extracted to the root directory "/", NCAR
+Graphics is restored; if the files are extracted to some other
+directory (for example, "/tmp/ncargfx", the directory hierarchy is
+created and the files in it may be checked for correctness without
+overwriting the current working version of NCAR Graphics.
