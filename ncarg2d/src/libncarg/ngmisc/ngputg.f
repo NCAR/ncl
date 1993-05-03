805e1dd1cec1d14fb726791ec7b0f83cb45d07ab
fred <fred>
1993-05-03 17:41:37 +0000

    Disabled the function code signal character in Plotchar since it is not used and since all printable characters shold be allowed.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1447 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/ngmisc/ngputg.f
+++ ncarg2d/src/libncarg/ngmisc/ngputg.f
@@ -1,13 +1,20 @@
-      SUBROUTINE NGPUTG(IFNT,CHAR,X,Y,SIZE,ICOLOR)
+      SUBROUTINE NGPUTG(IFNT,PCHAR,X,Y,SIZE,ICOLOR)
 C
 C  Draw the glyph associated with the ASCII character CHAR from
 C  font number IFNT.
 C
-      CHARACTER*1 CHAR
+      CHARACTER*1 PCHAR,TCHAR
 C
+C  Disable the function code flag since it is never used and since
+C  we want to allow for plotting all printable ASCII characters.
+C  Save and restore the current flag.
+C
+      CALL PCGETC('FC',TCHAR)
+      CALL PCSETC('FC',CHAR(30))
       CALL PCSETI('FN',IFNT) 
       CALL PCSETI('CC',ICOLOR)
-      CALL PLCHHQ(X,Y,CHAR,6.*SIZE/7.,0.,0.) 
+      CALL PLCHHQ(X,Y,PCHAR,6.*SIZE/7.,0.,0.) 
+      CALL PCSETC('FC',TCHAR)
 C
       RETURN
       END
