2c8cfe31090a9eed83c90701c93a088a659f1331
fred <fred>
1993-05-03 21:01:09 +0000

    Made sure that the original font number and character color settings for Plotchar were restored on exit, correcting an error reported by Dave Kennison.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1450 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/ngmisc/ngputg.f
+++ ncarg2d/src/libncarg/ngmisc/ngputg.f
@@ -7,14 +7,21 @@ C
 C
 C  Disable the function code flag since it is never used and since
 C  we want to allow for plotting all printable ASCII characters.
-C  Save and restore the current flag.
+C  Save and restore the current flag as well as the font and
+C  character color.
 C
       CALL PCGETC('FC',TCHAR)
+      CALL PCGETI('FN',IFNTO)
+      CALL PCGETI('CC',ICLRO)
+C
       CALL PCSETC('FC',CHAR(30))
       CALL PCSETI('FN',IFNT) 
       CALL PCSETI('CC',ICOLOR)
       CALL PLCHHQ(X,Y,PCHAR,6.*SIZE/7.,0.,0.) 
+C
       CALL PCSETC('FC',TCHAR)
+      CALL PCSETI('FN',IFNTO)
+      CALL PCSETI('CC',ICLRO)
 C
       RETURN
       END
