02827a35711d93e92b70a0ebe038445d5ccc9fc1
boote <boote>
1993-07-14 18:40:57 +0000

    Close.c:	Added some memory free'ing
    Convert.c:	Added a new addressing mode for the convert args to support
    		the new NhlCvtStringToEnum converter function.  Also changed
    		the args that are passed to converter functions to be
    		NhlConvertArg structures instead of NrmValue structures.
    
    Convert.h:	see Convert.c
    
    ConvertP.h:	see Convert.c
    
    Converters.c:	Added new NhlCvtStringToEnum function. If you want to see
    		how to use it look at the bottom of this file - I use it
    		to install a converter from string to Boolean.
    
    Error.c:	Replaced the NhlCvtStringToNhlErrorTypes function with the
    		NhlCvtStringToEnum converter.
    
    IrregularType2TransObj.c:	Removed automatic var's that were not being
    				used so ccenter would stop giving warnings.
    
    LogLinTransObj.c:	dito
    
    NresDB.c:	Added /* SUPPRESS nn */ comments so ccenter would stop giving
    		warnings.
    
    Quarks.c:	dito
    
    ResourcesP.h:	Added a prototype for the _NhlDestroyResDatabase function.
    
    TextItem.c:	Removed auto var's that were not being used.  Replaced the
    		NhlCvtStringToFQuality and NhlCvtStringToTextDirection
    		converters with the NhlCvtStringToEnum converter.
    
    TickMark.c:	Replaced NhlCvtStringToTickMarkModes and
    		NhlCvtStringToTickMarkStyles with NhlCvtStringToEnum.
    		Also removed auto var's that were not being used.
    
    Title.c:	Replaced NhlCvtStringToTitlePositions with NhlCvtStringToEnum.
    
    TransObj.c:	Added #include <string.h> so strcpy is prototyped.
    
    XyPlot.c:	Replaced NhlCvtStringToAlternatePlace and
    		NhlCvtStringToLineLabelModes with NhlCvtStringToEnum.
    
    hlu.h:		Added a #include <stdlib.h> in the __STDC__ part so the
    		other parts of the library get it automatically.
    
    yMakefile:	Added Converters.h to the HDRS list.
    
    Converters.h:	Prototypes for the default converter fucntions.  I decided
    		they should be Publicly accessable by users.
    
    jeff
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1550 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/hlu/Converters.h
@@ -0,0 +1,64 @@
+/*
+ *      $Id: Converters.h,v 1.1 1993-07-14 18:39:46 boote Exp $
+ */
+/************************************************************************
+*									*
+*			     Copyright (C)  1993			*
+*	     University Corporation for Atmospheric Research		*
+*			     All Rights Reserved			*
+*									*
+************************************************************************/
+/*
+ *	File:		Converters.h
+ *
+ *	Author:		Jeff W. Boote
+ *			National Center for Atmospheric Research
+ *			PO 3000, Boulder, Colorado
+ *
+ *	Date:		Wed Jul 14 10:41:03 MDT 1993
+ *
+ *	Description:	This file contains the declarations of all the
+ *			Publicly defined converter functions.
+ */
+#ifndef	_NCONVERTERS_H
+#define	_NCONVERTERS_H
+
+#include <ncarg/hlu/Convert.h>
+
+extern NhlErrorTypes NhlCvtStringToFloat(
+#ifdef	NhlNeedProto
+	NrmValue		*from,	/* ptr to from data	*/
+	NrmValue		*to,	/* ptr to to data	*/
+	NhlConvertArgList	args,	/* add'n args for conv	*/
+	int			nargs	/* number of args	*/
+#endif
+);
+
+extern NhlErrorTypes NhlCvtStringToInteger(
+#ifdef	NhlNeedProto
+	NrmValue		*from,	/* ptr to from data	*/
+	NrmValue		*to,	/* ptr to to data	*/
+	NhlConvertArgList	args,	/* add'n args for conv	*/
+	int			nargs	/* number of args	*/
+#endif
+);
+
+extern NhlErrorTypes NhlCvtStringToEnum(
+#ifdef	NhlNeedProto
+	NrmValue		*from,	/* ptr to from data	*/
+	NrmValue		*to,	/* ptr to to data	*/
+	NhlConvertArgList	args,	/* add'n args for conv	*/
+	int			nargs	/* number of args	*/
+#endif
+);
+
+extern NhlErrorTypes NhlCvtStringToChar(
+#ifdef	NhlNeedProto
+	NrmValue		*from,	/* ptr to from data	*/
+	NrmValue		*to,	/* ptr to to data	*/
+	NhlConvertArgList	args,	/* add'n args for conv	*/
+	int			nargs	/* number of args	*/
+#endif
+);
+
+#endif	/* _NCONVERTERS_H */
