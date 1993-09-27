c2fb2dcf52ff8997ba140e42b5b0b9b59d208830
kennison <kennison>
1993-09-27 19:56:21 +0000

    These are the "man" page files for the routines ARGETR and ARSETR.  These were apparently dropped out of the distribution somewhere along the line. (I'm pretty sure they were there when I was going through the "man" pages prior to the release of version 3.2, but perhaps they never got properly checked in.)
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1611 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/man/areas/argetr.m
@@ -0,0 +1,50 @@
+.TH ARGETR 3NCARG "March 1993" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+ARGETR - Retrieves the real value of an Areas parameter.
+.SH SYNOPSIS
+CALL ARGETR (PNAM,RVAL)
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_argetr (char *pnam, float *rval)
+.SH DESCRIPTION 
+.IP "PNAM" 12
+(an input constant or variable of type CHARACTER) -
+The name of the parameter that you want to retrieve.  The character string
+can be of any length, but only the first two characters
+of it will be examined.
+.IP "RVAL" 12
+(an output variable of type REAL) -
+A real variable to receive the desired parameter value.  If the internal
+parameter is of type REAL and has the value "r", RVAL will be given the
+value "r"; if the internal parameter is of type INTEGER and has the value
+"i", RVAL will be given the value "REAL(i)".
+.SH C-BINDING DESCRIPTION 
+The C-binding argument descriptions are the same as the FORTRAN 
+argument descriptions.
+.SH USAGE
+This routine allows you to retrieve the current value of Areas 
+parameters. For a complete list of parameters available in this 
+utility, see the areas_params man page.
+.SH ACCESS
+To use ARGETR, load the NCAR Graphics libraries ncarg, ncarg_gks,
+ncarg_c, and ncarg_loc, preferably in that order. To use c_argetr,
+load the NCAR Graphics libraries ncargC, ncarg_gksC, ncarg, 
+ncarg_gks, ncarg_c, and ncarg_loc, preferably in that order.
+.SH MESSAGES
+See the areas man page for a description of all Areas error
+messages and/or informational messages.
+.SH SEE ALSO
+Online:
+areas, areas_params, ardbpa, ardrln, aredam, argeti, argtai, arinam,
+armvam, arpram, arscam, arseti, arsetr, ncarg_cbind
+.sp
+Hardcopy:
+NCAR Graphics Contouring and Mapping Tutorial
+.SH COPYRIGHT
+Copyright 1987, 1988, 1989, 1991, 1993 University Corporation 
+for Atmospheric Research
+.br
+All Rights Reserved
---
+++ ncarg2d/man/areas/arsetr.m
@@ -0,0 +1,54 @@
+.TH ARSETR 3NCARG "March 1993" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+ARSETR - Provides a new real value for an Areas parameter.
+.SH SYNOPSIS
+CALL ARSETR (PNAM,RVAL)
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_arsetr (char *pnam, float rval)
+.SH DESCRIPTION 
+.IP "PNAM" 12
+(an input constant or variable of type CHARACTER) -
+The name of the parameter that you want to set. The character string
+can be of any length, but only the first two characters
+of it will be examined.
+.IP "RVAL" 12
+(an input expression of type REAL) -
+The real value you select for the parameter.  If the parameter is of type
+REAL, it will be given the value RVAL; if is is of type INTEGER, it will be
+given the value INT(RVAL).
+.SH C-BINDING DESCRIPTION 
+The C-binding argument descriptions are the same as the FORTRAN 
+argument descriptions.
+.SH USAGE
+This routine allows you to set the current value of Areas 
+parameters. For a complete list of parameters available in this 
+utility, see the areas_params man page.
+.SH EXAMPLES
+Use the ncargex command to see the following relevant
+examples: 
+cardb1,
+arex01.
+.SH ACCESS
+To use ARSETR, load the NCAR Graphics libraries ncarg, ncarg_gks,
+ncarg_c, and ncarg_loc, preferably in that order. To use c_arsetr,
+load the NCAR Graphics libraries ncargC, ncarg_gksC, ncarg, 
+ncarg_gks, ncarg_c, and ncarg_loc, preferably in that order.
+.SH MESSAGES
+See the areas man page for a description of all Areas error
+messages and/or informational messages.
+.SH SEE ALSO
+Online:
+areas, areas_params, ardbpa, ardrln, aredam, argeti, argetr, argtai,
+arinam, armvam, arpram, arscam, arseti, ncarg_cbind
+.sp
+Hardcopy:
+NCAR Graphics Contouring and Mapping Tutorial
+.SH COPYRIGHT
+Copyright 1987, 1988, 1989, 1991, 1993 University Corporation
+for Atmospheric Research
+.br
+All Rights Reserved
