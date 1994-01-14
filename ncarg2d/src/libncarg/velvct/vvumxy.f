94b470fc9d8b5c4c1c1218ef502d8206eb9917a0
Dave Brown <dbrown@ucar.edu>
1994-01-14 20:01:23 +0000

    Added explanatory comments to the user-modifiable mapping routine. Also corrected the default code so that it properly converts the output vector endpoints from user to NDC space.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1740 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/velvct/vvumxy.f
+++ ncarg2d/src/libncarg/velvct/vvumxy.f
@@ -1,5 +1,5 @@
 C
-C       $Id: vvumxy.f,v 1.4 1993-12-03 21:28:16 kennison Exp $
+C       $Id: vvumxy.f,v 1.5 1994-01-14 20:01:23 dbrown Exp $
 C
 C
 C-----------------------------------------------------------------------
@@ -7,13 +7,16 @@ C
       SUBROUTINE VVUMXY (X,Y,U,V,UVM,XB,YB,XE,YE,IST)
 C
 C This is a user modifiable routine that allows custom projections of
-C the vector space. Input is in the user coordinates. Output
-C is in fractional coordinates as defined by the SET call.
+C the vector space. X and Y give the vector position within the domain
+C of the data space. By default, this space is coincident with the
+C grid space (i.e. 1 through dimension lengths of the U and V arrays).
+C The vector endpoints are output in fractional coordinates (NDC space).
 C Note that this is different from the old MXF and MYF routines, which
-C output in 'plotter coordinate' space.
+C output in 'plotter coordinate' space. It also differs from the 
+C Conpack routine CPMPXY, which returns values in user space. 
 C 
 C VVUMXY (Velocity Vector -- User Map X,Y) is called whenever 
-C IMAP has a value other than 0,1, or 2.
+C the internal parameter MAP is set to a value other than 0, 1, or 2.
 C
 C Based on the magnitude and direction of the vector the start and 
 C ending points of the vector are returned in NDC space.
@@ -22,16 +25,22 @@ C Input parameters:
 C
 C X,Y   -- vector position in the user coordinate system
 C U,V   -- vector components from the U,V arrays for this position
-C UVM   -- magnitude of the U,V components (supplied for convenience)
+C UVM   -- magnitude of the U,V components (supplied for convenience
+C          and efficiency - but note that many mappings do not need 
+C          this value)
 C
 C Output parameters:
 C
-C XB,YB -- starting point of the vector in the user coordinate system
-C          as defined by the SET call
-C XE,YE -- ending point of the vector in the user coordinate system
-C          as defined by the SET call
-C IST   -- status results of the mapping: 0 indicates success
-C 
+C XB,YB -- starting point of the vector in fractional coordinates
+C          (NDC space), before offset based on the value of the
+C          vector positioning parameter, VPO.
+C XE,YE -- ending point of the vector in fractional coordinates
+C          (NDC space), before offset based on the value of the
+C          vector positioning parameter, VPO.
+C IST   -- status results of the mapping: 0 indicates success -- any
+C          non-zero value causes VVECTR to discard the vector at this
+C          location.
+C
 C
 C The mapping common block: made available to user mapping routines
 C
@@ -58,17 +67,65 @@ C
 C
 C --------------------------------------------------------------------
 C
-C Common block variables used:
+C Description of VVMAP contents:
 C
-C SXDC  -- scale factor from vector length to user coordinates, X
-C SYDC  -- scale factor from vector length to user coordinates, Y
+C IMAP                - value of the internal parameter 'MAP'
+C XVPL,XVPR,YVPB,YVPT - the currently set viewport values. (GETSET
+C                       arguments 1, 2, 3, and 4)
+C WXMN,WXMX,WYMN,WYMX - the min and max boundaries of user coordinate
+C                       space, (usually but not always equivalent to
+C                       window coordinates). WXMN and WYMN are true
+C                       minimum values even one or both axes is 
+C                       inverted. (i.e. WXMN = MIN of GETSET args 5,6;
+C                       WYMN = MIN of GETSET args 7,8)
+C XLOV,XHIV,YLOV,YHIV - min and max boundaries of the data space, by
+C                       default equivalent to the array grid space.
+C                       XLOV and YLOV are not necessarily less than 
+C                       XHIV and YHIV respectively.
+C SXDC,SYDC           - Scaling factors for converting vector component
+C                       values into lengths in NDC space.
+C NXCT,NYCT           - Length of each dimension of the U and V 
+C                       component arrays.
+C RLEN                - Length of the maximum vector in user 
+C                       coordinates.
+C LNLG                - The linear/log mode (GETSET argument 9)
+C INVX,INVY           - User coordinates inversion flags: 
+C                       0 - not inverted, 1 - inverted
+C ITRT                - value of the internal parameter TRT
+C IWCT                - not currently used
+C FW2W,FH2H           - scale factors for converting from fraction of
+C                       viewport width/height to NDC width/height 
+C DVMN,DVMX           - min/max vector lengths in NDC
+C RBIG,IBIG           - machine dependent maximum REAL/INTEGER values
 C
-C Identity transformation
+C --------------------------------------------------------------------
+C User-defined common block...
 C
-         XB=X
-         YB=Y
-         XE=X+U*SXDC
-         YE=Y+V*SYDC
+C --------------------------------------------------------------------
+C
+C     IF (IMAP .EQ. =user defined mapping code=) THEN
+C
+C User defined mapping code....
+C
+C     ELSE
+C
+C Default mapping:
+C
+C Note that the default version of VVUMXY included with
+C NCAR Graphics 3.2 incorrectly fails to convert the input X and Y
+C location into fractional coordinates (NDC space).
+C
+         IF (X.LT.WXMN .OR. X.GT.WXMX .OR. 
+     +        Y.LT.WYMN .OR. Y.GT.WYMX) THEN
+            IST = -1
+            RETURN
+         END IF
+         XB=CUFX(X)
+         YB=CUFY(Y)
+         XE=XB+U*SXDC
+         YE=YB+V*SYDC
+
+C     END IF
 C
 C Done.
 C
