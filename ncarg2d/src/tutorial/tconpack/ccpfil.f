a4d3a49b7244ccf3d845424c4239e766930a2c0c
Mary Haley <haley@ucar.edu>
1994-03-04 22:48:41 +0000

    Fixed spacing problems (that I introduced when fixing for Cray 3 port).
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1825 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/tutorial/tconpack/ccpfil.f
+++ ncarg2d/src/tutorial/tconpack/ccpfil.f
@@ -1,94 +1,94 @@
-        PARAMETER (MREG=50,NREG=50)
-	REAL XREG(MREG),YREG(NREG),ZREG(MREG,NREG)
+      PARAMETER (MREG=50,NREG=50)
+      REAL XREG(MREG),YREG(NREG),ZREG(MREG,NREG)
 
-	EXTERNAL COLOR
+      EXTERNAL COLOR
 
 C Get data array
-	CALL GETDAT(XREG,YREG,ZREG,MREG,NREG)
+      CALL GETDAT(XREG,YREG,ZREG,MREG,NREG)
 
 C Open GKS
-	CALL OPNGKS
+      CALL OPNGKS
 
 C Call Conpack color fill routine
 
-	CALL CCPFIL(ZREG,MREG,NREG,-15,COLOR)
+      CALL CCPFIL(ZREG,MREG,NREG,-15,COLOR)
 
 
 C Close frame and close GKS
-	CALL FRAME
-	CALL CLSGKS
+      CALL FRAME
+      CALL CLSGKS
 
-	STOP
-	END
+      STOP
+      END
 
-	SUBROUTINE CCPFIL(ZREG,MREG,NREG,NCL,COLOR)
+      SUBROUTINE CCPFIL(ZREG,MREG,NREG,NCL,COLOR)
 
-        PARAMETER (LRWK=5000,LIWK=5000,LMAP=50000,NWRK=5000,NOGRPS=5)
-	REAL ZREG(MREG,NREG),RWRK(LRWK), XWRK(NWRK), YWRK(NWRK)
-	INTEGER MREG,NREG,IWRK(LIWK)
-	INTEGER MAP(LMAP),IAREA(NOGRPS),IGRP(NOGRPS)
+      PARAMETER (LRWK=5000,LIWK=5000,LMAP=50000,NWRK=5000,NOGRPS=5)
+      REAL ZREG(MREG,NREG),RWRK(LRWK), XWRK(NWRK), YWRK(NWRK)
+      INTEGER MREG,NREG,IWRK(LIWK)
+      INTEGER MAP(LMAP),IAREA(NOGRPS),IGRP(NOGRPS)
 
-	EXTERNAL FILL
-        EXTERNAL COLOR
+      EXTERNAL FILL
+      EXTERNAL COLOR
 
 C Set up color table
-	CALL COLOR
+      CALL COLOR
 C Initialize Areas
-	CALL ARINAM(MAP, LMAP)
+      CALL ARINAM(MAP, LMAP)
 C Set number of contour levels and initialize Conpack
-	CALL CPSETI('CLS - CONTOUR LEVEL SELECTION FLAG',NCL)
-	CALL CPRECT(ZREG, MREG, MREG, NREG, RWRK, LRWK, IWRK, LIWK)
+      CALL CPSETI('CLS - CONTOUR LEVEL SELECTION FLAG',NCL)
+      CALL CPRECT(ZREG, MREG, MREG, NREG, RWRK, LRWK, IWRK, LIWK)
 C Add contours to area map
-	CALL CPCLAM(ZREG, RWRK, IWRK, MAP)
+      CALL CPCLAM(ZREG, RWRK, IWRK, MAP)
 C Set fill style to solid, and fill contours
-	CALL GSFAIS(1)
-	CALL ARSCAM(MAP, XWRK, YWRK, NWRK, IAREA, IGRP, NOGRPS, FILL)
+      CALL GSFAIS(1)
+      CALL ARSCAM(MAP, XWRK, YWRK, NWRK, IAREA, IGRP, NOGRPS, FILL)
 C Draw Perimeter
-	CALL CPBACK(ZREG, RWRK, IWRK)
+      CALL CPBACK(ZREG, RWRK, IWRK)
 C Draw Labels
-	CALL CPLBDR(ZREG,RWRK,IWRK)
+      CALL CPLBDR(ZREG,RWRK,IWRK)
 C Draw Contours
-	CALL CPCLDR(ZREG,RWRK,IWRK)
+      CALL CPCLDR(ZREG,RWRK,IWRK)
 
-	RETURN
-	END
+      RETURN
+      END
 
-	SUBROUTINE FILL (XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS)
+      SUBROUTINE FILL (XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS)
 C
-	DIMENSION XWRK(*),YWRK(*),IAREA(*),IGRP(*)
+      DIMENSION XWRK(*),YWRK(*),IAREA(*),IGRP(*)
 
-	DO 10, I=1,NGRPS
-	  IF (IGRP(I).EQ.3) IAREA3=IAREA(I)
- 10	CONTINUE
+      DO 10, I=1,NGRPS
+         IF (IGRP(I).EQ.3) IAREA3=IAREA(I)
+ 10   CONTINUE
 
-	IF (IAREA3 .GT. 0) THEN
+      IF (IAREA3 .GT. 0) THEN
 C If the area is defined by 3 or more points, fill it
-	   CALL GSFACI(IAREA3+1)
-	   CALL GFA(NWRK,XWRK,YWRK)
-	ENDIF
-	
+         CALL GSFACI(IAREA3+1)
+         CALL GFA(NWRK,XWRK,YWRK)
+      ENDIF
+    
 C Otherwise, do nothing
 
-	RETURN
-	END
+      RETURN
+      END
 
-	SUBROUTINE GETDAT(XREG,YREG,ZREG,MREG,NREG)
+      SUBROUTINE GETDAT(XREG,YREG,ZREG,MREG,NREG)
 
-        PARAMETER (NRAN=30,LRWK=5000,LIWK=5000)
-    INTEGER MREG, NREG
-	REAL XRAN(NRAN), YRAN(NRAN), ZRAN(NRAN)
-	REAL XREG(MREG), YREG(NREG), ZREG(MREG,NREG), RWRK(LRWK)
-	INTEGER IWRK(LIWK)
+      PARAMETER (NRAN=30,LRWK=5000,LIWK=5000)
+      INTEGER MREG, NREG
+      REAL XRAN(NRAN), YRAN(NRAN), ZRAN(NRAN)
+      REAL XREG(MREG), YREG(NREG), ZREG(MREG,NREG), RWRK(LRWK)
+      INTEGER IWRK(LIWK)
 
-	DATA XRAN /12., 60., 14., 33.,  8., 12., 43., 57., 22., 15.,
-     1		   19., 12., 64., 19., 15., 55., 31., 32., 33., 29.,
-     2		   18.,  1., 18., 42., 56.,  9.,  6., 12., 44., 19./
-	DATA YRAN / 1.,  2.,  3., 53.,  7., 11., 13., 17., 19., 49.,
-     1		    1., 31., 37.,  5.,  7., 47., 61., 17.,  5., 23.,
-     2		   29.,  3.,  5., 41., 43.,  9., 13., 59.,  1., 67./
-	DATA ZRAN /1.0, 1.5, 1.7, 1.4, 1.9, 1.0, 1.5, 1.2, 1.8, 1.4,
-     1		   1.8, 1.7, 1.9, 1.5, 1.2, 1.1, 1.3, 1.7, 1.2, 1.6,
-     2		   1.9, 1.0, 1.6, 1.3, 1.4, 1.8, 1.7, 1.5, 1.1, 1.0/
+      DATA XRAN /12., 60., 14., 33.,  8., 12., 43., 57., 22., 15.,
+     1    19., 12., 64., 19., 15., 55., 31., 32., 33., 29.,
+     2           18.,  1., 18., 42., 56.,  9.,  6., 12., 44., 19./
+      DATA YRAN / 1.,  2.,  3., 53.,  7., 11., 13., 17., 19., 49.,
+     1          1., 31., 37.,  5.,  7., 47., 61., 17.,  5., 23.,
+     2         29.,  3.,  5., 41., 43.,  9., 13., 59.,  1., 67./
+      DATA ZRAN /1.0, 1.5, 1.7, 1.4, 1.9, 1.0, 1.5, 1.2, 1.8, 1.4,
+     1         1.8, 1.7, 1.9, 1.5, 1.2, 1.1, 1.3, 1.7, 1.2, 1.6,
+     2         1.9, 1.0, 1.6, 1.3, 1.4, 1.8, 1.7, 1.5, 1.1, 1.0/
 
 C
 C  Set the min and max data values.
@@ -102,19 +102,19 @@ C Choose the X and Y coordinates for interpolation points on the
 C regular grid.
 C
       DO 101 I=1,MREG
-        XREG(I)=XMIN + (XMAX - XMIN)* REAL(I-1)/MREG
-  101 CONTINUE
+         XREG(I)=XMIN + (XMAX - XMIN)* REAL(I-1)/MREG
+ 101  CONTINUE
 C
       DO 102 I=1,NREG
-        YREG(I)=YMIN + (YMAX - YMIN)* REAL(I-1)/NREG
-  102 CONTINUE
+         YREG(I)=YMIN + (YMAX - YMIN)* REAL(I-1)/NREG
+ 102  CONTINUE
 
 C Interpolate data onto a regular grid
-	CALL IDSFFT (1,NRAN,XRAN,YRAN,ZRAN,
-     +		MREG,NREG,MREG,XREG,YREG,ZREG,IWRK,RWRK)
+      CALL IDSFFT (1,NRAN,XRAN,YRAN,ZRAN,
+     +      MREG,NREG,MREG,XREG,YREG,ZREG,IWRK,RWRK)
 
-	RETURN
-	END
+      RETURN
+      END
       SUBROUTINE COLOR
 C
 C     BACKGROUND COLOR
@@ -158,7 +158,7 @@ C Lavender
       CALL GSCR(1, 17, 0.8, 0.8, 1.0)
 C Done.
 C
-        RETURN
+      RETURN
 C
       END
 
--- ncarg2d/src/tutorial/tconpack/ccplbam.f
+++ ncarg2d/src/tutorial/tconpack/ccplbam.f
@@ -1,102 +1,100 @@
-        PARAMETER (MREG=50,NREG=50)
-	REAL XREG(MREG),YREG(NREG),ZREG(MREG,NREG)
+      PARAMETER (MREG=50,NREG=50)
+      REAL XREG(MREG),YREG(NREG),ZREG(MREG,NREG)
 
-	EXTERNAL COLOR
+      EXTERNAL COLOR
 
 C Get data array
-	CALL GETDAT(XREG,YREG,ZREG,MREG,NREG)
+      CALL GETDAT(XREG,YREG,ZREG,MREG,NREG)
 
 C Open GKS
-	CALL OPNGKS
+      CALL OPNGKS
 
 C Call Conpack color fill routine
-
-	CALL CCPLBM(ZREG,MREG,NREG,COLOR)
-
+      CALL CCPLBM(ZREG,MREG,NREG,COLOR)
 
 C Close frame and close GKS
-	CALL FRAME
-	CALL CLSGKS
+      CALL FRAME
+      CALL CLSGKS
 
-	STOP
-	END
+      STOP
+      END
 
-	SUBROUTINE CCPLBM(ZREG,MREG,NREG,COLOR)
+      SUBROUTINE CCPLBM(ZREG,MREG,NREG,COLOR)
 
-        PARAMETER (LRWK=5000,LIWK=5000,LMAP=50000,NWRK=5000,NOGRPS=5)
-	REAL ZREG(MREG,NREG),RWRK(LRWK), XWRK(NWRK), YWRK(NWRK)
-	INTEGER MREG,NREG,IWRK(LIWK)
-	INTEGER MAP(LMAP),IAREA(NOGRPS),IGRP(NOGRPS)
+      PARAMETER (LRWK=5000,LIWK=5000,LMAP=50000,NWRK=5000,NOGRPS=5)
+      REAL ZREG(MREG,NREG),RWRK(LRWK), XWRK(NWRK), YWRK(NWRK)
+      INTEGER MREG,NREG,IWRK(LIWK)
+      INTEGER MAP(LMAP),IAREA(NOGRPS),IGRP(NOGRPS)
 
-	EXTERNAL FILL
-        EXTERNAL COLOR
+      EXTERNAL FILL
+      EXTERNAL COLOR
 
 C Initialize Areas
-	CALL ARINAM(MAP, LMAP)
+      CALL ARINAM(MAP, LMAP)
 
 C Set contour interval and min and max contours 
-	CALL CPSETR('CIS',0.1)
-	CALL CPSETR('CMN',-0.4)
-	CALL CPSETR('CMX',1.8)
+      CALL CPSETR('CIS',0.1)
+      CALL CPSETR('CMN',-0.4)
+      CALL CPSETR('CMX',1.8)
 C Initialize Conpack and pick contour levels
-	CALL CPRECT(ZREG, MREG, MREG, NREG, RWRK, LRWK, IWRK, LIWK)
-	CALL CPPKCL(ZREG,RWRK,IWRK)
+      CALL CPRECT(ZREG, MREG, MREG, NREG, RWRK, LRWK, IWRK, LIWK)
+      CALL CPPKCL(ZREG,RWRK,IWRK)
 C Set up color table
-	CALL CPGETI('NCL',NCL)
-	CALL COLOR(NCL+1)
+      CALL CPGETI('NCL',NCL)
+      CALL COLOR(NCL+1)
 C Add contours to area map
-	CALL CPCLAM(ZREG, RWRK, IWRK, MAP)
+      CALL CPCLAM(ZREG, RWRK, IWRK, MAP)
 
 C Add label boxes to area map
-	CALL CPLBAM(ZREG, RWRK, IWRK, MAP)
+      CALL CPLBAM(ZREG, RWRK, IWRK, MAP)
 
 C Set fill style to solid, and fill contours
-	CALL GSFAIS(1)
-	CALL ARSCAM(MAP, XWRK, YWRK, NWRK, IAREA, IGRP, NOGRPS, FILL)
+      CALL GSFAIS(1)
+      CALL ARSCAM(MAP, XWRK, YWRK, NWRK, IAREA, IGRP, NOGRPS, FILL)
 
 C Draw Perimeter
-	CALL CPBACK(ZREG, RWRK, IWRK)
+      CALL CPBACK(ZREG, RWRK, IWRK)
 
 C Draw Labels
-	CALL CPLBDR(ZREG,RWRK,IWRK)
+      CALL CPLBDR(ZREG,RWRK,IWRK)
 
-	RETURN
-	END
+      RETURN
+      END
 
-	SUBROUTINE FILL (XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS)
+      SUBROUTINE FILL (XWRK,YWRK,NWRK,IAREA,IGRP,NGRPS)
 C
-	DIMENSION XWRK(*),YWRK(*),IAREA(*),IGRP(*)
+      DIMENSION XWRK(*),YWRK(*),IAREA(*),IGRP(*)
 
-	DO 10, I=1,NGRPS
-	  IF (IGRP(I).EQ.3) IAREA3=IAREA(I)
- 10	CONTINUE
-
-	IF (IAREA3 .GT. 0) THEN
+      DO 10 I=1,NGRPS
+         IF (IGRP(I).EQ.3) IAREA3=IAREA(I)
+ 10   CONTINUE
+      
+      IF (IAREA3 .GT. 0) THEN
 C If the area is defined by 3 or more points, fill it
-	   CALL GSFACI(IAREA3+2)
-	   CALL GFA(NWRK,XWRK,YWRK)
-	ENDIF
+         CALL GSFACI(IAREA3+2)
+         CALL GFA(NWRK,XWRK,YWRK)
+      ENDIF
 	
 C Otherwise, do nothing
+      
+      RETURN
+      END
 
-	RETURN
-	END
+      SUBROUTINE GETDAT(XREG,YREG,ZREG,MREG,NREG)
 
-	SUBROUTINE GETDAT(XREG,YREG,ZREG,MREG,NREG)
+      PARAMETER (NRAN=30,LRWK=5000,LIWK=5000)
+      INTEGER MREG, NREG
+      REAL XRAN(NRAN), YRAN(NRAN), ZRAN(NRAN)
+      REAL XREG(MREG), YREG(NREG), ZREG(MREG,NREG), RWRK(LRWK)
+      INTEGER IWRK(LIWK)
 
-        PARAMETER (NRAN=30,LRWK=5000,LIWK=5000)
-    INTEGER MREG, NREG
-	REAL XRAN(NRAN), YRAN(NRAN), ZRAN(NRAN)
-	REAL XREG(MREG), YREG(NREG), ZREG(MREG,NREG), RWRK(LRWK)
-	INTEGER IWRK(LIWK)
-
-	DATA XRAN /12., 60., 14., 33.,  8., 12., 43., 57., 22., 15.,
+      DATA XRAN /12., 60., 14., 33.,  8., 12., 43., 57., 22., 15.,
      1		   19., 12., 64., 19., 15., 55., 31., 32., 33., 29.,
      2		   18.,  1., 18., 42., 56.,  9.,  6., 12., 44., 19./
-	DATA YRAN / 1.,  2.,  3., 53.,  7., 11., 13., 17., 19., 49.,
+      DATA YRAN / 1.,  2.,  3., 53.,  7., 11., 13., 17., 19., 49.,
      1		    1., 31., 37.,  5.,  7., 47., 61., 17.,  5., 23.,
      2		   29.,  3.,  5., 41., 43.,  9., 13., 59.,  1., 67./
-	DATA ZRAN /1.0, 1.5, 1.7, 1.4, 1.9, 1.0, 1.5, 1.2, 1.8, 1.4,
+      DATA ZRAN /1.0, 1.5, 1.7, 1.4, 1.9, 1.0, 1.5, 1.2, 1.8, 1.4,
      1		   1.8, 1.7, 1.9, 1.5, 1.2, 1.1, 1.3, 1.7, 1.2, 1.6,
      2		   1.9, 1.0, 1.6, 1.3, 1.4, 1.8, 1.7, 1.5, 1.1, 1.0/
 
@@ -112,47 +110,47 @@ C Choose the X and Y coordinates for interpolation points on the
 C regular grid.
 C
       DO 101 I=1,MREG
-        XREG(I)=XMIN + (XMAX - XMIN)* REAL(I-1)/MREG
-  101 CONTINUE
+         XREG(I)=XMIN + (XMAX - XMIN)* REAL(I-1)/MREG
+ 101  CONTINUE
 C
       DO 102 I=1,NREG
-        YREG(I)=YMIN + (YMAX - YMIN)* REAL(I-1)/NREG
-  102 CONTINUE
+         YREG(I)=YMIN + (YMAX - YMIN)* REAL(I-1)/NREG
+ 102  CONTINUE
 
 C Interpolate data onto a regular grid
-	CALL IDSFFT (1,NRAN,XRAN,YRAN,ZRAN,
-     +		MREG,NREG,MREG,XREG,YREG,ZREG,IWRK,RWRK)
+      CALL IDSFFT (1,NRAN,XRAN,YRAN,ZRAN,
+     +     MREG,NREG,MREG,XREG,YREG,ZREG,IWRK,RWRK)
 
-	RETURN
-	END
+      RETURN
+      END
 
-        SUBROUTINE COLOR (N)
+      SUBROUTINE COLOR (N)
 
 C BACKGROUND COLOR
 C BLACK
-        CALL GSCR(1,0,0.,0.,0.)
+      CALL GSCR(1,0,0.,0.,0.)
 C First foreground color is white
-        CALL GSCR(1,1,1.,1.,1.)
+      CALL GSCR(1,1,1.,1.,1.)
 C Second foreground color is gray
-        CALL GSCR(1,2,.75,.75,.75)
+      CALL GSCR(1,2,.75,.75,.75)
 C Choose other foreground colors spaced equally around the spectrum
-        ICNT=0
-        HUES=360./N
+      ICNT=0
+      HUES=360./N
 C REDLN is intended to be the line between red and violet values
-	REDLN=36.0
-	LAP=INT(REDLN/HUES)
-        DO 10, I=1,N
-          XHUE=I*HUES
-          CALL HLSRGB(XHUE,60.,75.,RED,GREEN,BLUE)
+      REDLN=36.0
+      LAP=INT(REDLN/HUES)
+      DO 10, I=1,N
+         XHUE=I*HUES
+         CALL HLSRGB(XHUE,60.,75.,RED,GREEN,BLUE)
 C Sort colors so that the redest is first, and violetest is last
-          IF (XHUE.LE.REDLN) THEN
+         IF (XHUE.LE.REDLN) THEN
             CALL GSCR(1,(N+2)-(LAP-I),RED,GREEN,BLUE)
             ICNT=ICNT+1
-          ELSE
+         ELSE
             CALL GSCR(1,I-ICNT+2,RED,GREEN,BLUE)
-          ENDIF
- 10     CONTINUE
-
-        RETURN
-        END
+         ENDIF
+ 10   CONTINUE
+      
+      RETURN
+      END
 
