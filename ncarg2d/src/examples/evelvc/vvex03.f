81f647f0814fc21cefa49bc95a7170c4d65ceeab
Dave Brown <dbrown@ucar.edu>
1994-03-08 00:41:57 +0000

    Added a third frame to this example that demonstrates how to map a scattered vector dataset through an Ezmap projection. Also fixed an inconsistency in the argument lists for two routines.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1840 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/examples/evelvc/vvex03.f
+++ ncarg2d/src/examples/evelvc/vvex03.f
@@ -1,14 +1,15 @@
 C
-C       $Id: vvex03.f,v 1.1 1994-01-15 00:45:53 dbrown Exp $
+C       $Id: vvex03.f,v 1.2 1994-03-08 00:41:57 dbrown Exp $
 C
       PROGRAM VVEX03
 C
 C
 C This example illustrates use of the user-modifiable routine
 C VVUMXY to create user-defined mappings of vector data.
-C The first frame shows how to plot data contained in an 
-C irregularly spaced grid. 
+C The first frame demonstrates a method of plotting data contained
+C  in an irregularly spaced rectangular grid. 
 C The second frame plots scattered (non-gridded) data.
+C The third frame maps scattered data through an EZMAP projection.
 C
 C Open GKS, open workstation of type 1, activate workstation
 C
@@ -16,13 +17,17 @@ C
       CALL GOPWK (1, 2, 1)
       CALL GACWK (1) 
 C
-C Draw the irregularly gridded vectors
+C Draw an irregularly gridded vector plot.
 C
-      CALL IRREX(IERR)
+      CALL IRREX
 C
-C Draw the scattered vectors
+C Draw a scattered vector plot.
 C
-      CALL SCTREX(IERR)
+      CALL SCTREX
+C
+C Draw a plot of scattered vectors mapped through an Ezmap projection.
+C
+      CALL SCEZEX
 C
 C     Deactivate and close workstation, close GKS.
 C
@@ -40,30 +45,35 @@ C
       PARAMETER (M=10,N=10)
       DIMENSION U(M,N), V(M,N)
 C
-C User-defined common block containing irregular grid coordinates
+C User-defined common block containing irregular grid coordinates.
 C
+      PARAMETER (PI=3.14159)
       PARAMETER (MAXDIM = 100)
       COMMON /VVUSER/ XCOORD(MAXDIM),YCOORD(MAXDIM)
       SAVE /VVUSER/
 C
-C Local arrays to hold the coordinate data
+C Local arrays to hold the coordinate data.
 C
       DIMENSION XCLOC(M), YCLOC(N)
 C
-      DATA XCLOC / 0.0,7.5,20.0,35.0,45.0,65.0,75.0,80.0,90.0,95.0 /
-      DATA YCLOC / 12.5,22.5,30.0,35.0,50.0,60.0,70.0,75.0,85.0,90.0 /
+      DATA XGRID / 100.0 /
+      DATA YGRID / 100.0 /
+      DATA XCLOC / 0.0,7.5,20.0,40.0,45.0,65.0,70.0,77.0,90.0,95.0 /
+      DATA YCLOC / 12.5,22.5,30.0,35.0,52.0,58.0,70.0,75.0,85.0,90.0 /
 C
 C
 C Specify the NDC coordinates for a plot title.
 C
-        DATA FX / 0.15 /, FY / 0.975 /
+        DATA FX / 0.5 /, FY / 0.975 /
 C
-C Set up a test uniform velocity field
+C Set up a functionally defined vector field
 C
+      GXSIZE = PI/XGRID
+      GYSIZE = PI/YGRID
       DO  20 I=1,M
          DO  10 J=1,N
-            U(I,J) = 1.0
-            V(I,J) = 1.0
+            U(I,J) = COS(GXSIZE*(XCLOC(I)))
+            V(I,J) = SIN(GYSIZE*(YCLOC(J)))
    10    CONTINUE
    20 CONTINUE
 C
@@ -116,7 +126,7 @@ C
          CALL VVECTR(U,V,RDM,IDM,IDM,RDM)
          CALL PERIM(1,0,1,0)
 C
-C Save the current normalization transformation then set it to 0
+C Save the current normalization transformation then set it to 0.
 C
          CALL GQCNTN(IERR,ICN)
          CALL GSELNT(0)
@@ -126,10 +136,10 @@ C
 C Call PLCHLQ to write the plot title.
 C
          CALL PLCHLQ (X,Y,
-     1           'Plot of irregularly gridded vector data',
-     2           16.,0.,-1.)
+     1           'Irregularly gridded vector data',
+     2           16.,0.,0.)
 C
-C Restore the normalization transformation
+C Restore the normalization transformation.
 C
          CALL GSELNT(ICN)
          CALL FRAME
@@ -164,7 +174,7 @@ C
       COMMON /VVUSER/ XCOORD(MAXDIM),YCOORD(MAXDIM)
       SAVE /VVUSER/
 C
-C All data lies within a 100 x 100 square (origin 0.0 at lower left)
+C All data lies within a 100 x 100 square (origin 0.0 at lower left).
 C
       DIMENSION XCLOC(M*N), YCLOC(M*N)
 C
@@ -176,9 +186,9 @@ C
 C
 C Specify the NDC coordinates for a plot title.
 C
-        DATA FX / 0.15 /, FY / 0.975 /
+        DATA FX / 0.5 /, FY / 0.975 /
 C
-C Copy coordinate values into the VVUSER common block arrays
+C Copy coordinate values into the VVUSER common block arrays.
 C
       DO 30 I=1,M*N
          XCOORD(I) = XCLOC(I)
@@ -227,7 +237,7 @@ C
          CALL PERIM(1,0,1,0)
 C
 C
-C Save the current normalization transformation then set it to 0
+C Save the current normalization transformation then set it to 0.
 C
          CALL GQCNTN(IERR,ICN)
          CALL GSELNT(0)
@@ -237,10 +247,10 @@ C
 C Call PLCHLQ to write the plot title.
 C
          CALL PLCHLQ (X,Y,
-     1           'Plot of scattered vector data',
-     2           16.,0.,-1.)
+     1           'Scattered vector data',
+     2           16.,0.,0.)
 C
-C Restore the normalization transformation
+C Restore the normalization transformation.
 C
          CALL GSELNT(ICN)
          CALL FRAME
@@ -253,8 +263,124 @@ C
 C
 C =================================================================
 C
-C Modified version of VVUMXY that implements irregular and scattered
-C data mappings
+      SUBROUTINE SCEZEX
+C
+C This routine maps geographically scattered vector data
+C through an Ezmap projection. In order to make the effect of the 
+C projection more obvious the vectors are given uniform direction 
+C and magnitude.
+C  
+      PARAMETER (M=20,N=1)
+      DIMENSION U(M*N), V(M*N)
+C
+C User-defined common block containing X and Y locations of each 
+C vector.
+C
+      PARAMETER (MAXDIM = 100)
+      COMMON /VVUSER/ XCOORD(MAXDIM),YCOORD(MAXDIM)
+      SAVE /VVUSER/
+C
+C All data lies within the region latitude 0.0,90.0 and 
+C and longitude 0.0,100.0.
+C
+      DIMENSION XCLOC(M*N), YCLOC(M*N)
+C
+      DATA XCLOC / 40.0,7.5,20.0,35.0,40.0,65.0,75.0,80.0,90.0,95.0, 
+     +     50.0,80.0,20.0,60.0,15.0,80.0,20.0,45.0,60.0,50.0 /
+      DATA YCLOC / 50.0,80.0,20.0,60.0,15.0,80.0,20.0,45.0,60.0,50.0, 
+     +     40.0,7.5,20.0,35.0,45.0,65.0,75.0,13.0,24.0,32.0 /
+      DATA U / 20 * 1.0 /
+      DATA V / 20 * 1.0 /
+C
+C
+C Specify the NDC coordinates for a plot title.
+C
+        DATA FX / 0.5 /, FY / 0.975 /
+C
+C Copy coordinate values into the VVUSER common block arrays.
+C
+      DO 30 I=1,M*N
+         XCOORD(I) = XCLOC(I)
+ 30   CONTINUE
+C
+      DO 40 I=1,M*N
+         YCOORD(I) = YCLOC(I)
+ 40   CONTINUE
+C
+C Set up a Satellite view EZMAP projection. Draw the map grid only.
+C
+      CALL MAPSET('MA',0.0,0.0,0.0,0.0)
+      CALL MAPROJ('SV',45.0,50.0,0.0)
+      CALL MAPINT
+      CALL MAPGRD
+C
+C Don't let Vectors do a SET call
+C
+      CALL VVSETI('SET - Do-SET-Call Flag', 0)
+C
+C Set the data coordinate boundaries equal to the grid coordinate
+C boundaries (i.e. the range of the array indexes). This is actually
+C the default condition for Vectors, but is included for emphasis here.
+C
+      CALL VVSETR('XC1 -- Lower X Bound', 1.0)
+      CALL VVSETR('XCM -- Upper X Bound', FLOAT(M))
+      CALL VVSETR('YC1 -- Lower Y Bound', 1.0)
+      CALL VVSETR('YCN -- Upper Y Bound', FLOAT(N))
+C
+C Set the MAP parameter to a value indicating a user-defined mapping
+C (anything other than 0, 1, or 2). The routine VVUMXY must be modified
+C to recognize this value for MAP.
+C
+      CALL VVSETI('MAP - user-defined mapping', 5) 
+      CALL VVSETR('LWD - vector linewidth', 2.0) 
+      IDM=0
+      RDM=0.0
+      CALL VVINIT(U,M,V,M,RDM,IDM,M,N,RDM,IDM)
+C
+C Adjust vector sizes.
+C
+      VSZADJ = 0.1
+      CALL VVGETR('DMX - NDC Maximum Vector Size', DMX)
+      CALL GETSET(VL,VR,VB,VT,UL,UR,UB,UT,LL)
+      VRL = VSZADJ * DMX / (VR - VL)
+      CALL VVSETR('VRL - Vector Realized Length', VRL)
+C
+      CALL VVECTR(U,V,RDM,IDM,IDM,RDM)
+      CALL PERIM(1,0,1,0)
+C
+C
+C Save the current normalization transformation then set it to 0.
+C
+      CALL GQCNTN(IERR,ICN)
+      CALL GSELNT(0)
+      X = CFUX(FX)
+      Y = CFUY(FY)
+C
+C Call PLCHLQ to write the plot title.
+C
+      CALL PLCHLQ (X,Y,
+     1     'Scattered vector data mapped through an Ezmap projection',
+     2     16.,0.,0.)
+C
+C Restore the normalization transformation.
+C
+      CALL GSELNT(ICN)
+      CALL FRAME
+C
+ 100  CONTINUE
+C
+      RETURN
+C
+      END
+C
+C =================================================================
+C
+C Modified version of VVUMXY that implements mapping for the 
+C following values of the Vectors 'MAP' internal parameter:
+C 
+C 3 - irregular rectangular grid mapping
+C 4 - scattered data mapping
+C 5 - scattered data mapped through an Ezmap projection.
 C
       SUBROUTINE VVUMXY (X,Y,U,V,UVM,XB,YB,XE,YE,IST)
 C
@@ -292,7 +418,7 @@ C          non-zero value causes VVECTR to discard the vector at this
 C          location
 C
 C The mapping common block: made available to user mapping routines.
-C NOTE: all these variables should be considered read-only by VVUMXY
+C NOTE: all these variables should be considered read-only by VVUMXY.
 C
       COMMON /VVMAP/
      +                IMAP       ,
@@ -337,7 +463,7 @@ C                       viewport width/height to NDC width/height
 C DVMN,DVMX           - min/max vector lengths in NDC
 C RBIG,IBIG           - machine dependent maximum REAL/INTEGER values
 C
-C Math constants
+C Math constants:
 C
       PARAMETER (PDTOR  = 0.017453292519943,
      +           PRTOD  = 57.2957795130823,
@@ -347,13 +473,32 @@ C
      +           P5D2PI = 7.85398163397448) 
 C
 C --------------------------------------------------------------------
-C User-defined common block
+C User-defined common block:
 C
       PARAMETER (MAXDIM = 100)
 C
       COMMON /VVUSER/ XCOORD(MAXDIM), YCOORD(MAXDIM)
       SAVE /VVUSER/
 C --------------------------------------------------------------------
+C Local parameters (used for the Ezmap projection only):
+C
+C PRCFAC - Precision factor used to resolve float equality within
+C            the precision of a 4 byte REAL
+C PVFRAC - Initial fraction of the vector magnitude used to
+C            determine the differential increment
+C PFOVFL - Floating point overflow value
+C IPMXCT - Number of times to allow the differential to increase
+C PDUVML - Multiplier when the differential is increased
+C PCSTST - Test value for closeness to 90 degree latitude
+C
+      PARAMETER (PRCFAC=1E5,
+     +           PVFRAC=0.001,
+     +           PFOVFL=1E12,
+     +           IPMXCT=40,
+     +           PDUVML=2.0,
+     +           IPCTST=PRCFAC*90)
+C
+C ---------------------------------------------------------------------
 C
       IF (IMAP .EQ. 3) THEN
 C
@@ -396,6 +541,8 @@ C
          XE=XB+U*SXDC
          YE=YB+V*SYDC
 C
+C ---------------------------------------------------------------------
+C
       ELSE IF (IMAP .EQ. 4) THEN
 C
 C Mapping for scattered vector data.
@@ -425,14 +572,113 @@ C
          XE=XB+U*SXDC
          YE=YB+V*SYDC
 C
+C ---------------------------------------------------------------------
+C
+      ELSE IF (IMAP .EQ. 5) THEN
+C
+C Mapping for scattered vector data projected through Ezmap. XCOORD and
+C YCOORD contain the Longitude and Latitude respectively of each vector
+C datum. 
+C  
+C 
+         I = NINT(X)
+         J = NINT(Y)
+C
+         IF (I.LT.1 .OR. I.GT.NXCT .OR. J.LT.1 .OR. J.GT.NYCT) THEN
+            IST = -1
+            RETURN
+         END IF
+C
+C Since XCOORD and YCOORD are actually single dimensional arrays,
+C convert the 2-d indexes supplied to VVUMXY into their 1-d equivalent 
+C to index into the coordinate arrays.
+C
+         XT = XCOORD(NXCT*(J-1)+I)
+         YT = YCOORD(NXCT*(J-1)+I)
+C
+C The following code is adapted from the Ezmap projection code in 
+C VVMPXY. An iterative technique is used that handles most vectors 
+C arbitrarily close to the projection limb.
+C XT is longitude, YT is latitude.
+C
+C Test for 90 degree latitude.
+C
+         IF (IFIX(ABS(YT)*PRCFAC+0.5).EQ.IPCTST) THEN
+            IST=-1
+            RETURN
+         END IF
+C
+C Project the starting value: bail out if outside the window
+C
+         CALL MAPTRN (YT,XT,XB,YB)
+         IF (XB .LT. WXMN .OR. XB .GT. WXMX .OR.
+     +       YB .LT. WYMN .OR. YB .GT. WYMX) THEN
+            IST=-5
+            RETURN
+         END IF
+C
+C Check the vector magnitude
+C
+         IF (IFIX(UVM*PRCFAC+0.5) .EQ. 0) THEN
+            IST=-2
+            RETURN
+         END IF
+C
+C The incremental distance is proportional to a small fraction
+C of the vector magnitude
+C
+         DUV=PVFRAC/UVM
+         CLT=COS(Y*PDTOR)
+C
+C Project the incremental distance. If the positive difference doesn't
+C work, try the negative difference. If the difference results in a
+C zero length vector, try a number of progressively larger increments.
+C
+         ICT=0
+         SGN=1.0
+ 20      CONTINUE
 
+         CALL MAPTRN(YT+SGN*V*DUV,XT+SGN*U*DUV/CLT,XT,YT)
+
+         DV1=SQRT((XT-XB)*(XT-XB)+(YT-YB)*(YT-YB))
+         IF (DV1 .GT. RLEN) THEN
+            IF (SGN .EQ. -1.0) THEN
+               IST=-4
+               RETURN
+            ELSE
+               SGN=-1.0
+               GO TO 20
+            END IF
+         END IF
+C
+         IF (IFIX(DV1*PRCFAC) .EQ. 0) THEN
+            IF (ICT .LT. IPMXCT) THEN
+               ICT = ICT + 1
+               DUV=DUV*PDUVML
+               GO TO 20
+            ELSE
+               IST=-3
+               RETURN
+            END IF
+         END IF
+C
+         IF (ABS(XT) .GE. PFOVFL .OR. ABS(YT) .GE. PFOVFL) THEN
+            IST=-6
+            RETURN
+         END IF
+C
+         T=SGN*((XT-XB)/DV1)*UVM
+         XB=CUFX(XB)
+         XE=XB+T*SXDC
+         T=SGN*((YT-YB)/DV1)*UVM
+         YB=CUFY(YB)
+         YE=YB+T*SYDC
+C
+C ---------------------------------------------------------------------
+C
       ELSE
 C
 C Default mapping:
-C
-C Note: this code corrects the default version of VVUMXY included with
-C NCAR Graphics 3.2 incorrectly fails to convert the input X and Y
-C location into fractional coordinates (NDC space).
 C
 C WXMN, WXMX, WYMN, and WYMX contain the minimum and maximum values of
 C the user coordinate space. Somewhat inaccurately, the mmenomic 'W'
