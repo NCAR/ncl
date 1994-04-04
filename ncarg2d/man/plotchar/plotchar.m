303d65620ff898cac20cc9c829f6b5b4669fb375
kennison <kennison>
1994-04-04 23:53:56 +0000

    THIS IS FOR 3.2.1.
    Updated the "man" page to include the latest list of error messages.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@1945 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/plotchar/plotchar.m
+++ ncarg2d/man/plotchar/plotchar.m
@@ -115,118 +115,106 @@ the Plotchar
 C-binding routines load the NCAR Graphics libraries ncargC, ncarg_gksC, 
 ncarg, ncarg_gks, ncarg_c, and ncarg_loc, preferably in that order.
 .SH MESSAGES
-The following error messages may be written by Plotchar.
-They are written by means of a call to the error-handling
-support routine SETER; in all cases, the final argument in
-the call says that the error is fatal and that execution
-should be terminated.
+Various error conditions can occur in Plotchar.  Each of these results in
+a call to the error-handling routine SETER, with a final argument indicating
+that the error is recoverable; by default, an error message is printed and
+execution is terminated, but, if you turn on error recovery
+(as described in the "man" page for "error_handling"), you
+can get control back.
+.sp
+The error messages are as follows; all should be
+more or less self-explanatory.
 .sp
 .in +5
-PLCHHQ - DATASET NOT LOADED CORRECTLY
-.in +5
-.sp
-Probably indicates an error in the implementation of
-Plotchar. See a consultant.
-.sp
-.in -5
-PLCHHQ - INTERNAL LOGIC ERROR (NDGU = 0) - SEE CONSULTANT
-.br
-PLCHHQ - INTERNAL LOGIC ERROR (XCRA/YCRA TOO SMALL) - SEE
-CONSULTANT
-.br
-PLCHHQ - INTERNAL LOGIC ERROR (NPCS TOO BIG) - SEE
-CONSULTANT
-.br
-PLCHHQ - INTERNAL LOGIC ERROR (NCRA TOO BIG) - SEE
-CONSULTANT
-.br
-PLCHHQ - INTERNAL LOGIC ERROR (XCP1 OR XCP2 = OORV - SEE
-CONSULTANT
-.in +5
-.sp
-An "impossible" situation has arisen. Indicates a compiler
-problem, core clobbering, an error in the implementation or
-installation of Plotchar, and/or the presence of unfriendly
-gremlins. See a consultant.
-.sp
-.in -5
-PLCHHQ - ERROR EXIT FROM GQPLCI
-.br
-PLCHHQ - ERROR EXIT FROM GQFACI
-.br
-PLCHHQ - ERROR EXIT FROM GQTXCI
-.br
-PLCHHQ - ERROR EXIT FROM GQLWSC
-.br
-PLCHHQ - ERROR EXIT FROM GQFAIS
-.sp
-.in +5
-One of the GKS "query" routines has returned an error flag.
-This may mean that GKS is not in the proper state.
-.sp
-.in -5
 PCDLSC - FIRST COLOR INDEX IS LESS THAN ZERO
-.sp
-.in +5
-PCDLSC has been called with an argument whose value is less
-than zero, which is illegal.
-.sp
-.in -5
+.br
+PCDLSC - UNCLEARED PRIOR ERROR
+.br
+PCGETC - UNCLEARED PRIOR ERROR
+.br
 PCGETC - UNRECOGNIZED PARAMETER NAME
+.br
+PCGETI - UNCLEARED PRIOR ERROR
+.br
+PCGETR - BOX COLOR ARRAY INDEX IS OUT OF RANGE
+.br
+PCGETR - COLOR ARRAY INDEX IS OUT OF RANGE
+.br
+PCGETR - UNCLEARED PRIOR ERROR
 .br
 PCGETR - UNRECOGNIZED PARAMETER NAME
 .br
-PCSETC - UNRECOGNIZED PARAMETER NAME
+PCHIQU - UNCLEARED PRIOR ERROR
 .br
-PCSETR - UNRECOGNIZED PARAMETER NAME
-.sp
-.in +5
-The parameter name used is not recognized. Since PCGETI is
-implemented using a call to PCGETR, the second error
-message above may result from a call to PCGETI. Similarly,
-since PCSETI is implemented using a call to PCSETR, the
-fourth error message may result from a call to PCSETI.
-.sp
-.in -5
-PCGETR - BOX COLOR ARRAY INDEX IS OUT OF RANGE
+PCLOQU - UNCLEARED PRIOR ERROR
 .br
-PCSETR - BOX COLOR ARRAY INDEX IS OUT OF RANGE
-.sp
-.in +5
-The parameter name begins with \'BC\' and contains a
-subscript that is outside the range 1 to 3, which is the
-legal range for that internal parameter.
-.sp
-.in -5
-PCGETR - COLOR ARRAY INDEX IS OUT OF RANGE
-.br
-PCSETR - COLOR ARRAY INDEX IS OUT OF RANGE
-.sp
-.in +5
-The parameter name begins with \'CC\' and contains a
-subscript that is outside the range from 0 to 16, which is
-the legal range for that internal parameter.
-.sp
-.in -5
-PCPNWI - PARAMETER NAME TOO SHORT
+PCMEQU - UNCLEARED PRIOR ERROR
 .br
 PCPNWI - INTERNAL ERROR - SEE CONSULTANT
-.sp
-.in +5
-PCPNWI is an internal routine that examines a parameter
-name string for an embedded subscript. Both of these error
-messages indicate a problem with such a parameter name.
-.sp
-.in -5
+.br
+PCPNWI - PARAMETER NAME TOO SHORT
+.br
+PCSETC - UNCLEARED PRIOR ERROR
+.br
 PCSETC - UNRECOGNIZED FONT NAME
-.sp
-.in +5
-If PCSETC is called with first argument \'FN\', the second
-argument must be the name of a font to be used. The name of
-the font must be spelled exactly as given in Table 2, "Font
-Numbers vs. Font Names", above. In particular, the name
-must be given in upper case.
+.br
+PCSETC - UNRECOGNIZED PARAMETER NAME
+.br
+PCSETI - UNCLEARED PRIOR ERROR
+.br
+PCSETR - BOX COLOR ARRAY INDEX IS OUT OF RANGE
+.br
+PCSETR - COLOR ARRAY INDEX IS OUT OF RANGE
+.br
+PCSETR - UNCLEARED PRIOR ERROR
+.br
+PCSETR - UNRECOGNIZED PARAMETER NAME
+.br
+PLCHHQ - DATASET NOT LOADED CORRECTLY
+.br
+PLCHHQ - ERROR EXIT FROM GQCNTN
+.br
+PLCHHQ - ERROR EXIT FROM GQFACI
+.br
+PLCHHQ - ERROR EXIT FROM GQFAIS
+.br
+PLCHHQ - ERROR EXIT FROM GQLWSC
+.br
+PLCHHQ - ERROR EXIT FROM GQNT
+.br
+PLCHHQ - ERROR EXIT FROM GQPLCI
+.br
+PLCHHQ - ERROR EXIT FROM GQTXCI
+.br
+PLCHHQ - INTERNAL LOGIC ERROR (NCRA TOO BIG) - SEE CONSULTANT
+.br
+PLCHHQ - INTERNAL LOGIC ERROR (NDGU = 0) - SEE CONSULTANT
+.br
+PLCHHQ - INTERNAL LOGIC ERROR (NPCS TOO BIG) - SEE CONSULTANT
+.br
+PLCHHQ - INTERNAL LOGIC ERROR (XCP1 OR XCP2 = OORV) - SEE CONSULTANT
+.br
+PLCHHQ - INTERNAL LOGIC ERROR (XCRA/YCRA TOO SMALL) - SEE CONSULTANT
+.br
+PLCHHQ - UNCLEARED PRIOR ERROR
+.br
+PLCHLQ - ERROR EXIT FROM GQCHH
+.br
+PLCHLQ - ERROR EXIT FROM GQCHUP
+.br
+PLCHLQ - ERROR EXIT FROM GQCNTN
+.br
+PLCHLQ - ERROR EXIT FROM GQNT
+.br
+PLCHLQ - ERROR EXIT FROM GQTXAL
+.br
+PLCHLQ - ERROR EXIT FROM GQTXP
+.br
+PLCHLQ - UNCLEARED PRIOR ERROR
+.br
+PLCHMQ - UNCLEARED PRIOR ERROR
 .in -5
+.sp
 .SH SEE ALSO
 Online:
 plotchar_params,
