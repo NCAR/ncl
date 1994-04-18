5fee543104d68e19c213467404579b5f6d3b02f2
kennison <kennison>
1994-04-18 16:58:53 +0000

    I have updated this "man" page to give thumbnail descriptions of the three new routines that I added last week.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2019 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/ngmisc/ngmisc.m
+++ ncarg2d/man/ngmisc/ngmisc.m
@@ -7,11 +7,20 @@ Ngmisc - A collection of miscellaneous NCAR Graphics routines.
 NGDOTS - Draws filled circular dots at coordinate positions
 (X(I),Y(I),I=1,NUM) at size SIZE with color given by the color index ICOLOR.
 .sp
+NGGCOG - Returns the latitudes and longitudes of a set of points approximating
+a circle at a given point on the surface of the globe.
+.sp
+NGGSOG - Returns the latitudes and longitudes of six points defining a
+five-pointed star at a given point on the surface of the globe.
+.sp
 NGPICT - Effects a break in the picture drawing sequence in a FORTRAN
 code using GKS.  The actions taken depend on whether the designated
 workstation is a metafile or an output/input workstation.  An option
 is provided for prompting the user when an output/input workstation is
 ready and waiting after a pause.
+.sp
+NGRITD - Given the coordinates of a point, this routine performs a rotation
+of that point about a specified axis by a specified angle.
 .sp
 NGWSYM - Draws a symbol from the standard WMO/NOAA meteorological
 fonts by reference to the font name and symbol number within that
@@ -19,7 +28,13 @@ font.
 .SH C-BINDING SYNOPSIS
 c_ngdots
 .br
+c_nggcog
+.br
+c_nggsog
+.br
 c_ngpict
+.br
+c_ngritd
 .br
 c_ngwsym
 .SH USER-MODIFIABLE INTERNAL ROUTINES
@@ -32,7 +47,10 @@ and ncarg_loc, preferably in that order.
 .SH SEE ALSO
 Online:
 NGDOTS(3NCARG),
+NGGCOG(3NCARG)
+NGGSOG(3NCARG)
 NGPICT(3NCARG),
+NGRITD(3NCARG)
 NGWSYM(3NCARG)
 .SH COPYRIGHT
 Copyright 1987, 1988, 1989, 1991, 1993 University Corporation
