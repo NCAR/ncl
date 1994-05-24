e965a47c95eb9a969e96bb859d3927d8b88d51cf
Mary Haley <haley@ucar.edu>
1994-05-24 13:46:06 +0000

    Updated man page.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2147 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/ncargcex.m
+++ ncarg2d/man/other/ncargcex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargcex.m,v 1.5 1994-03-24 18:30:47 haley Exp $
+.\"	$Id: ncargcex.m,v 1.6 1994-05-24 13:46:06 haley Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGCEX 1NCARG "January 1993" NCAR "NCAR GRAPHICS"
@@ -8,16 +8,25 @@ ncargcex \- NCAR Graphics C Examples
 .SH SYNOPSIS
 \fBncargcex\fP 
 [\fB\-all, -A\fR]
+[\f\B\-autograph\fR]
+[\f\B\-bivar\fR]
+[\f\B\-conpack\fR]
+[\f\B\-ezmap\fR]
+[\f\B\-gks\fR]
+[\f\B\-labelbar\fR]
+[\f\B\-plotchar\fR]
+[\f\B\-scrolled_title\fR]
+[\f\B\-softfill\fR]
+[\f\B\-inter\fR]
 [\fB\-clean\fR]
 [\fB\-n\fR]
 [\fB\-noX11\fR]
 [\fB\-onebyone\fR]
 \fBname ...\fR
 .SH DESCRIPTION
-.LP
 .I ncargcex
 provides the user with access to C example source code that
-uses the NCAR Graphics C-bindings.  These examples correspond
+uses the NCAR Graphics C-bindings.  Some of these examples correspond
 with the Fortran examples that you can get with the \fIncargex\fP
 command.  Not every Fortran example is included here, however.
 These examples should be enough to get one started in learning
@@ -45,66 +54,85 @@ NCGM file be left in your directory and that all other files
 created by \fIncargcex\fP be deleted.
 The argument \fIname\fP may be
 selected from the lists that appear below.
-.LP
+.sp
 .I OPTIONS
-.LP
+.sp
 .IP "\-all, \-A"
 Generate all available examples.
-.LP
+.sp
+.IP "\-autograph\fR"
+Generate all autograph examples.
+.sp
+.IP "\-bivar\fR"
+Generate all bivar examples.
+.sp
+.IP "\-conpack\fR"
+Generate all conpack examples.
+.sp
+.IP "\-ezmap\fR"
+Generate all ezmap examples.
+.sp
+.IP "\-gks\fR"
+Generate all gks examples.
+.sp
+.IP "\-labelbar\fR"
+Generate all labelbar examples.
+.sp
+.IP "\-plotchar\fR"
+Generate all plotchar examples.
+.sp
+.IP "\-scrolled_title\fR"
+Generate all scrolled examples.
+.sp
+.IP "\-softfill\fR"
+Generate all softfill examples.
+.sp
 .IP \-clean " " ""
 Remove everything but the ".ncgm" file.
-.LP
+.sp
 .IP \-n " " ""
 Specifies that the example should not be compiled, linked, or run.
-.LP
+.sp
 .IP \-noX11 " " ""
 Do not link in the X library when linking the selected examples.
-.LP
+.sp
 .IP \-onebyone " " ""
 Specifies that the selected examples should be generated one
 at a time and viewed as they are generated.  This is intended for use during
 testing of new releases at NCAR.
-.LP
+.sp
 .I "EXAMPLES AVAILABLE"
-.LP
-.I "AUTOGRAPH Example:"
-.nf
+.sp
+.I "AUTOGRAPH Examples:"
 	c_agex07
-.fi
-.LP
-.I "EZMAP Examples:"
-.nf
-	c_mpex05 c_eezmpa
-.fi
-.LP
-.I "CONPACK Example:"
-.nf
+.sp
+.I "BIVAR Examples:"
+	c_cbex01
+.sp
+.I "CONPACK Examples:"
 	c_colcon
-.fi
-.LP
-.I "LABELBAR Example:"
-.nf
-	c_elblba
-.fi
-.LP
-.I "SOFTFILL Example:"
-.nf
-	c_sfex02
-.fi
-.LP
-.I "PLOTCHAR Example:"
-.nf
-	c_epltch
-.fi
-.LP
+.sp
+.I "EZMAP Examples:"
+	c_eezmpa c_mpex05
+.sp
 .I "GKS Examples:"
-.nf
-	c_gtxpac c_xwndws
-.fi
-.LP
-.I "Miscellaneous Examples:"
-.nf
-	c_cbex01 c_slex01
+	c_gtxpac
+.sp
+.I "LABELBAR Examples:"
+	c_elblba
+.sp
+.I "PLOTCHAR Examples:"
+	c_epltch
+.sp
+.I "SCROLLED TITLE Examples:"
+	c_slex01
+.sp
+.I "SOFTFILL Examples:"
+	c_sfex02
+.sp
+.I "Interactive Examples:"
+	c_xwndws
+.sp
 .fi
 .SH SEE ALSO
 Online:
@@ -117,7 +145,7 @@ Online:
 Hardcopy:
 NCAR Graphics Fundamentals, UNIX Version
 .SH COPYRIGHT
-Copyright 1987, 1988, 1989, 1991, 1993 University Corporation
+Copyright 1987, 1988, 1989, 1991, 1993, 1994 University Corporation
 for Atmospheric Research
 .br
 All Rights Reserved
