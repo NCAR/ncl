9bf3b75903bda6dcd77a5ece44d496d02b08e0ff
Mary Haley <haley@ucar.edu>
1994-08-11 18:37:35 +0000

    Wrapped new NGCALLF macro around Fortran subroutine names.  Removed reference to ncarg_loc.h.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2425 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/libncarg/plotchar/bofred.c
@@ -0,0 +1,47 @@
+/*
+ * $Id: bofred.c,v 1.1 1994-08-11 18:37:33 haley Exp $
+ */
+
+#include <stdio.h>
+#include <ncarg/c.h>
+
+/* Procedure bofred_(unit, fnum, ios, status)
+ *
+ *	bofred_ creates a fontname using the index "fnum" and opens
+ *	the file in either the default library location, or locally.
+ *	The file descriptor is returned to the FORTRAN caller in
+ *	"unit", which simply passes it to other
+ *	underlying C routines so FORTRAN never has cause to become
+ *	upset because it is not actually a Logical Unit. The return
+ *	code, "status", is not 0 if an error occurred, and 0 if
+ *	all went well. The I/O status, "ios", is not used, but
+ *	it is set equal to "status" so that it is used and
+ *	compilers don't complain.
+ */
+NGCALLF(bofred,BOFRED)(unit, fnum, ios, status)
+	int		*unit, *fnum, *ios, *status;
+{
+	int		fd;
+	char	fontname[32];
+	char	*pathname;
+
+	/* Get the path name to the fontcap file using the font number */
+
+	(void) sprintf(fontname, "font%d", *fnum);
+
+	if ( (pathname = getFcapname(fontname)) == (char *) NULL)
+	{
+		(void) fprintf(stderr, 
+		"Error in bofred_(): Could not find <%s>\n", fontname);
+		*status = 1;
+		*unit = -1;
+	}
+	else
+		if ( (fd = open(pathname, 0)) == -1)
+			*status = 1;
+		else
+			*status = 0;
+
+	*unit = fd;
+	*ios = *status;
+}
---
+++ ncarg2d/src/libncarg/plotchar/pcbnrd.c
@@ -0,0 +1,55 @@
+/*
+ * $Id: pcbnrd.c,v 1.1 1994-08-11 18:37:34 haley Exp $
+ */
+
+#include <stdio.h>
+#include <ncarg/c.h>
+
+/***********************************************************************
+*
+*       Procedure:      pcbnrd_() (Fortran-callable)
+*       Revision:       11/1/89
+*       Purpose:        To read from an unformatted file,
+*                       opened by BOPRED, into a FORTRAN
+*                       INTEGER array.
+*
+*       Input Variables:
+*
+*       unit -  A UNIX file-descriptor, but passed
+*               in from a calling FORTRAN procedure.
+*       count - The number of integers to read. On most 16 and
+*               32 bit systems, integers are 32 bits, and on
+*               the Cray system, all integers are 64 bits, except
+*               when compiling with Cray's Fortran 77 compiler
+*               which uses 48 bit integers. Since this C function
+*               is unlikely to be aware of what Fortran 77 is doing,
+*               all code should be compiled for 64 bit arithmetic.
+*
+*
+*       Output Variables:
+*
+*       buffer - The integer buffer to read the data into.
+*
+*       ios:    The I/O status - valid only if "status"
+*               is non-zero.
+*
+*       status: Success status. 0 if good; non-zero if bad.
+*
+*       Warning: This procedure may be rewritten at install time
+*       to reflect the C/FORTRAN calling characteristics of
+*       the compilers in use.
+*
+***********************************************************************/
+NGCALLF(pcbnrd,PCBNRD)(unit, count, buffer, ios, status)
+	int		*unit, *count, buffer[], *ios, *status;
+{
+	int		nbytes, nbread;
+
+	nbytes = *count * sizeof(int);
+
+	nbread = read( *unit, (char *) buffer, nbytes);
+
+	*status = 0;
+
+	*ios = *status;
+}
