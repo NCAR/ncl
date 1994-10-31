0b64a87a4ece9e49b6d8dd7901768b9d0e228acd
Mary Haley <haley@ucar.edu>
1994-10-31 04:08:17 +0000

    No longer need .sed files.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2624 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/examplesC/streamlines/c_ffex03.c
@@ -0,0 +1,304 @@
+/*
+ *  $Id: c_ffex03.c,v 1.1 1994-10-31 04:08:15 haley Exp $
+ */
+#include <stdio.h>
+#include <math.h>
+
+#include <ncarg/ncargC.h>
+#include <ncarg/gks.h>
+
+#define WSTYPE SED_WSTYPE
+#define WKID   1
+
+#define MSIZE   36
+#define NSIZE   33
+#define ISTWKL  2*MSIZE*NSIZE
+#define ICPIWL  1000
+#define ICPRWL  5000
+#define IAREAL  20000
+#define MCRA    64
+#define NOGI    64
+#define NCLRS   3
+
+/*
+ * Define a set of rgb color triples
+ */
+int iclr[NCLRS];
+
+float u[NSIZE][MSIZE], v[NSIZE][MSIZE], p[NSIZE][MSIZE];
+
+main()
+{
+	int i, iclrix, nclv, idm;
+	float xdm;
+    float p1[2],p2[2],p3[2],p4[2];
+/*
+ * This program requires the input data file 'ffex02.dat'
+ * It reads the data from standard input, e.g.: ffex03 < ffex02.dat
+ */
+	float wrk[ISTWKL];
+/*
+ * Conpack work space and Areas area map
+ */
+	float rwrk[ICPRWL];
+	int iwrk[ICPIWL], iam[IAREAL];
+/*
+ * Arrays for drawing masked grids
+ */
+	float xcra[MCRA],ycra[MCRA];
+	int iaai[NOGI],iagi[NOGI];
+/*
+ * External subroutine declarations
+ */
+	extern int drawcl();
+	extern int NGCALLF(stumsl,STUMSL)();
+	extern void rddata();
+    extern void setcgt(), setcla();
+	Gcolr_rep rgb[NCLRS];
+
+	rgb[0].rgb.red = 1.0; rgb[0].rgb.green = 0.0; rgb[0].rgb.blue = 0.0;
+	rgb[1].rgb.red = 0.0; rgb[1].rgb.green = 0.0; rgb[1].rgb.blue = 1.0;
+	rgb[2].rgb.red = 0.5; rgb[2].rgb.green = 0.5; rgb[2].rgb.blue = 0.5;
+/*
+ *  Open gks, open and activate a workstation.
+ */
+    gopen_gks("stdout",0);
+    gopen_ws(WKID, NULL, WSTYPE);
+    gactivate_ws(WKID);
+/*
+ * Read the input array data
+ */
+	rddata();
+/*
+ * Set up colors for fixed table grayscale and color workstations
+ * colors allocated as follows: 1, map outline; 2, contour lines;
+ *  3, grid lines; (streamlines and labels use default black or white.
+ */
+	for( i = 1; i <= NCLRS; i++ ) {
+		iclrix=64+(i-1)*196/NCLRS;
+		gset_colr_rep(WKID,iclrix,&rgb[i-1]);
+		iclr[i-1] = iclrix;
+	}
+/*
+ * Set up the ezmap transformation
+ */
+    p1[0] = 60.;
+    p2[0] = -120.;
+    p3[0] = 23.;
+    p4[0] = -60.;
+    p1[1] = p2[1] = p3[1] = p4[1] = 0.;
+	c_mapset("co",p1,p2,p3,p4);
+	c_maproj("lc",0.0,-75.0,45.0);
+	c_mapint();
+	c_mapsti("c5 - continental outline color",iclr[0]);
+/*
+ * Tell streamlines to use the mapping established by ezmap
+ */
+	c_stseti("map -- mapping flag", 1);
+	c_stseti("set -- set call flag", 0);
+/*
+ * Set up data coordinate boundaries and special value processing 
+ * appropriately for the dataset 
+ */
+	c_stsetr("xc1 -- lower x bound", -140.0);
+	c_stsetr("xcm -- upper x bound", -52.5);
+	c_stsetr("yc1 -- lower x bound", 20.0)   ;
+	c_stsetr("ycn -- upper y bound", 60.0);
+
+	c_stseti("svf -- special values flag", 1);
+	c_stsetr("usv -- u special value", -9999.0);
+	c_stsetr("vsv -- v special value", -9999.0);
+/*
+ * Do the equivalent Conpack setup; note that the special value
+ * parameter works a bit differently, and also conpack requires
+ * an out of range value to be set whenever the data grid extends
+ * outside the map boundaries. The standard value for the default
+ * version of cpmpxy is 1.0e12
+ */
+    c_cpseti("map -- mapping flag", 1);
+    c_cpseti("set -- set call flag", 0);
+    c_cpsetr("xc1 -- lower x bound", -140.0);
+    c_cpsetr("xcm -- upper x bound", -52.5);
+    c_cpsetr("yc1 -- lower x bound", 20.0)   ;
+    c_cpsetr("ycn -- upper y bound", 60.0);
+    c_cpsetr("spv -- special value",-9999.0);
+    c_cpsetr("orv -- out of range value",1.0e12);
+/*
+ * Set conpack graphics text parameters
+ */
+    setcgt();
+/*
+ * Draw the continental outline using a wide line
+ */
+    gset_linewidth(4.0);
+    c_maplot();
+    gset_linewidth(1.0);
+/*
+ * Initialize the drawing of the contour plot, and tell conpack
+ * to pick contour levels.
+ */
+    c_cprect ((float *)p,MSIZE,MSIZE,NSIZE,rwrk,ICPRWL,iwrk,ICPIWL);
+    c_cppkcl ((float *)p,rwrk,iwrk);
+/*
+ * Set up contour line attributes
+ */
+    c_cpgeti("ncl - number of contour levels",&nclv);
+    setcla(nclv);
+/*
+ * Initialize the area map, add the conpack labels to the area map,
+ * then draw the labels, draw masked contour lines, and finally
+ * draw the masked map grids. Note that there is currently no way
+ * to draw a masked continental outline.
+ */
+    c_arinam (iam,IAREAL);
+    c_cplbam ((float *)p,rwrk,iwrk,iam);
+    c_cplbdr ((float *)p,rwrk,iwrk);
+    c_cpcldm ((float *)p,rwrk,iwrk,iam,drawcl);
+    c_mapsti("c2 - grid",iclr[2]);
+    gset_line_colr_ind(iclr[2]);
+    c_mapgrm (iam,xcra,ycra,MCRA,iaai,iagi,NOGI,drawcl);
+    gset_line_colr_ind(1);
+/*
+ * Adjust streamline rendering options and turn on statistics
+ */
+    c_stsetr("lwd -- streamline line width",1.75);
+    c_stseti("msk -- streamline masking",1);
+    c_stsetr("ssp -- stream spacing", 0.012);
+    c_stsetr("dfm -- differential magnitude", 0.012);
+    c_stseti("sst -- streamline statistics", 1);
+/*
+ * Initialize streamlines
+ */
+    c_stinit ((float *)u,MSIZE,(float *)v,MSIZE,&xdm,idm,MSIZE,NSIZE,wrk,ISTWKL);
+/*
+ * Draw the streamline field plot
+ */
+    c_stream ((float *)u,(float *)v,&xdm,iam,NGCALLF(stumsl,STUMSL),wrk);
+/*
+ * Draw a perimeter boundary and eject the frame
+ */
+	c_perim(1,0,1,0);
+	c_frame();
+/*
+ * Deactivate and close workstation, close gks.
+ */
+    gdeactivate_ws (WKID);
+    gclose_ws(WKID);
+    gclose_gks();
+}
+
+void rddata()
+{
+	char ch;
+	int i, j;
+/*
+ * Read the data arrays from the standard input 
+ */
+	for( j = 0; j < NSIZE; j++ ) {
+		for( i = 0; i < MSIZE; i++ ) {
+			fscanf( stdin, "%g", &u[j][i]);
+			fscanf( stdin, "%c", &ch );
+		}
+	}
+	for( j = 0; j < NSIZE; j++ ) {
+		for( i = 0; i < MSIZE; i++ ) {
+			fscanf( stdin, "%g", &v[j][i]);
+			fscanf( stdin, "%c", &ch );
+		}
+	}
+	for( j = 0; j < NSIZE; j++ ) {
+		for( i = 0; i < MSIZE; i++ ) {
+			fscanf( stdin, "%g", &p[j][i]);
+			fscanf( stdin, "%c", &ch );
+		}
+	}
+	return;
+}
+
+int drawcl (xcs,ycs,ncs,iai,iag,nai)
+float *xcs, *ycs;
+int *ncs, *iai, *iag, *nai;
+{
+/*
+ * Routine for masked drawing of contour and grid lines
+ *
+ * This version of drawcl draws the polyline defined by the points
+ * ((xcs(i),ycs(i)),i=1,ncs) if and only if none of the area identifiers
+ * for the area containing the polyline are negative.  The dash package
+ * routine curved is called to do the drawing.
+ *
+ * Turn on drawing.
+ */
+	int i, idr;
+
+	idr = 1;
+/*
+ * If any area identifier is negative, turn off drawing.
+ */
+	for( i = 0; i < *nai; i++ ) {
+		if (iai[i] < 0) idr=0;
+	}
+/*
+ * If drawing is turned on, draw the polyline.
+ */
+	if (idr) c_curved (xcs,ycs,*ncs);
+/*
+ * Done.
+ */
+	return(0);
+}
+
+void setcgt()
+{
+/*
+ * Sets conpack graphics text parameters
+ */
+	c_cpseti ("llp - line label positioning",3);
+	c_cpseti ("rwc - real workspace for contours",200);
+	c_cpseti ("hlb - high/low label box flag",1);
+	c_cpseti ("llb - high/low label box flag",0);
+	c_cpsetc ("ilt - information label text"," ");
+	c_cpseti ("hlo - high/low label overlap flag",11);
+	c_cpsetr ("cwm - character width multiplier",1.25);
+
+	return;
+}
+
+void setcla (nclv)
+int nclv;
+{
+/*
+ * Sets contour line attributes
+ */
+	int iclu, iclv;
+
+	for( iclv = 1; iclv <= nclv; iclv++ ) {
+		c_cpseti ("pai - parameter array index",iclv);
+		c_cpgeti ("clu - contour level use flag",&iclu);
+		c_cpseti ("cll - contour-line line width",3);
+		c_cpseti ("clc - contour-line color", iclr[1]);
+		if (iclu == 3) {
+            c_cpseti ("cll - contour-line line width",6);
+		}
+	}
+/*
+ * "Special" contour lines - grid, special values, and out of range
+ * boundaries
+ */
+	c_cpseti ("pai - parameter array index",-1);
+	c_cpseti ("clu - contour level use flag",1);
+	c_cpsetr ("cll - contour level line width",2.);
+	c_cpseti ("clc - contour level line color",iclr[2]);
+
+	c_cpseti ("pai - parameter array index",-2);
+	c_cpseti ("clu - contour level use flag",1);
+	c_cpsetr ("cll - contour level line width",2.);
+	c_cpseti ("clc - contour level line color",iclr[2]);
+
+	c_cpseti ("pai - parameter array index",-3);
+	c_cpseti ("clu - contour level use flag",1);
+	c_cpsetr ("cll - contour level line width",2.);
+	c_cpseti ("clc - contour level line color",iclr[2]);
+
+	return;
+}
---
+++ ncarg2d/src/examplesC/streamlines/c_stex01.c
@@ -0,0 +1,132 @@
+/*
+ *  $Id: c_stex01.c,v 1.1 1994-10-31 04:08:16 haley Exp $
+ */
+#include <stdio.h>
+#include <ncarg/ncargC.h>
+#include <ncarg/gks.h>
+
+#define M 20
+#define N 36
+
+#define WSTYPE SED_WSTYPE
+#define WKID   1
+
+main()
+{
+    float a[N][M], b[N][M], wrk[2*M*N], *p, rval;
+    int i, j, *idm, lp, ival;
+    extern int NGCALLF(stumsl,STUMSL)();
+	extern void dfclrs();
+/*
+ *     open gks, open workstation, activate workstation.
+ */
+    gopen_gks("stdout",0);
+    gopen_ws(WKID, NULL, WSTYPE);
+    gactivate_ws(WKID);
+
+/*
+ * Define a GKS color table
+ */
+	dfclrs();
+/*
+ * Do the set call, set the mapping mode and data coordinate
+ * boundaries appropriately for polar coordinate mapping
+ */
+    c_set (0.05,0.95,0.05,0.95,-20.0,20.0,-20.0,20.0,1);
+    c_stseti("MAP -- mapping mode", 2);
+    c_stseti("SET -- do set call", 0);
+    c_stsetr("XC1 -- lower x bound", 1.0);
+    c_stsetr("XCM -- upper x bound", 20.0);
+    c_stsetr("YC1 -- lower y bound", 0.0);
+    c_stsetr("YCN -- upper y bound", 360.0);
+    for( i = 0; i < N; i++ ) {
+        for( j = 0; j < M; j++ ) {
+            a[i][j] = 1.0;
+            b[i][j] = 0.;
+        }
+    }
+
+    c_stinit((float *)a,M,(float *)b,M,p,lp,M,N,wrk,2*M*N);
+    gset_line_colr_ind(7);
+    c_stream((float *)a,(float *)b,p,idm,NGCALLF(stumsl,STUMSL),wrk);
+    gset_line_colr_ind(2);
+    for( i = 0; i < N; i++ ) {
+        for( j = 0; j < M; j++ ) {
+            a[i][j] = 0.;
+            b[i][j] = -1.;
+        }
+    }
+    c_stinit((float *)a,M,(float *)b,M,p,lp,M,N,wrk,2*M*N);
+    c_stream((float *)a,(float *)b,p,idm,NGCALLF(stumsl,STUMSL),wrk);
+    c_frame();
+/*
+ *     deactivate and close workstation, close gks.
+ */
+/*
+ * Test c_stgeti
+ */
+    c_stseti("MAP -- mapping mode", 2);
+    c_stgeti("MAP -- mapping mode", &ival);
+	if( ival != 2 ) {
+		fprintf( stderr, "c_stgeti test UNSUCCESSFUL\n" );
+		fprintf( stderr, "ival should be 2, ival is really %d\n", ival );
+	}
+/*
+ * Test c_stgetr
+ */
+    c_stsetr("YCN -- upper y bound", 85.0);
+    c_stgetr("YCN -- upper y bound", &rval);
+	if( rval != 85. ) {
+		fprintf( stderr, "c_stgetr test UNSUCCESSFUL\n" );
+	    fprintf( stderr, "rval should be 85.0, rval is really %g\n", rval );
+	}
+/*
+ * Close and deactivate workstation
+ */
+    gdeactivate_ws (WKID);
+    gclose_ws(WKID);
+/*
+ * Close GKS
+ */
+    gclose_gks();
+}
+
+#define  NCLRS   16
+
+void dfclrs()
+{
+	int i;
+/*
+ * Define a set of RGB color triples for colors 0 through 15.
+ */
+	Gcolr_rep rgb[NCLRS];
+/*
+ * Define the RGB color triples needed below.
+ */
+	rgb[0].rgb.red = 0.00 ; rgb[0].rgb.green = 0.00 ; rgb[0].rgb.blue = 0.00 ;
+	rgb[1].rgb.red = 1.00 ; rgb[1].rgb.green = 1.00 ; rgb[1].rgb.blue = 1.00 ;
+	rgb[2].rgb.red = 0.70 ; rgb[2].rgb.green = 0.70 ; rgb[2].rgb.blue = 0.70 ;
+	rgb[3].rgb.red = 0.75 ; rgb[3].rgb.green = 0.50 ; rgb[3].rgb.blue = 1.00 ;
+	rgb[4].rgb.red = 1.00 ; rgb[4].rgb.green = 0.00 ; rgb[4].rgb.blue = 1.00 ;
+	rgb[5].rgb.red = 0.00 ; rgb[5].rgb.green = 0.00 ; rgb[5].rgb.blue = 1.00 ;
+	rgb[6].rgb.red = 0.00 ; rgb[6].rgb.green = 0.50 ; rgb[6].rgb.blue = 1.00 ;
+	rgb[7].rgb.red = 0.00 ; rgb[7].rgb.green = 1.00 ; rgb[7].rgb.blue = 1.00 ;
+	rgb[8].rgb.red = 0.00 ; rgb[8].rgb.green = 1.00 ; rgb[8].rgb.blue = 0.60 ;
+	rgb[9].rgb.red = 0.00 ; rgb[9].rgb.green = 1.00 ; rgb[9].rgb.blue = 0.00 ;
+	rgb[10].rgb.red = 0.70; rgb[10].rgb.green = 1.00; rgb[10].rgb.blue = 0.00 ;
+	rgb[11].rgb.red = 1.00; rgb[11].rgb.green = 1.00; rgb[11].rgb.blue = 0.00 ;
+	rgb[12].rgb.red = 1.00; rgb[12].rgb.green = 0.75; rgb[12].rgb.blue = 0.00 ;
+	rgb[13].rgb.red = 1.00; rgb[13].rgb.green = 0.38; rgb[13].rgb.blue = 0.38 ;
+	rgb[14].rgb.red = 1.00; rgb[14].rgb.green = 0.00; rgb[14].rgb.blue = 0.38 ;
+	rgb[15].rgb.red = 1.00; rgb[15].rgb.green = 0.00; rgb[15].rgb.blue = 0.00 ;
+/*
+ * Define 16 different color indices, for indices 0 through 15.  The
+ * color corresponding to index 0 is black and the color corresponding
+ * to index 1 is white.
+ */
+	for( i = 0; i < NCLRS; i++ ) {
+	    gset_colr_rep(WKID, i, &rgb[i] );
+	}
+    return;
+}
+
