be7c9a371cc9e3dead3d11c813628691d62a9b4b
Mary Haley <haley@ucar.edu>
1994-11-15 20:49:43 +0000

    Corrected minor syntax errors causing error messages on an IBM RS/6000.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2699 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/fundamental/fgks/fgkgtx.f
+++ ncarg2d/src/fundamental/fgks/fgkgtx.f
@@ -4,12 +4,12 @@ C  and workstation ID.
 C
       PARAMETER (IERRF=6, LUNIT=2, IWTYPE=SED_WSTYPE, IWKID=1)
 C
-	  REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
+      REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
 
-	  DATA PLIM1 /0.,0./
-	  DATA PLIM2 /0.,0./
-	  DATA PLIM3 /0.,0./
-	  DATA PLIM4 /0.,0./
+      DATA PLIM1 /0.,0./
+      DATA PLIM2 /0.,0./
+      DATA PLIM3 /0.,0./
+      DATA PLIM4 /0.,0./
 C
 C  Open GKS, open and activate a workstation.
 C
@@ -27,419 +27,419 @@ C
       CALL GCLWK (IWKID)
       CALL GCLKS
 
-	  STOP
-	  END
+      STOP
+      END
 
       SUBROUTINE GTXEXP(PROJ, PLAT, PLON, ROTA, OUTLN,
-	1	   JLIM, PLIM1, PLIM2, PLIM3, PLIM4, IWKID)
+     1                  JLIM, PLIM1, PLIM2, PLIM3, PLIM4, IWKID)
 
-	  CHARACTER*2 PROJ, OUTLN, JLIM
-	  REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
+      CHARACTER*2 PROJ, OUTLN, JLIM
+      REAL PLIM1(2), PLIM2(2), PLIM3(2), PLIM4(2)
 C
 C PURPOSE                To provide a simple demonstration of the
 C                        GKS GTX character drawing techniques.
 C
 C Set up a color table
 C
-	  CALL DFCLRS (IWKID)
+      CALL DFCLRS (IWKID)
 C
 C Draw Continental, political outlines 
 C
-	  CALL MAPSTC ('OU - OUTLINE DATASET SELECTOR',OUTLN)
+      CALL MAPSTC ('OU - OUTLINE DATASET SELECTOR',OUTLN)
 C
 C Set up projection
 C
-	  CALL MAPROJ (PROJ,PLAT,PLON,ROTA)
+      CALL MAPROJ (PROJ,PLAT,PLON,ROTA)
 C
 C If it's a satellite projection, choose a satellite distance
 C
-	  IF (PROJ.EQ.'SV') CALL MAPSTR ('SA - SATELLITE DISTANCE',5.)
+      IF (PROJ.EQ.'SV') CALL MAPSTR ('SA - SATELLITE DISTANCE',5.)
 C
 C Set limits of map
 C
-	  CALL MAPSET (JLIM,PLIM1,PLIM2,PLIM3,PLIM4)
+      CALL MAPSET (JLIM,PLIM1,PLIM2,PLIM3,PLIM4)
 C
 C Turn off Grid lines
 C
-	  CALL MAPSTR ('GR',0.)
+      CALL MAPSTR ('GR',0.)
 C
 C Turn off perimeter of map
 C
-	  CALL MAPSTI ('PE', 0)
+      CALL MAPSTI ('PE', 0)
 C
 C Turn off map labels
 C
-	  CALL MAPSTI ('LA', 0)
+      CALL MAPSTI ('LA', 0)
 C
 C Draw map
 C
-	  CALL MAPDRW
+      CALL MAPDRW
 C
 C Label Brazil
 C
 C Set text color
 C
-	  CALL GSTXCI(4)
+      CALL GSTXCI(4)
 C
 C  Set text size
 C
-	  CALL GSCHH(3.)
+      CALL GSCHH(3.)
 C
 C Set the font
 C
-	  CALL GSTXFP(-16,2)
+      CALL GSTXFP(-16,2)
 C
 C  Transform the coordinates
 C
-	  CALL MAPTRA(-10.,-60.,X,Y)
+      CALL MAPTRA(-10.,-60.,X,Y)
 C
 C  Draw the text
 C
-	  CALL GTX(X,Y,'Brazil')
+      CALL GTX(X,Y,'Brazil')
 C
 C Label Argentina
 C
 C Set text color
 C
-	  CALL GSTXCI(5)
+      CALL GSTXCI(5)
 C
 C  Set text size
 C
-	  CALL GSCHH(2.)
+      CALL GSCHH(2.)
 C
 C Set the font
 C
-	  CALL GSTXFP(-14,2)
+      CALL GSTXFP(-14,2)
 C
 C Transform the coordinates
 C
-	  CALL MAPTRA(-43.,-68.,X,Y)
+      CALL MAPTRA(-43.,-68.,X,Y)
 C
 C Set the angle
 C
-	  CALL GSCHUP (-1.,.3)
+      CALL GSCHUP (-1.,.3)
 C
 C Draw the text
 C
-	  CALL GTX(X,Y,'Argentina')
+      CALL GTX(X,Y,'Argentina')
 C
 C Label Uruguay
 C
 C Set text color
 C
-	  CALL GSTXCI(6)
+      CALL GSTXCI(6)
 C
 C Set text size
 C
-	  CALL GSCHH(1.0)
+      CALL GSCHH(1.0)
 C
 C Set the font
 C
-	  CALL GSTXFP(-4,2)
+      CALL GSTXFP(-4,2)
 C
 C Transform the coordinates
 C
-	  CALL MAPTRA(-32.5,-58.,X,Y)
+      CALL MAPTRA(-32.5,-58.,X,Y)
 C
 C Set the angle
 C
-	  CALL GSCHUP (.7,1.)
+      CALL GSCHUP (.7,1.)
 C
 C Draw the text
 C
-	  CALL GTX(X,Y,'Uruguay')
+      CALL GTX(X,Y,'Uruguay')
 C
 C Label Paraguay
 C
 C Set text color
 C
-	  CALL GSTXCI(5)
+      CALL GSTXCI(5)
 C
 C Set text size
 C
-	  CALL GSCHH(1.0)
+      CALL GSCHH(1.0)
 C
 C Set the font
 C
-	  CALL GSTXFP(-4,2)
+      CALL GSTXFP(-4,2)
 C
 C Transform the coordinates
 C
-	  CALL MAPTRA(-21.,-61.,X,Y)
+      CALL MAPTRA(-21.,-61.,X,Y)
 C
 C Set the angle
 C
-	  CALL GSCHUP (1.,1.)
+      CALL GSCHUP (1.,1.)
 C
 C Draw the text
 C
-	  CALL GTX(X,Y,'Paraguay')
+      CALL GTX(X,Y,'Paraguay')
 C
 C Label Bolivia
 C
 C Set text color
 C
-	  CALL GSTXCI(14)
+      CALL GSTXCI(14)
 C
 C Set text size
 C
-	  CALL GSCHH(1.6)
+      CALL GSCHH(1.6)
 C
 C Set the font
 C
-	  CALL GSTXFP(-6,2)
+      CALL GSTXFP(-6,2)
 C
 C Transform the coordinates
 C
-	  CALL MAPTRA(-17.,-68.,X,Y)
+      CALL MAPTRA(-17.,-68.,X,Y)
 C
 C Set the angle
 C
-	  CALL GSCHUP (0.,1.)
+      CALL GSCHUP (0.,1.)
 C
 C Draw the text
 C
-	  CALL GTX(X,Y,'Bolivia')
+      CALL GTX(X,Y,'Bolivia')
 C
 C Label Chile
 C
 C  Set text color
 C
-	  CALL GSTXCI(4)
+      CALL GSTXCI(4)
 C
 C Set text size
 C
-	  CALL GSCHH(1.5)
+      CALL GSCHH(1.5)
 C
 C Set the font
 C
-	  CALL GSTXFP(-7,2)
+      CALL GSTXFP(-7,2)
 C
 C Transform the coordinates
 C
-	  CALL MAPTRA(-40.,-72.,X,Y)
+      CALL MAPTRA(-40.,-72.,X,Y)
 C
 C Set the angle
 C
-	  CALL GSCHUP (-1.,0.03)
+      CALL GSCHUP (-1.,0.03)
 C
 C Expand the spacing between characters
 C
-	  CALL GSCHSP(2.)
+      CALL GSCHSP(2.)
 C
 C Draw the text
 C
-	  CALL GTX(X,Y,'Chile')
+      CALL GTX(X,Y,'Chile')
 C
 C Label Peru
 C
 C Set text color
 C
-	  CALL GSTXCI(14)
+      CALL GSTXCI(14)
 C
 C Set text size
 C
-	  CALL GSCHH(3.)
+      CALL GSCHH(3.)
 C
 C Set the font
 C
-	   CALL GSTXFP(-15,2)
+       CALL GSTXFP(-15,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(-6.,-79.,X,Y)
+       CALL MAPTRA(-6.,-79.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (1.,1.)
+       CALL GSCHUP (1.,1.)
 C
 C Reset the spacing between characters
 C
-	   CALL GSCHSP(0.)
+       CALL GSCHSP(0.)
 C
 C  Draw the text
 C
-	   CALL GTX(X,Y,'Peru')
+       CALL GTX(X,Y,'Peru')
 C
 C Label Equador
 C
 C Set text color
 C
-	   CALL GSTXCI(15)
+       CALL GSTXCI(15)
 C
 C  Set text size
 C
-	   CALL GSCHH(1.5)
+       CALL GSCHH(1.5)
 C
 C Set the font
 C
-	   CALL GSTXFP(1,2)
+       CALL GSTXFP(1,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(0.,-86.,X,Y)
+       CALL MAPTRA(0.,-86.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (0.,1.)
+       CALL GSCHUP (0.,1.)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Equador')
+       CALL GTX(X,Y,'Equador')
 C
 C Label Colombia
 C
 C Set text color
 C
-	   CALL GSTXCI(1)
+       CALL GSTXCI(1)
 C
 C Set text size
 C
-	   CALL GSCHH(1.3)
+       CALL GSCHH(1.3)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-10,2)
+       CALL GSTXFP(-10,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(7.,-77.,X,Y)
+       CALL MAPTRA(7.,-77.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (.8,1.)
+       CALL GSCHUP (.8,1.)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Colombia')
+       CALL GTX(X,Y,'Colombia')
 
 C
 C Label Venezuela
 C
 C Set text color
-	   CALL GSTXCI(3)
+       CALL GSTXCI(3)
 C
 C Set text size
 C
            CALL GSCHH(1.5)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-6,2)
+       CALL GSTXFP(-6,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(7.,-70.,X,Y)
+       CALL MAPTRA(7.,-70.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (0.,1.)
+       CALL GSCHUP (0.,1.)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Venezuela')
+       CALL GTX(X,Y,'Venezuela')
 
 C
 C Label Guyana
 C
 C Set text color
 C
-	   CALL GSTXCI(1)
+       CALL GSTXCI(1)
 C
 C Set text size
 C
            CALL GSCHH(1.0)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-4,2)
+       CALL GSTXFP(-4,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(7.,-59.5,X,Y)
+       CALL MAPTRA(7.,-59.5,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (1.,0.2)
+       CALL GSCHUP (1.,0.2)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Guyana')
+       CALL GTX(X,Y,'Guyana')
 
 C
 C Label Fr. Guyana
 C
 C Set text color
-	   CALL GSTXCI(1)
+       CALL GSTXCI(1)
 C
 C Set text size
 C
            CALL GSCHH(1.2)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-4,2)
+       CALL GSTXFP(-4,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(2.,-53.5,X,Y)
+       CALL MAPTRA(2.,-53.5,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (-1.,.5)
+       CALL GSCHUP (-1.,.5)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Fr. Guyana')
+       CALL GTX(X,Y,'Fr. Guyana')
 C
 C Label Suriname
 C
 C Set text color
 C
-	   CALL GSTXCI(14)
+       CALL GSTXCI(14)
 C
 C Set text size
 C
            CALL GSCHH(1.2)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-4,2)
+       CALL GSTXFP(-4,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(2.5,-56.,X,Y)
+       CALL MAPTRA(2.5,-56.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (-1.,.2)
+       CALL GSCHUP (-1.,.2)
 C
 C Draw the text
 C
-	   CALL GTX(X,Y,'Suriname')
+       CALL GTX(X,Y,'Suriname')
 
 C
 C Label the plot
 C
 C Set text color
 C
-	   CALL GSTXCI(1)
+       CALL GSTXCI(1)
 C
 C Set text size
 C
            CALL GSCHH(4.)
 C
-C	   Set the font
+C      Set the font
 C
-	   CALL GSTXFP(-14,2)
+       CALL GSTXFP(-14,2)
 C
 C Transform the coordinates
 C
-   	   CALL MAPTRA(15.,-80.,X,Y)
+       CALL MAPTRA(15.,-80.,X,Y)
 C
 C Set the angle
 C
-	   CALL GSCHUP (0.,1.)
+       CALL GSCHUP (0.,1.)
 C
 C Draw the text
 C
@@ -466,12 +466,12 @@ C
 C
 C Advance the frame.
 C
-        CALL FRAME
+      CALL FRAME
 C
 C Done.
 C
-        RETURN
-	END
+      RETURN
+      END
 
       SUBROUTINE DFCLRS (IWKID)
 C
