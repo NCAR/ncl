5c3589f52755f25cba27a5cc4277aa7e2af5000b
ethan <ethan>
1994-12-22 01:46:29 +0000

    NclCoordVar.c NclCoordVar.h: Need to redesign philosophy of coord vars and 	coord selection. These files are special subclasses of the variable 	class
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2757 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/ncl/NclCoordVar.c
@@ -0,0 +1,115 @@
+
+
+/*
+ *      $Id: NclCoordVar.c,v 1.1 1994-12-22 01:46:27 ethan Exp $
+ */
+/************************************************************************
+*									*
+*			     Copyright (C)  1994			*
+*	     University Corporation for Atmospheric Research		*
+*			     All Rights Reserved			*
+*									*
+************************************************************************/
+/*
+ *	File:		
+ *
+ *	Author:		Ethan Alpert
+ *			National Center for Atmospheric Research
+ *			PO 3000, Boulder, Colorado
+ *
+ *	Date:		Thu Jan 13 15:04:25 MST 1994
+ *
+ *	Description:	
+ */
+#include <ncarg/hlu/hlu.h>
+#include <ncarg/hlu/NresDB.h>
+#include "defs.h"
+#include "Symbol.h"
+#include "NclMdInc.h"
+#include "Machine.h"
+#include "DataSupport.h"
+#include "NclCoordVar.h"
+
+
+NclCoordVarClassRec nclCoordVarClassRec = {
+	{
+		"NclCoordVarClass",
+		sizeof(NclCoordVarRec),
+		(NclObjClass)&nclVarClassRec,
+		0,
+		(NclGenericFunction)NULL,
+		(NclSetStatusFunction)NULL /*VarSetStatus*/,
+		(NclInitPartFunction)NULL,
+		(NclInitClassFunction)NULL,
+		(NclAddParentFunction)NULL,
+                (NclDelParentFunction)NULL,
+/* NclPrintFunction print */	NULL
+	},
+	{
+/* NclRepValueFunc rep_val */		NULL,
+/* NclGetValFunc get_val*/		NULL,
+/* NclVarCoerceFunc */			NULL,
+/* NclCopyVarFunc */			NULL,
+
+/* NclAssignFunction write_func */	NULL,
+/* NclAssignVarToVarFunction write_vv_func */	NULL,
+/* NclReadFunction read_func */		NULL,
+/* NclReadValueFunction read_func */	NULL,
+
+/* NclReadAttribute read_att_func*/	NULL,
+/* NclIsA is_att_func*/			NULL,
+/* NclWriteAttribute write_att_func*/	NULL,
+
+/* NclIsAFunc is_dim_func */		NULL,
+/* NclReadDimension read_dim_func*/	NULL,
+/* NclGetDimInfo read_dim_func*/	NULL,
+/* NclWriteDimension write_dim_func*/	NULL,
+
+/* NclIsAFunc is_coord_func */		NULL,
+/* NclReadCoordinate read_coordinate*/	NULL,
+/* NclWriteCoordinate write_coordinate*/ NULL
+	},
+	{
+		NULL
+	}
+};
+
+NclObjClass nclCoordVarClass = (NclObjClass)&nclCoordVarClassRec;
+
+
+
+
+
+struct _NclVarRec *_NclCoordVarCreate
+#if  __STDC__
+(NclVar inst,
+NclObjClass theclass,
+NclObjTypes obj_type,
+unsigned int obj_type_mask,
+struct _NclSymbol *thesym,
+struct _NclMultiDValDataRec * value,
+NclDimRec *dim_info,
+int att_id,
+int *coords,
+NclVarTypes var_type,
+char *var_name)
+#else
+(inst,theclass,obj_type,obj_type_mask,thesym,value,dim_info,att_id,coords,var_type,var_name)
+	NclVar inst;
+	NclObjClass theclass;
+	NclObjTypes obj_type;
+	unsigned int obj_type_mask;
+	struct _NclSymbol *thesym;
+	struct _NclMultiDValDataRec *value;
+	NclDimRec *dim_info;
+	int att_id;
+	int *coords;
+	NclVarTypes var_type;
+	char *var_name;
+#endif
+{
+	NclCoordVar cvar = NULL;
+	
+	return(_NclVarCreate(inst,theclass,obj_type,obj_type_mask,thesym,value,dim_info,att_id,coords,var_type,var_name));
+}
+
---
+++ ni/src/ncl/NclCoordVar.h
@@ -0,0 +1,71 @@
+
+
+/*
+ *      $Id: NclCoordVar.h,v 1.1 1994-12-22 01:46:29 ethan Exp $
+ */
+/************************************************************************
+*									*
+*			     Copyright (C)  1994			*
+*	     University Corporation for Atmospheric Research		*
+*			     All Rights Reserved			*
+*									*
+************************************************************************/
+/*
+ *	File:		
+ *
+ *	Author:		Ethan Alpert
+ *			National Center for Atmospheric Research
+ *			PO 3000, Boulder, Colorado
+ *
+ *	Date:		Thu Jan 13 15:04:41 MST 1994
+ *
+ *	Description:	
+ */
+#ifndef NclCoordVar_h
+#define NclCoordVar_h
+#include "NclVar.h"
+
+typedef struct _NclCoordVarClassPart {
+	char *foo;
+} NclCoordVarClassPart;
+
+typedef struct _NclCoordVarPart {
+	char *foo;
+}NclCoordVarPart;
+ 
+typedef struct _NclCoordVarClassRec{
+	NclObjClassPart	obj_class;
+	NclVarClassPart var_class;
+	NclCoordVarClassPart hvar_class;
+}NclCoordVarClassRec;
+
+typedef struct _NclCoordVarRec {
+	NclObjPart      obj;
+	NclVarPart	var;
+	NclCoordVarPart	hvar;
+}NclCoordVarRec;
+
+typedef NclCoordVarRec *NclCoordVar;
+typedef NclCoordVarClassRec *NclCoordVarClass;
+
+extern NclObjClass nclCoordVarClass;
+
+extern NclCoordVarClassRec nclCoordVarClassRec;
+
+extern struct _NclVarRec *_NclCoordVarCreate(
+#if     NhlNeedProto
+        struct _NclVarRec *     /* inst */,
+        struct _NclObjClassRec *        /* theclass */,
+        NclObjTypes     /* obj_type */,
+        unsigned int    /* obj_type_mask */,
+        struct _NclSymbol  * /* thesym */,
+        struct _NclMultiDValDataRec * /* value */,
+        struct _NclDimRec * /*dim_info*/,
+        int             /*att_id*/,
+        int*            /*coords*/,
+        NclVarTypes /* var_type */,
+        char * /*var_name*/
+#endif
+);
+
+#endif /* NclCoordVar_h */
