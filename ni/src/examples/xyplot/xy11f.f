f17dbf4e826860e3946bed18942552f6b39f65cc
Mary Haley <haley@ucar.edu>
1995-03-03 20:59:00 +0000

    Changed xyCurveData to xyCoordData.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@2979 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/examples/xyplot/xy11f.f
+++ ni/src/examples/xyplot/xy11f.f
@@ -1,5 +1,5 @@
 C
-C     $Id: xy11f.f,v 1.4 1995-02-22 16:35:48 haley Exp $
+C     $Id: xy11f.f,v 1.5 1995-03-03 20:59:00 haley Exp $
 C
 C****************************************************************
 C                                                               *
@@ -114,7 +114,7 @@ C
       call nhlfrlsetfloat(list,'vpWidthF',0.5,ierr)
       call nhlfrlsetfloat(list,'vpHeightF',0.5,ierr)
 
-      call nhlfrlsetinteger(list,'xyCurveData',idata,ierr)
+      call nhlfrlsetinteger(list,'xyCoordData',idata,ierr)
 
       call nhlfrlsetinteger(list,'tiMainOn',1,ierr)
       call nhlfrlsetinteger(list,'tiXAxisOn',1,ierr)
