8fd5ce54158403ba3eb47b74b107edfb708f2752
Tim Scheitlin <scheitln@ucar.edu>
1995-06-07 23:11:53 +0000

    Adding Bob's basic07 example that demonstrates how to set the workstation output. t.s.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3387 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/examples/basic/basic07.res
@@ -0,0 +1,12 @@
+*txPosXF              : 0.5
+*txPosYF              : 0.5
+*txAngleF             : 0.0
+basic07.basic07ps*txString              : PostScript output
+basic07.basic07x11*txString              : X11 output
+basic07.basic07ncgm*txString              : NCGM output
+*txFont                : 22
+*txFontColor           : 1
+*txPerimOn             : True
+*txPerimColor          : 1
+*txBackgroundFillColor : 44
+
---
+++ ni/src/examples/basic/basic07f.f
@@ -0,0 +1,122 @@
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+C                                                                      C
+C                Copyright (C)  1995                                   C
+C        University Corporation for Atmospheric Research               C
+C                all rights reserved                                   C
+C                                                                      C
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+C
+C      File:           basic07f.f
+C
+C      Author:         Bob Lackman (converted by Ed Stautler)
+C          National Center for Atmospheric Research
+C          PO 3000, Boulder, Colorado
+C
+C      Date:           Fri May 25 18:31:18 mdt 1995
+C
+C      Description:    Demonstrates creating 3 simultaneous workstations.
+C                      The TextItem output states which type of workstation,
+C                      out of NCGM, PostScript, and X11.
+C
+      external NhlFAppClass
+      external NhlFPSWorkstationClass
+      external NhlFXWorkstationClass
+      external NhlFNcgmWorkstationClass
+      external NhlFTextItemClass
+
+      integer appid, widx,widn,widp, pidx,pidn,pidp
+      integer srlist, grlist,ierr
+      integer m,i
+      integer NCGM, PS, X11
+C------------------------------------------------------
+C  For HLU/LLU use:
+      INTEGER GLIST,GKSWID
+C------------------------------------------------------
+C
+C  Call a subroutine that generates an LLU
+C
+C Default is to create a metafile.
+C
+      DATA NCGM, PS, X11 /1,1,1/
+C
+C Initialize the high level utility library
+C
+      call NhlFInitialize
+C
+C Create an application context. Set the app dir to the current
+C directory so the application looks for a resource file in the
+C working directory. In this example the resource file supplies the
+C plot title only.
+C
+      call NhlFRLCreate(srlist,'setrl')
+      call NhlFRLClear(srlist)
+      call NhlFRLSetString(srlist,'appUsrDir','./',ierr)
+      call NhlFRLSetString(srlist,'appDefaultParent','True',ierr)
+      call NhlFCreate(appid,'basic07',NhlFAppClass,0,srlist,ierr)
+
+      if (NCGM.eq.1) then
+C
+C Create an NCGM workstation.
+C
+         call NhlFRLClear(srlist)
+	 call NhlFRLSetString(srlist,'wkMetaName','basic07.ncgm',ierr)
+	 call NhlFCreate(widn,'basic07ncgm',NhlFNcgmWorkstationClass,0,
+     1        srlist,ierr)
+C
+      end if
+      if (PS.eq.1) then
+C
+C Create a PostScript workstation.
+C
+         call NhlFRLClear(srlist)
+	 call NhlFRLSetString(srlist,'wkPSFileName','basic07.ps',ierr)
+	 call NhlFRLSetString(srlist,'wkOrientation','portrait',ierr)
+	 call NhlFRLSetString(srlist,'wkPSFormat','ps',ierr)
+	 call NhlFCreate(widp,'basic07ps',NhlFPSWorkstationClass,0,
+     1        srlist,ierr)
+      end if
+      if (X11.eq.1) then
+C
+C Create an X Workstation.
+C
+         call NhlFRLClear(srlist)
+         call NhlFRLSetString(srlist,'wkPause','True',ierr)
+	 call NhlFCreate(widx,'basic07x11',NhlFXWorkstationClass,
+     $        0,srlist,ierr)
+      endif
+C
+C Create three plots, one for each workstation type.
+C
+C  Use color index 2
+	 i = 2
+         call NhlFRLClear(srlist)
+         call NhlFRLSetinteger(srlist,'txBackgroundFillColor',
+     $        i,ierr)
+	 call NhlFCreate(pidx,'TextItems',NhlFTextItemClass,
+     $        widx,srlist,ierr)
+
+	 call NhlFCreate(pidn,'TextItems',NhlFTextItemClass,
+     $        widn,srlist,ierr)
+
+	 call NhlFCreate(pidp,'TextItems',NhlFTextItemClass,
+     $        widp,srlist,ierr)
+
+	 call NhlFDraw(pidx,ierr)
+	 call NhlFDraw(pidn,ierr)
+	 call NhlFDraw(pidp,ierr)
+	 call NhlFFrame(widx,ierr)
+	 call NhlFFrame(widp,ierr)
+	 call NhlFFrame(widn,ierr)
+C
+      call NhlFDestroy(pidx,ierr)
+      call NhlFDestroy(pidn,ierr)
+      call NhlFDestroy(pidp,ierr)
+      call NhlFDestroy(widx,ierr)
+      call NhlFDestroy(widp,ierr)
+      call NhlFDestroy(widn,ierr)
+      call NhlFDestroy(appid,ierr)
+      call NhlFClose
+C
+      stop
+      end
+
