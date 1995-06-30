e84ced9f00a765221e8751c239b469105c065000
kennison <kennison>
1995-06-30 23:33:02 +0000

    Okay, here I'm probably screwing things up.  The situation is as follows:
      The four new files with names of the form "EzmapAreaInfo.*", which are just
      ASCII files, are to be installed directly.
    
      The program "ConvertMapData.f" is to be compiled and run; it reads "ezmapdat"
      and, during a single execution, writes four new binary files, with names of
      the form "EzmapOutlines.*"; those files are to be installed.
    
      The program "ConvertMapData.f" needs some routines from the C source file
      "rwchinfl.c" that I just checked into one of the "common" directories.
    
      The program "mapconvrt.f" is no longer to be used and has been removed.
    
      The file "ezmap_area_ids" is to be installed just as before.
    
    If I can figure out how to accomplish all of the above, I'll check in further
    changes in this directory, but I'm going to check in the Ezmap/Ezmapa files
    first, because that's relatively easy.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3512 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/db/ezmap/ConvertMapData.f
@@ -0,0 +1,104 @@
+
+      PROGRAM CONVRT
+C
+C This program reads data from the ASCII file "ezmapdat" and writes
+C four outline datasets for use by EZMAP.
+C
+C The data in the file "ezmapdat" are grouped in logical records, each
+C of which represents a single outline segment and exists as a pair of
+C FORTRAN records.  The first record of each contains four integers and
+C four reals.  The second record of each pair contains a varying number
+C of real lat/lon coordinate pairs.  In terms of the variables into
+C which the data are read:
+C
+C   INTS(1) = the number of reals in second FORTRAN record, but if
+C             the value is 1, there is no second FORTRAN record: this
+C             signals the end of a group of data in "ezmapdat".
+C   INTS(2) = group identifier for outline segment.
+C   INTS(3) = left area identifier for outline segment.
+C   INTS(4) = right area identifier for outline segment.
+C   FLTS(1) = biggest latitude in outline segment.
+C   FLTS(2) = smallest latitude in outline segment.
+C   FLTS(3) = biggest longitude in outline segment.
+C   FLTS(4) = smallest longitude in outline segment.
+C  (FLTS(I),I=5,4+INTS(1)) = lat/lon pairs for outline segment.
+C
+C Dimension the arrays into which the data are to be read.
+C
+        DIMENSION INTS(4),FLTS(204)
+C
+C Set up an equivalent name for the array element specifying the length
+C of the second FORTRAN record.
+C
+        EQUIVALENCE (INTS(1),LOSR)
+C
+C Declare variables involved in forming names of output files.
+C
+        CHARACTER*17 FLNM
+C
+        CHARACTER*2 NOOD(4)
+C
+C Define the suffixes for the names of the four output datasets.
+C
+        DATA NOOD / 'CO','US','PS','PO' /
+C
+C Open the input unit and make sure it's rewound.
+C
+        OPEN (UNIT=1,STATUS='OLD',FILE='ezmapdat',ERR=901)
+        REWIND 1
+C
+C Initialize the count of output files written so far.
+C
+        NOUT=0
+C
+C Open the next output file.
+C
+  101   NOUT=NOUT+1
+        FLNM='EzmapOutlines.'//NOOD(NOUT)//CHAR(0)
+        CALL NGOFWO (FLNM,IFDE,ISTA)
+        IF (ISTA.NE.0) GO TO 902
+C
+C Read the next input record and copy the integer part of it to the
+C output file.
+C
+  102   READ (1,'(4I4,4F8.3)',END=903) (INTS(I),I=1,4),(FLTS(I),I=1,4)
+C
+        CALL NGWRIN (IFDE,INTS,4,ISTA)
+        IF (ISTA.NE.4) GO TO 904
+C
+C If lat/lon data follow, copy them to the output file and go back for
+C another data record.  Otherwise, close the output file, and, if it
+C wasn't the fourth, go back to open the next.
+C
+        IF (LOSR.GT.1) THEN
+          READ (1,'(10F8.3)',END=903) (FLTS(I),I=5,4+LOSR)
+          CALL NGWRFL (IFDE,FLTS,4+LOSR,ISTA)
+          IF (ISTA.NE.4+LOSR) GO TO 905
+          GO TO 102
+        ELSE
+          CALL NGCLFI (IFDE)
+          IF (NOUT.LT.4) GO TO 101
+        END IF
+C
+C Normal exit.
+C
+        STOP
+C
+C Error exits.
+C
+  901   PRINT * , 'ERROR - Can''t open file "ezmapdat".'
+        STOP
+C
+  902   PRINT * , 'ERROR - NGOFWO returns bad status:',ISTA
+        STOP
+C
+  903   PRINT * , 'ERROR - EOF on input unit.'
+        STOP
+C
+  904   PRINT * , 'ERROR - NGWRIN returns bad status:',ISTA
+        STOP
+C
+  905   PRINT * , 'ERROR - NGWRFL returns bad status:',ISTA
+        STOP
+C
+      END
