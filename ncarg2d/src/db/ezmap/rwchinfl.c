fc0a3bab64f8bfb1680b899a0ade14b9f8c97548
kennison <kennison>
1995-07-01 01:00:15 +0000

    Someone who knows what they are doing needs to review this "yMakefile", but I think it will now probably work as is.  As a temporary measure, I checked in a copy of the file "rwchinfl.c" that I already put in a "common" directory, so that I can use routines from it here.  I also put in "CleanFilesTarget" entries for a number of things that one would want to disappear on a "make clean" and I put in a specific rule saying how to make "ConvertMapData" from "ConvertMapData.o" (from a FORTRAN compilation) and "rwchinfl.o" (from a C compilation) because I couldn't figure out how to modify the macro for a "non-installed-something-or-other-target" do what I wanted.  I have tested this with a "make clean" and a "make all" and it seems to do the right things. Nevertheless, please, look this over and tell me what I've done that's wrong or stupid or dangerous or ... :-)
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3516 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/db/ezmap/rwchinfl.c
@@ -0,0 +1,290 @@
+
+
+#include <ncarg/c.h>
+#include <sys/file.h>
+
+
+#if defined (cray)
+#include <fcntl.h>
+#include <sys/types.h>
+#include <fortran.h>
+#endif
+
+
+NGCALLF(ngclfi,NGCLFI)(fdes)
+/*
+ * This routine closes an open file; the argument "fdes" is the file
+ * descriptor which was returned by ngofro, ngofrw, or ngofwo.
+ */
+  int *fdes;
+{
+  (void)close((int)*fdes);
+}
+
+
+NGCALLF(ngofro,NGOFRO)(flnm,fdes,stat)
+/*
+ * This routine opens an existing file for reading only.  The argument
+ * "flnm" is input; it contains a file name, in the form of a FORTRAN
+ * CHARACTER*1 string terminated by a null.  The argument "fdes"
+ * receives the file descriptor for the file.  The argument "stat"
+ * receives a zero if the operation was successful, non-zero otherwise.
+ */
+#if defined (cray)
+  _fcd flnm;
+#else
+  char flnm[];
+#endif
+  int *fdes,*stat;
+{
+  int st,flgs;
+  *stat = 0;
+/*
+ * Attempt to open the file.
+ */
+  flgs = O_RDONLY;
+#if defined (cray)
+  st = open(_fcdtocp(flnm),flgs,0);
+#else
+  st = open(flnm,flgs,0);
+#endif
+/*
+ * If "st" is -1, set error flag; otherwise, return file descriptor.
+ */
+  if (st == -1)
+    *stat = 1;
+  else
+    *fdes = st;
+}
+
+
+NGCALLF(ngofrw,NGOFRW)(flnm,fdes,stat)
+/*
+ * This routine opens a file for reading and writing both.  The argument
+ * "flnm" is input; it contains a file name, in the form of a FORTRAN
+ * CHARACTER*1 string terminated by a null.  The argument "fdes"
+ * receives the file descriptor for the file.  The argument "stat"
+ * receives a zero if the operation was successful, non-zero otherwise.
+ */
+#if defined (cray)
+  _fcd flnm;
+#else
+  char flnm[];
+#endif
+  int *fdes,*stat;
+{
+  int st,flgs;
+  *stat = 0;
+/*
+ * Attempt to open the file.
+ */
+  flgs = O_RDWR | O_CREAT;
+#if defined (cray)
+  st = open(_fcdtocp(flnm),flgs,0666);
+#else
+  st = open(flnm,flgs,0666);
+#endif
+/*
+ * If "st" is -1, set error flag; otherwise, return file descriptor.
+ */
+  if (st == -1)
+    *stat = 1;
+  else
+    *fdes = st;
+}
+
+
+NGCALLF(ngofwo,NGOFWO)(flnm,fdes,stat)
+/*
+ * This routine opens a file for writing only.  The argument "flnm" is
+ * input; it contains a file name, in the form of a FORTRAN CHARACTER*1
+ * string terminated by a null.  The argument "fdes" receives the file
+ * descriptor for the file.  The argument "stat" receives a zero if the
+ * operation was successful, non-zero otherwise.
+ */
+#if defined (cray)
+  _fcd flnm;
+#else
+  char flnm[];
+#endif
+  int *fdes,*stat;
+{
+  int st,flgs;
+  *stat = 0;
+/*
+ * Attempt to open the file.
+ */
+  flgs = O_WRONLY | O_CREAT;
+#if defined (cray)
+  st = open(_fcdtocp(flnm),flgs,0666);
+#else
+  st = open(flnm,flgs,0666);
+#endif
+/*
+ * If "st" is -1, set error flag; otherwise, return file descriptor.
+ */
+  if (st == -1)
+    *stat = 1;
+  else
+    *fdes = st;
+}
+
+
+NGCALLF(ngrdch,NGRDCH)(fdes,buff,lbuf,stat)
+/*
+ * This routine reads characters (bytes) from a file to an internal
+ * buffer.  The argument "fdes" is the file descriptor returned by the
+ * routine that opened the file.  The argument "buff" is a FORTRAN
+ * CHARACTER*1 array.  The argument "lbuf" says how many bytes are
+ * to be transferred.  The argument "stat" is returned with a positive
+ * value indicating how many bytes were actually transferred, a zero
+ * value if an end-of-file was encountered, or a negative value if an
+ * error occurred.
+ */
+#if defined (cray)
+  _fcd buff;
+#else
+  char buff[];
+#endif
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+#if defined (cray)
+  ret = read((int)*fdes,_fcdtocp(buff),(int)*lbuf);
+#else
+  ret = read((int)*fdes,buff,(int)*lbuf);
+#endif
+  *stat = ret;
+}
+
+
+NGCALLF(ngrdfl,NGRDFL)(fdes,buff,lbuf,stat)
+/*
+ * This routine reads floats (reals) from a file to an internal buffer.
+ * The argument "fdes" is the file descriptor returned by the routine
+ * that opened the file.  The argument "buff" is a FORTRAN REAL array.
+ * The argument "lbuf" says how many reals are to be transferred.  The
+ * argument "stat" is returned with a positive value indicating how
+ * many reals were actually transferred, a zero value if an end-of-file
+ * was encountered, or a negative value if an error occurred.
+ */
+  float buff[];
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+  ret = read((int)*fdes,(char*)buff,(int)(*lbuf*sizeof(float)));
+  if (ret%sizeof(float)!=0) ret=-1;
+  else if (ret>0) ret=ret/sizeof(float);
+  *stat = ret;
+}
+
+
+NGCALLF(ngrdin,NGRDIN)(fdes,buff,lbuf,stat)
+/*
+ * This routine reads integers from a file to an internal buffer.  The
+ * argument "fdes" is the file descriptor returned by the routine that
+ * opened the file.  The argument "buff" is a FORTRAN INTEGER array.
+ * The argument "lbuf" says how many integers are to be transferred.
+ * The argument "stat" is returned with a positive value indicating
+ * how many integers were actually transferred, a zero value if an
+ * end-of-file was encountered, or a negative value if an error
+ * occurred.
+ */
+  int buff[];
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+  ret = read((int)*fdes,(char*)buff,(int)(*lbuf*sizeof(int)));
+  if (ret%sizeof(int)!=0) ret=-1;
+  else if (ret>0) ret=ret/sizeof(int);
+  *stat = ret;
+}
+
+
+NGCALLF(ngrmfi,NGRMFI)(flnm)
+/*
+ * This routine removes an existing file.  The argument "flnm" is input;
+ * it contains a file name, in the form of a FORTRAN CHARACTER*1 string
+ * terminated by a null.
+ */
+#if defined (cray)
+  _fcd flnm;
+#else
+  char flnm[];
+#endif
+{
+#if defined (cray)
+  (void)unlink(_fcdtocp(flnm));
+#else
+  (void)unlink(flnm);
+#endif
+}
+
+
+NGCALLF(ngwrch,NGWRCH)(fdes,buff,lbuf,stat)
+/*
+ * This routine writes characters (bytes) to a file from an internal
+ * buffer.  The argument "fdes" is the file descriptor returned by the
+ * routine that opened the file.  The argument "buff" is a FORTRAN
+ * CHARACTER*1 array.  The argument "lbuf" says how many bytes are to
+ * be transferred.  The argument "stat" is returned with a positive
+ * value indicating how many bytes were actually transferred or a value
+ * less than or equal to zero if an error occurred.
+ */
+#if defined (cray)
+  _fcd buff;
+#else
+  char buff[];
+#endif
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+#if defined (cray)
+  ret = write((int)*fdes,_fcdtocp(buff),(int)*lbuf);
+#else
+  ret = write((int)*fdes,buff,(int)*lbuf);
+#endif
+  *stat = ret;
+}
+
+
+NGCALLF(ngwrfl,NGWRFL)(fdes,buff,lbuf,stat)
+/*
+ * This routine writes reals (floats) to a file from an internal
+ * buffer.  The argument "fdes" is the file descriptor returned by the
+ * routine that opened the file.  The argument "buff" is a FORTRAN
+ * REAL array.  The argument "lbuf" says how many reals are to be
+ * transferred.  The argument "stat" is returned with a positive value
+ * indicating how many reals were actually transferred or a value less
+ * than or equal to zero if an error occurred.
+ */
+  float buff[];
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+  ret = write((int)*fdes,(char*)buff,(int)(*lbuf*sizeof(float)));
+  if (ret%sizeof(float)!=0) ret=-1;
+  else if (ret>0) ret=ret/sizeof(float);
+  *stat = ret;
+}
+
+
+NGCALLF(ngwrin,NGWRIN)(fdes,buff,lbuf,stat)
+/*
+ * This routine writes integers to a file from an internal buffer.  The
+ * argument "fdes" is the file descriptor returned by the routine that
+ * opened the file.  The argument "buff" is a FORTRAN INTEGER array.
+ * The argument "lbuf" says how many integers are to be transferred.
+ * The argument "stat" is returned with a positive value indicating
+ * how many integers were actually transferred or a value less than or
+ * equal to zero if an error occurred.
+ */
+  int buff[];
+  int *fdes,*lbuf,*stat;
+{
+  int ret;
+  ret = write((int)*fdes,(char*)buff,(int)(*lbuf*sizeof(int)));
+  if (ret%sizeof(int)!=0) ret=-1;
+  else if (ret>0) ret=ret/sizeof(int);
+  *stat = ret;
+}
