c7f1c41ac33abf808411f0d14b3f1d29c71b6ad9
boote <boote>
1996-01-09 16:39:30 +0000

    Fixing a converter problem.  I accidentally was installing the NhlTLineLabelMode converter to the XyPlot class instead of the XyData class...
    jeff
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3699 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/hlu/XyPlot.c
+++ ni/src/lib/hlu/XyPlot.c
@@ -1,5 +1,5 @@
 /*
- *      $Id: XyPlot.c,v 1.52 1995-12-19 20:39:41 boote Exp $
+ *      $Id: XyPlot.c,v 1.53 1996-01-09 16:39:30 boote Exp $
  */
 /************************************************************************
 *									*
@@ -684,7 +684,7 @@ XyDataClassInitialize
 		{NhlCUSTOM,	"custom"}
 	};
 
-	_NhlRegisterEnumType(NhlxyPlotClass,NhlTLineLabelMode,lblmode,
+	_NhlRegisterEnumType(NhlxyDataClass,NhlTLineLabelMode,lblmode,
 		NhlNumber(lblmode));
 	Qint = NrmStringToQuark(NhlTInteger);
 	Qstring = NrmStringToQuark(NhlTString);
