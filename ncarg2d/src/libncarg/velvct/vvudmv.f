12b95a24d3386f2f9526b381f5100f03fc0ff45e
Dave Brown <dbrown@ucar.edu>
1996-01-25 16:19:07 +0000

    Fixed a problem with a misnamed variable that Mary found. Made some additional changes to support various filled arrow options.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3738 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/velvct/vvudmv.f
+++ ncarg2d/src/libncarg/velvct/vvudmv.f
@@ -1,5 +1,5 @@
 C
-C       $Id: vvudmv.f,v 1.5 1996-01-19 17:21:52 dbrown Exp $
+C       $Id: vvudmv.f,v 1.6 1996-01-25 16:19:07 dbrown Exp $
 C
       SUBROUTINE VVUDMV(XCS,YCS,NCS,IAI,IAG,NAI)
 C
@@ -16,8 +16,8 @@ C NAI        - number of area/group identifiers
 C
 C --------------------------------------------------------------------
 C
-      PARAMETER (PMXCNT=64)
-      REAL XC(PMXCNT),YC(PMXCNT)
+      PARAMETER (IPMXCT=64)
+      REAL XC(IPMXCT),YC(IPMXCT)
       INTEGER ICNT
       DATA ICNT /0/
       SAVE XC,YC,ICNT
@@ -32,51 +32,79 @@ C Depending on the arrow style, draw a polyline or a filled polygon
 C
       IF (IAS.EQ.0) THEN
          CALL CURVE(XCS,YCS,NCS)
-      ELSE
-         IF (ICNT.NE.0) THEN
-            IF (XCS(1).NE.XC(ICNT) .OR. YCS(1).NE.YC(ICNT)) THEN
-               ICNT = 0
-            ELSE IF (ICNT + NCS - 1 .GT. PMXCNT) THEN
+         RETURN
+      END IF
+C
+C If the 'filled' arrows are hollow, just draw the line as above
+C
+      CALL VVGETI('ACM',ICM)
+      IF (ICM.EQ.-1) THEN
+         CALL CURVE(XCS,YCS,NCS)
+         RETURN
+      END IF
+C
+C This routine to draw filled arrows is rather cumbersome and
+C completely eliminates arrows that partially intrude into
+C a masked area
+C
+      IF (ICNT.NE.0) THEN
+         IF (XCS(1).NE.XC(ICNT) .OR. YCS(1).NE.YC(ICNT)) THEN
+            ICNT = 0
+         ELSE IF (ICNT + NCS - 1 .GT. IPMXCT) THEN
 C
 C If the buffer size is exceeded throw away the points involved
+C Uncomment the following to print out an error message in this case
 C
-C              WRITE(*,*) "ERROR IN VVUDMV: LOCAL BUFFER OVERFLOW"
+C     WRITE(*,*) "ERROR IN VVUDMV: LOCAL BUFFER OVERFLOW"
 C
-               ICNT = 0
-            ELSE
-               DO 10 I = 2,NCS
-                  ICNT = ICNT+1
-                  XC(ICNT) = XCS(I)
-                  YC(ICNT) = YCS(I)
- 10            CONTINUE
-C
-C If the points form a closed polygon draw them and empty the buffer
-C
-               IF (XC(1).EQ.XC(ICNT) .AND. YC(1).EQ.YC(ICNT)) THEN
-                  CALL GSPLCI(0)
-                  CALL CURVE(XC,YC,ICNT)
-                  CALL GFA(ICNT,XC,YC)
-                  write(*,*) icnt
-                  ICNT = 0
-               END IF
-               RETURN
-            END IF
-         END IF
-C
-C Draw the polygon if the points close; otherwise buffer the points.
-C
-         IF (XCS(1).EQ.XCS(NCS).AND.YCS(1).EQ.YCS(NCS)) THEN
-            CALL GSPLCI(0)
-            CALL CURVE(XCS,YCS,NCS)
-            CALL GFA(NCS,XCS,YCS)
+            ICNT = 0
          ELSE
-            DO 20 I = 1,NCS
+            DO 10 I = 2,NCS
                ICNT = ICNT+1
                XC(ICNT) = XCS(I)
                YC(ICNT) = YCS(I)
- 20         CONTINUE
-         ENDIF
+ 10         CONTINUE
+C
+C If the points form a closed polygon draw them and empty the buffer
+C
+            IF (XC(1).EQ.XC(ICNT) .AND. YC(1).EQ.YC(ICNT)) THEN
+C
+               CALL VVGETI('AFO',IFO)
+               IF (IFO.GT.0 .AND. ICM.GT.-2) THEN
+                  CALL CURVE(XC,YC,ICNT)
+               END IF
+               CALL GFA(ICNT,XC,YC)
+               IF (IFO.LE.0 .AND. ICM.GT.-2) THEN
+                  CALL CURVE(XC,YC,ICNT)
+               END IF
+C
+               ICNT = 0
+            END IF
+            RETURN
+         END IF
       END IF
+C
+C Draw the polygon if the points close; otherwise buffer the points.
+C
+      IF (XCS(1).EQ.XCS(NCS).AND.YCS(1).EQ.YCS(NCS)) THEN
+C
+         CALL VVGETI('ACM',ICM)
+         CALL VVGETI('AFO',IFO)
+         IF (IFO.GT.0 .AND. ICM.GT.-2) THEN
+            CALL CURVE(XCS,YCS,NCS)
+         END IF
+         CALL GFA(NCS,XCS,YCS)
+         IF (IFO.LE.0 .AND. ICM.GT.-2) THEN
+            CALL CURVE(XCS,YCS,NCS)
+         END IF
+C     
+      ELSE
+         DO 20 I = 1,NCS
+            ICNT = ICNT+1
+            XC(ICNT) = XCS(I)
+            YC(ICNT) = YCS(I)
+ 20      CONTINUE
+      ENDIF
 C
 C Done.
 C
