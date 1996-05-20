91a88f968f1ed5c0e0e6242add35ae304e59efab
kennison <kennison>
1996-05-20 20:29:14 +0000

    This is to recreate some "man" pages that mysteriously disappeared from the distribution.  Luckily, I still had them in my checked-out version. I have altered the copyright entries in these files to match what appears to be the current convention.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@3978 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/man/scrolled_title/slogap.m
@@ -0,0 +1,70 @@
+.TH SLOGAP 3NCARG "July 1995" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+SLOGAP -
+Generates TIME seconds worth of blank frames in a manner consistent with FTITLE.
+.SH SYNOPSIS
+CALL SLOGAP (TIME,MTST)
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_slogap (float time, int mtst)
+.SH DESCRIPTION 
+.IP TIME 12
+(an input expression of type REAL) specifies the number of seconds worth of
+blank frames to be generated.
+.IP MTST 12
+(an input expression of type INTEGER) is a switch to
+indicate whether this is a "real" run or a "practice" run.
+.RS
+.IP 0
+means "real run".
+.IP 1 
+means "practice run".
+.RE
+.IP ""
+During real runs, the blank frames themselves are created.
+.sp
+During practice runs, a single frame is created with a legend saying how many
+seconds of blank frames it stands for.
+.SH C-BINDING DESCRIPTION
+The C-binding argument descriptions are the same as the FORTRAN 
+argument descriptions.
+.SH USAGE
+This routine allows you to output blank frames in a manner consistent with
+FTITLE.
+.SH EXAMPLES
+Use the ncargex command to see the following relevant
+example:
+slex02.
+.SH ACCESS
+To use SLOGAP, load the NCAR Graphics libraries ncarg, ncarg_gks,
+ncarg_c, and ncarg_c, preferably in that order.  To use c_slogap, load
+the NCAR Graphics libraries ncargC, ncarg_gksC, ncarg, ncarg_gks,
+ncarg_c, and ncarg_c, preferably in that order.
+.SH MESSAGES
+See the scrolled_title man page for a description of all Scrolled_title error
+messages and/or informational messages.
+.SH SEE ALSO
+Online:
+ftitle,
+scrolled_title,
+scrolled_title_params,
+slgeti,
+slgetr,
+slrset,
+slseti,
+slsetr,
+stitle,
+ncarg_cbind.
+.sp
+Hardcopy:
+NCAR Graphics Fundamentals, UNIX Version;
+User's Guide for NCAR GKS-0A Graphics
+.SH COPYRIGHT
+Copyright (C) 1987-1996
+.br
+University Corporation for Atmospheric Research
+.br
+The use of this Software is governed by a License Agreement.
---
+++ ncarg2d/man/scrolled_title/slrset.m
@@ -0,0 +1,52 @@
+.TH SLRSET 3NCARG "July 1995" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+SLRSET - Resets all parameters of Scrolled_title to the initial default values.
+.SH SYNOPSIS
+CALL SLRSET
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_slrset()
+.SH DESCRIPTION 
+SLRSET has no arguments.
+.SH USAGE
+Calling this routine restores all
+Scrolled_title parameters to their initial default values.
+For a complete list of parameters available
+in this utility, see the scrolled_title_params man page.
+.SH EXAMPLES
+Use the ncargex command to see the following relevant
+example:
+slex02.
+.SH ACCESS
+To use SLRSET, load the NCAR Graphics libraries ncarg, ncarg_gks,
+and ncarg_c, preferably in that order.  To use c_slrset, load
+the NCAR Graphics libraries ncargC, ncarg_gksC, ncarg, ncarg_gks,
+and ncarg_c, preferably in that order.
+.SH MESSAGES
+See the scrolled_title man page for a description of all Scrolled_title error
+messages and/or informational messages.
+.SH SEE ALSO
+Online:
+ftitle,
+scrolled_title,
+scrolled_title_params,
+slgeti,
+slgetr,
+slogap,
+slseti,
+slsetr,
+stitle,
+ncarg_cbind.
+.sp
+Hardcopy:
+NCAR Graphics Fundamentals, UNIX Version;
+User's Guide for NCAR GKS-0A Graphics
+.SH COPYRIGHT
+Copyright (C) 1987-1996
+.br
+University Corporation for Atmospheric Research
+.br
+The use of this Software is governed by a License Agreement.
