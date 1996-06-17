124ae4975afe245ea448bc95a5c83726bb304137
Mary Haley <haley@ucar.edu>
1996-06-17 21:23:58 +0000

    Lynn added vc04c.c and vc04f.f
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4013 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/examples/vectorplot/vc04c.c
@@ -0,0 +1,221 @@
+/*
+ *      
+ */
+/***********************************************************************
+*                                                                      *
+*                Copyright (C)  1993                                   *
+*        University Corporation for Atmospheric Research               *
+*                All Rights Reserved                                   *
+*                                                                      *
+***********************************************************************/
+/*
+ *  File:       vc04c.c
+ *
+ *  Author:     David Brown (converted to C by Lynn Hermanson)
+ *              National Center for Atmospheric Research
+ *              PO 3000, Boulder, Colorado
+ *
+ *  Date:       June 13, 1996
+ *
+ *
+ * Description: Manipulates the FillArrow resources to demonstrate some
+ *               of the possible stylistic variations on the appearance
+ *               of the filled vector arrows.
+ *               The data is extracted from an NMC forecast dataset for
+ *               11/10/1994.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <math.h>
+#include <ncarg/gks.h>
+#include <ncarg/ncargC.h>
+#include <ncarg/hlu/hlu.h>
+#include <ncarg/hlu/App.h>
+#include <ncarg/hlu/NcgmWorkstation.h>
+#include <ncarg/hlu/PSWorkstation.h>
+#include <ncarg/hlu/XWorkstation.h>
+#include <ncarg/hlu/VectorPlot.h>
+
+
+#define a 2
+#define b 37
+#define c 37
+
+main(int argc, char *argv[])
+{
+    int NCGM=0, X11=1, PS=0;
+    int appid,wid,vcid,vfid;
+    int rlist,grlist;
+    int len_dims[3];
+    float reflen, ref, tiheight, tiht;
+    float x[a][b][c];
+    FILE * fd;
+    int i,j,k;
+/*
+ * Generate vector data array
+ */
+    char  filename[256];   
+    const char *dir = _NGGetNCARGEnv("data");
+    sprintf( filename, "%s/asc/uvdata0.asc", dir );
+
+    fd = fopen(filename,"r");
+       
+    for (k = 0; k < a; k++) {
+        for (j = 0; j < b; j++) {
+            for (i = 0; i < c; i++) {       
+                fscanf(fd,"%f", &x[k][j][i]);
+            }
+        }
+    }
+
+/*
+ * Initialize the high level utility library
+ */
+    NhlInitialize();
+/*
+ * Create an application context. Set the app dir to the current
+ * directory so the application looks for a resource file in the working
+ * directory. 
+ */
+    rlist = NhlRLCreate(NhlSETRL);
+    grlist = NhlRLCreate(NhlGETRL);
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNappUsrDir,"./");
+    NhlCreate(&appid,"vc04",NhlappClass,NhlDEFAULT_APP,rlist);
+
+    if (NCGM) {
+/*
+ * Create a meta file workstation.
+ */
+        NhlRLClear(rlist);
+        NhlRLSetString(rlist,NhlNwkMetaName,"./vc04c.ncgm");
+        NhlCreate(&wid,"vc04Work",
+                  NhlncgmWorkstationClass,NhlDEFAULT_APP,rlist);
+    }
+    else if (X11) {
+/*
+ * Create an X workstation.
+ */
+        NhlRLClear(rlist);
+        NhlRLSetInteger(rlist,NhlNwkPause,True);
+        NhlCreate(&wid,"vc04Work",NhlxWorkstationClass,appid,rlist);
+    }
+
+    else if (PS) {
+/*
+ * Create a PS workstation.
+ */
+        NhlRLClear(rlist);
+        NhlRLSetString(rlist,NhlNwkPSFileName,"vc04c.ps");
+        NhlCreate(&wid,"vc04Work",NhlpsWorkstationClass,appid,rlist);
+    }
+/*
+ * Create a VectorField data object using the data set defined above.
+ * By default the array bounds will define the data boundaries (zero-based,
+ * as in C language conventions)
+ */
+
+    len_dims[0] = a;
+    len_dims[1] = b;
+    len_dims[2] = c;
+    NhlRLClear(rlist);
+    NhlRLSetMDFloatArray(rlist,NhlNvfDataArray,&x[0][0][0],3,len_dims);
+    NhlRLSetFloat(rlist,NhlNvfXCStartV, -180.0);
+    NhlRLSetFloat(rlist,NhlNvfXCEndV, 0.0);
+    NhlRLSetFloat(rlist,NhlNvfYCStartV, 0.0);
+    NhlRLSetFloat(rlist,NhlNvfYCEndV, 90.0);
+    NhlRLSetFloat(rlist,NhlNvfYCStartSubsetV, 20.0);
+    NhlRLSetFloat(rlist,NhlNvfYCEndSubsetV, 80.0);
+   
+    NhlCreate(&vfid,"vectorfield",NhlvectorFieldClass,appid,rlist);
+
+/*
+ * Create a VectorPlot object, supplying the VectorField object as data
+ * Setting vcMonoFillArrowFillColor False causes VectorPlot to color the
+ * vector arrows individually based, by default, on the vector magnitude.
+ */
+
+    NhlRLClear(rlist);
+    NhlRLSetFloat(rlist,NhlNvcRefMagnitudeF, 20.0);
+    NhlRLSetString(rlist,NhlNvcMonoFillArrowFillColor, "False");
+    NhlRLSetString(rlist,NhlNvcFillArrowsOn, "True");
+    NhlRLSetFloat(rlist,NhlNvcMinFracLengthF, 0.25);
+
+    NhlRLSetInteger(rlist,NhlNvcVectorFieldData,vfid);
+
+    NhlCreate(&vcid,"vectorplot",NhlvectorPlotClass,wid,rlist);
+
+
+
+    NhlRLClear(grlist);
+    NhlRLGetFloat(grlist,NhlNvcRefLengthF,&reflen);
+    NhlRLGetFloat(grlist,NhlNtiMainFontHeightF,&tiheight);
+    NhlGetValues(vcid,grlist);
+
+    ref= 1.75 * reflen;
+    tiht = 0.9 * tiheight;
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNtiMainString,"How to Rotate a VectorPlot 90:F34:0:F:");
+    NhlRLSetFloat(rlist,NhlNvcRefLengthF,ref);
+    NhlRLSetFloat(rlist,NhlNtiMainFontHeightF,tiht);
+    NhlSetValues(vcid,rlist);
+
+    NhlDraw(vcid);
+    NhlFrame(wid);
+
+
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNtiMainString,
+           "1:: Exchange the Dimensions");
+    NhlSetValues(vcid,rlist);
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNvfExchangeDimensions, "True");
+    NhlSetValues(vfid,rlist);
+
+    NhlDraw(vcid);
+    NhlFrame(wid);
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNtiMainString,"2:: Exchange the U and V Data");
+    NhlSetValues(vcid,rlist);
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNvfExchangeUVData,"True");
+    NhlSetValues(vfid,rlist);
+
+    NhlDraw(vcid);
+    NhlFrame(wid);
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNtiMainString,
+                   "3a:: Reverse the Y-Axis for Clockwise Rotation");
+    NhlRLSetString(rlist,NhlNtrYReverse, "True");
+    NhlSetValues(vcid,rlist);
+
+    NhlDraw(vcid);
+    NhlFrame(wid);
+
+    NhlRLClear(rlist);
+    NhlRLSetString(rlist,NhlNtiMainString,
+                   "3b:: Or the X-Axis for Counter-Clockwise Rotation");
+    NhlRLSetString(rlist,NhlNtrYReverse, "False");
+    NhlRLSetString(rlist,NhlNtrXReverse, "True");
+    NhlSetValues(vcid,rlist);
+
+    NhlDraw(vcid);
+    NhlFrame(wid);
+/*
+ * Destroy the objects created, close the HLU library and exit.
+ */
+    NhlDestroy(appid);
+    NhlClose();
+    exit(0);
+}
---
+++ ni/src/examples/vectorplot/vc04f.f
@@ -0,0 +1,205 @@
+C
+C
+C
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+C                                                                    
+C                Copyright (C)  1993                                 
+C        University Corporation for Atmospheric Research             
+C                All Rights Reserved                                  
+C                                                                   
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+C
+C   File:       vc04f.f
+C 
+C   Author:     David Brown (converted to FORTRAN by Lynn Hermanson)
+C               National Center for Atmospheric Research
+C               PO 3000, Boulder, Colorado
+C 
+C   Date:       June 17, 1996
+C 
+C 
+C  Description: Manipulates the FillArrow resources to demonstrate some
+C                of the possible stylistic variations on the appearance
+C                of the filled vector arrows.
+C                The data is extracted from an NMC forecast dataset for
+C                11/10/1994.
+C
+       external NhlFAppClass
+       external NhlFNcgmWorkstationClass
+       external NhlFPSWorkstationClass
+       external NhlFXWorkstationClass
+       external NhlFVectorPlotClass
+       external NhlFVectorFieldClass
+
+       parameter(a= 2)
+       parameter( b= 37)
+       parameter( c= 37)
+       integer NCGM, X11, PS
+       integer appid,wid,vcid,vfid
+       integer rlist,grlist
+       integer len_dims(3)
+       integer flen,ierr
+       real x(c,b,a)
+       integer i,j,k
+       real reflen, ref, tiheight, tiht
+C
+C Generate vector data array
+C
+      character*256  filename
+      call gngpat(filename,'data',ierr)
+      flen = 15
+
+      do 10 i=1,256
+         if( filename(i:i).eq.char(0) ) then
+            filename(i:i+flen)='/asc/uvdata0.asc'
+            goto 15
+         endif
+ 10   continue
+
+ 15   open(UNIT=10,FILE=filename,READONLY,STATUS='OLD')
+      read(10,*)(((x(i,j,k),i=1,c),j=1,b),k=1,a)
+C
+C Initialize the HLU library and set up resource template.
+C  
+      call NhlFInitialize
+      call NhlFRLCreate(rlist,'setrl')
+      call NhlFRLCreate(grlist, 'getrl')
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'appDefaultParent','True',ierr)
+      call NhlFRLSetString(rlist,'appUsrDir','./',ierr)
+      call NhlFCreate(appid,'vc04',NhlFAppClass,0,rlist,ierr)
+C
+C Default is to create an X11 window.
+C
+      NCGM=0
+      X11=1
+      PS=0
+
+      if (NCGM.eq.1) then
+C
+C Create an NCGM workstation.
+C
+         call NhlFRLClear(rlist)
+         call NhlFRLSetString(rlist,'wkMetaName','./vc04f.ncgm',ierr)
+         call NhlFCreate(wid,'vc04Work',
+
+     +        NhlFNcgmWorkstationClass,0,rlist,ierr)
+      else if (X11.eq.1) then
+C
+C Create an xworkstation object.
+C
+         call NhlFRLClear(rlist)
+         call NhlFRLSetString(rlist,'wkPause','True',ierr)
+         call NhlFCreate(wid,'vc04Work',NhlFXWorkstationClass,
+     +        0,rlist,ierr)
+      else if (PS.eq.1) then
+C
+C Create a PostScript workstation.
+C
+         call NhlFRLClear(rlist)
+         call NhlFRLSetString(rlist,'wkPSFileName','./vc04f.ps',ierr)
+         call NhlFCreate(wid,'vc04Work',
+     +        NhlFPSWorkstationClass,0,rlist,ierr)
+      endif
+C
+C  Create a VectorField data object using the data set defined above.
+C  By default the array bounds will define the data boundaries 
+C  (zero-based,
+C  as in C language conventions)
+C
+      len_dims(1) = c
+      len_dims(2) = b
+      len_dims(3) = a
+      call NhlFRLClear(rlist)
+      call NhlFRLSetMDFloatArray(rlist,'vfDataArray',x,3,len_dims,ierr)
+      call NhlFRLSetFloat(rlist,'vfXCStartV', -180.0,ierr)
+      call NhlFRLSetFloat(rlist,'vfXCEndV', 0.0,ierr)
+      call NhlFRLSetFloat(rlist,'vfYCStartV', 0.0,ierr)
+      call NhlFRLSetFloat(rlist,'vfYCEndV', 90.0,ierr)
+      call NhlFRLSetFloat(rlist,'vfYCStartSubsetV', 20.0,ierr)
+      call NhlFRLSetFloat(rlist,'vfYCEndSubsetV', 80.0,ierr)
+      call NhlFCreate(vfid,'vectorfield',NhlFvectorFieldClass,appid,
+     +rlist,ierr)
+C
+C  Create a VectorPlot object, supplying the VectorField object as data
+C  Setting vcMonoFillArrowFillColor False causes VectorPlot to color
+C   the vector arrows individually based, by default, on the vector
+C   magnitude.
+C
+      call NhlFRLClear(rlist)
+      call NhlFRLSetFloat(rlist,'vcRefMagnitudeF', 20.0,ierr)
+      call NhlFRLSetString(rlist,'vcFillArrowsOn', 'True',ierr)
+      call NhlFRLSetFloat(rlist,'vcMinFracLengthF', 0.25,ierr)
+      call NhlFRLSetInteger(rlist,'vcVectorFieldData',vfid,ierr)
+      call NhlFRLSetString(rlist,'vcMonoFillArrowFillColor', 'False',
+     +ierr)
+      call NhlFCreate(vcid,'vectorplot',NhlFvectorPlotClass,wid,rlist,
+     +ierr)
+
+      call NhlFRLClear(grlist)
+      call NhlFRLGetFloat(grlist,'vcRefLengthF',reflen,ierr)
+      call NhlFRLGetFloat(grlist,'tiMainFontHeightF',tiheight,ierr)
+      call NhlFGetValues(vcid,grlist,ierr)
+
+      ref= 1.75 * reflen
+      tiht = 0.9 * tiheight
+      call NhlFRLClear(rlist)
+      call NhlFRLSetFloat(rlist,'vcRefLengthF',ref,ierr)
+      call NhlFRLSetString(rlist,'tiMainString','How to Rotate a Vector
+     +Plot 90:F34:0:F:',ierr) 
+      call NhlFRLSetFloat(rlist,'tiMainFontHeightF',tiht,ierr)
+      call NhlFSetValues(vcid,rlist,ierr)
+
+      call NhlFDraw(vcid,ierr)
+      call NhlFFrame(wid,ierr)
+
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'tiMainString',
+     + '1::Exchange the Dimensions',ierr)
+      call NhlFSetValues(vcid,rlist,ierr)
+
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'vfExchangeDimensions', 'True',ierr)
+      call NhlFSetValues(vfid,rlist,ierr)
+ 
+      call NhlFDraw(vcid,ierr)
+      call NhlFFrame(wid,ierr)
+
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'tiMainString','2:: Exchange the U and 
+     +V Data',ierr)
+      call NhlFSetValues(vcid,rlist,ierr)
+
+      call NhlFRLClear(rlist) 
+      call NhlFRLSetString(rlist,'vfExchangeUVData', 'True',ierr)
+      call NhlFSetValues(vfid,rlist,ierr)
+
+      call NhlFDraw(vcid,ierr)
+      call NhlFFrame(wid,ierr)
+
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'tiMainString','3a:: Reverse the Y-Axiss
+     + for Clockwise Rotation',ierr)
+      call NhlFRLSetString(rlist,'trYReverse', 'True',ierr)
+
+      call NhlFSetValues(vcid,rlist,ierr)
+
+      call NhlFDraw(vcid,ierr)
+      call NhlFFrame(wid,ierr)
+      call NhlFRLClear(rlist)
+      call NhlFRLSetString(rlist,'tiMainString','3b:: Or the X-Axis
+     + for Counter Clockwise Rotation',ierr)
+      call NhlFRLSetString(rlist,'trYReverse', 'False',ierr)
+      call NhlFRLSetString(rlist,'trXReverse', 'True',ierr)
+      call NhlFSetValues(vcid,rlist,ierr)
+
+      call NhlFDraw(vcid,ierr)
+      call NhlFFrame(wid,ierr)
+C
+C Destroy the objects created, close the HLU library and exit.
+C
+      call NhlFRLDestroy(rlist)
+      call NhlFDestroy(appid,ierr)
+      call NhlFClose
+      stop
+      end
