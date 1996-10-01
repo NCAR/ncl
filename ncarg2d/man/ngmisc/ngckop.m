1b27e38803faa1d0461d88c22e6fd27c14af9abb
fred <fred>
1996-10-01 22:53:22 +0000

    This function in ngmisc did not have a man page.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4105 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/man/ngmisc/ngckop.m
@@ -0,0 +1,41 @@
+.TH NGCKOP 3NCARG "October 1996" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+NGCKOP - a function to check if a specified GKS workstation is open.
+.SH SYNOPSIS
+INTEGER FUNCTION NGCKOP(WKID)
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+int c_ngckop(int wkid)
+.SH DESCRIPTION 
+.IP WKID 12
+(an input variable of type INTEGER) specifying a  GKS workstation
+identifier.
+.SH C-BINDING DESCRIPTION
+The C binding argument descriptions are the same as the FORTRAN
+argument descriptions.
+.SH USAGE
+The function returns a "1" if the workstation identified by
+WKID is open, otherwise it returns a "0".
+.SH EXAMPLES
+.nf
+
+        IOPEN = NGCKOP(WKID)
+
+.fi
+sets IOPEN to "1" if WKID is open and sets IOPEN to "0" otherwise.
+.SH ACCESS
+To use NGCKOP, load the NCAR Graphics libraries ncarg, ncarg_gks,
+and ncarg_c, preferably in that order.  To use c_ngdots, load the
+NCAR Graphics libraries ncargC, ncarg_gksC, ncarg, ncarg_gks, and 
+ncarg_c, preferably in that order.
+.SH SEE ALSO
+Online URL:
+.SH COPYRIGHT
+Copyright (C) 1987-1996
+.br
+University Corporation for Atmospheric Research
+.br
+The use of this Software is governed by a License Agreement.
