b4818168069d4cbab406ca4608957f25809bc58f
fred <fred>
1997-03-04 23:52:00 +0000

    Added initial include file for libngmath that will be installed. Changed the yMakefile to accommodate for this.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4352 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ngmath/src/lib/ngmath.h
@@ -0,0 +1,26 @@
+/*
+ *  Function prototypes for the natgrid package.
+ */
+void    c_nnseti(char *, int);
+void    c_nngeti(char *, int *);
+void    c_nnsetr(char *, float);
+void    c_nngetr(char *, float *);
+void    c_nnsetc(char *, char *);
+void    c_nngetc(char *, char *);
+void    c_nngetslopes(int, int, float *, int *);
+void    c_nngetaspects(int, int, float *, int *);
+void    c_nnpntinits(int, float [], float [], float []);
+void    c_nnpnts(float, float, float *);
+void    c_nnpntend();
+float   **c_natgrids(int, float [], float [], float [],
+                     int, int, float [], float [], int *);
+
+void    c_nnsetrd(char *, double);
+void    c_nngetrd(char *, double *);
+void    c_nngetsloped(int, int, double *, int *);
+void    c_nngetaspectd(int, int, double *, int *);
+void    c_nnpntinitd(int, double [], double [], double []);
+void    c_nnpntd(double, double, double *);
+void    c_nnpntendd();
+double  **c_natgridd(int, double [], double [], double [],
+                     int, int, double [], double [], int *);
