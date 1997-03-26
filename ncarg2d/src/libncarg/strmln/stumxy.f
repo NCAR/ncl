b7bb9ea05081ca020b141f651645ac3e986808b1
Mary Haley <haley@ucar.edu>
1997-03-26 15:39:48 +0000

    Put stumxy.f back the way it was, with stuixy and stumta part of it.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4410 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/strmln/stumxy.f
+++ ncarg2d/src/libncarg/strmln/stumxy.f
@@ -1,5 +1,5 @@
 C
-C       $Id: stumxy.f,v 1.5 1997-03-25 21:32:07 haley Exp $
+C       $Id: stumxy.f,v 1.6 1997-03-26 15:39:48 haley Exp $
 C
 C-----------------------------------------------------------------------
 C
@@ -49,6 +49,111 @@ C
       IST=0
       XUS=XDA
       YUS=YDA
+C
+C Done.
+C
+      RETURN
+C
+      END
+C
+C ---------------------------------------------------------------------
+C
+      SUBROUTINE STUIXY(XUS,YUS,XDA,YDA,IST)
+C
+C User modifiable routine for inversely transforming
+C a point in user coordinate space to data space
+C
+C Input parameters:
+C
+C XUS,YUS - Point in user coordinate space
+C
+C Output parameters:
+C
+C XDA,YDA - Point in data coordinate space
+C IST     - Status code indicating success or failure
+C
+C --------------------------------------------------------------------
+C
+C The mapping common block: made available to user mapping routines
+C
+      COMMON /STMAP/
+     +                IMAP       ,LNLG       ,INVX       ,INVY       ,
+     +                XLOV       ,XHIV       ,YLOV       ,YHIV       ,
+     +                WXMN       ,WXMX       ,WYMN       ,WYMX       ,
+     +                XVPL       ,XVPR       ,YVPB       ,YVPT       ,
+     +                XGDS       ,YGDS       ,NXCT       ,NYCT       ,
+     +                ITRT       ,FW2W       ,FH2H       ,
+     +                DFMG       ,VNML       ,RBIG       ,IBIG
+C
+      SAVE /STMAP/
+C
+C Math constants
+C
+      PARAMETER (PDTOR  = 0.017453292519943,
+     +           PRTOD  = 57.2957795130823,
+     +           P1XPI  = 3.14159265358979,
+     +           P2XPI  = 6.28318530717959,
+     +           P1D2PI = 1.57079632679489,
+     +           P5D2PI = 7.85398163397448) 
+C
+C ---------------------------------------------------------------------
+C
+      IST=0
+      XDA=XUS
+      YDA=YUS
+C
+C Done
+C
+      RETURN
+      END
+C
+C ---------------------------------------------------------------------
+C
+      SUBROUTINE STUMTA(XDA,YDA,XUS,YUS,XND,YND,DU,DV,TA,IST)
+C
+C User modifiable routine for mapping a tangent angle in data space to 
+C normalized device coordinate space.
+C
+C Input parameters:
+C
+C XDA,YDA - Point in data coordinate space
+C XUS,YUS - Point in user coordinate space
+C XND,YND - Point in NDC space
+C DU,DV   - Differential vector components in data space
+C
+C Output parameters:
+C
+C TA      - Streamline tangent angle in NDC space
+C IST     - Status code indicating success or failure
+C
+C --------------------------------------------------------------------
+C
+C The mapping common block: made available to user mapping routines
+C
+      COMMON /STMAP/
+     +                IMAP       ,LNLG       ,INVX       ,INVY       ,
+     +                XLOV       ,XHIV       ,YLOV       ,YHIV       ,
+     +                WXMN       ,WXMX       ,WYMN       ,WYMX       ,
+     +                XVPL       ,XVPR       ,YVPB       ,YVPT       ,
+     +                XGDS       ,YGDS       ,NXCT       ,NYCT       ,
+     +                ITRT       ,FW2W       ,FH2H       ,
+     +                DFMG       ,VNML       ,RBIG       ,IBIG
+C
+      SAVE /STMAP/
+C
+C Math constants
+C
+      PARAMETER (PDTOR  = 0.017453292519943,
+     +           PRTOD  = 57.2957795130823,
+     +           P1XPI  = 3.14159265358979,
+     +           P2XPI  = 6.28318530717959,
+     +           P1D2PI = 1.57079632679489,
+     +           P5D2PI = 7.85398163397448) 
+C
+C ---------------------------------------------------------------------
+C
+      IST=0
+      TA=ATAN2(DV,DU)
 C
 C Done.
 C
