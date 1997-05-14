dac55f6b6837923b47903f63105614dffd3f2d75
boote <boote>
1997-05-14 23:08:13 +0000

    Linux changes...
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4499 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ common/src/libncarg_c/g77.c
@@ -0,0 +1,25 @@
+/*
+ *	$Id: g77.c,v 1.1 1997-05-14 23:08:12 boote Exp $
+ */
+/************************************************************************
+*									*
+*			     Copyright (C)  1997			*
+*	     University Corporation for Atmospheric Research		*
+*			     All Rights Reserved			*
+*									*
+************************************************************************/
+/*
+ *	File:		g77.c
+ *
+ *	Author:		Jeff W. Boote
+ *			National Center for Atmospheric Research
+ *			PO 3000, Boulder, Colorado
+ *
+ *	Date:		Tue May 13 18:02:18 MDT 1997
+ *
+ *	Description:    
+ */
+int
+MAIN__(void){
+	return 0;
+}
