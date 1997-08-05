03ccae9469f8124a2762acc1fbeb78304bee765d
fred <fred>
1997-08-05 23:14:44 +0000

    Added function prototypes for the dsgrid pacakge.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4629 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ngmath/src/lib/ngmath.h
+++ ngmath/src/lib/ngmath.h
@@ -1,3 +1,39 @@
+#include <stdio.h>
+
+/*
+ *  This file contains the function prototypes for all
+ *  user entry points in the ngmath library.
+ */
+
+
+/*
+ *  Function prototypes for the dsgrid package.
+ */
+void     c_dssetc(char *, char *);
+void     c_dsgetc(char *, char *);
+void     c_dsseti(char *, int);
+void     c_dsgeti(char *, int *);
+void     c_dsgetr(char *, float *);
+void     c_dssetr(char *, float);
+void     c_dssetrd(char *, double);
+void     c_dsgetrd(char *, double *);
+float    *c_dsgrid2s(int, float [], float [], float [],
+                     int, int, float [], float [], int *);
+double   *c_dsgrid2d(int, double [], double [], double [],
+                     int, int, double [], double [], int *);
+float    *c_dsgrid3s(int, float [], float [], float [], float [],
+                     int, int, int, float [], float [], float [], int *);
+double   *c_dsgrid3d(int, double [], double [], double [], double [],
+                     int, int, int, double [], double [], double [], int *);
+void     c_dspnt3d(int, double [], double [], double [], double [],
+                   int, double [], double [], double [], double [], int *);
+void     c_dspnt2d(int, double [], double [], double [],
+                   int, double [], double [], double [], int *);
+void     c_dspnt3s(int, float [], float [], float [], float [],
+                   int, float [], float [], float [], float [], int *);
+void     c_dspnt2s(int, float [], float [], float [],
+                   int, float [], float [], float [], int *);
+
 /*
  *  Function prototypes for the natgrid package.
  */
