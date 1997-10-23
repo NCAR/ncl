a95e5357935fead16b481f1b7e5fb2c724f28048
Dave Brown <dbrown@ucar.edu>
1997-10-23 17:34:49 +0000

    Removed unnecessary test code from this file.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@4741 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/ngo/mwin.c
+++ ni/src/lib/ngo/mwin.c
@@ -1,5 +1,5 @@
 /*
- *      $Id: mwin.c,v 1.12 1997-10-23 02:17:54 dbrown Exp $
+ *      $Id: mwin.c,v 1.13 1997-10-23 17:34:49 dbrown Exp $
  */
 /************************************************************************
 *									*
@@ -19,7 +19,6 @@
  *
  *	Description:	
  */
-#include "../libncarg_c/c.h"
 #include <ncarg/ngo/mwinP.h>
 
 #include <ncarg/ngo/nclstate.h>
@@ -122,10 +121,7 @@ MWInitialize
 	NgMWin			mwin = (NgMWin)new;
 	NgMWinPart		*np = &((NgMWin)new)->mwin;
 	NgMWinPart		*rp = &((NgMWin)req)->mwin;
-	const char		*version;
 
-	version = GetNCARGVersion();
-	version = NULL;
 	fprintf(stderr,"NCARG Version: %s\n",GetNCARGVersion());
 	return NhlNOERROR;
 }
