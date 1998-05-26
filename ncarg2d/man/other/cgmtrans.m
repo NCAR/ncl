1a6d93fc2bc609d73480e1c8d2b50c1f307d40b6
Mary Haley <haley@ucar.edu>
1998-05-26 16:51:04 +0000

    cgmtrans and ftrans are now officially obsolete.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5023 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/cgmtrans.m
+++
@@ -1,7 +0,0 @@
-.\"
-.\"	$Id: cgmtrans.m,v 1.5 1995-04-20 20:59:10 haley Exp $
-.\"
-.TH CGMTRANS 1NCARG "April 1995" NCAR "NCAR GRAPHICS"
-.SH NAME
-cgmtrans - as advertised, this command is now obsolete.  Please use
-\fBctrans\fR or \fBictrans\fR instead.
--- ncarg2d/man/other/ftrans.m
+++
@@ -1,7 +0,0 @@
-.\"
-.\"	$Id: ftrans.m,v 1.5 1995-04-20 20:59:13 haley Exp $
-.\"
-.TH FTRANS 1NCARG "April 1995" NCAR "NCAR GRAPHICS"
-.SH NAME
-ftrans - as advertised, this command is now obsolete.  Please use
-\fBctrans\fR or \fBictrans\fR instead.
