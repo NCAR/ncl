abae1d0712056f6887323b4379335ab9c916d83c
Mary Haley <haley@ucar.edu>
2000-01-26 07:44:00 +0000

    Modified to allow numeric (instead of just float) input. Did this one mostly as a test.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5745 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/wrunaveW.c
+++ ni/src/lib/nfp/wrunaveW.c
@@ -14,48 +14,57 @@
 #include <math.h>
 #include <ncarg/gks.h>
 
+extern void NGCALLF(wgtrunave,WGTRUNAVE)(double*,int*,double*,int*,int*,
+                                         double*,double*,int*,int*);
+
+extern void NGCALLF(runave,RUNAVE)(double*,int*,int*,int*,double*,double*,int*,
+                                   int*);
+
 NhlErrorTypes wgt_runave_W( void )
 {
 /*
  * Input array variables
  */
-  float *x, *x2, *wgt;
+  void *x, *wgt;
+  float *fx;
+  double *dx, *dwgt;
   int *kopt;
   int ndims_x, dsizes_x[NCL_MAX_DIMENSIONS], has_missing_x;
   int ndims_wgt, dsizes_wgt[NCL_MAX_DIMENSIONS];
+  NclBasicDataTypes type_x, type_wgt;
   NclScalar missing_x;
 /*
  * Work array.
  */
   int lwork;
-  float *work;
+  double *work;
 /*
  * Declare various variables for random purposes.
  */
-  int i, j, npts, nwgt, ier, size_x;
-  float xmsg;
+  int i, j, npts, nwgt, ier, size_x, total_size_x;
+  double xmsg;
 
 /*
  * Retrieve arguments.
  */
-  x = (float*)NclGetArgValue(
+  x = (void*)NclGetArgValue(
           0,
           3,
           &ndims_x,
           dsizes_x,
 		  &missing_x,
 		  &has_missing_x,
-          NULL,
+          &type_x,
           2);
 
-  wgt = (float*)NclGetArgValue(
+  wgt = (void*)NclGetArgValue(
           1,
           3,
           &ndims_wgt,
           dsizes_wgt,
           NULL,
           NULL,
-          NULL,
+          &type_wgt,
           2);
 
   kopt = (int*)NclGetArgValue(
@@ -68,25 +77,19 @@ NhlErrorTypes wgt_runave_W( void )
           NULL,
           2);
 /*
- * Test for missing values.
+ * Check input dimension sizes.
  */
-  if( has_missing_x ) {
-	xmsg = missing_x.floatval;
-  }
-  else {
-	xmsg = ((NclTypeClass)nclTypefloatClass)->type_class.default_mis.floatval;
+  npts = dsizes_x[ndims_x-1];
+  nwgt = dsizes_wgt[0];
+  if( nwgt > npts ) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: The length of wgt must be less than or equal to the last dimension of x");
+    return(NhlFATAL);
   }
 /*
  * wgt must be one-dimensional
  */
   if( ndims_wgt != 1 ) {
     NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: The input array wgt must be one-dimensional"); 
-    return(NhlFATAL);
-  }
-  npts = dsizes_x[ndims_x-1];
-  nwgt = dsizes_wgt[0];
-  if( nwgt > npts ) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: The length of wgt must be less than or equal to the last dimension of x");
     return(NhlFATAL);
   }
 /*
@@ -96,14 +99,64 @@ NhlErrorTypes wgt_runave_W( void )
   for( i = 0; i < ndims_x-1; i++ ) {
     size_x *= dsizes_x[i];
   }
+  total_size_x = size_x * npts;
+
 /*
- * Allocate space for work and output array.
+ * Coerce data to double if necessary. Since we have to make a copy of
+ * the first input array anyway (the input array will be overwritten by
+ * output array), we coerce it no matter what.
+ */
+  dx   = (double*)NclMalloc(sizeof(double)*total_size_x);
+  if( dx == NULL ) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: Unable to allocate memory for coercing input array");
+    return(NhlFATAL);
+  }
+  _Nclcoerce((NclTypeClass)nclTypedoubleClass,
+			 dx,
+			 x,
+			 total_size_x,
+			 &missing_x,
+			 NULL,
+			 _NclTypeEnumToTypeClass(_NclBasicDataTypeToObjType(type_x)));
+  
+  if(type_wgt != NCL_double) {
+	dwgt = (double*)NclMalloc(sizeof(double)*nwgt);
+	if( dwgt == NULL ) {
+	  NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: Unable to allocate memory for coercing weight array");
+	  return(NhlFATAL);
+	}
+	_Nclcoerce((NclTypeClass)nclTypedoubleClass,
+			   dwgt,
+			   wgt,
+			   nwgt,
+			   NULL,
+			   NULL,
+			   _NclTypeEnumToTypeClass(_NclBasicDataTypeToObjType(type_wgt)));
+  }
+  else {
+	dwgt = (double*)wgt;
+  }
+/*
+ * Test for missing values.
+ */
+  if( has_missing_x ) {
+	if(type_x == NCL_double) {
+	  xmsg = (double)missing_x.doubleval;
+	}
+	else {
+	  xmsg = (double)missing_x.floatval;
+	}
+  }
+  else {
+	xmsg = ((NclTypeClass)nclTypedoubleClass)->type_class.default_mis.doubleval;
+  }
+/*
+ * Allocate space for work array.
  */
   lwork = npts+2*(nwgt/2);
-  work = (float *)NclMalloc(lwork*sizeof(float));
-  x2   = (float *)NclMalloc( size_x*npts*sizeof(float));
-  if( x2 == NULL || work == NULL ) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: Unable to allocate memory for work arrays");
+  work = (double *)NclMalloc(lwork*sizeof(double));
+  if( work == NULL ) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: Unable to allocate memory for work array");
     return(NhlFATAL);
   }
   
@@ -111,23 +164,45 @@ NhlErrorTypes wgt_runave_W( void )
  * Call the Fortran version of this routine.
  *
  */
-  for( i = 0; i < size_x*npts; i++ ) {
-	x2[i] = x[i];
-  }
-
   j = 0;
   for( i = 0; i < size_x; i++ ) {
-	NGCALLF(wgtrunave, WGTRUNAVE)(&x2[j],&npts,wgt,&nwgt,kopt,&xmsg,
+	NGCALLF(wgtrunave, WGTRUNAVE)(&dx[j],&npts,dwgt,&nwgt,kopt,&xmsg,
 									work,&lwork,&ier);
 	j += npts;
   }
   free(work);
+  if((void*)dwgt != wgt) {
+	NclFree(dwgt);
+  }
 	
-  if( has_missing_x ) {
-	return(NclReturnValue((void*)x2,ndims_x,dsizes_x,&missing_x,NCL_float,0));
+  if(type_x != NCL_double) {
+/*
+ * Copy double values to float values.
+ */
+	fx = (float*)NclMalloc(sizeof(float)*total_size_x);
+	if( fx == NULL ) {
+	  NhlPError(NhlFATAL,NhlEUNKNOWN,"wgt_runave: Unable to allocate memory for return array");
+	  return(NhlFATAL);
+	}
+    for( i = 0; i < total_size_x; i++ ) {
+      fx[i] = (float)dx[i];
+    }
+
+	NclFree(dx);
+	if( has_missing_x ) {
+	  return(NclReturnValue((void*)fx,ndims_x,dsizes_x,&missing_x,NCL_float,0));
+	}
+	else {
+	  return(NclReturnValue((void*)fx,ndims_x,dsizes_x,NULL,NCL_float,0));
+	}
   }
   else {
-	return(NclReturnValue((void*)x2,ndims_x,dsizes_x,NULL,NCL_float,0));
+	if( has_missing_x ) {
+	  return(NclReturnValue((void*)dx,ndims_x,dsizes_x,&missing_x,NCL_double,0));
+	}
+	else {
+	  return(NclReturnValue((void*)dx,ndims_x,dsizes_x,NULL,NCL_double,0));
+	}
   }
 }
 
@@ -137,32 +212,35 @@ NhlErrorTypes runave_W( void )
 /*
  * Input array variables
  */
-  float *x, *x2;
+  void *x;
+  double *dx;
+  float *fx;
   int *nave, *kopt;
   int ndims_x, dsizes_x[NCL_MAX_DIMENSIONS], has_missing_x;
+  NclBasicDataTypes type_x;
   NclScalar missing_x;
 /*
  * Work array.
  */
   int lwork;
-  float *work;
+  double *work;
 /*
  * Declare various variables for random purposes.
  */
-  int i, j, k, npts, ier, size_x;
-  float xmsg;
+  int i, j, k, npts, ier, total_size_x, size_x;
+  double xmsg;
 
 /*
  * Retrieve arguments.
  */
-  x = (float*)NclGetArgValue(
+  x = (void*)NclGetArgValue(
           0,
           3,
           &ndims_x,
           dsizes_x,
 		  &missing_x,
 		  &has_missing_x,
-          NULL,
+          &type_x,
           2);
 
   nave = (int*)NclGetArgValue(
@@ -185,15 +263,6 @@ NhlErrorTypes runave_W( void )
           NULL,
           2);
 /*
- * Test for missing values.
- */
-  if( has_missing_x ) {
-	xmsg = missing_x.floatval;
-  }
-  else {
-	xmsg = ((NclTypeClass)nclTypefloatClass)->type_class.default_mis.floatval;
-  }
-/*
  * x and wgt must have the same leftmost dimensions.
  */
   npts = dsizes_x[ndims_x-1];
@@ -209,14 +278,46 @@ NhlErrorTypes runave_W( void )
   for( i = 0; i < ndims_x-1; i++ ) {
     size_x *= dsizes_x[i];
   }
+  total_size_x = size_x * npts;
+/*
+ * Coerce data to double if necessary. Since we have to make a copy of
+ * the first input array anyway (the input array will be overwritten by
+ * output array), we coerce it no matter what.
+ */
+  dx   = (double*)NclMalloc(sizeof(double)*total_size_x);
+  if( dx == NULL ) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"runave: Unable to allocate memory for coercing input array");
+    return(NhlFATAL);
+  }
+  _Nclcoerce((NclTypeClass)nclTypedoubleClass,
+			 dx,
+			 x,
+			 total_size_x,
+			 &missing_x,
+			 NULL,
+			 _NclTypeEnumToTypeClass(_NclBasicDataTypeToObjType(type_x)));
+  
+/*
+ * Test for missing values.
+ */
+  if( has_missing_x ) {
+	if(type_x == NCL_double) {
+	  xmsg = missing_x.doubleval;
+	}
+	else {
+	  xmsg = (double)missing_x.floatval;
+	}
+  }
+  else {
+	xmsg = ((NclTypeClass)nclTypedoubleClass)->type_class.default_mis.doubleval;
+  }
 /*
  * Allocate space for work array.
  */
   lwork = npts+2*(*nave/2);
-  work = (float *)NclMalloc(lwork*sizeof(float));
-  x2   = (float *)NclMalloc( size_x*npts*sizeof(float));
-  if( x2 == NULL || work == NULL ) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"runave: Unable to allocate memory for work arrays");
+  work = (double *)NclMalloc(lwork*sizeof(double));
+  if( work == NULL ) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"runave: Unable to allocate memory for work array");
     return(NhlFATAL);
   }
   
@@ -224,21 +325,43 @@ NhlErrorTypes runave_W( void )
  * Call the Fortran version of this routine.
  *
  */
-  for( i = 0; i < size_x*npts; i++ ) {
-	x2[i] = x[i];
-  }
+
   j = 0;
   for( i = 0; i < size_x; i++ ) {
-	NGCALLF(runave, RUNAVE)(&x2[j],&npts,nave,kopt,&xmsg,work,&lwork,&ier);
+	NGCALLF(runave, RUNAVE)(&dx[j],&npts,nave,kopt,&xmsg,work,&lwork,&ier);
 	j += npts;
   }
   free(work);
 	
-  if( has_missing_x ) {
-	return(NclReturnValue((void*)x2,ndims_x,dsizes_x,&missing_x,NCL_float,0));
+  if(type_x != NCL_double) {
+/*
+ * Copy double values to float values.
+ */
+	fx = (float*)NclMalloc(sizeof(float)*total_size_x);
+	if( fx == NULL ) {
+	  NhlPError(NhlFATAL,NhlEUNKNOWN,"runave: Unable to allocate memory for return array");
+	  return(NhlFATAL);
+	}
+    for( i = 0; i < total_size_x; i++ ) {
+      fx[i] = (float)dx[i];
+    }
+
+	NclFree(dx);
+
+	if( has_missing_x ) {
+	  return(NclReturnValue((void*)fx,ndims_x,dsizes_x,&missing_x,NCL_float,0));
+	}
+	else {
+	  return(NclReturnValue((void*)fx,ndims_x,dsizes_x,NULL,NCL_float,0));
+	}
   }
   else {
-	return(NclReturnValue((void*)x2,ndims_x,dsizes_x,NULL,NCL_float,0));
+	if( has_missing_x ) {
+	  return(NclReturnValue((void*)dx,ndims_x,dsizes_x,&missing_x,NCL_double,0));
+	}
+	else {
+	  return(NclReturnValue((void*)dx,ndims_x,dsizes_x,NULL,NCL_double,0));
+	}
   }
 }
 
