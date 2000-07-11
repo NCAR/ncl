cbe4ca956b53b9ea3edcbcab9e0516b0a145e313
Mary Haley <haley@ucar.edu>
2000-07-11 21:29:50 +0000

    Added limitation of warranty header.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5976 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- common/src/fontcap/ffprcf.f
+++ common/src/fontcap/ffprcf.f
@@ -1,6 +1,26 @@
 C
-C	$Id: ffprcf.f,v 1.1 1997-01-24 21:59:52 haley Exp $
+C	$Id: ffprcf.f,v 1.2 2000-07-11 21:29:48 haley Exp $
+C                                                                      
+C			     Copyright (C)  1997
+C	     University Corporation for Atmospheric Research
+C			     All Rights Reserved
 C
+C This file is free software; you can redistribute it and/or modify
+C it under the terms of the GNU Lesser General Public License as
+C published by the Free Software Foundation; either version 2.1 of the
+C License, or (at your option) any later version.
+C
+C This software is distributed in the hope that it will be useful, but
+C WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+C Lesser General Public License for more details.
+C
+C You should have received a copy of the GNU Lesser General Public
+C License along with this library; if not, write to the Free Software
+C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+C USA.
+C
+
       SUBROUTINE FFPRCF(IER)
 C
 C  Process the character entries in the ASCII fontcap.
