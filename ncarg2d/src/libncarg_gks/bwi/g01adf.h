0791e077ee35ce2dcb200d1bf072b2b40c51db03
Mary Haley <haley@ucar.edu>
2000-07-12 16:51:04 +0000

    Added limitation of warranty clause
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5986 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg_gks/bwi/g01adf.h
+++ ncarg2d/src/libncarg_gks/bwi/g01adf.h
@@ -1,3 +1,22 @@
+C
+C $Id: g01adf.h,v 1.2 2000-07-12 16:50:41 haley Exp $
+C
+C This file is free software; you can redistribute it and/or modify
+C it under the terms of the GNU Lesser General Public License as
+C published by the Free Software Foundation; either version 2.1 of the
+C License, or (at your option) any later version.
+C
+C This software is distributed in the hope that it will be useful, but
+C WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+C Lesser General Public License for more details.
+C
+C You should have received a copy of the GNU Lesser General Public
+C License along with this software; if not, write to the Free Software
+C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+C USA.
+C
+
       COMMON  /G01ADF/  MDPLIX  ,MDLTYP ,ADLWSC ,MDPLCI ,MDPMIX ,
      +                  MDMTYP  ,ADMSZS ,MDPMCI ,MDTXIX ,MDTXP  ,
      +                  MDTXAL(2)       ,MDCHH  ,MDCHOV(4)      ,
--- ncarg2d/src/libncarg_gks/bwi/wks.c
+++ ncarg2d/src/libncarg_gks/bwi/wks.c
@@ -1,15 +1,32 @@
 /*
- *      $Id: wks.c,v 1.3 1998-09-18 23:02:54 boote Exp $
+ *      $Id: wks.c,v 1.4 2000-07-12 16:51:04 haley Exp $
  */
 /***********************************************************************
 *                                                                      *
 *		NCAR Graphics - UNIX Version 3.1.3b                    *
-*		Copyright (C) 1987, 1988, 1989, 1991                   *
+*		Copyright (C) 1987, 1988, 1989, 1991, 2000             *
 *		University Corporation for Atmospheric Research        *
 *		All Rights Reserved                                    *
 *                                                                      *
 *                                                                      *
-***********************************************************************/
+************************************************************************
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU Lesser General Public License as        *
+* published by the Free Software Foundation; either version 2.1 of the  *
+* License, or (at your option) any later version.                       *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* Lesser General Public License for more details.                       *
+*                                                                       *
+* You should have received a copy of the GNU Lesser General Public      *
+* License along with this software; if not, write to the Free Software  *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
 
 /***********************************************************************
 *
