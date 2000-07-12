55cd3c3f12ebd951adcf70dca1cc7967071ec64c
Mary Haley <haley@ucar.edu>
2000-07-12 16:54:45 +0000

    Added limitation of warranty clause.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5988 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg_gks/wiss/gwiopc.h
+++ ncarg2d/src/libncarg_gks/wiss/gwiopc.h
@@ -1,4 +1,26 @@
 C
+C $Id: gwiopc.h,v 1.2 2000-07-12 16:54:43 haley Exp $
+C                                                                      
+C                Copyright (C)  2000
+C        University Corporation for Atmospheric Research
+C                All Rights Reserved
+C
+C This file is free software; you can redistribute it and/or modify
+C it under the terms of the GNU Lesser General Public License as
+C published by the Free Software Foundation; either version 2.1 of the
+C License, or (at your option) any later version.
+C
+C This software is distributed in the hope that it will be useful, but
+C WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+C Lesser General Public License for more details.
+C
+C You should have received a copy of the GNU Lesser General Public
+C License along with this software; if not, write to the Free Software
+C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+C USA.
+C
+C
 C  Id code parameters for every element, and class codes for each class.
 C
       COMMON /GWIOPC/ IDNOOP, IDBEGM, IDENDM, IDBEGP, IDBGPB, IDENDP
