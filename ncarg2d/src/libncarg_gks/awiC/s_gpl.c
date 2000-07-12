be0a5ab8ad7dffd6873c57d92959d5dd820f549e
Mary Haley <haley@ucar.edu>
2000-07-12 17:06:26 +0000

    Added limitation of warranty clause.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@5990 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg_gks/awiC/s_gpl.c
+++ ncarg2d/src/libncarg_gks/awiC/s_gpl.c
@@ -1,6 +1,29 @@
 /*
- *	$Id: s_gpl.c,v 1.2 1999-07-27 20:31:03 haley Exp $
+ *	$Id: s_gpl.c,v 1.3 2000-07-12 17:06:09 haley Exp $
  */
+/************************************************************************
+*                                                                       *
+*                Copyright (C)  2000                                    *
+*        University Corporation for Atmospheric Research                *
+*                All Rights Reserved                                    *
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU Lesser General Public License as        *
+* published by the Free Software Foundation; either version 2.1 of the  *
+* License, or (at your option) any later version.                       *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* Lesser General Public License for more details.                       *
+*                                                                       *
+* You should have received a copy of the GNU Lesser General Public      *
+* License along with this software; if not, write to the Free Software  *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
+
 /*
  *  Polyline  
  */
--- ncarg2d/src/libncarg_gks/awiC/s_gschxp.c
+++ ncarg2d/src/libncarg_gks/awiC/s_gschxp.c
@@ -1,6 +1,29 @@
 /*
- *	$Id: s_gschxp.c,v 1.1 1997-03-05 19:13:21 haley Exp $
+ *	$Id: s_gschxp.c,v 1.2 2000-07-12 17:06:21 haley Exp $
  */
+/************************************************************************
+*                                                                       *
+*                Copyright (C)  2000                                    *
+*        University Corporation for Atmospheric Research                *
+*                All Rights Reserved                                    *
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU Lesser General Public License as        *
+* published by the Free Software Foundation; either version 2.1 of the  *
+* License, or (at your option) any later version.                       *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* Lesser General Public License for more details.                       *
+*                                                                       *
+* You should have received a copy of the GNU Lesser General Public      *
+* License along with this software; if not, write to the Free Software  *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
+
 /*
  *  Set character expansion factor  
  */
