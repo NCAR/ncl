6ba13e505ac41bc61720d8bd7619a1f4d5e25a3f
Mary Haley <haley@ucar.edu>
2000-08-22 04:07:01 +0000

    Updated license.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6051 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- common/man/graphcap.m
+++ common/man/graphcap.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: graphcap.m,v 1.15 2000-07-11 22:06:40 haley Exp $
+.\"	$Id: graphcap.m,v 1.16 2000-08-22 04:07:01 haley Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH GRAPHCAP 5NCARG "March 1994" NCAR "NCAR GRAPHICS"
@@ -190,16 +190,16 @@ University Corporation for Atmospheric Research
 .br
 
 This documentation is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation; either version 2.1 of the
-License, or (at your option) any later version.
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This software is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this software; if not, write to the Free Software
+You should have received a copy of the GNU General Public License
+along with this software; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 USA.
