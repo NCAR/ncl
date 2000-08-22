b0375c9eddc7528c9efca43b9c1182106b87ad09
Mary Haley <haley@ucar.edu>
2000-08-22 04:16:58 +0000

    Updated license.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6052 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/gks/gqsgus.m
+++ ncarg2d/man/gks/gqsgus.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: gqsgus.m,v 1.9 2000-07-11 23:03:13 haley Exp $
+.\"	$Id: gqsgus.m,v 1.10 2000-08-22 04:16:01 haley Exp $
 .\"
 .TH GQSGUS 3NCARG "March 1993" UNIX "NCAR GRAPHICS"
 .SH NAME
@@ -54,16 +54,16 @@ University Corporation for Atmospheric Research
 .br
 
 This documentation is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation; either version 2.1 of the
-License, or (at your option) any later version.
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This software is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this software; if not, write to the Free Software
+You should have received a copy of the GNU General Public License
+along with this software; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 USA.
--- ncarg2d/man/other/ncargex.m
+++ ncarg2d/man/other/ncargex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargex.m,v 1.45 2000-07-11 23:03:58 haley Exp $
+.\"	$Id: ncargex.m,v 1.46 2000-08-22 04:16:26 haley Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGEX 1NCARG "June 1998" NCAR "NCAR GRAPHICS"
@@ -537,16 +537,16 @@ University Corporation for Atmospheric Research
 .br
 
 This documentation is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation; either version 2.1 of the
-License, or (at your option) any later version.
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This software is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this software; if not, write to the Free Software
+You should have received a copy of the GNU General Public License
+along with this software; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 USA.
