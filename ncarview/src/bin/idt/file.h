cbb4bbd8db18139c44e032066804437fae2c018d
Mary Haley <haley@ucar.edu>
2000-08-22 03:34:58 +0000

    Updated license.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6048 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarview/src/bin/idt/file.h
+++ ncarview/src/bin/idt/file.h
@@ -1,5 +1,5 @@
 /* 
- * $Id: file.h,v 1.3 2000-07-12 18:13:26 haley Exp $
+ * $Id: file.h,v 1.4 2000-08-22 03:30:17 haley Exp $
  */
 /************************************************************************
 *                                                                       *
@@ -8,17 +8,17 @@
 *                All Rights Reserved                                    *
 *                                                                       *
 * This file is free software; you can redistribute it and/or modify     *
-* it under the terms of the GNU Lesser General Public License as        *
-* published by the Free Software Foundation; either version 2.1 of the  *
-* License, or (at your option) any later version.                       *
+* it under the terms of the GNU General Public License as published     *
+* by the Free Software Foundation; either version 2 of the License, or  *
+* (at your option) any later version.                                   *
 *                                                                       *
 * This software is distributed in the hope that it will be useful, but  *
 * WITHOUT ANY WARRANTY; without even the implied warranty of            *
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-* Lesser General Public License for more details.                       *
+* General Public License for more details.                              *
 *                                                                       *
-* You should have received a copy of the GNU Lesser General Public      *
-* License along with this software; if not, write to the Free Software  *
+* You should have received a copy of the GNU General Public License     *
+* along with this software; if not, write to the Free Software          *
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
 * USA.                                                                  *
 *                                                                       *
