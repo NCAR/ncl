d3779bd8a76a282604063eb98bc24e6996195d56
kennison <kennison>
2000-10-27 19:45:14 +0000

    I have improved this routine to trim blanks from both ends of an incoming numeric label and to trim off any "1" or "1." or "1.0" or "1.00", etc., that precedes the "E" signalling an exponent (so that the resulting label will be of the form "10 [superscript] n", without the unnecessary "1 x" preceding it).
    There's a "feature" on some machines (SGI's, for example) that will cause
    this routine to fail to carry out its intended mission.  On such machines,
    using the default format '(E10.3)' can create something like " -0.100E-2"
    and this routine is not smart enough to realize that could be expressed as
    "10 [superscript] -3".  On such machines, you just have to change the format
    that is used to include a "1P" preceding the "E".
    
    So now GRIDAL has taken a step in the direction of becoming AUTOGRAPH ...
    which is how AUTOGRAPH came to be ... sigh ...
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6129 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/gridal/gaplch.f
+++ ncarg2d/src/libncarg/gridal/gaplch.f
@@ -1,5 +1,5 @@
 C
-C $Id: gaplch.f,v 1.1 2000-10-25 22:27:36 kennison Exp $
+C $Id: gaplch.f,v 1.2 2000-10-27 19:45:14 kennison Exp $
 C
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
@@ -25,14 +25,22 @@ C
         CHARACTER*128 LBL2
         CHARACTER*1 ISFC
         NCH1=LEN(LBL1)
+  101   IF (NCH1.GT.1) THEN
+          IF (LBL1(NCH1:NCH1).EQ.' ') THEN
+            NCH1=NCH1-1
+            GO TO 101
+          END IF
+        END IF
         IF (NCH1.GT.116) THEN
           CALL PLCHHQ (XPOS,YPOS,LBL1(1:NCH1),RCHW,RORI,RCEN)
         ELSE
           CALL PCGETC ('FC',ISFC)
           CALL PCSETC ('FC',':')
           NCH2=0
+          IEXP=0
           IRLZ=0
-          DO 101 I=1,NCH1
+          DO 104 I=1,NCH1
+            IF (LBL1(I:I).EQ.' ') GO TO 104
             IF (LBL1(I:I).NE.'E'.AND.LBL1(I:I).NE.'e') THEN
               IF (IRLZ.EQ.0.OR.LBL1(I:I).EQ.'-') THEN
                 NCH2=NCH2+1
@@ -44,12 +52,34 @@ C
                 IRLZ=0
               END IF
             ELSE
-              NCH2=NCH2+10
-              LBL2(NCH2-9:NCH2)=':L1:410:S:'
+              IEXP=1
               IRLZ=1
+              IBEG=1
+              IF (LBL2(1:1).EQ.'+'.OR.LBL2(1:1).EQ.'-') IBEG=2
+              IF (LBL2(IBEG:IBEG).EQ.'1') THEN
+                IF (IBEG.EQ.NCH2) THEN
+                  NCH2=IBEG+4
+                  LBL2(IBEG:IBEG+4)='10:S:'
+                ELSE IF (LBL2(IBEG+1:IBEG+1).EQ.'.') THEN
+                  DO 102 J=IBEG+2,NCH2
+                    IF (LBL2(J:J).NE.'0') GO TO 103
+  102             CONTINUE
+                  NCH2=IBEG+4
+                  LBL2(IBEG:IBEG+4)='10:S:'
+                  GO TO 104
+  103             NCH2=NCH2+10
+                  LBL2(NCH2-9:NCH2)=':L1:410:S:'
+                ELSE
+                  NCH2=NCH2+10
+                  LBL2(NCH2-9:NCH2)=':L1:410:S:'
+                END IF
+              ELSE
+                NCH2=NCH2+10
+                LBL2(NCH2-9:NCH2)=':L1:410:S:'
+              END IF
             END IF
-  101     CONTINUE
-          IF (NCH2.NE.NCH1) THEN
+  104     CONTINUE
+          IF (IEXP.NE.0) THEN
             NCH2=NCH2+3
             LBL2(NCH2-2:NCH2)=':N:'
           END IF
