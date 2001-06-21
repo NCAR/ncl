c72c8508f99f5cf2651d0f648a8278ff1ae59111
kennison <kennison>
2001-06-21 22:44:52 +0000

    The "man" page for "ncargex" now knows about "tdex07".
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6276 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/ncargex.m
+++ ncarg2d/man/other/ncargex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargex.m,v 1.47 2001-02-09 17:13:30 haley Exp $
+.\"	$Id: ncargex.m,v 1.48 2001-06-21 22:44:52 kennison Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGEX 1NCARG "June 1998" NCAR "NCAR GRAPHICS"
@@ -493,7 +493,7 @@ fsrezsrf fsrpwrzs fsrsrfac srex01 tsrfac tpwrzs c_srex01
 .sp
 .I "TDPACK Examples:"
 .sp
-tdex01 tdex02 tdex03 tdex04 tdex05 tdex06 c_tdex01 c_tdex03
+tdex01 tdex02 tdex03 tdex04 tdex05 tdex06 tdex07 c_tdex01 c_tdex03
 .sp
 .I "THREED Examples:"
 .sp
