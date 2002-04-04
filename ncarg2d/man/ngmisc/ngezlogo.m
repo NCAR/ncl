45d1c0b1619e21b2dcc581e5262201514f096537
fred <fred>
2002-04-04 22:08:47 +0000

    Added man pages for nglogo and ngezlogo.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6545 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/man/ngmisc/ngezlogo.m
@@ -0,0 +1,119 @@
+.TH NGEZLOGO 3NCARG "April 2002" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+NGEZLOGO - Draws various NCAR and UCAR logos, the default being
+to draw an NCAR logo of reasonable size in the lower right corner
+of a plot.
+.SH SYNOPSIS
+CALL NGEZLOGO()
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_ngezlogo()
+.SH USAGE
+Call this subroutine to produce a logo.  By default a
+call to this subroutine will draw an NCAR logo of reasonable 
+size in the lower right corner of the plot.  This will be
+a full-color logo for PostScript output, otherwise it will
+be a one-color logo.
+.sp
+Calls to the ngmisc paramter setting routines NGSETI and
+NGSETR can be used for
+more control over the ouput.  Calls to these routines can reproduce
+the functionality of the subroutine NGLOGO, except that the
+requested logo will be plotted to all active workstations, rather
+than a specified one.  Here is a list of
+control parameters and what they do:
+.RS
+.IP "'LT'" 
+\-  Use with NGSETI to specify the logo type.  See the documentation
+for NGLOGO for a list of the logo types, or execute "ncargex miex01"
+for an example plot that lists the logo types.
+.IP "'OS'" 
+\-  Use with NGSETR to specify the logo size in normalized device
+coordinates (a number between 0. and 1.).  For example, a size
+specification of 0.1 would give a logo size one-tenth the maximum
+plot height.
+.IP "'OX'" 
+\-  Use with NGSETR to specify the logo X-coordinate position in
+normalized device coordinates.
+.IP "'OY'" 
+\-  Use with NGSETR to specify the logo Y-coordinate position in
+normalized device coordinates.
+.IP "'LC'" 
+\-  Use with NGSETI to specify the logo color (except in the
+case of an NCAR logo being plotted to a PostScript workstation,
+in which case you will get the full-color logo).
+.IP "'LB'" 
+\-  Use with NGSETI to specify the secondary logo color (this is
+applicable only for logo type 5, in which case the secondary color
+is used for the text string "UCAR").
+.RE
+.sp
+.SH EXAMPLES
+.IP "CALL NGEZLOGO()"
+Plots an NCAR logo on all active workstations in the lower right
+corner of the plot.  In the case of PostScript output, a full-color
+logo will be produced, otherwise a one-color logo will be produced.
+.IP "Logo of type 2:"
+.RS
+CALL NGSETI('LT',2)
+.br
+CALL NGEZLOGO()
+.sp
+Plots a UCAR logo (just the star) in the lower right corner of
+the plot.
+.RE
+.sp
+.IP "More complete example:"
+.RS
+CALL NGSETI('LT',5)
+.br
+CALL NGSETR('OX',0.5)
+.br
+CALL NGSETR('OY',0.5)
+.br
+CALL NGSETR('OS',0.1)
+.br
+CALL NGSETR('LC',2)
+.br
+CALL NGSETR('LB',3)
+.br
+CALL NGEZLOGO()
+.sp
+Plots a logo of type 5 (UCAR star with text "UCAR") in the middle
+of the plot with size one-tenth the maximum plot height using color
+index 2 for the star and color index 3 for the text.
+.sp
+.RE
+.SH ACCESS
+To use NGEZLOGO or c_ngezlogo, load the NCAR Graphics 
+libraries ncarg, ncarg_gks,
+and ncarg_c, preferably in that order.  
+.SH SEE ALSO
+Online:
+nglogo, ngseti, ngsetr (3NCARG),
+.sp
+Hardcopy:
+NCAR Graphics Fundamentals, UNIX Version
+.SH COPYRIGHT
+Copyright (C) 1987-2002
+.br
+University Corporation for Atmospheric Research
+.br
+
+This documentation is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This software is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this software; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA.
---
+++ ncarg2d/man/ngmisc/nglogo.m
@@ -0,0 +1,102 @@
+.TH NGLOGO 3NCARG "April 2002" UNIX "NCAR GRAPHICS"
+.na
+.nh
+.SH NAME
+NGLOGO - Draws various NCAR and UCAR logos on a specified workstation.
+.SH SYNOPSIS
+CALL NGLOGO(IWK,X,Y,SIZE,ITYPE,ICOL1,ICOL2)
+.SH C-BINDING SYNOPSIS
+#include <ncarg/ncargC.h>
+.sp
+void c_nglogo(int iwk, float x, float y, float size, 
+.br
+int itype, int icol1, int icol2)
+.br
+.SH DESCRIPTION 
+.IP IWK 12
+(an input parameter of type INTEGER) The workstation identifier for 
+the workstation you want the logo plotted to.
+.IP X,Y 12
+(input parameters of type REAL) specifying the X-Y coordinate 
+position, in normalized device coordinates (between 0. and 1.), 
+where the symbol is to be positioned.  
+This position marks the horizontal and vertical center of the symbol.
+.IP SIZE 12
+(an input parameter of type REAL) The desired height of the logo, 
+expressed in normalized device coordinates.
+.IP ITYPE 12
+(an input parameter of type INTEGER) the logo type.  The choices are:
+.RS
+.IP "'1'" 
+\-  An NCAR logo.  This logo will be drawn in a single color if IWK 
+is not a PostScript workstation.  A full color logo will be put to a 
+PostScript workstation.
+.IP "'2'" 
+\-  A UCAR logo (just the UCAR star symbol).
+.IP "'3'" 
+\-  The text "NCAR" in Bell Gothic Black font.
+.IP "'4'" 
+\-  The text "UCAR" in Bell Gothic Black font.
+.IP "'5'" 
+\-  UCAR star logo, plus "UCAR" in Bell Gothic font at
+half the height of the star.  In this case, the
+coordinate (X,Y) specifies the center of the star part
+of the logo.
+.RE
+.IP ICOL1 12
+(an input parameter of type INTEGER) The color index to be used for the 
+logo color.  For the NCAR logo on PostScript output, this argument 
+is ignored.
+.IP ICOL2 12
+(an input parameter of type INTEGER) A secondary color index used 
+only for logo type 5.  For that type, the UCAR star logo is drawn 
+using color index ICOL1 and the text string "UCAR" is drawn using 
+color index ICOL2.
+.SH C-BINDING DESCRIPTION
+The C binding argument descriptions are the same as the FORTRAN
+argument descriptions.
+.SH USAGE
+Call this subroutine to produce the desired logo.
+.sp
+Use the ncargex command to see the following relevant
+example: 
+miex01.
+.SH EXAMPLES
+.IP "CALL NGWSYM(1, 0.5, 0.5, 0.1, 1, 1, 1)
+Plots an NCAR logo on workstation with ID 1 in the center of the plot 
+with height one-tenth the maximum plot size.  If going to a PostScript 
+workstation, the logo will be a full-color logo, otherwise
+the logo will be plotted using color index 1.
+.IP "CALL NGWSYM(1, 0.5, 0.5, 0.1, 2, 2, 1)
+Plots a UCAR star logo on workstation with ID 1 in the center of the plot
+with height one-tenth the maximum plot size. 
+The logo will be plotted using color index 2.
+.SH ACCESS
+To use NGLOGO or c_nglogo, load the NCAR Graphics libraries ncarg, ncarg_gks,
+and ncarg_c, preferably in that order.  
+.SH SEE ALSO
+Online:
+plotchar (font 35) (3NCARG),
+.sp
+Hardcopy:
+NCAR Graphics Fundamentals, UNIX Version
+.SH COPYRIGHT
+Copyright (C) 1987-2002
+.br
+University Corporation for Atmospheric Research
+.br
+
+This documentation is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This software is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this software; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA.
