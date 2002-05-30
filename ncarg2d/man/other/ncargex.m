ce11e23b431ab036dd9d1f6b51578b4ff8f072f6
kennison <kennison>
2002-05-30 16:37:29 +0000

    Updates the "man" page for "ncargex" to mention "cpex16".
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6614 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/ncargex.m
+++ ncarg2d/man/other/ncargex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargex.m,v 1.48 2001-06-21 22:44:52 kennison Exp $
+.\"	$Id: ncargex.m,v 1.49 2002-05-30 16:37:29 kennison Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGEX 1NCARG "June 1998" NCAR "NCAR GRAPHICS"
@@ -338,7 +338,7 @@ ccppc3 ccppc4 ccppkcl ccppole ccprc ccprect ccprwc ccprwu
 ccpscam ccpset ccpsps1 ccpsps2 ccpspv ccpt2d ccptitle
 ccpvp ccpvs cidsfft colcon cpex01 cpex02 cpex03 cpex04
 cpex05 cpex06 cpex07 cpex08 cpex09 cpex10 cpex11 cpex12 cpex13
-cpex14 cpex15 tconpa c_cbex01 c_colcon
+cpex14 cpex15 cpex16 tconpa c_cbex01 c_colcon
 .sp
 .I "CONRAN FAMILY Examples:"
 .sp
