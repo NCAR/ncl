13b9802c252662309232e4b24d905d259288ea8b
Mary Haley <haley@ucar.edu>
2002-12-20 17:41:49 +0000

    AIX didn't like "if(imsg)" test, so changed to "if(imsg.eq.1)".
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6747 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/round.f
+++ ni/src/lib/nfp/round.f
@@ -6,7 +6,7 @@
       integer  n
 
       do n=1,npts
-         if (.not.ismsg.or.(ismsg.and.xin(n).ne.xmsg)) then 
+         if (ismsg.eq.0.or.(ismsg.eq.1.and.xin(n).ne.xmsg)) then 
              xout(n) = anint(xin(n))
          else
              xout(n) = xmsg 
