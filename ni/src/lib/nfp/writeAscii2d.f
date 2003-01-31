a0c4b0c3d157af5657b955c5db7294af7493e2c3
grubin <grubin>
2003-01-31 19:35:27 +0000

    Subroutine print2d() is three separate routines, based on data type: NCL_int (INTEGER): print2di()  NCL_float REAL): print2df() NCL_double (DOUBLE PRECISION): print2dd()
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6778 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/writeAscii2d.f
+++ ni/src/lib/nfp/writeAscii2d.f
@@ -1,6 +1,6 @@
 C NCLFORTSTART
 
-      subroutine print2d(fname,ncol,nrow,x,fmtx,title,titsp,iopt)
+      subroutine print2df(fname,ncol,nrow,x,fmtx,title,titsp,iopt)
       implicit      none
 
 C NCL procedure: print2df(fname, x, fmtx, title, titsp, iopt)
@@ -8,19 +8,18 @@ C NCL procedure: print2df(fname, x, fmtx, title, titsp, iopt)
       character*(*) title, fname, fmtx
 
       integer titsp
-      character*3 titspc
+      character*2 titspc
 
       integer       ncol,nrow,iopt
-      double precision  x(nrow,ncol)
+      real          x(nrow,ncol)
 
 C NCLEND
 
       integer       nr, nc
       character*96  newfmt, titfmt
 
-
       if (titsp.gt.0) then
-          write(titspc,"(i3)") titsp
+          write(titspc,"(i2)") titsp
           titfmt = "(" // titspc // "x, a)" 
       else
           titfmt = "(x, a)" 
@@ -35,13 +34,14 @@ c write to a file
 
           newfmt = "(" // fmtx // ")" 
 
+          write(31, titfmt) " "
           if (iopt.eq.0) then
               newfmt = "(" // fmtx // ")" 
               do nr=1,nrow
                  write (31,newfmt) (x(nr,nc),nc=1,ncol)
               end do
           else
-              newfmt = "(i5," // fmtx // ")" 
+              newfmt = "(i5," // "2X" // fmtx // ")" 
               do nr=1,nrow
                  write (31,newfmt) nr, (x(nr,nc),nc=1,ncol)
               end do
@@ -60,7 +60,147 @@ c write to standard out
                  write (*,newfmt) (x(nr,nc),nc=1,ncol)
               end do
           else
-              newfmt = "(i5," // fmtx // ")" 
+              newfmt = "(i5," // "2X" // fmtx // ")" 
+              do nr=1,nrow
+                 write (*,newfmt) nr, (x(nr,nc),nc=1,ncol)
+              end do
+          end if
+          print *," "
+      end if
+
+      return
+      end
+
+
+C NCLFORTSTART
+
+      subroutine print2di(fname,ncol,nrow,x,fmtx,title,titsp,iopt)
+      implicit      none
+
+C NCL procedure: print2df(fname, x, fmtx, title, titsp, iopt)
+
+      character*(*) title, fname, fmtx
+      integer       ncol,nrow,iopt,titsp
+      integer       x(nrow,ncol)
+
+C NCLEND
+
+      integer       nr, nc
+      character*96  newfmt, titfmt
+      character*2   titspc
+
+      if (titsp.gt.0) then
+          write(titspc,"(i2)") titsp
+          titfmt = "(" // titspc // "x, a)"
+      else
+          titfmt = "(x, a)"
+      end if
+
+      if (fname.ne."*") then
+c write to a file
+          open  (31,file=fname,form="formatted")
+          if (.not.(title.eq."" .or. title.eq." ")) then
+              write (31,titfmt) title
+          end if
+
+          newfmt = "(" // fmtx // ")"
+
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) (x(nr,nc),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) nr, (x(nr,nc),nc=1,ncol)
+              end do
+          end if
+          close (31)
+      else
+c write to standard out
+          if (.not.(title.eq."" .or. title.eq." ")) then
+              write (*,titfmt) title
+          end if
+
+          print *," "
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) (x(nr,nc),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) nr, (x(nr,nc),nc=1,ncol)
+              end do
+          end if
+          print *," "
+      end if
+
+      return
+      end
+
+
+C NCLFORTSTART
+
+      subroutine print2dd(fname,ncol,nrow,x,fmtx,title,titsp,iopt)
+      implicit      none
+
+C NCL procedure: print2df(fname, x, fmtx, title, titsp, iopt)
+
+      character*(*) title, fname, fmtx
+      integer       ncol,nrow,iopt,titsp
+      double precision       x(nrow,ncol)
+
+C NCLEND
+      integer       nr, nc
+      character*96  newfmt, titfmt
+      character*2   titspc
+
+      if (titsp.gt.0) then
+          write(titspc,"(i2)") titsp
+          titfmt = "(" // titspc // "x, a)"
+      else
+          titfmt = " "
+      end if
+
+      if (fname.ne."*") then
+
+c write to a file
+          open  (31,file=fname,form="formatted")
+          if (.not.(title.eq."" .or. title.eq." ")) then
+              write (31,titfmt) title
+          end if
+
+          newfmt = "(" // fmtx // ")"
+
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) (x(nr,nc),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) nr, (x(nr,nc),nc=1,ncol)
+              end do
+          end if
+          close (31)
+      else
+c write to standard out
+          if (.not.(title.eq."" .or. title.eq." ")) then
+              write (*,titfmt) title
+          end if
+
+          print *," "
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) (x(nr,nc),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // fmtx // ")"
               do nr=1,nrow
                  write (*,newfmt) nr, (x(nr,nc),nc=1,ncol)
               end do
