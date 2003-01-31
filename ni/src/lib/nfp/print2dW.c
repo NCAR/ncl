26794e8a0b54aecf2105758091cd02b675cfa1ec
grubin <grubin>
2003-01-31 19:31:41 +0000

    Calls a proper variant of the FORTRAN function print2d() based on data type: NCL_int (INTEGER): print2di(), NCL_float (REAL): print2df(), NCL_double (DOUBLE PRECISION): print2dd().
    Data dimensions are taken from input data's metadata, rather than as a user
    supplied argument.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@6776 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/print2dW.c
+++ ni/src/lib/nfp/print2dW.c
@@ -1,3 +1,9 @@
+/*
+ * print2d
+ *
+ * Print 2D numeric array data
+ */
+
 # include   <stdio.h>
 
 /*
@@ -7,12 +13,25 @@
 # include   "wrapper.h"
 
 /*
- * FORTRAN function prototype.  All character pointer arguments must have
+ * FORTRAN function prototypes.  All character pointer arguments must have
  * a corresponding length argument passed to the FORTRAN function.  These
- * arguments follow the function's signature arguments. 
+ * arguments follow the function's signature arguments (as type "int").
+ *
+ * There are three different functions, based on data type:
+ *      NCL_int (short/int/long): FORTRAN type INTEGER -- print2di()
+ *      NCL_float (float): FORTRAN type REAL -- print2df()
+ *      NCL_double (double): FORTRAN type DOUBLE PRECISION -- print2dd()
  */
-extern void NGCALLF(print2d, PRINT2D)(char *, int *, int *, double *, char *,
-                                      char *, int *, int *, int, int, int);
+
+extern void NGCALLF(print2di, PRINT2DI)(char *, int *, int *, int *, char *,
+                                        char *, int *, int *, int, int, int);
+
+extern void NGCALLF(print2df, PRINT2DF)(char *, int *, int *, float *, char *,
+                                        char *, int *, int *, int, int, int);
+
+extern void NGCALLF(print2dd, PRINT2DD)(char *, int *, int *, double *, char *,
+                                        char *, int *, int *, int, int, int);
+
 
 
 NhlErrorTypes   print2d_W(void)
@@ -21,34 +40,21 @@ NhlErrorTypes   print2d_W(void)
      * Input variables and placeholders
      */
 
-    /* filename, data output format, title */
+    /* from NCL: filename, data output format, title */
     string  *fname,
             *fmtx,
             *t;
 
-    /* number of columns/rows of data, row number option */
-    int *columns,
-        *rows,
-        *rownumbers,
+    /* row number option, title spaces option */
+    int *rownumbers,
         *tspace;
 
-    /* data components: dimensions, missing data/value, type */
-    int dimsz_cols[2],
-        dimsz_rows[2];
-
+    /* 2D rectangular array components */
     void    *data;
-
-    int ndims,
-        has_missing,
+    int has_missing,
         dimsz[NCL_MAX_DIMENSIONS];
-
     NclScalar   missing;
     NclBasicDataTypes   dtype;
-
-    /* converted data parameters */
-    double  *data_dbl;
-    int total_data_sz = 0;
-    NclScalar   missing_data_dbl;
 
     /* functional character parameters */
     char    *filename,
@@ -60,71 +66,21 @@ NhlErrorTypes   print2d_W(void)
      * can be set to NULL, indicating you don't care about its value.
      */
 
-    /* Parameter #1: output filename */
-    fname = (string *) NclGetArgValue(
-        0,
-        8,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        2);
-    filename = NrmQuarkToString(*fname);
-
-    /* Parameter #2: number of data columns */
-    columns = (int *) NclGetArgValue(
-        1,
-        8,
-        NULL,
-        dimsz_cols,
-        NULL,
-        NULL,
-        NULL,
-        0);
-
-    /* Parameter #3: number of data rows */
-    rows = (int *) NclGetArgValue(
-        2,
-        8,
-        NULL,
-        dimsz_rows,
-        NULL,
-        NULL,
-        NULL,
-        0);
-
-    /* Parameter #4: data (2D rectangular array) */
+    /* Parameter #1: data (2D rectangular array) */
     data = (void *) NclGetArgValue(
-        3,
-        8,
-        &ndims,
+        0,
+        6,
+        NULL,
         dimsz,
         &missing,
         &has_missing,
         &dtype,
         0);
 
-    if (ndims < 2) {
-        NhlPError(NhlFATAL, NhlEUNKNOWN, "print2d: input data must be 2-dimensional.");
-        return NhlFATAL;
-    }
-
-    /* presently, all data is converted to type "double" */
-    total_data_sz = dimsz[0] * dimsz[1];
-    data_dbl = (double *) coerce_input_double(data, dtype, total_data_sz,
-                                              has_missing, &missing,
-                                              &missing_data_dbl);
-
-    if (data_dbl == NULL) {
-        NhlPError(NhlFATAL, NhlEUNKNOWN, "print2d: cannot allocate memory for data.");
-        return NhlFATAL;
-    }
-
-    /* Parameter #5: data print format */
+    /* Parameter #2: data print format */
     fmtx = (string *) NclGetArgValue(
-        4,
-        8,
+        1,
+        6,
         NULL,
         NULL,
         NULL,
@@ -133,10 +89,22 @@ NhlErrorTypes   print2d_W(void)
         0);
     format = NrmQuarkToString(*fmtx);
 
-    /* Parameter #6: title */
+    /* Parameter #3: output filename */
+    fname = (string *) NclGetArgValue(
+        2,
+        6,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        2);
+    filename = NrmQuarkToString(*fname);
+
+    /* Parameter #4: title */
     t = (string *) NclGetArgValue(
-        5,
-        8,
+        3,
+        6,
         NULL,
         NULL,
         NULL,
@@ -145,10 +113,10 @@ NhlErrorTypes   print2d_W(void)
         0);
     title = NrmQuarkToString(*t);
 
-    /* Parameter #7: number of spaces to print before the title */
+    /* Parameter #5: number of spaces to print before the title */
     tspace = (int *) NclGetArgValue(
+        4,
         6,
-        8,
         NULL,
         NULL,
         NULL,
@@ -156,10 +124,10 @@ NhlErrorTypes   print2d_W(void)
         NULL,
         0);
 
-    /* Parameter #8: option to print row numbers */
+    /* Parameter #6: option to print row numbers */
     rownumbers = (int *) NclGetArgValue(
-        7,
-        8,
+        5,
+        6,
         NULL,
         NULL,
         NULL,
@@ -168,13 +136,35 @@ NhlErrorTypes   print2d_W(void)
         0);
 
     /* call FORTRAN function */
-    NGCALLF(print2d, PRINT2D)(filename, columns, rows, data_dbl,
-            format, title, tspace, rownumbers,
-            strlen(filename), strlen(format), strlen(title));
+    switch (dtype) {
+        case NCL_short:
+            /* fall through */
+        case NCL_int:
+            /* fall through */
+        case NCL_long:
+            NGCALLF(print2di, PRINT2DI)(filename, &dimsz[0], &dimsz[1], data,
+                    format, title, tspace, rownumbers,
+                    strlen(filename), strlen(format), strlen(title));
+            break;
 
-    /* clean up */
-    if (data_dbl != (double *) NULL)
-        NclFree(data_dbl);
+        case NCL_float:
+            NGCALLF(print2df, PRINT2DF)(filename, &dimsz[0], &dimsz[1], data,
+                    format, title, tspace, rownumbers,
+                    strlen(filename), strlen(format), strlen(title));
+            break;
+
+        case NCL_double:
+            NGCALLF(print2dd, PRINT2DD)(filename, &dimsz[0], &dimsz[1], data,
+                    format, title, tspace, rownumbers,
+                    strlen(filename), strlen(format), strlen(title));
+            break;
+
+        default:
+            NhlPError(NhlFATAL, NhlEUNKNOWN,
+                "print2d: input data must be of numeric type");
+            return NhlFATAL;
+            break;
+    }
 
     return NhlNOERROR;
 }
