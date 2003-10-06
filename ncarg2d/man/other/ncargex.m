780d736bdc926772453d22092d5111288354f177
kennison <kennison>
2003-10-06 23:05:17 +0000

    Updated to mention "c_ctllg3".
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@7135 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/man/other/ncargex.m
+++ ncarg2d/man/other/ncargex.m
@@ -1,5 +1,5 @@
 .\"
-.\"	$Id: ncargex.m,v 1.52 2003-09-18 18:34:35 kennison Exp $
+.\"	$Id: ncargex.m,v 1.53 2003-10-06 23:05:17 kennison Exp $
 .\"
 .\" @(#)f77.1 1.4 86/07/31 SMI; 
 .TH NCARGEX 1NCARG "June 1998" NCAR "NCAR GRAPHICS"
@@ -347,7 +347,7 @@ cpex14 cpex15 cpex16 tconpa c_cbex01 c_colcon
 .I "CONPACKT Examples:"
 .sp
 ctex01 ctex02 ctfite ctgaus ctgeo1 ctgeo2 ctiscp ctllg1 ctllg2 ctllg3 ctorca
-ctpopg ctswth ctwng1 ctwng2
+ctpopg ctswth ctwng1 ctwng2 c_ctllg3
 .sp
 .I "CONRAN FAMILY Examples:"
 .sp
