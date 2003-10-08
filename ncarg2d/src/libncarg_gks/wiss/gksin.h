6d3d0d33fbd9cd7321383e7db69c27a1fd851471
fred <fred>
2003-10-08 22:39:52 +0000

    Another correction to a common block length.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@7143 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg_gks/wiss/gksin.h
+++ ncarg2d/src/libncarg_gks/wiss/gksin.h
@@ -1,5 +1,5 @@
 C
-C $Id: gksin.h,v 1.3 2000-08-22 03:45:43 haley Exp $
+C $Id: gksin.h,v 1.4 2003-10-08 22:39:52 fred Exp $
 C                                                                      
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
@@ -29,5 +29,5 @@ C
       INTEGER        FCODE , CONT  , IL1   , IL2   , ID    , IC1   ,
      +               IC2   , IC    , RL1   , RL2   , STRL1 , STRL2 ,
      +               RERR
-      CHARACTER*80   STR
+      CHARACTER*160  STR
       REAL           RX    , RY
