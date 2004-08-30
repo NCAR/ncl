8b327c23463775c6b78e9fdadc0f0c78bfee7612
Dennis Shea <shea@ucar.edu>
2004-08-30 22:25:44 +0000

    This replaces the code in the prefious file of the same name.
    [1] The interface/subroutine DSIMPSN no longer exists.
    
    [2] DSIMPSN has been replaced by DSIMPEQ. It has the
        exact same interface as DSIMPSN so it should cause
        Mary only trivial changes [name change].
    
    [3] There is a 2nd function in the same file, DSIMPNE.
        This allows unequally spaced data.
    
        Unlike the Fitgrid "ftcurvi", it allows missing values.
        It ignores them. :-) Further, it accepts 'numeric'
        type rather than "float"
    
    C NCLFORTSTART
          SUBROUTINE DSIMPNE(NUM,X,Y,YMSG,ANS)
          IMPLICIT NONE
          INTEGER  NUM
          DOUBLE PRECISION X(NUM), Y(NUM), ANS, YMSG
    C NCLEND
    
    
    =======
    The original way this came about was cuz a user was looking
    for integration functions. He looked under "Computational"
    and did not see any.
    
    Note: my prototype documentation explicitly has links
          to "ftcurvi" and "vibeta". The latter is a very
          special way to perform vertical integration
          in the atmosphere.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@7695 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/simpson_dp.f
+++ ni/src/lib/nfp/simpson_dp.f
@@ -1,36 +1,181 @@
 C NCLFORTSTART
-      SUBROUTINE DSIMPN(N,DX,FI,S)
+      SUBROUTINE DSIMPEQ (NUM,DX,Y,ANS)
       IMPLICIT NONE
-C
-C NCL:   fx = simpson(f,dx)
-C
-C Subroutine for integration over f(x) with the Simpson rule.
-C N: # pts; FI: integrand f(x); DX: interval; S: integral.
-C
-C     -----------------> INPUT
-      INTEGER N
-      DOUBLE PRECISION FI(N),DX
-C     -----------------> OUTPUT
-      DOUBLE PRECISION S
+      INTEGER  NUM
+      DOUBLE PRECISION DX,Y(NUM),ANS
 C NCLEND
-C     -----------------> LOCAL
+C
+C THE NSSL "simpsn.f" DOES NOT WORK. THIS IS NOW JUST A FRONT-END
+C DRIVER THAT ALLOWS THE SAME INTERFACE TO BE USED BUT IT INVOKES
+C THE *SLIGHTLY* LESS ACCURATE "simpne.f" TO PERFORM THE INTEGRATION.
+C
+C SOURCE                 NCAR's NSSL [simpsn.f]
+C ARGUMENTS
+C
+C ON INPUT               DX
+C                          A SINGLE VARIABLE SPECIFYING THE INCREMENT
+C                          FOR THE EQUALLY SPACED ABSCISSAS.
+C
+C                        Y
+C                          VECTOR OF ORDINATES.
+C
+C                        NUM
+C                          LENGTH OF VECTOR Y (SHOULD BE .GT. 2).
+C
+C ON OUTPUT              ANS
+C                          ANSWER [set to -9999. if error]
+C
+C ALGORITHM              SIMPSON'S RULE AND LAGRANGIAN INTERPOLATION
+C
+C ACCURACY               FOR EQUALLY SPACED ABSCISSAE THE ERROR IS
+C                        PROPORTIONAL TO  H**4 TIMES THE FOURTH
+C                        DERIVATIVE OF F  AT C, WHERE C IS SOME
+C                        POINT WITHIN THE LIMITS OF INTEGRATION.
+C                        FOR UNEQUALLY SPACED ABSCISSAE THE ERROR
+C                        IS PROPORTIONAL TO THE FOURTH DERIVATIVE
+C                        OF F AT X TIMES THE FOURTH POWER OF THE
+C                        MAXIMUM DISTANCE BETWEEN ADJACENT ABSCISSAE.
+C **********************************************************************
+
+C LOCAL
+      DOUBLE PRECISION X(NUM)
       INTEGER I
-      DOUBLE PRECISION S0,S1,S2
 
-      S  = 0.D0
-      S0 = 0.D0
-      S1 = 0.D0
-      S2 = 0.D0
-      DO I=2,N-1,2
-         S1 = S1 + FI(I-1)
-         S0 = S0 + FI(I)
-         S2 = S2 + FI(I+1)
+C CREATE A 1D EQUALLY SPACED ARRAY
+      DO I=1,NUM
+         X(I) = DBLE(I-1)*DX
       END DO
-      S = DX*(S1+4.D0*S0+S2)/3.D0
-C
-C If N is even, add the last slice separately
-C
-      IF (MOD(N,2).EQ.0) S= S+DX*(5.D0*FI(N)+8.D0*FI(N-1)-FI(N-2))/12.D0
 
+C INVOKE THE SIMPSON RULE FOR UNEQUALLY SPACED DATA
+      CALL DSIMPNE2(NUM,X,Y,ANS)
+
+      RETURN
+      END
+C ========================================================
+C NCLFORTSTART
+      SUBROUTINE DSIMPNE(NUM,X,Y,YMSG,ANS)
+      IMPLICIT NONE
+      INTEGER  NUM
+      DOUBLE PRECISION X(NUM), Y(NUM), ANS, YMSG
+C NCLEND
+      INTEGER N,NUSE
+      DOUBLE PRECISION XX(NUM), YY(NUM)
+
+C STRIP MSG VALUES
+      NUSE = 0
+      DO N=1,NUM
+         IF (Y(N).NE.YMSG) THEN
+             NUSE = NUSE + 1
+             XX(NUSE) = X(N)
+             YY(NUSE) = Y(N)
+         END IF
+      END DO
+
+      CALL DSIMPNE2(NUSE,XX,YY,ANS)
+
+      RETURN
+      END
+C ---------------------------------------------------------
+      SUBROUTINE DSIMPNE2(NUM,X,Y,ANS)
+      IMPLICIT NONE
+      INTEGER  NUM
+      DOUBLE PRECISION X(NUM), Y(NUM), ANS
+C
+C SOURCE                 NCAR's NSSL [simpne.f]
+C
+C DIMENSION OF           X(NUM),Y(NUM)
+C ARGUMENTS
+C
+C PURPOSE                TO INTEGRATE OVER A GIVEN SET OF ORDINATES
+C                        BY ANALYTICALLY INTEGRATING A 3-POINT
+C                        LAGRANGIAN INTERPOLATION POLYNOMIAL
+C                        FITTING THE ORDINATES.
+C
+C ARGUMENTS
+C
+C ON INPUT               X
+C                          THIS IS A VECTOR OF LENGTH NUM SPECIFYING
+C                          THE UNEQUALLY SPACED ABSCISSAS.
+C
+C                        Y
+C                          VECTOR OF ORDINATES.
+C
+C                        NUM
+C                          LENGTH OF VECTOR Y (SHOULD BE .GT. 2).
+C
+C **********************************************************************
+      DOUBLE PRECISION DEL(3),PI(3),G(3),THIRD,HALF,TWO3R
+      DOUBLE PRECISION X1,X2,X3,E,F,FEINTS,DELPRD,SUMT
+      INTEGER IER,N,I
+
+      IF (NUM.LE.2) THEN
+          IER = 32
+          ANS = -9999.D0
+          RETURN
+      END IF
+
+      IER = 0
+      N   = 1
+      ANS = 0.D0
+
+      THIRD = 1.D0/3.D0
+      HALF  = 1.D0/2.D0
+      TWO3R = 2.D0/3.D0
+
+  105 CONTINUE
+      X1 = X(N)
+      X2 = X(N+1)
+      X3 = X(N+2)
+      E  = X3*X3 - X1*X1
+      F  = X3*X3*X3 - X1*X1*X1
+      FEINTS = X3 - X1
+      DEL(1) = X3 - X2
+      DEL(2) = -FEINTS
+      DEL(3) = X2 - X1
+      G(1)   = X2 + X3
+      G(2)   = X1 + X3
+      G(3)   = X1 + X2
+      PI(1)  = X2*X3
+      PI(2)  = X1*X3
+      PI(3)  = X1*X2
+      DELPRD = DEL(1)*DEL(2)*DEL(3)
+
+      SUMT = 0.D0
+      DO I = 1,3
+          SUMT = SUMT + Y(N-1+I)*DEL(I)*
+     +           (THIRD*F-G(I)*HALF*E+PI(I)*FEINTS)
+      END DO
+      ANS = ANS - SUMT/DELPRD
+
+      N = N + 2
+      IF (NUM.GT. (N+1)) GO TO 105
+
+      IF (MOD(NUM,2).NE.0) RETURN
+
+      N  = NUM - 2
+      X3 = X(NUM)
+      X2 = X(NUM-1)
+      X1 = X(NUM-2)
+      E  = X3*X3 - X2*X2
+      F  = X3*X3*X3 - X2*X2*X2
+      FEINTS = X3 - X2
+      DEL(1) = FEINTS
+      DEL(2) = X1 - X3
+      DEL(3) = X2 - X1
+      G(1)   = X2 + X3
+      G(2)   = X1 + X3
+      G(3)   = X1 + X2
+      PI(1)  = X2*X3
+      PI(2)  = X1*X3
+      PI(3)  = X1*X2
+      DELPRD = DEL(1)*DEL(2)*DEL(3)
+
+  107 SUMT = 0.D0
+      DO I = 1,3
+          SUMT = SUMT + Y(N-1+I)*DEL(I)*
+     +           (THIRD*F-G(I)*HALF*E+PI(I)*FEINTS)
+      END DO
+
+      ANS = ANS - SUMT/DELPRD
       RETURN
       END
