c2e7eba51e9b81846737bc497ccfb95e991d8f7b
kennison <kennison>
2005-04-14 20:23:37 +0000

    This adds two routines to the C interface for EZMAP, enabling the two new FORTRAN routines to be called from a C program.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@8150 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ncarg2d/src/libncarg/ezmapCC/c_maplmm.c
@@ -0,0 +1,32 @@
+/*
+ *      $Id: c_maplmm.c,v 1.1 2005-04-14 20:23:36 kennison Exp $
+ */
+/************************************************************************
+*                                                                       *
+*                Copyright (C)  2000                                    *
+*        University Corporation for Atmospheric Research                *
+*                All Rights Reserved                                    *
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published     *
+* by the Free Software Foundation; either version 2 of the License, or  *
+* (at your option) any later version.                                   *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* General Public License for more details.                              *
+*                                                                       *
+* You should have received a copy of the GNU General Public License     *
+* along with this software; if not, write to the Free Software         *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
+
+#include <ncarg/ncargC.h>
+
+void c_maplmm()
+{
+    NGCALLF(maplmm,MAPLMM)();
+}
