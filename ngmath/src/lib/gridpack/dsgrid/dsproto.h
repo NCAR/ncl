e726c49242af7c905db8348056b84c9a66750177
fred <fred>
2005-05-06 21:20:43 +0000

    Put the macros "MAX" and "MIN" back in here.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@8201 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ngmath/src/lib/gridpack/dsgrid/dsproto.h
+++ ngmath/src/lib/gridpack/dsgrid/dsproto.h
@@ -21,6 +21,14 @@
 *                                                                       *
 ************************************************************************/
 
+#ifndef MAX
+#define MAX(a,b)        (((a)>(b))?(a):(b))
+#endif
+
+#ifndef MIN
+#define MIN(a,b)        (((a)<(b))?(a):(b))
+#endif
+
 /*
  *  Specify function prototypes.
  */
