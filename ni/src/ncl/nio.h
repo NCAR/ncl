2f9e90bdb510bf4f666484f655989c3fd338ba05
Dave Brown <dbrown@ucar.edu>
2005-05-27 00:11:21 +0000

    This is the header file that needs to be included when building an interface that uses libnio.a.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@8221 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/ncl/nio.h
@@ -0,0 +1,11 @@
+#include <ncarg/hlu/hlu.h>
+#include <ncarg/hlu/NresDB.h>
+#include "defs.h"
+#include "Symbol.h"
+#include "NclDataDefs.h"
+#include "NclFile.h"
+#include "NclVar.h"
+#include "NclFileInterfaces.h"
+#include "DataSupport.h"
+#include "FileSupport.h"
+#include "NclMdInc.h"
