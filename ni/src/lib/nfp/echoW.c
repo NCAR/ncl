5ad86c8aa892db26b6d22a0fa698713b3893a18f
Mary Haley <haley@ucar.edu>
2005-07-19 13:21:48 +0000

    It was getting difficult to keep adding echo_on and echo_off to this file everytime I made a chance to it, so I decided to move these two routines to their own file and check them in.
    These two files are still only a part of wrapper.c in a comment
    statement, and this file has not yet been added to yMakefile.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@8270 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfp/echoW.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "wrapper.h"
+
+extern short NCLecho;
+extern short load_stmt;
+extern char *cur_load_file;
+
+short   echo_val = 0;
+
+NhlErrorTypes echo_on_W(void)
+{
+    echo_val = NCLecho;
+    NCLecho = 1;
+    load_stmt = 0;
+    (void) nclfprintf(stdout, "+ %s : %s\n", "echo_on()", cur_load_file);
+    return(NhlNOERROR);
+}
+
+NhlErrorTypes echo_off_W(void)
+{
+    if (NCLecho) {
+        NCLecho = echo_val;
+        load_stmt = 1;
+    }
+    echo_val = 0;
+    return(NhlNOERROR);
+}
