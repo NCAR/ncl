36ffffec2be5b15d89591d3e5e7ebb9fca0d6694
Mary Haley <haley@ucar.edu>
2006-09-05 22:31:14 +0000

    Dennis is in sooooo much trouble. He is asking for an NCL interface to be made to this dim_avg_wgt routine.
    But, he did answer all those ncl-talk questions over the weekend, so
    maybe we're even...
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@9129 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfpfort/dimavgwgt.f
@@ -0,0 +1,46 @@
+C NCLFORTSTART
+      subroutine dimavgwgt(nx, x, xmsg, wgt, iopt, xavg)
+      implicit none
+C                                 INPUT
+      integer  nx, iopt
+      double precision x(nx), wgt(nx), xmsg
+C                                 OUTPUT
+      double precision xavg
+C NCLEND
+      integer n, npts
+      double precision xsum, wsum 
+
+C NCL:  xWgtAvg = dim_avg_wgt(x, wgt, opt)
+
+      xavg = xmsg
+      xsum = 0.d0
+      wsum = 0.d0
+      npts = 0
+
+      do n=1,nx
+         if (x(n).ne.xmsg) then
+             xsum = xsum + x(n)*wgt(n)
+             wsum = wsum + wgt(n)
+             npts = npts + 1
+         end if
+      end do
+
+      if (wsum.gt.0.d0) then
+          if (npts.eq.nx ) then
+              xavg = xsum/wsum
+              return
+          end if
+
+          if (iopt.eq.1) then
+              xavg = xsum/wsum
+              return
+          end if
+
+          if (npts.ge.iopt .and. wsum.gt.0.d0) then
+              xavg = xsum/wsum
+              return
+          end if
+      end if
+
+      return
+      end
