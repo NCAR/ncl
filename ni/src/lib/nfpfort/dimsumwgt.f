419261f2259b3d3e74f96237cb925fdd9bc0711a
Mary Haley <haley@ucar.edu>
2006-09-08 20:57:54 +0000

    While I'm sitting here, debugging the rip stuff (which takes a few minutes), I'm going to create the wrapper for this one.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@9141 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfpfort/dimsumwgt.f
@@ -0,0 +1,39 @@
+C NCLFORTSTART
+      subroutine dimsumwgt(nx, x, xmsg, wgt, iopt, sumx)
+      implicit none
+C                                 INPUT
+      integer  nx, iopt
+      double precision x(nx), wgt(nx), xmsg
+C                                 OUTPUT
+      double precision sumx
+C NCLEND
+      integer n, npts
+      double precision xsum 
+
+C NCL:  xWgtSum = dim_sum_wgt(x, wgt, opt)
+
+      sumx = xmsg
+      xsum = 0.d0
+      npts = 0
+
+      do n=1,nx
+         if (x(n).ne.xmsg) then
+             xsum = xsum + x(n)*wgt(n)
+             npts = npts + 1
+         end if
+      end do
+
+      if (npts.gt.0) then
+          if (npts.eq.nx .or. iopt.eq.1) then
+              sumx = xsum
+              return
+          end if
+
+          if (npts.ge.iopt) then
+              sumx = xsum
+              return
+          end if
+      end if
+
+      return
+      end
