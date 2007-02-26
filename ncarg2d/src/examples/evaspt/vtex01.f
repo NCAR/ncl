7a8f29e0e49800922e87c8fa3f9296a2b409c7fd
kennison <kennison>
2007-02-26 18:37:41 +0000

    Makes the test fields used in these examples, which are generated using a random number generator, the same, no matter what machine you run the example on.  This is to address an issue raised by Mary.
    I think, however, that the plots you get will still vary from machine
    to machine, because a different random number generator is used inside
    the VASPACKT routines that position simple vectors, curly vectors, and
    streamlines.  (The same field will be depicted now, but the details of
    the depiction may vary.)
    
    I am reluctant to change the internal random number generator.  I will
    have to think about the ins and outs of this.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@9484 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/examples/evaspt/vtex01.f
+++ ncarg2d/src/examples/evaspt/vtex01.f
@@ -1214,9 +1214,12 @@ C
         NCNT=NLOW+NHGH
 C
         DO 101 ICNT=1,NCNT
-          CCNT(1,ICNT)=XMIN+XRNG*FRAN()
-          CCNT(2,ICNT)=YMIN+YRNG*FRAN()
-          CCNT(3,ICNT)=ZMIN+ZRNG*FRAN()
+C         CCNT(1,ICNT)=XMIN+XRNG*FRAN()
+C         CCNT(2,ICNT)=YMIN+YRNG*FRAN()
+C         CCNT(3,ICNT)=ZMIN+ZRNG*FRAN()
+          CCNT(1,ICNT)=XMIN+XRNG*GRAN()
+          CCNT(2,ICNT)=YMIN+YRNG*GRAN()
+          CCNT(3,ICNT)=ZMIN+ZRNG*GRAN()
           IF (ICNT.LE.NLOW) THEN
             CCNT(4,ICNT)=-1.
           ELSE
@@ -1264,6 +1267,31 @@ C
 C
         RETURN
 C
+      END
+
+
+      FUNCTION GRAN ()
+C
+C This function generates a sequence of "random" numbers.  Obviously,
+C it repeats after the 100th such number.  This is not very important,
+C because of the way in which these numbers are being used.
+C
+      DIMENSION RSEQ (100)
+      SAVE ISEQ
+      DATA RSEQ / .749,.973,.666,.804,.081,.483,.919,.903,.951,.960 ,
+     +            .039,.269,.270,.756,.222,.478,.621,.063,.550,.798 ,
+     +            .027,.569,.149,.697,.451,.738,.508,.041,.266,.249 ,
+     +            .019,.191,.266,.625,.492,.940,.508,.406,.972,.311 ,
+     +            .757,.378,.299,.536,.619,.844,.342,.295,.447,.499 ,
+     +            .688,.193,.225,.520,.954,.749,.997,.693,.217,.273 ,
+     +            .961,.948,.902,.104,.495,.257,.524,.100,.492,.347 ,
+     +            .981,.019,.225,.806,.678,.710,.235,.600,.994,.758 ,
+     +            .682,.373,.009,.469,.203,.730,.588,.603,.213,.495 ,
+     +            .884,.032,.185,.127,.010,.180,.689,.354,.372,.429 /
+      DATA ISEQ / 0 /
+      ISEQ=MOD(ISEQ,100)+1
+      GRAN=RSEQ(ISEQ)
+      RETURN
       END
 
 
