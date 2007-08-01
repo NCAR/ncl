5b0e91499c0c97e37f92413c9258602bc50b3ea1
Dennis Shea <shea@ucar.edu>
2007-08-01 16:17:48 +0000

    *** THIS IS NOT A SHOW-STOPPER *** for 4.3.1
    review priority items
    
    28. Update write_matrix to handle short and byte (rg/ds/mh)
    
    
    Added f77 code that allows printing short and byte.
    
    Note:
    
         integer*1
         integer*2
    
    are non-standard f77. These are common extensions ands are supported by g77
    
    C interfaces need to be added for these to work.
    
    ========================================================================
     A shared object version of the following test script  worked fine.
     See below script.
    ========================================================================
    
    begin
      print ("TEST WRITE_MATRIX: BYTE and SHORT")
    
      ncol =5             ; faster varying subscript
      nrow =3             ; slower varying subscript
      XI   =round( random_uniform( 0, 255, (/nrow,ncol/) ), 3)
    
      xb   = integertobyte( XI )
      xb!0="row"
      xb!1="col"
    
      xs   = integertoshort( XI )
      xs!0="row"
      xs!1="col"
    
      print(XI+"   "+xb+"   "+xs)
    
      opt = True
    
      print("=========================================")
      fmt = ncol+"i5"
      opt@title = "BYTE Printed as integer"
      write_matrix(xb, fmt, opt)
    
      print("=========================================")
      fmt = ncol+"z5"
      opt@title = "BYTE Printed as Hex"
      write_matrix(xb, fmt, opt)
    
      print("=========================================")
      fmt = ncol+"i5"
      opt@title = "SHORT Printed as integer"
      write_matrix(xs, fmt, opt)
      print("=========================================")
    
      XI   =round( random_uniform( 0, 32767, (/nrow,ncol/) ), 3)
      xs   = integertoshort( XI )
      fmt = ncol+"i8"
      opt@title = "SHORT [big numbers] Printed as integer"
      print("=========================================")
    
    end
    
    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    
    shared object output
    
    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    0)     TEST WRITE_MATRIX
    (0,0)   83   0x53   83
    (0,1)   83   0x53   83
    (0,2)   126  0x7e   126
    (0,3)   74   0x4a   74
    (0,4)   90   0x5a   90
    (1,0)   39   0x27   39
    (1,1)   144  0x90   144
    (1,2)   209  0xd1   209
    (1,3)   83   0x53   83
    (1,4)   87   0x57   87
    (2,0)   228  0xe4   228
    (2,1)   86   0x56   86
    (2,2)   73   0x49   73
    (2,3)   128  0x80   128
    (2,4)   197  0xc5   197
    (0)     =========================================
       BYTE
    
       83   83  126   74   90
       39 -112  -47   83   87
      -28   86   73 -128  -59
    
    (0)     =========================================
       SHORT
    
       83   83  126   74   90
       39  144  209   83   87
      228   86   73  128  197
    
    (0)     =========================================
       SHORT
    
         693    1213    9522    2821   24496
       11778    6820    2614    4847   23204
       12958   25048   21166   15501   28165
    
    (0)     =========================================
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@9737 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/writematrix.f
+++ ni/src/lib/nfpfort/writematrix.f
@@ -209,3 +209,152 @@ c write to standard out
 
       return
       end
+
+C NCLFORTSTART
+      subroutine writematrixb(fname,nrow,ncol,x,fmtx,title,titsp,iopt)
+      implicit      none
+
+C NCL procedure: writematrixb(fname, x, fmtx, title, titsp, iopt)
+
+      character*(*) title, fname, fmtx
+      integer       ncol,nrow,iopt,titsp
+      integer*1     x(ncol,nrow)
+
+C NCLEND
+c ------------------------------------------------------
+c NOTE: integer*1  is non-standard fortran
+c       The f77 standard does not support byte or short
+c       However, these are common extensions and are supported
+c       by g77.
+c ------------------------------------------------------
+
+      integer       nr, nc
+      character*96  newfmt, titfmt
+      character*2   titspc
+
+      if (titsp.gt.0) then
+          write(titspc,"(i2)") titsp
+          titfmt = "(" // titspc // "x, a)"
+      else
+          titfmt = "(x, a)"
+      end if
+
+      if (fname.ne."*") then
+c write to a file
+          open  (31,file=fname,form="formatted")
+          if (.not.(title.eq." ")) then
+              write (31,titfmt) title
+          end if
+
+          newfmt = "(" // fmtx // ")"
+
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) (x(nc,nr),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // "1X," // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) nr-1, (x(nc,nr),nc=1,ncol)
+              end do
+          end if
+          close (31)
+      else
+c write to standard out
+          if (.not.(title.eq." ")) then
+              write (*,titfmt) title
+          end if
+
+          print *, " "
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) (x(nc,nr),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // "1X," // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) nr-1, (x(nc,nr),nc=1,ncol)
+              end do
+          end if
+          print *," "
+      end if
+
+      return
+      end
+
+C NCLFORTSTART
+
+      subroutine writematrixs(fname,nrow,ncol,x,fmtx,title,titsp,iopt)
+      implicit      none
+
+C NCL procedure: writematrixi(fname, x, fmtx, title, titsp, iopt)
+
+      character*(*) title, fname, fmtx
+      integer       ncol,nrow,iopt,titsp
+      integer*2     x(ncol,nrow)
+
+C NCLEND
+c ------------------------------------------------------
+c NOTE: integer*2  is non-standard fortran
+c       The f77 standard does not support byte or short
+c       However, these are common extensions and are supported
+c       by g77.
+c ------------------------------------------------------
+
+      integer       nr, nc
+      character*96  newfmt, titfmt
+      character*2   titspc
+
+      if (titsp.gt.0) then
+          write(titspc,"(i2)") titsp
+          titfmt = "(" // titspc // "x, a)"
+      else
+          titfmt = "(x, a)"
+      end if
+
+      if (fname.ne."*") then
+c write to a file
+          open  (31,file=fname,form="formatted")
+          if (.not.(title.eq." ")) then
+              write (31,titfmt) title
+          end if
+
+          newfmt = "(" // fmtx // ")"
+
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) (x(nc,nr),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // "1X," // fmtx // ")"
+              do nr=1,nrow
+                 write (31,newfmt) nr-1, (x(nc,nr),nc=1,ncol)
+              end do
+          end if
+          close (31)
+      else
+c write to standard out
+          if (.not.(title.eq." ")) then
+              write (*,titfmt) title
+          end if
+
+          print *, " "
+          if (iopt.eq.0) then
+              newfmt = "(" // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) (x(nc,nr),nc=1,ncol)
+              end do
+          else
+              newfmt = "(i5," // "1X," // fmtx // ")"
+              do nr=1,nrow
+                 write (*,newfmt) nr-1, (x(nc,nr),nc=1,ncol)
+              end do
+          end if
+          print *," "
+      end if
+
+      return
+      end
