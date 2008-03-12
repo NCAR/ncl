f1d05fde56db10e80ecf6883af897eeaefad27b0
Dennis Shea <shea@ucar.edu>
2008-03-12 19:57:03 +0000

    Added extra argument [ PMSG ] to allow for missing surface values.
    This affects *two* NCL functions:
    
         "pres_hybrid_ccm" and "dpres_hybrid_ccm"
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10048 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/phybrid_ccm.f
+++ ni/src/lib/nfpfort/phybrid_ccm.f
@@ -1,4 +1,4 @@
-      SUBROUTINE DPHYBRID(P0,HYA,HYB,PSFC,MLON,NLAT,KLEV,PHY)
+      SUBROUTINE DPHYBRID(P0,HYA,HYB,PSFC,MLON,NLAT,KLEV,PHY,PMSG)
 
 c NCL: phy = pres_hybrid_ccm (psfc,p0,hya,hyb)
 
@@ -21,7 +21,7 @@ c          phy    - pressure at hybrid levels [Pa]
 
       IMPLICIT NONE
       INTEGER MLON,NLAT,KLEV
-      DOUBLE PRECISION P0,HYA(KLEV),HYB(KLEV),PSFC(MLON,NLAT)
+      DOUBLE PRECISION P0,HYA(KLEV),HYB(KLEV),PSFC(MLON,NLAT),PMSG
 
       DOUBLE PRECISION PHY(MLON,NLAT,KLEV)
 
@@ -30,7 +30,9 @@ c f77
       DO KL = 1,KLEV
           DO NL = 1,NLAT
               DO ML = 1,MLON
-                  PHY(ML,NL,KL) = HYA(KL)*P0 + HYB(KL)*PSFC(ML,NL)
+                 IF (PSFC(ML,NL).NE.PMSG) then
+                     PHY(ML,NL,KL) = HYA(KL)*P0 + HYB(KL)*PSFC(ML,NL)
+                 END IF
               END DO
           END DO
       END DO
@@ -43,12 +45,12 @@ c f90 end do
       END
 
 C NCLFORTSTART
-      SUBROUTINE DDPHYBRID(P0,HYA,HYB,PSFC,MLON,NLAT,KLEV,DPHY)
+      SUBROUTINE DDPHYBRID(P0,HYA,HYB,PSFC,MLON,NLAT,KLEV,DPHY,PMSG)
       IMPLICIT NONE
 
 c input
       INTEGER MLON,NLAT,KLEV
-      DOUBLE PRECISION P0,HYA(KLEV),HYB(KLEV),PSFC(MLON,NLAT)
+      DOUBLE PRECISION P0,HYA(KLEV),HYB(KLEV),PSFC(MLON,NLAT),PMSG
 c output
       DOUBLE PRECISION DPHY(MLON,NLAT,KLEV-1)
 C NCLEND
@@ -80,11 +82,13 @@ c local
       DO NL = 1,NLAT
           DO ML = 1,MLON
               DO KL = 1,KLEV - 1
-                  PA = P0*HYA(KL) + HYB(KL)*PSFC(ML,NL)
-                  PB = P0*HYA(KL+1) + HYB(KL+1)*PSFC(ML,NL)
-                  DPHY(ML,NL,KL) = ABS(PB-PA)
+                 IF (PSFC(ML,NL).NE.PMSG) then
+                     PA = P0*HYA(KL)   + HYB(KL)*PSFC(ML,NL)
+                     PB = P0*HYA(KL+1) + HYB(KL+1)*PSFC(ML,NL)
+                     DPHY(ML,NL,KL) = ABS(PB-PA)
 c c c       dphy(ml,nl,kl) = abs((hya(kl+1)-hya(kl))*p0
 c c c*                     -     (hyb(kl+1)-hya(kl))*psfc(ml,nl))
+                 END IF
               END DO
           END DO
       END DO
