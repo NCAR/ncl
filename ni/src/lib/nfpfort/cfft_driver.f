3578f2f9fc5d4ea3810c53c0f2ab91536de0ac78
Dennis Shea <shea@ucar.edu>
2008-03-31 23:03:01 +0000

    These are prototype interfaces to the complex FFT routines used in the CAM variability processor. Since NCL does not support the complex numeric type, the real and imaginary coef are are explicitly passed .
    MH can write a driver which initializes once and then loops
    over multiple dimensions.
    
    I am doing this to preserve (as much as possible) the current
    processor codes developed originally by Mark Stevens and,
    subsequently, modified by both Rich Neale and John Truesdale.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10089 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfpfort/cfft_driver.f
@@ -0,0 +1,62 @@
+C NLFORTSTART
+      subroutine cfftfdriver (nmx,x,acoef,bcoef)
+      implicit none
+c                                      ! INPUT       
+      integer  nmx
+      double precision x(nmx)
+c                                      ! OUTPUT       
+      double precision acoef(nmx), bcoef(nmx) 
+c NCLEND
+c
+c NCL:  coef = cfftf( x )
+c                                      ! LOCAL        
+      integer  n
+      double precision work(4*nmx+25)
+      double complex   carr(nmx)       
+
+      call cffti(nmx,work)               
+c                                      ! create complex input
+      do n=1,nmx
+         carr(n) = cmplx( x(n), 0.0d0 )     
+      end do
+
+      call cfftf(nmx, carr, work)
+
+      do n=1,nmx
+         acoef(n) = dble ( carr(n) )
+         bcoef(n) = dimag( carr(n) )
+      end do
+
+      return
+      end
+
+C NLFORTSTART
+      subroutine cfftbdriver (nmx,x,acoef,bcoef)
+      implicit none
+c                                      ! INPUT       
+      integer  nmx
+      double precision acoef(nmx), bcoef(nmx) 
+c                                      ! OUTPUT       
+      double precision x(nmx)
+c NCLEND
+c
+c NCL:  coef = cfftb( acoef, bcoef )
+c                                      ! LOCAL        
+      integer  n
+      double precision work(4*nmx+25)
+      double complex   carr(nmx)       
+
+      call cffti(nmx,work)               
+c                                      ! create complex input
+      do n=1,nmx
+         carr(n) = cmplx( acoef(n), bcoef(n))     
+      end do
+
+      call cfftb(nmx, carr, work)
+
+      do n=1,nmx
+         x(n) = dble( carr(n) )
+      end do
+
+      return
+      end
