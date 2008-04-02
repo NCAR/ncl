aa4bc1e5f965ec883e352800452e0794004e664c
Dennis Shea <shea@ucar.edu>
2008-04-02 16:46:04 +0000

    Simple fortran subroutine to calculate the appropriate frequencies associated with the components of the complex discreate forward transform.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10095 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfpfort/frq_cfft.f
@@ -0,0 +1,21 @@
+C NCLFORTSTART
+      subroutine frq_cfft (npts,fmsg,frq)
+      implicit none
+c                                                  ! input
+      integer npts
+      double precision fmsg
+c                                                  ! output
+      double precision frq(npts)
+C NCLEND
+c                                                  ! local
+      integer n
+      double precision df
+
+      df     = 1.0d0/(npts-2)
+      frq(1) = fmsg
+      do n=2,npts
+         frq(n) = -0.5d0 + (n-2)*df
+      end do
+
+      return
+      end
