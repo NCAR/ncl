db96cbcf0dc8d619e5e71894ee69d779140ee316
Dennis Shea <shea@ucar.edu>
2008-06-16 18:07:07 +0000

    This just has a bogus f90 type driver tha MH can use as a bais for the C-interface.
    Basically, it return a 1D or 2 D array at the
    user option.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10262 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/covcorm_driver.f
+++ ni/src/lib/nfpfort/covcorm_driver.f
@@ -1,3 +1,35 @@
+c BOGUS driver
+c     subroutine dcovcordriver (nrow,ncol,x,xmsg,iopt,ier)
+c     implicit none
+c                                              ! input
+c     integer nrow, ncol, iopt(2), ier
+c     double precision x(nrow,ncol), xmsg 
+c                                              ! output
+c     real, allocatable, dimension(:)   :: vcm1d
+c     real, allocatable, dimension(:,:) :: vcm2d
+c     
+c     integer lvcm
+c
+c fortran is column major
+c     lvcm = (ncol*(ncol+1))/2
+c
+c     if (iopt(1).eq.0) then
+c                                              ; return sym storage mode
+c         allocate (vcm1d(lvcm), stat=ier)
+c         call dcovcormssm (nrow,ncol,x,xmsg,iopt(2),vcm1d,lvcm,ier) 
+c         return(vcm1d)
+c     else
+c                                              ; return as 2D
+c         allocate (vcm2d(ncol,ncol), stat=ier)
+c         call dcovcorm (nrow,ncol,x,xmsg, iopt, vcm2d, lvcm,ier)
+c         return(vcm2d)
+c     end if
+
+c     return 
+c     end
+          
+      
+c    
 C NCLFORTSTART
       subroutine dcovcormssm (nrow,ncol,x,xmsg,iopt,vcm,lvcm,ier)
 
