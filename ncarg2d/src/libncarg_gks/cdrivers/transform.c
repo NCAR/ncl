c87b40326c568dc27034c1257918e144f33cc373
Mary Haley <haley@ucar.edu>
2008-07-23 17:31:26 +0000

    Replaced references to the GPL with a generic software license reference.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10303 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg_gks/cdrivers/transform.c
+++ ncarg2d/src/libncarg_gks/cdrivers/transform.c
@@ -1,5 +1,5 @@
 /*
- *      $Id: transform.c,v 1.4 2003-01-06 23:30:17 fred Exp $
+ *      $Id: transform.c,v 1.5 2008-07-23 17:28:01 haley Exp $
  */
 /************************************************************************
 *                                                                       *
@@ -7,25 +7,12 @@
 *        University Corporation for Atmospheric Research                *
 *                All Rights Reserved                                    *
 *                                                                       *
-* This file is free software; you can redistribute it and/or modify     *
-* it under the terms of the GNU General Public License as published     *
-* by the Free Software Foundation; either version 2 of the License, or  *
-* (at your option) any later version.                                   *
-*                                                                       *
-* This software is distributed in the hope that it will be useful, but  *
-* WITHOUT ANY WARRANTY; without even the implied warranty of            *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-* General Public License for more details.                              *
-*                                                                       *
-* You should have received a copy of the GNU General Public License     *
-* along with this software; if not, write to the Free Software         *
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
-* USA.                                                                  *
+*    The use of this Software is governed by a License Agreement.       *
 *                                                                       *
 ************************************************************************/
 
 /*
- *      $Id: transform.c,v 1.4 2003-01-06 23:30:17 fred Exp $
+ *      $Id: transform.c,v 1.5 2008-07-23 17:28:01 haley Exp $
  */
 /************************************************************************
 *                                                                       *
