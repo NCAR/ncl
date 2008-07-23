05fcc6fea69d0083fa539595bfdea49e502cafa5
Mary Haley <haley@ucar.edu>
2008-07-23 16:17:09 +0000

    Replaced references to the GPL with a generic software license reference.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10301 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/wmapC/c_wmbarb.c
+++ ncarg2d/src/libncarg/wmapC/c_wmbarb.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: c_wmbarb.c,v 1.4 2000-08-22 15:07:50 haley Exp $
+ *	$Id: c_wmbarb.c,v 1.5 2008-07-23 16:17:08 haley Exp $
  */
 /************************************************************************
 *                                                                       *
@@ -7,20 +7,7 @@
 *        University Corporation for Atmospheric Research                *
 *                All Rights Reserved                                    *
 *                                                                       *
-* This file is free software; you can redistribute it and/or modify     *
-* it under the terms of the GNU General Public License as published     *
-* by the Free Software Foundation; either version 2 of the License, or  *
-* (at your option) any later version.                                   *
-*                                                                       *
-* This software is distributed in the hope that it will be useful, but  *
-* WITHOUT ANY WARRANTY; without even the implied warranty of            *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-* General Public License for more details.                              *
-*                                                                       *
-* You should have received a copy of the GNU General Public License     *
-* along with this software; if not, write to the Free Software         *
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
-* USA.                                                                  *
+*    The use of this Software is governed by a License Agreement.       *
 *                                                                       *
 ************************************************************************/
 
