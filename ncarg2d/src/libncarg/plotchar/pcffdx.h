cfd63d92dc22836b60c7465b9b7c4f90780dc03f
Mary Haley <haley@ucar.edu>
2008-07-27 01:15:51 +0000

    Updated license statement from a verbose one to a single line one.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10313 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/plotchar/pcffdx.h
+++ ncarg2d/src/libncarg/plotchar/pcffdx.h
@@ -1,24 +1,11 @@
 C
-C	$Id: pcffdx.h,v 1.4 2002-03-28 00:18:07 fred Exp $
+C	$Id: pcffdx.h,v 1.5 2008-07-27 01:15:50 haley Exp $
 C                                                                      
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
 C                All Rights Reserved
 C
-C This file is free software; you can redistribute it and/or modify
-C it under the terms of the GNU General Public License as published
-C by the Free Software Foundation; either version 2 of the License, or
-C (at your option) any later version.
-C
-C This software is distributed in the hope that it will be useful, but
-C WITHOUT ANY WARRANTY; without even the implied warranty of
-C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-C General Public License for more details.
-C
-C You should have received a copy of the GNU General Public License
-C along with this software; if not, write to the Free Software
-C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-C USA.
+C The use of this Software is governed by a License Agreement.
 C
       PARAMETER (IFCLEN=6000, ICLEN=300)
       COMMON /PCINDX/IBFC(IFCLEN)    ,SFLGS(ICLEN)   ,CHRPNT(128),
--- ncarg2d/src/libncarg/plotchar/pcffsp.h
+++ ncarg2d/src/libncarg/plotchar/pcffsp.h
@@ -1,25 +1,12 @@
 C
-C	$Id: pcffsp.h,v 1.3 2000-08-22 03:45:46 haley Exp $
+C	$Id: pcffsp.h,v 1.4 2008-07-27 01:15:50 haley Exp $
 C
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
 C                All Rights Reserved
 C
-C This file is free software; you can redistribute it and/or modify
-C it under the terms of the GNU General Public License as published
-C by the Free Software Foundation; either version 2 of the License, or
-C (at your option) any later version.
+C The use of this Software is governed by a License Agreement.
 C
-C This software is distributed in the hope that it will be useful, but
-C WITHOUT ANY WARRANTY; without even the implied warranty of
-C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-C General Public License for more details.
-C
-C You should have received a copy of the GNU General Public License
-C along with this software; if not, write to the Free Software
-C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-C USA.
-      PARAMETER (IBZL = 129)
       COMMON /PCBZSP/BCNTLX(4), BCNTLY(4), BZXC(IBZL), BZYC(IBZL)
       REAL           BCNTLX   , BCNTLY   , BZXC      , BZYC
       SAVE   /PCBZSP/
