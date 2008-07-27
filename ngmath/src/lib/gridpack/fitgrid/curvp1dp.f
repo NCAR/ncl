2e3f9087e347e7bc15bd0201d9237e0d56aa2ed3
Mary Haley <haley@ucar.edu>
2008-07-27 03:12:40 +0000

    Updated license statement from a verbose one to a single line one.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10314 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ngmath/src/lib/gridpack/fitgrid/curvp1dp.f
+++ ngmath/src/lib/gridpack/fitgrid/curvp1dp.f
@@ -1,24 +1,11 @@
 C
-C $Id: curvp1dp.f,v 1.2 2003-08-11 22:43:59 haley Exp $
+C $Id: curvp1dp.f,v 1.3 2008-07-27 03:10:10 haley Exp $
 C
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
 C                All Rights Reserved
 C
-C This file is free software; you can redistribute it and/or modify
-C it under the terms of the GNU General Public License as published
-C by the Free Software Foundation; either version 2 of the License, or
-C (at your option) any later version.
-C
-C This software is distributed in the hope that it will be useful, but
-C WITHOUT ANY WARRANTY; without even the implied warranty of
-C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-C General Public License for more details.
-C
-C You should have received a copy of the GNU General Public License
-C along with this software; if not, write to the Free Software
-C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-C USA.
+C The use of this Software is governed by a License Agreement.
 C
 C NOTE: If you make any changes to this software, please remember to
 C make the same changes to the corresponding single precision routine.
