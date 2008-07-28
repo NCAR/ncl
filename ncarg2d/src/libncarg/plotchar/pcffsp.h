b1d76a07ca6ed5f2a00d78b239db465071e9bdde
Mary Haley <haley@ucar.edu>
2008-07-28 14:03:54 +0000

    This file had the following line removed accidentally:
           PARAMETER (IBZL = 129)
    
    I'm not sure why, because the other *.h file were okay. I've also
    checked most every *.f and *.c file that was changed, and they all
    seem to be okay.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10322 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/plotchar/pcffsp.h
+++ ncarg2d/src/libncarg/plotchar/pcffsp.h
@@ -1,5 +1,5 @@
 C
-C	$Id: pcffsp.h,v 1.4 2008-07-27 01:15:50 haley Exp $
+C	$Id: pcffsp.h,v 1.5 2008-07-28 14:03:54 haley Exp $
 C
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
@@ -7,6 +7,7 @@ C                All Rights Reserved
 C
 C The use of this Software is governed by a License Agreement.
 C
+      PARAMETER (IBZL = 129)
       COMMON /PCBZSP/BCNTLX(4), BCNTLY(4), BZXC(IBZL), BZYC(IBZL)
       REAL           BCNTLX   , BCNTLY   , BZXC      , BZYC
       SAVE   /PCBZSP/
