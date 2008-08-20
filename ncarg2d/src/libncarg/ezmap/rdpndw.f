2f1199bd5fff1a548a1086180b26811be748eac3
kennison <kennison>
2008-08-20 19:13:49 +0000

    Corrects an incorrect comment line that I noticed while looking at another issue.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10385 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/libncarg/ezmap/rdpndw.f
+++ ncarg2d/src/libncarg/ezmap/rdpndw.f
@@ -1,5 +1,5 @@
 C
-C $Id: rdpndw.f,v 1.3 2008-07-27 00:17:06 haley Exp $
+C $Id: rdpndw.f,v 1.4 2008-08-20 19:13:49 kennison Exp $
 C
 C                Copyright (C)  2000
 C        University Corporation for Atmospheric Research
@@ -11,7 +11,7 @@ C
 C
         DOUBLE PRECISION DVIN
 C
-C "RDPNUW" stands for "Round Double Precision Number UpWard".  The
+C "RDPNDW" stands for "Round Double Precision Number DownWard".  The
 C value of RDPNDW(DVIN) is the smallest real number whose value is
 C greater than or equal to the value of the double-precision argument
 C DVIN.
