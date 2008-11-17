eebd797eee264bfd202776136d5d075c6101df0b
Mary Haley <haley@ucar.edu>
2008-11-17 21:03:35 +0000

    This checks in some more changes to the function now called "mjo_cross_segment". It no longer checks for leftmost dimensions, and it tests for missing values and  returns all missing values if any are encountered.
    I'm checking this in so I can test on a CGD machine.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10527 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfp/space_timeW.c
+++ ni/src/lib/nfp/space_timeW.c
@@ -6,7 +6,7 @@ extern void NGCALLF(spctimcross2,SPCTIMCROSS2)(int *, int *, int *, double *,
                                                int *, double *, int *,
                                                double *, int *);
 
-NhlErrorTypes space_time_cross_segment_W( void )
+NhlErrorTypes mjo_cross_segment_W( void )
 {
 /*
  * Input variables
@@ -16,8 +16,7 @@ NhlErrorTypes space_time_cross_segment_W( void )
  */
   void *x;
   double *tmp_x;
-  int ndims_x, dsizes_x[NCL_MAX_DIMENSIONS];
-  int has_missing_x;
+  int dsizes_x[3], has_missing_x;
   NclScalar missing_x, missing_flt_x, missing_dbl_x;
   NclBasicDataTypes type_x;
 
@@ -26,8 +25,7 @@ NhlErrorTypes space_time_cross_segment_W( void )
  */
   void *y;
   double *tmp_y;
-  int ndims_y, dsizes_y[NCL_MAX_DIMENSIONS];
-  int has_missing_y;
+  int dsizes_y[3], has_missing_y;
   NclScalar missing_y, missing_flt_y, missing_dbl_y;
   NclBasicDataTypes type_y;
 
@@ -40,17 +38,16 @@ NhlErrorTypes space_time_cross_segment_W( void )
  */
   void *stc;
   double *tmp_stc;
-  int *dsizes_stc, has_missing_stc;
-  NclScalar missing_stc, missing_flt_stc, missing_dbl_stc;
+  int dsizes_stc[3], has_missing_stc;
+  NclScalar missing_stc;
   NclBasicDataTypes type_stc;
 
 /*
  * Various
  */
-  int nt, nm, nl, ntml, nt2p1, nlp1, lsave1, lsave2;
-  int index_x, index_stc;
   double *wsave1, *wsave2;
-  int i, ndims_leftmost, size_leftmost, size_stc, size_output, ret;
+  int nt, nm, nl, ntml, nt2p1, nlp1, lsave1, lsave2;
+  int i, size_stc, ret, found_missing_x, found_missing_y;
 
 /*
  * Retrieve parameters.
@@ -64,7 +61,7 @@ NhlErrorTypes space_time_cross_segment_W( void )
   x = (void*)NclGetArgValue(
            0,
            3,
-           &ndims_x,
+           NULL,
            dsizes_x,
            &missing_x,
            &has_missing_x,
@@ -72,22 +69,14 @@ NhlErrorTypes space_time_cross_segment_W( void )
            2);
 
 /*
- * Check dimension sizes.
- */
-  if(ndims_x < 3) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: The x array must have at least 3 dimensions");
-    return(NhlFATAL);
-  }
-
-/*
  * Coerce missing value to double if necessary.
  */
   coerce_missing(type_x,has_missing_x,&missing_x,&missing_dbl_x,
                  &missing_flt_x);
 
-  nt = dsizes_x[ndims_x-3];
-  nm = dsizes_x[ndims_x-2];
-  nl = dsizes_x[ndims_x-1];
+  nt = dsizes_x[0];
+  nm = dsizes_x[1];
+  nl = dsizes_x[2];
   ntml = nt * nm * nl;
 
 /*
@@ -96,7 +85,7 @@ NhlErrorTypes space_time_cross_segment_W( void )
   y = (void*)NclGetArgValue(
            1,
            3,
-           &ndims_y,
+           NULL,
            dsizes_y,
            &missing_y,
            &has_missing_y,
@@ -106,15 +95,9 @@ NhlErrorTypes space_time_cross_segment_W( void )
 /*
  * Check dimension sizes.
  */
-  if(ndims_y != ndims_x) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: The x and y arrays must have the same number of dimensions");
+  if(dsizes_y[0] != nt || dsizes_y[1] != nm || dsizes_y[2] != nl) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"mjo_cross_segment: The x and y arrays must have the same dimension sizes");
     return(NhlFATAL);
-  }
-  for(i = 0; i < ndims_x; i++) {
-    if(dsizes_y[i] != dsizes_x[i]) {
-      NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: The x and y arrays must have the same dimension sizes");
-      return(NhlFATAL);
-    }
   }
 
 /*
@@ -137,155 +120,99 @@ NhlErrorTypes space_time_cross_segment_W( void )
            2);
 
 /*
- * Calculate size of leftmost dimensions.
- */
-  size_leftmost  = 1;
-  ndims_leftmost = ndims_x-3;
-  for(i = 0; i < ndims_leftmost; i++) size_leftmost *= dsizes_x[i];
-
-/*
  * The output type defaults to float, unless this input array is double.
  */
-  type_stc = NCL_float;
+  if(type_x == NCL_double || type_y == NCL_double) type_stc = NCL_double;
+  else                                             type_stc = NCL_float;
 
 /*
- * Allocate space for coercing input arrays.  If any of the input
- * is already double, then we don't need to allocate space for
- * temporary arrays, because we'll just change the pointer into
- * the void array appropriately.
+ * Allocate x,y arrays to double if necessary.
  */
-/*
- * Allocate space for tmp_x.
- */
-  if(type_x != NCL_double) {
-    tmp_x = (double *)calloc(ntml,sizeof(double));
-    if(tmp_x == NULL) {
-      NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for coercing input array to double");
-      return(NhlFATAL);
-    }
-  }
-  else {
-    type_stc = NCL_double;
-  }
-/*
- * Allocate space for tmp_y.
- */
-  if(type_y != NCL_double) {
-    tmp_y = (double *)calloc(ntml,sizeof(double));
-    if(tmp_y == NULL) {
-      NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for coercing input array to double");
-      return(NhlFATAL);
-    }
-  }
-  else {
-    type_stc = NCL_double;
+  tmp_x = coerce_input_double(x,type_x,ntml,0,NULL,NULL);
+  tmp_y = coerce_input_double(y,type_y,ntml,0,NULL,NULL);
+
+  if(tmp_x == NULL || tmp_y == NULL) {
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"mjo_cross_segment: Unable to allocate memory for coercing input arrays to double");
+    return(NhlFATAL);
   }
 
 /*
- * Calculate size of output array.
+ * Test for presence of missing values in tmp_x and tmp_y.
  */
-  nlp1  = nl+1;
-  nt2p1 = nt/2+1;
-
-  size_stc    = nlp1 * nt2p1 * 16;
-  size_output = size_leftmost * size_stc;
+  found_missing_x = contains_missing(tmp_x,ntml,has_missing_x,
+				     missing_dbl_x.doubleval);
+  found_missing_y = contains_missing(tmp_y,ntml,has_missing_y,
+				     missing_dbl_y.doubleval);
 
 /* 
  * Allocate space for output array.
  */
+  nlp1     = nl+1;
+  nt2p1    = nt/2+1;
+  size_stc = nlp1 * nt2p1 * 16;
+
   if(type_stc != NCL_double) {
-    stc     = (void *)calloc(size_output, sizeof(float));
+    stc     = (void *)calloc(size_stc, sizeof(float));
     tmp_stc = (double *)calloc(size_stc,sizeof(double));
     if(tmp_stc == NULL) {
-      NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for temporary output array");
+      NhlPError(NhlFATAL,NhlEUNKNOWN,"mjo_cross_segment: Unable to allocate memory for temporary output array");
       return(NhlFATAL);
     }
   }
   else {
-    stc = (void *)calloc(size_output, sizeof(double));
+    stc     = (void *)calloc(size_stc, sizeof(double));
+    tmp_stc = &((double*)stc)[0];
+
   }
   if(stc == NULL) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for output array");
+    NhlPError(NhlFATAL,NhlEUNKNOWN,"mjo_cross_segment: Unable to allocate memory for output array");
     return(NhlFATAL);
   }
-  if(has_missing_x) {
-    if(type_stc == NCL_double) missing_stc = missing_dbl_x;
-    else                       missing_stc = missing_flt_x;
-    missing_dbl_stc = missing_dbl_x;
-  }
 
-/* 
- * Allocate space for output dimension sizes and set them.
+  if(found_missing_x || found_missing_y) {
+    NhlPError(NhlWARNING,NhlEUNKNOWN,"mjo_cross_segment: input array(s) contained missing values. All missing values will be returned");
+
+    if(has_missing_x) {
+      if(type_stc == NCL_double) missing_stc = missing_dbl_x;
+      else                       missing_stc = missing_flt_x;
+      set_subset_output_missing(stc,0,type_stc,size_stc,
+				missing_dbl_x.doubleval);
+    }
+    else if(has_missing_y) {
+      if(type_stc == NCL_double) missing_stc = missing_dbl_y;
+      else                       missing_stc = missing_flt_y;
+      set_subset_output_missing(stc,0,type_stc,size_stc,
+				missing_dbl_y.doubleval);
+    }
+/*
+ * Set all elements of stc to a missing value.
  */
-  dsizes_stc = (int*)calloc(ndims_x,sizeof(int));  
-  if( dsizes_stc == NULL ) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for holding dimension sizes");
-    return(NhlFATAL);
   }
-  for(i = 0; i < ndims_x-3; i++) dsizes_stc[i] = dsizes_x[i];
-  dsizes_stc[ndims_x-3] = 16;
-  dsizes_stc[ndims_x-2] = nt2p1;
-  dsizes_stc[ndims_x-1] = nlp1;
-
+  else {
 /* 
  * Allocate space for work arrays.
  */
-  lsave1 = 4*nl + 15;
-  lsave2 = 4*nt + 15;
-  wsave1 = (double *)calloc(lsave1, sizeof(double));
-  wsave2 = (double *)calloc(lsave2, sizeof(double));
-  if(wsave1 == NULL || wsave2 == NULL) {
-    NhlPError(NhlFATAL,NhlEUNKNOWN,"space_time_cross_segment: Unable to allocate memory for work arrays");
-    return(NhlFATAL);
-  }
-
-/*
- * Loop across leftmost dimensions and call the Fortran routine for each
- * subsection of the input arrays.
- */
-  index_x = index_stc = 0;
-
-  for(i = 0; i < size_leftmost; i++) {
-/*
- * Coerce subsection of x (tmp_x) to double if necessary.
- */
-    if(type_x != NCL_double) {
-      coerce_subset_input_double(x,tmp_x,index_x,type_x,ntml,0,NULL,NULL);
+    lsave1 = 4*nl + 15;
+    lsave2 = 4*nt + 15;
+    wsave1 = (double *)calloc(lsave1, sizeof(double));
+    wsave2 = (double *)calloc(lsave2, sizeof(double));
+    if(wsave1 == NULL || wsave2 == NULL) {
+      NhlPError(NhlFATAL,NhlEUNKNOWN,"mjo_cross_segment: Unable to allocate memory for work arrays");
+      return(NhlFATAL);
     }
-    else {
-      tmp_x = &((double*)x)[index_x];
-    }
-
-/*
- * Coerce subsection of y (tmp_y) to double if necessary.
- */
-    if(type_y != NCL_double) {
-      coerce_subset_input_double(y,tmp_y,index_x,type_y,ntml,0,NULL,NULL);
-    }
-    else {
-      tmp_y = &((double*)y)[index_x];
-    }
-
-/*
- * Point temporary output array to void output array if appropriate.
- */
-    if(type_stc == NCL_double) tmp_stc = &((double*)stc)[index_stc];
-
+    
 /*
  * Call the Fortran routine.
  */
     NGCALLF(spctimcross2,SPCTIMCROSS2)(&nl, &nm, &nt, tmp_x, tmp_y, tmp_stc, 
-                                       &nlp1, &nt2p1, wsave1, &lsave1,
-                                       wsave2,&lsave2);
-
+				       &nlp1, &nt2p1, wsave1, &lsave1,
+				       wsave2,&lsave2);
 /*
  * Coerce output back to float if necessary.
  */
-    if(type_stc == NCL_float) {
-      coerce_output_float_only(stc,tmp_stc,size_stc,index_stc);
-    }
-    index_x   += ntml;
-    index_stc += size_stc;
+    if(type_stc == NCL_float) coerce_output_float_only(stc,tmp_stc,size_stc,0);
+    NclFree(wsave1);
+    NclFree(wsave2);
   }
 
 /*
@@ -294,19 +221,24 @@ NhlErrorTypes space_time_cross_segment_W( void )
   if(type_x   != NCL_double) NclFree(tmp_x);
   if(type_y   != NCL_double) NclFree(tmp_y);
   if(type_stc != NCL_double) NclFree(tmp_stc);
-  NclFree(wsave1);
-  NclFree(wsave2);
 
 /*
  * Return value back to NCL script.
  */
-  if(type_stc != NCL_double) {
-    ret = NclReturnValue(stc,ndims_x,dsizes_stc,&missing_flt_stc,type_stc,0);
+  
+/* 
+ * Set dimension sizes for output array.
+ */
+  dsizes_stc[0] = 16;
+  dsizes_stc[1] = nt2p1;
+  dsizes_stc[2] = nlp1;
+
+  if(found_missing_x || found_missing_y) {
+    ret = NclReturnValue(stc,3,dsizes_stc,&missing_stc,type_stc,0);
   }
   else {
-    ret = NclReturnValue(stc,ndims_x,dsizes_stc,&missing_dbl_stc,type_stc,0);
+    ret = NclReturnValue(stc,3,dsizes_stc,NULL,type_stc,0);
   }
-  NclFree(dsizes_stc);
   return(ret);
 }
 
