640b52d1c6b5bf4b99d5ab77dbc0e7e364c51d77
kennison <kennison>
2009-01-26 03:12:18 +0000

    Fixes an error, reported by Fred, in the way in which IWTY and IWTYPE are specified.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@10662 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ncarg2d/src/examples/evaspt/vtex01.f
+++ ncarg2d/src/examples/evaspt/vtex01.f
@@ -9,11 +9,13 @@ C Define the error file, the Fortran unit number, the workstation type,
 C and the workstation ID, to be used in calls to GKS routines.  Use one
 C of the following:
 C
-        PARAMETER (IERF=6,LUNI=2,IWTY=1 ,IWID=1)  !  NCGM
+C       PARAMETER (IERF=6,LUNI=2,IWTY=1 ,IWID=1)  !  NCGM
 C       PARAMETER (IERF=6,LUNI=2,IWTY=8 ,IWID=1)  !  X Windows
 C       PARAMETER (IERF=6,LUNI=2,IWTY=20,IWID=1)  !  PostScript
 C       PARAMETER (IERF=6,LUNI=2,IWTY=11,IWID=1)  !  PDF, Portrait
 C       PARAMETER (IERF=6,LUNI=2,IWTY=12,IWID=1)  !  PDF, Landscape
+C
+        PARAMETER (IERF=6,LUNI=2,IWTYPE=1,IWTY=IWTYPE,IWID=1)
 C
 C Define a parameter saying how many pieces each edge of an icosahedron
 C is to be broken into in forming the geodesic mesh.
