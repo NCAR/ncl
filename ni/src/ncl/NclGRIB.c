b2b334421c49f243ab0e831cb829427940f5442e
huangwei <huangwei>
2010-05-07 17:06:14 +0000

    one more fix related to read_file_test.ncl test
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@11507 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/ncl/NclGRIB.c
+++ ni/src/ncl/NclGRIB.c
@@ -7583,6 +7583,8 @@ while(step != NULL) {
 	if(step->var_info.var_name_quark == var_name) {
 		tmp = (NclFVarRec*)NclMalloc(sizeof(NclFVarRec));
 		tmp->var_name_quark  = step->var_info.var_name_quark;
+		tmp->var_full_name_quark  = step->var_info.var_name_quark;
+		tmp->var_real_name_quark  = step->var_info.var_name_quark;
 		tmp->data_type  = step->var_info.data_type;
 		tmp->num_dimensions  = step->var_info.num_dimensions;
 		for(i=0;i< tmp->num_dimensions;i++) {
