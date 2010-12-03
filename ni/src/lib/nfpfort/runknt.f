c5dd08438356eedccda216ed51a6d8f694355d30
Dennis Shea <shea@ucar.edu>
2010-12-03 22:16:16 +0000

    This is the code to be used for dim_numrun_n. The 1st subroutine does the looping in fortran for better efficiency.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@11968 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/runknt.f
+++ ni/src/lib/nfpfort/runknt.f
@@ -1,4 +1,57 @@
-      subroutine numrunn(nx, xin, optknt, xknt )
+      subroutine numrun2(nx, ntim, xin, optknt, xknt )
+      implicit none
+C
+C knt = dim_numrun_n( x, optknt, n)
+C                                                      INPUT
+      integer  nx, ntim, optknt, optend
+      integer  xin(nx,ntim) 
+C                                                      OUTPUT
+      integer  xknt(nx,ntim) 
+C                                                      LOCAL 
+      integer  n, j, t, knt, yin(0:nx+1)
+
+      if (optknt.eq.0) then
+          do t=1,ntim
+            do n=1,nx
+               xknt(n,t) = 0
+              do j=1,nx-n+1
+                 if (all(xin(j:j+n-1,t).eq.1)) then
+                     xknt(n,t) = xknt(n,t)+1
+                 end if
+              end do
+            end do
+          end do
+
+          return
+      end if
+
+c count unique sequences: pad ending with 0 to force a count
+
+      if (optknt.eq.1) then
+          do t=1,ntim
+
+             yin(0)    = 0
+             yin(1:nx) = xin(:,t)
+             yin(nx+1) = 0
+
+            do n=1,nx
+               xknt(n,t) = 0
+              do j=1,nx-n+1
+                 if (yin(j-1).eq.0          .and. 
+     +               all(yin(j:j+n-1).eq.1) .and.
+     +               yin(j+n).eq.0        ) then
+                     xknt(n,t) = xknt(n,t)+1
+                 end if
+              end do
+            end do
+          end do
+          return
+      end if
+
+      return
+      end
+C ------
+      subroutine numrun1(nx, xin, optknt, xknt )
       implicit none
 C
 C knt = dim_numrun_n( x, optknt, n)
