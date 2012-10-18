9a3dd9b010c1beff80db948253e5461443d76d95
Dennis Shea <shea@ucar.edu>
2012-10-18 13:58:43 +0000

    Added code to avoid a situatione where a beta distribution is being fit to a series of constant values.
    This has not been tested extensively. 
    Given that MH is leaving ... this does not have to be in 6.1.0 
    In fact, given the lack of extensive testing, maybe it should not be
    in 6.1.0
    
    
    
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@13849 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/spid.f
+++ ni/src/lib/nfpfort/spid.f
@@ -7,14 +7,14 @@ C NCLEND
 c compute SPI: Standardized Precipitation Index using gamma distribution
 
 c local/dynamic
-      integer maxyrs, j, i, im, n, kmsg, kntp
-      double precision tmparr(ntim/12+1)
+      integer maxyrs, j, i, im, n, kmsg, k
+      double precision tmparr(ntim/12+1), pcon
       double precision alpha(12), beta(12), gamm(12), pzero(12)
       double precision anvnrmd, gamcdfd
 
       maxyrs = ntim/12
 c
-c	Initialize index and check for all missing values
+c	Initialize index and check for all missing values 
 c
       kmsg = 0
       do j=1,ntim    
@@ -24,6 +24,31 @@ c
          end if
       end do
       if (kmsg.eq.ntim) return
+c
+c	check for all constant values; can not fit a distribution
+c
+      do j=1,ntim    
+         if (pp(j).ne.pmsg) then
+             pcon = pp(j)
+             go to 13
+         end if
+      end do
+   13 continue
+
+      do j=1,ntim    
+         if (pp(j).ne.pmsg .and. pp(j).ne.pcon) then
+             go to 18
+         end if
+      end do
+      return
+   18 continue
+
+      do j=1,12
+         beta(j)  = pmsg
+         gamm(j)  = pmsg
+         alpha(j) = pmsg
+         pzero(j) = pmsg
+      end do
 c
 c	Sum nrun precip. values;
 c	store them in the appropriate index location.
@@ -61,17 +86,26 @@ c
 c	Here's where we do the fitting.
 c
         if (n.gt.0) then
-            call gamfitd(tmparr,n
+c
+c avoid trying to fit a series of constant values
+c
+            do k=2,n
+               if (tmparr(k).ne.tmparr(1)) then     
+                   call gamfitd(tmparr,n
      +                  ,alpha(im), beta(im), gamm(im), pzero(im))
+                   go to 37
+               end if
+            end do
         end if
 
+   37   continue
       end do
 c
 c	Replace precip. sums stored in index with SPI's
 c
       do j = nrun, ntim
          im = mod (j-1,12) + 1
-         if (index(j) .ne. pmsg) then
+         if (index(j) .ne. pmsg .and. beta(im).ne.pmsg) then
 c
 c	Get the probability
 c
@@ -94,8 +128,8 @@ ccc
       double precision function anvnrmd (prob)
       implicit none
       double precision sign, prob, t, c0, c1, c2, d1, d2, d3
-      data c0, c1, c2 /2.515517, 0.802853, 0.010328/
-      data d1, d2, d3 /1.432788, 0.189269, 0.001308/
+      data c0, c1, c2 /2.515517d0, 0.802853d0, 0.010328d0/
+      data d1, d2, d3 /1.432788d0, 0.189269d0, 0.001308d0/
 
       if (prob .gt. 0.5d0) then
           sign = 1.0d0
@@ -103,8 +137,8 @@ ccc
       else
           sign = -1.0d0
       endif
-      if (prob .lt. 0.0) then
-          write(*, *) 'Error in anvnrmd(). Prob. not in [0,1.0]'
+      if (prob .lt. 0.0d0) then
+c c c     write(*, *) 'Error in anvnrmd(). Prob. not in [0,1.0]'
           anvnrmd = 0.0d0
           return
       endif
@@ -237,34 +271,34 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 
       integer niter
       double precision eps, phigh, thigh, tlow, t, p
-      data eps /1.0e-7/
+      data eps /1.0d-7/
       double precision gamcdfd
 
 c	Check if prob < prob of zero
       if (prob .le. pzero) then
-          gaminvd = 0.0
+          gaminvd = 0.0d0
           return
       endif
 
 c	Otherwise adjust prob
-      prob = (prob - pzero) / (1.0 - pzero)
+      prob = (prob - pzero) / (1.0d0 - pzero)
 
 c	Make initial guess. Keep doubling estimate until prob is
 c	bracketed.
-      thigh = 2.0*eps
+      thigh = 2.0d0*eps
  10   continue
       phigh = gamcdfd (beta, gamm, pzero, thigh)
       if(phigh .ge. prob) goto 20
-      thigh = thigh*2.0
+      thigh = thigh*2.0d0
       goto 10
  20   continue
-      tlow = thigh / 2.0
+      tlow = thigh / 2.0d0
 c     Iterate to find root.
       niter = 0
  30   continue
       if((thigh - tlow) .le. eps) goto 40
          niter = niter + 1
-         t = (tlow + thigh) / 2.0
+         t = (tlow + thigh) / 2.0d0
          p = gamcdfd (beta, gamm, pzero, t)
          if (p .lt. prob) then
              tlow = t
@@ -274,7 +308,7 @@ c     Iterate to find root.
       goto 30
  40   continue
 
-      gaminvd = (tlow + thigh) / 2.0
+      gaminvd = (tlow + thigh) / 2.0d0
       return
       end
 ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -311,15 +345,15 @@ c     Maximum number of iterations, and bound on error.
       gln = gammlnd (a)
 
       if (x .eq. 0.0) then
-          gamserd = 0.0
+          gamserd = 0.0d0
           return
       endif
 
       ap  = a
-      sum = 1.0 / a
+      sum = 1.0d0 / a
       del = sum
       do 10 n = 1, maxitr
-         ap  = ap + 1.0
+         ap  = ap + 1.0d0
          del = del * (x / ap)
          sum = sum + del
          if (abs (del) .lt. eps * abs (sum)) goto 20
@@ -346,17 +380,17 @@ c
       integer maxitr, nwarn, n
       double precision    eps, gln, gold, a0,a1,b0,b1,fac,an,ana,anf,g
       parameter (maxitr=200, eps=3.0d-7)
-      data nwarn / 0 /, g / 0.0 /
+      data nwarn / 0 /, g / 0.0d0 /
 
       double precision gammlnd
 
       gln  = gammlnd (a)
-      gold = 0.0
-      a0   = 1.0
+      gold = 0.0d0
+      a0   = 1.0d0
       a1   = x
-      b0   = 0.0
-      b1   = 1.0
-      fac  = 1.0
+      b0   = 0.0d0
+      b1   = 1.0d0
+      fac  = 1.0d0
 
       do 10 n = 1, maxitr
          an  = n
@@ -366,8 +400,8 @@ c
          anf = an * fac
          a1  = x * a0 + anf * a1
          b1  = x * b0 + anf * b1
-         if (a1 .ne. 0.0) then
-             fac = 1.0 / a1
+         if (a1 .ne. 0.0d0) then
+             fac = 1.0d0 / a1
              g = b1 * fac
              if (abs((g - gold) / g) .lt. eps) goto 20
              gold = g
@@ -394,10 +428,10 @@ c
       double precision a, x
       double precision gamserd, gammcfd
 
-      if (x .lt. a + 1.0) then
+      if (x .lt. a + 1.0d0) then
           gammapd = gamserd (a, x)
       else
-          gammapd = 1.0 - gammcfd (a, x)
+          gammapd = 1.0d0 - gammcfd (a, x)
       endif
 
       return
@@ -411,8 +445,8 @@ c
       double precision a, x
       double precision gamserd, gammcfd
 
-      if (x .lt. a + 1.0) then
-          gammaqd = 1.0 - gamserd (a, x)
+      if (x .lt. a + 1.0d0) then
+          gammaqd = 1.0d0 - gamserd (a, x)
       else
           gammaqd = gammcfd (a, x)
       endif
@@ -427,20 +461,20 @@ c
        
       integer j
       double precision cof(6), x, tmp, ser
-      data cof /76.18009173, -86.50532033, 24.01409822, -1.231739516,
-     1           0.120858003e-2, -0.536382e-5/
+      data cof / 76.18009173d0, -86.50532033d0, 24.01409822d0
+     +         , -1.231739516d0, 0.120858003d-2, -0.536382d-5/
 
-      x   = xx - 1.0
-      tmp = x + 5.5
-      tmp = tmp - (x+0.5) * log(tmp)
-      ser = 1.0
+      x   = xx - 1.0d0
+      tmp = x + 5.5d0
+      tmp = tmp - (x+0.5d0) * log(tmp)
+      ser = 1.0d0
 
       do 10 j = 1, 5
-         x   = x + 1.0
+         x   = x + 1.0d0
          ser = ser + cof(j) / x
  10   continue
 
-      gammlnd = -tmp + log(2.50662827465 * ser)
+      gammlnd = -tmp + log(2.50662827465d0 * ser)
 
       return
       end
