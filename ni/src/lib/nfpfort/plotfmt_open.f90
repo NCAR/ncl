970283c727ed5a7ae4952e8206fb76beef7b9f73
Mary Haley <haley@ucar.edu>
2013-07-22 05:54:24 +0000

    Removed some debug prints.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@14433 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/plotfmt_open.f90
+++ ni/src/lib/nfpfort/plotfmt_open.f90
@@ -12,10 +12,7 @@ subroutine plotfmt_open(fname,istatus)
 
   ! Open intermediate file
   funit = 10
-  print *,"fname = '",fname,"'"
-  print *,'before istatus = ',istatus
   open(funit,file=fname,status='old',form='unformatted',convert='big_endian',iostat=io_status)
-  print *,'after istatus = ',istatus
 
   if(io_status > 0) istatus = 1
 
