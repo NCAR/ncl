b007552742e204664daf5f0aac0c3e3e682aaaa2
Mary Haley <haley@ucar.edu>
2015-03-02 21:18:06 +0000

    Converted wrfcttcalc to double precision and removed unused variables.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@16018 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/wrf_fctt.f
+++ ni/src/lib/nfpfort/wrf_fctt.f
@@ -1,47 +1,43 @@
-c                                                                     c
-c*********************************************************************c
-c 
 C NCLFORTSTART                                                        
       subroutine wrfcttcalc(prs,tk,qci,qcw,qvp,ght,ter,ctt,
      &                      haveqci,nz,ns,ew)
 
       implicit none
-      integer nt,nz,ns,ew,haveqci 
-      real    ght(ew,ns,nz)
-      real    prs(ew,ns,nz),tk(ew,ns,nz)
-      real    qci(ew,ns,nz),qcw(ew,ns,nz)
-      real    qvp(ew,ns,nz)
-      real    ctt(ew,ns),ter(ew,ns)
-c      real    znfac(nz)
+      integer nz,ns,ew,haveqci 
+      double precision    ght(ew,ns,nz)
+      double precision    prs(ew,ns,nz),tk(ew,ns,nz)
+      double precision    qci(ew,ns,nz),qcw(ew,ns,nz)
+      double precision    qvp(ew,ns,nz)
+      double precision    ctt(ew,ns),ter(ew,ns)
+c      double precision    znfac(nz)
 C NCLEND
 c
 c
-      integer i,j,k,l,mjx,miy,mkzh,ripk,wrfout
-      integer ntimes
-      real    vt,rgas,grav,opdepthu,opdepthd,dp
-      real    ratmix,eps,arg1,arg2,agl_hgt,ussalr
-      real    abscoefi,abscoef,fac,prsctt,celkel
-c      real    ght(ew,ns,nz),stuff(ew,ns)
-      real    pf(ns,ew,nz),p1,p2
+      integer i,j,k,mjx,miy,mkzh,ripk,wrfout
+      double precision    vt,rgas,grav,opdepthu,opdepthd,dp
+      double precision    ratmix,eps,arg1,arg2,agl_hgt,ussalr
+      double precision    abscoefi,abscoef,fac,prsctt,celkel
+c      double precision    ght(ew,ns,nz),stuff(ew,ns)
+      double precision    pf(ns,ew,nz),p1,p2
 c
 c
        mjx      =   ew 
        miy      =   ns 
        mkzh     =   nz 
-       eps      = 0.622
-       ussalr   = .0065      ! deg C per m
-       rgas     = 287.04         !J/K/kg
-       grav     = 9.81
-       abscoefi = .272     ! cloud ice absorption coefficient in m^2/g
-       abscoef  =.145      ! cloud water absorption coefficient in m^2/g
-       celkel   = 273.15 
+       eps      = 0.622d0
+       ussalr   = .0065d0      ! deg C per m
+       rgas     = 287.04d0     !J/K/kg
+       grav     = 9.81d0
+       abscoefi = .272d0  ! cloud ice absorption coefficient in m^2/g
+       abscoef  =.145d0   ! cloud water absorption coefficient in m^2/g
+       celkel   = 273.15d0 
        wrfout = 1
 
 
 cCalculate the surface pressure 
        do j=1,ew
        do i=1,ns
-           ratmix     = .001*qvp(j,i,1)
+           ratmix     = .001d0*qvp(j,i,1)
            arg1       = eps + ratmix
            arg2       = eps*(1.+ratmix)
            vt         =  tk(j,i,1) * arg1/arg2 !Virtual temperature
@@ -58,14 +54,14 @@ c
        do i=1,ns
           do k=1,nz-1
             ripk = nz-k+1
-            pf(i,j,k)=.5*(prs(j,i,ripk)+prs(j,i,ripk-1))
+            pf(i,j,k)=.5d0*(prs(j,i,ripk)+prs(j,i,ripk-1))
           enddo
       enddo
       enddo
 
       do 190 j=1,ew
       do 190 i=1,ns
-         opdepthd=0.
+         opdepthd=0.d0
          k=0
 
 c
@@ -77,9 +73,9 @@ c
          ripk = nz-k+1
 
          if (k.eq.1) then
-            dp=200.*(pf(i,j,1)-prs(j,i,nz))  ! should be in Pa
+            dp=200.d0*(pf(i,j,1)-prs(j,i,nz))  ! should be in Pa
          else
-            dp=100.*(pf(i,j,k)-pf(i,j,k-1))  ! should be in Pa
+            dp=100.d0*(pf(i,j,k)-pf(i,j,k-1))  ! should be in Pa
          endif 
          if (haveqci .eq. 0) then
             if (tk(i,j,k).lt.celkel) then
@@ -91,7 +87,7 @@ c                   so no convrsion needed
             endif
          else
             opdepthd=opdepthd+(abscoef*qcw(j,i,ripk)+
-     &                        abscoefi*qci(j,i,ripk))*dp/grav
+     &                         abscoefi*qci(j,i,ripk))*dp/grav
          endif
          
           if (opdepthd.lt.1..and.k.lt.nz) then
