95f8f07eaf692668c1d0f9e506c77af109d85dec
Mary Haley <haley@ucar.edu>
2015-03-06 20:30:13 +0000

    Fixed to work with north-to-south latitudes.
    I used bindata.f as a template for this fix.
    
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@16051 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/lib/nfpfort/binavg.f
+++ ni/src/lib/nfpfort/binavg.f
@@ -19,13 +19,17 @@ c NCL:   function bin_avg(zlon[*],zlat[*],z[*], glon[*], glat[*], opt )
 
       IER = 0
 
-      DLAT = ABS(GLAT(2)-GLAT(1))
-      DLON = ABS(GLON(2)-GLON(1))
-
-c error checking
-      IF (DLAT.EQ.0.0D0 .OR. DLON.EQ.0.0D0) THEN
+c                            ; monotonically {in/de}creasing
+      DLAT = GLAT(2)-GLAT(1)
+      IF (DLAT.GT.0.0D0) THEN
+          IFLAG = 1
+      ELSEIF (DLAT.LT.0.0D0) THEN
+          IFLAG = -1
+      ELSE
           IER = 1
       END IF
+      DLAT = ABS(DLAT)
+
 c                            check for equal lat spacing
       DO NL = 1,NLAT - 1
           IF (ABS(GLAT(NL+1)-GLAT(NL)).NE.DLAT) THEN
@@ -34,9 +38,15 @@ c                            check for equal lat spacing
           END IF
       END DO
    10 CONTINUE
+
 c                            check for equal lon spacing
+      DLON = GLON(2)-GLON(1)
+      IF (DLON.LE.0.0D0) THEN
+          IER = 1
+      END IF
+
       DO ML = 1,MLON - 1
-          IF (ABS(GLON(2)-GLON(1)).NE.DLON) THEN
+          IF (ABS(GLON(ML+1)-GLON(ML)).NE.DLON) THEN
               IER = IER + 100
               GO TO 20
           END IF
@@ -59,12 +69,6 @@ c                            initialize
               GBINKNT(ML,NL,2) = 0.0D0
           END DO
       END DO
-c                            ; monotonically {in/de}creasing
-      IF ((GLAT(2)-GLAT(1)).GT.0.0D0) THEN
-          IFLAG = 1
-      ELSE
-          IFLAG = -1
-      END IF
 
       GLATBND1 = MIN(GLAT(1),GLAT(NLAT)) - DLAT/2
       GLATBND2 = MAX(GLAT(1),GLAT(NLAT)) + DLAT/2
@@ -75,16 +79,14 @@ c                            ; monotonically {in/de}creasing
           IF (Z(K).NE.ZMSG .AND.
      +       (ZLAT(K).GE.GLATBND1 .AND. ZLAT(K).LE.GLATBND2)  .AND.
      +       (ZLON(K).GE.GLONBND1 .AND. ZLON(K).LE.GLONBND2)) THEN
-              NL = ABS((ZLAT(K)-GLATBND1)/DLAT) + 1
               ML = ABS((ZLON(K)-GLONBND1)/DLON) + 1
+              NL = ABS((ZLAT(K)-GLATBND1)/DLAT) + 1
               IF (IFLAG.EQ.-1) NL = NLAT-NL+1
-
 c c c         IF (NL.GT.0 .AND. NL.LE.NLAT .AND. ML.GT.0 .AND.
-c c c+            ML.LE.MLON) THEN
+c c c             ML.LE.MLON) THEN
                   GBINKNT(ML,NL,1) = GBINKNT(ML,NL,1) + Z(K)
                   GBINKNT(ML,NL,2) = GBINKNT(ML,NL,2) + 1
 c c c         END IF
-
           END IF
       END DO
 c                                     compute bin average
