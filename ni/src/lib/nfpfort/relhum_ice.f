25f428a16769a6d6106844193b797fb7679edbf9
Mary Haley <haley@ucar.edu>
2015-09-29 17:42:30 +0000

    Added two Fortran codes contributed by Alan Brammer for special relative humidity calculations.
    This partially addresses NCL-2133.
    
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@16299 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ ni/src/lib/nfpfort/relhum_ice.f
@@ -0,0 +1,48 @@
+C NCLFORTSTART
+      SUBROUTINE DRELHUMI(P,TK,QW, RH)
+      IMPLICIT NONE
+C NCL: qw = relhum_ice(p:numeric,tk:numeric,qw:numeric)
+
+c"Improved Magnus' Form Approx. of Saturation Vapor pressure"
+c Oleg A. Alduchov and Robert E. Eskridge
+c  http://www.osti.gov/scitech/servlets/purl/548871/
+c  One of Three best approx for +50 to -80 C max relative errors of (0.337/0.823) ?
+
+c input
+      DOUBLE PRECISION P,TK,QW
+c output
+      DOUBLE PRECISION RH
+c NCLEND
+
+c computes rh (relative humidity) from p, t and qw (mixing ratio)
+
+c definition of mixing ratio
+c if,
+c    es  - is the saturation mixing ratio
+c    ep  - is the ratio of the molecular weights of water vapor to
+c          dry air
+c    p   - is the atmospheric pressure
+c    rh  - is the relative humidity (given as a percent)
+
+c    rh =  100*  q / ( (ep*es)/(p-es) )
+
+c input-
+c    p      - pressure (hPa or mb)
+c    tk     - temperature (k)
+c    qw     - mixing ratio (kg/kg)
+
+c output-
+c    rh     - relative humidity as %
+
+c local
+      DOUBLE PRECISION T0,EP,ONEMEP,ES0,A,B,EST,QST
+      DATA T0,EP,ONEMEP,ES0,A,B/273.15D0,0.622D0,0.378D0,6.1128D0,
+     +     22.571D0,273.71D0/
+
+      EST = ES0*EXP((A* (TK-T0))/ ((TK-T0)+B))
+      QST = (EP*EST)/ (P-ONEMEP*EST)
+c                                    mix ratio
+      RH =  100* (QW / QST)
+
+      RETURN
+      END
---
+++ ni/src/lib/nfpfort/relhum_water.f
@@ -0,0 +1,46 @@
+C NCLFORTSTART
+      SUBROUTINE DRELHUMW(P,TK,QW, RH)
+      IMPLICIT NONE
+C NCL: qw = relhum_water(p:numeric,tk:numeric,qw:numeric)
+
+C  Calculate rel hum with respect to water
+C  Same constants as mixhum_ptrh  reversible calculation
+
+c input
+      DOUBLE PRECISION P,TK,QW
+c output
+      DOUBLE PRECISION RH
+c NCLEND
+
+c computes rh (relative humidity) from p, t and qw (mixing ratio)
+
+c definition of mixing ratio
+c if,
+c    es  - is the saturation mixing ratio
+c    ep  - is the ratio of the molecular weights of water vapor to
+c          dry air
+c    p   - is the atmospheric pressure
+c    rh  - is the relative humidity (given as a percent)
+
+c    rh =  100*  q / ( (ep*es)/(p-es) )
+
+c input-
+c    p      - pressure (hPa or mb)
+c    tk     - temperature (k)
+c    qw     - mixing ratio (kg/kg)
+
+c output-
+c    rh     - relative humidity as %
+
+c local
+      DOUBLE PRECISION T0,EP,ONEMEP,ES0,A,B,EST,QST
+      DATA T0,EP,ONEMEP,ES0,A,B/273.15D0,0.622D0,0.378D0,6.11D0,
+     +     17.269D0,35.86D0/
+
+      EST = ES0*EXP((A* (TK-T0))/ (TK-B))
+      QST = (EP*EST)/ (P-ONEMEP*EST)
+c                                    mix ratio
+      RH =  100* (QW / QST)
+
+      RETURN
+      END
