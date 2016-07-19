a7e449c3b3ec51e4ea559057019be3b2776150ed
Dave Brown <dbrown@ucar.edu>
2016-07-19 19:58:08 +0000

    changed the variable 'numberOfPreloadedScripts' from 5 to 7.
    git-svn-id: https://subversion.ucar.edu/ncldev/ncarg/trunk@16560 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- ni/src/ncl/NclDriver.c
+++ ni/src/ncl/NclDriver.c
@@ -11,7 +11,7 @@ int quark_comp(const void *q1, const void *q2)
 	return(strcmp((const char*)NrmQuarkToString(*(NrmQuark *)q1),(const char*)NrmQuarkToString(*(NrmQuark *) q2)));
 }
 
-static int numberOfPreloadedScripts = 5;
+static int numberOfPreloadedScripts = 7;
 
 char *preload_scripts[7] = {"$NCARG_ROOT/lib/ncarg/nclscripts/utilities.ncl",
                             "$NCARG_ROOT/lib/ncarg/nclscripts/csm/gsn_code.ncl",
