09276c02a8e33feb43031df0efd02bf33bc5eb89
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2021-09-27 16:02:28 +0200

    Fix formatting

--- ni/src/lib/nfp/TransformCoordinate.c
+++ ni/src/lib/nfp/TransformCoordinate.c
@@ -5,46 +5,45 @@
 #include "TransformCoordinate.h"
 
 int TransformCoordinate(char * SrcProjStr, char * DstProjStr,
-        double * x, double * y, double * z,
-        unsigned int nPoint) {
-    projPJ SrcProj, DstProj;
-    int Err, i;
+                        double * x, double * y, double * z,
+                        unsigned int nPoint) {
+  projPJ SrcProj, DstProj;
+  int Err, i;
 
-    /* Constructing the projections */
-    if (!(SrcProj = pj_init_plus(SrcProjStr))) {
-        printf("FATAL ERROR: Can not make a projection out of <%s>\n", SrcProjStr);
-        return (1);
-    }
-    if (!(DstProj = pj_init_plus(DstProjStr))) {
-        printf("FATAL ERROR: Can not make a projection out of <%s>\n", DstProjStr);
-        return (2);
-    }
+  /* Constructing the projections */
+  if (!(SrcProj = pj_init_plus(SrcProjStr))) {
+    printf("FATAL ERROR: Can not make a projection out of <%s>\n", SrcProjStr);
+    return (1);
+  }
+  if (!(DstProj = pj_init_plus(DstProjStr))) {
+    printf("FATAL ERROR: Can not make a projection out of <%s>\n", DstProjStr);
+    return (2);
+  }
 
-    /* Converting to radian if needed */
-    if (pj_is_latlong(SrcProj)) {
-        for (i = 0; i < nPoint; i++) {
-            x[i] *= DEG_TO_RAD;
-            y[i] *= DEG_TO_RAD;
-        }
+  /* Converting to radian if needed */
+  if (pj_is_latlong(SrcProj)) {
+    for (i = 0; i < nPoint; i++) {
+      x[i] *= DEG_TO_RAD;
+      y[i] *= DEG_TO_RAD;
     }
+  }
 
-    /* Transforming the coordinates */
-    if ((Err = pj_transform(SrcProj, DstProj, nPoint, 1, x, y, z)) != 0) {
-        printf("FATAL ERROR: %s\n", pj_strerrno(Err));
-        return (3);
+  /* Transforming the coordinates */
+  if ((Err = pj_transform(SrcProj, DstProj, nPoint, 1, x, y, z)) != 0) {
+    printf("FATAL ERROR: %s\n", pj_strerrno(Err));
+    return (3);
+  }
+
+  /* converting to degree if needed */
+  if (pj_is_latlong(DstProj)) {
+    for (i = 0; i < nPoint; i++) {
+      x[i] *= RAD_TO_DEG;
+      y[i] *= RAD_TO_DEG;
     }
+  }
 
-    /* converting to degree if needed */
-    if (pj_is_latlong(DstProj)) {
-        for (i = 0; i < nPoint; i++) {
-            x[i] *= RAD_TO_DEG;
-            y[i] *= RAD_TO_DEG;
-        }
-    }
-
-    /* freeing the projection */
-    pj_free(DstProj);
-    pj_free(SrcProj);
-    return (0);
+  /* freeing the projection */
+  pj_free(DstProj);
+  pj_free(SrcProj);
+  return (0);
 }
-
