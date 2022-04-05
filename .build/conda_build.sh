feba8426a169a35433af1e00232afd7552a764c6
erogluorhan <erogluorhan@gmail.com>
2022-04-05 14:14:24 -0600

    Use conda_build.sh from develop branch

--- .build/conda_build.sh
+++ .build/conda_build.sh
@@ -7,6 +7,9 @@ export RECIPE_DIR="${SRC_DIR}/$(basename "${RECIPE_DIR}")"
 
 cd ${SRC_DIR}
 
+#conda env create -f "${RECIPE_DIR}/envs/`uname`.yml" 2>/dev/null
+source activate ncl_build 2>/dev/null || conda activate ncl_build
+
 
 export PREFIX="${CONDA_PREFIX}"
 export CXXFLAGS="-fPIC $CXXFLAGS"
