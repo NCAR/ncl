5e9b99fdae4689fd9099addad2b0c1cd346fefa9
erogluorhan <erogluorhan@gmail.com>
2022-05-05 07:04:12 -0600

    Remove micromamba ties from conda_build.sh

--- .build/conda_build.sh
+++ .build/conda_build.sh
@@ -8,10 +8,10 @@ export RECIPE_DIR="${SRC_DIR}/$(basename "${RECIPE_DIR}")"
 cd ${SRC_DIR}
 
 #conda env create -f "${RECIPE_DIR}/envs/`uname`.yml" 2>/dev/null
-#source activate ncl_build 2>/dev/null || conda activate ncl_build
-eval "$(micromamba shell hook --shell=bash)"
-micromamba shell init --shell=bash --prefix=~/micromamba
-micromamba activate ncl_build
+source activate ncl_build 2>/dev/null || conda activate ncl_build
+#eval "$(micromamba shell hook --shell=bash)"
+#micromamba shell init --shell=bash --prefix=~/micromamba
+#micromamba activate ncl_build
 
 export PREFIX="${CONDA_PREFIX}"
 export CXXFLAGS="-fPIC $CXXFLAGS"
