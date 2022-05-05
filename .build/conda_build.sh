541f163f1d585d18b9f810ba9536a79f5fb46d81
erogluorhan <erogluorhan@gmail.com>
2022-05-05 07:29:47 -0600

    Add conda config to get bash recognize conda activate

--- .build/conda_build.sh
+++ .build/conda_build.sh
@@ -8,6 +8,7 @@ export RECIPE_DIR="${SRC_DIR}/$(basename "${RECIPE_DIR}")"
 cd ${SRC_DIR}
 
 #conda env create -f "${RECIPE_DIR}/envs/`uname`.yml" 2>/dev/null
+source ~/micromamba/etc/profile.d/conda.sh
 source activate ncl_build 2>/dev/null || conda activate ncl_build
 #eval "$(micromamba shell hook --shell=bash)"
 #micromamba shell init --shell=bash --prefix=~/micromamba
